b0b3287bbdadad47757f3543a6560494af9175a8
Cole Helbling <cole.e.helbling@gmail.com>
2019-05-14 13:20:57 -0700

    Implement abort command
    This allows the user to close the compose tab without sending their
    current composition.

---
+++ commands/compose/abort.go
@@ -0,0 +1,23 @@
+package compose
+
+import (
+	"errors"
+
+	"git.sr.ht/~sircmpwn/aerc2/widgets"
+)
+
+func init() {
+	register("abort", CommandAbort)
+}
+
+func CommandAbort(aerc *widgets.Aerc, args []string) error {
+	if len(args) != 1 {
+		return errors.New("Usage: abort")
+	}
+	composer, _ := aerc.SelectedTab().(*widgets.Composer)
+
+	aerc.RemoveTab(composer)
+	composer.Close()
+
+	return nil
+}
