ce475e4952fe182a15d8897c3cd94f59df4b64e1
Réouven Assouly <reouvenassouly@yahoo.fr>
2019-06-16 22:52:44 +0200

    commands/msgview: add open command

---
+++ commands/msgview/open.go
@@ -0,0 +1,61 @@
+package msgview
+
+import (
+	"encoding/base64"
+	"errors"
+	"io"
+	"io/ioutil"
+	"mime/quotedprintable"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+
+	"git.sr.ht/~sircmpwn/aerc/widgets"
+)
+
+func init() {
+	register("open", Open)
+}
+
+func Open(aerc *widgets.Aerc, args []string) error {
+	if len(args) != 1 {
+		return errors.New("Usage: open")
+	}
+
+	mv := aerc.SelectedTab().(*widgets.MessageViewer)
+	p := mv.CurrentPart()
+
+	p.Store.FetchBodyPart(p.Msg.Uid, p.Index, func(reader io.Reader) {
+		// email parts are encoded as 7bit (plaintext), quoted-printable, or base64
+
+		if strings.EqualFold(p.Part.Encoding, "base64") {
+			reader = base64.NewDecoder(base64.StdEncoding, reader)
+		} else if strings.EqualFold(p.Part.Encoding, "quoted-printable") {
+			reader = quotedprintable.NewReader(reader)
+		}
+
+		tmpFile, err := ioutil.TempFile(os.TempDir(), "aerc-")
+		if err != nil {
+			aerc.PushError(" " + err.Error())
+			return
+		}
+		defer tmpFile.Close()
+
+		_, err = io.Copy(tmpFile, reader)
+		if err != nil {
+			aerc.PushError(" " + err.Error())
+			return
+		}
+
+		cmd := exec.Command("xdg-open", tmpFile.Name())
+		err = cmd.Run()
+		if err != nil {
+			aerc.PushError(" " + err.Error())
+		}
+
+		aerc.PushStatus("Opened", 10*time.Second)
+	})
+
+	return nil
+}
