rjarry/aerc

commit 546dfcd76dd228e0605359e9985f0b6a5672e8fa

Add new lib/dirstore to source completions from

widgets/dirlist.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gdamore/tcell"
 
 	"git.sr.ht/~sircmpwn/aerc/config"
+	"git.sr.ht/~sircmpwn/aerc/lib"
 	"git.sr.ht/~sircmpwn/aerc/lib/ui"
 	"git.sr.ht/~sircmpwn/aerc/worker/types"
 )
@@ -15,6 +16,7 @@ type DirectoryList struct {
 	ui.Invalidatable
 	acctConf  *config.AccountConfig
 	uiConf    *config.UIConfig
+	store     *lib.DirStore
 	dirs      []string
 	logger    *log.Logger
 	selecting string
@@ -31,6 +33,7 @@ func NewDirectoryList(acctConf *config.AccountConfig, uiConf *config.UIConfig,
 		uiConf:   uiConf,
 		logger:   logger,
 		spinner:  NewSpinner(),
+		store:    lib.NewDirStore(),
 		worker:   worker,
 	}
 	dirlist.spinner.OnInvalidate(func(_ ui.Drawable) {
@@ -54,7 +57,7 @@ func (dirlist *DirectoryList) UpdateList(done func(dirs []string)) {
 				dirs = append(dirs, msg.Name)
 			case *types.Done:
 				sort.Strings(dirs)
-				dirlist.dirs = dirs
+				dirlist.store.Update(dirs)
 				dirlist.spinner.Stop()
 				dirlist.Invalidate()
 				if done != nil {
@@ -107,14 +110,14 @@ func (dirlist *DirectoryList) Draw(ctx *ui.Context) {
 		return
 	}
 
-	if len(dirlist.dirs) == 0 {
+	if len(dirlist.store.List()) == 0 {
 		style := tcell.StyleDefault
 		ctx.Printf(0, 0, style, dirlist.uiConf.EmptyDirlist)
 		return
 	}
 
 	row := 0
-	for _, name := range dirlist.dirs {
+	for _, name := range dirlist.store.List() {
 		if row >= ctx.Height() {
 			break
 		}
@@ -180,7 +183,7 @@ func (dirlist *DirectoryList) filterDirsByFoldersConfig() {
 		return
 	}
 	var filtered []string
-	for _, folder := range dirlist.dirs {
+	for _, folder := range dirlist.store.List() {
 		for _, cfgfolder := range dirlist.acctConf.Folders {
 			if folder == cfgfolder {
 				filtered = append(filtered, folder)

lib/dirstore.go
@@ -1,0 +1,20 @@
+package lib
+
+type DirStore struct {
+	dirs []string
+}
+
+func NewDirStore() *DirStore {
+	return &DirStore{
+		dirs: make([]string, 0),
+	}
+}
+
+func (store *DirStore) Update(dirs []string) {
+	store.dirs = make([]string, len(dirs))
+	copy(store.dirs, dirs)
+}
+
+func (store *DirStore) List() []string {
+	return store.dirs
+}

commit 0e55637aac92e748267559b7aa91a188a17c386f

Remove dirs field and references to it
This fixes prev/next-folder that broke after
546dfcd76dd228e0605359e9985f0b6a5672e8fa

widgets/dirlist.go
@@ -17,7 +17,6 @@ type DirectoryList struct {
 	acctConf  *config.AccountConfig
 	uiConf    *config.UIConfig
 	store     *lib.DirStore
-	dirs      []string
 	logger    *log.Logger
 	selecting string
 	selected  string
@@ -44,7 +43,7 @@ func NewDirectoryList(acctConf *config.AccountConfig, uiConf *config.UIConfig,
 }
 
 func (dirlist *DirectoryList) List() []string {
-	return dirlist.dirs
+	return dirlist.store.List()
 }
 
 func (dirlist *DirectoryList) UpdateList(done func(dirs []string)) {
@@ -78,16 +77,18 @@ func (dirlist *DirectoryList) Select(name string) {
 				dirlist.selected = dirlist.selecting
 				dirlist.filterDirsByFoldersConfig()
 				hasSelected := false
-				for _, d := range dirlist.dirs {
+				dirs := dirlist.store.List()
+				for _, d := range dirs {
 					if d == dirlist.selected {
 						hasSelected = true
 						break
 					}
 				}
 				if !hasSelected && dirlist.selected != "" {
-					dirlist.dirs = append(dirlist.dirs, dirlist.selected)
+					dirs = append(dirs, dirlist.selected)
 				}
-				sort.Strings(dirlist.dirs)
+				sort.Strings(dirs)
+				dirlist.store.Update(dirs)
 			}
 			dirlist.Invalidate()
 		})
@@ -139,10 +140,10 @@ func (dirlist *DirectoryList) Draw(ctx *ui.Context) {
 }
 
 func (dirlist *DirectoryList) nextPrev(delta int) {
-	for i, dir := range dirlist.dirs {
+	for i, dir := range dirlist.store.List() {
 		if dir == dirlist.selected {
 			var j int
-			ndirs := len(dirlist.dirs)
+			ndirs := len(dirlist.store.List())
 			for j = i + delta; j != i; j += delta {
 				if j < 0 {
 					j = ndirs - 1
@@ -150,7 +151,7 @@ func (dirlist *DirectoryList) nextPrev(delta int) {
 				if j >= ndirs {
 					j = 0
 				}
-				name := dirlist.dirs[j]
+				name := dirlist.store.List()[j]
 				if len(dirlist.acctConf.Folders) > 1 && name != dirlist.selected {
 					idx := sort.SearchStrings(dirlist.acctConf.Folders, name)
 					if idx == len(dirlist.acctConf.Folders) ||
@@ -161,7 +162,7 @@ func (dirlist *DirectoryList) nextPrev(delta int) {
 				}
 				break
 			}
-			dirlist.Select(dirlist.dirs[j])
+			dirlist.Select(dirlist.store.List()[j])
 			break
 		}
 	}
@@ -191,5 +192,5 @@ func (dirlist *DirectoryList) filterDirsByFoldersConfig() {
 			}
 		}
 	}
-	dirlist.dirs = filtered
+	dirlist.store.Update(filtered)
 }