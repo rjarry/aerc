rjarry/aerc

commit 334ca89bea38132252d092ad6066af100768eff7

folder filter: only assume regex if filter is ~fmt

widgets/dirlist.go
@@ -161,12 +161,17 @@ func (dirlist *DirectoryList) Prev() {
 }
 
 func folderMatches(folder string, pattern string) bool {
-	r, err := regexp.Compile(pattern)
-	if err != nil {
+	if len(pattern) == 0 {
 		return false
 	}
-
-	return r.Match([]byte(folder))
+	if pattern[0] == '~' {
+		r, err := regexp.Compile(pattern)
+		if err != nil {
+			return false
+		}
+		return r.Match([]byte(folder))
+	}
+	return pattern == folder
 }
 
 // filterDirsByFoldersConfig sets dirlist.dirs to the filtered subset of the

doc/aerc-config.5.scd
@@ -226,7 +226,7 @@ Note that many of these configuration options are written for you, such as
 
 *folders*
 	Specifies the comma separated list of folders to display in the sidebar.
-	Supports regex patterns.
+	Names prefixed with ~ are interpreted as regular expressions.
 
 	Default: all folders
 

commit 6fcc047c3116b6ef59cae39b3beb9e815f9b62a6

Only compile regex portion of folder filter
The code was trying to compile the `~` as well. In this case, it was
trying to match a literal `~` to the front of the supplied regex.

Fixes: 334ca89bea381 ("folder filter: only assume regex if filter is
~fmt")

Signed-off-by: Daniel Xu <dxu@dxuuu.xyz>

widgets/dirlist.go
@@ -165,7 +165,7 @@ func folderMatches(folder string, pattern string) bool {
 		return false
 	}
 	if pattern[0] == '~' {
-		r, err := regexp.Compile(pattern)
+		r, err := regexp.Compile(pattern[1:])
 		if err != nil {
 			return false
 		}