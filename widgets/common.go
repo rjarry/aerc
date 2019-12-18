684978e77da62c02d99eb8310b8cc75fdbca9888
Reto Brunner <reto@labrat.space>
2019-12-18 06:33:56 +0100

    widgets: add msgInfoFromUids helper

---
+++ widgets/common.go
@@ -0,0 +1,20 @@
+package widgets
+
+import (
+	"fmt"
+
+	"git.sr.ht/~sircmpwn/aerc/lib"
+	"git.sr.ht/~sircmpwn/aerc/models"
+)
+
+func msgInfoFromUids(store *lib.MessageStore, uids []uint32) ([]*models.MessageInfo, error) {
+	infos := make([]*models.MessageInfo, len(uids))
+	for i, uid := range uids {
+		var ok bool
+		infos[i], ok = store.Messages[uid]
+		if !ok {
+			return nil, fmt.Errorf("uid not found")
+		}
+	}
+	return infos, nil
+}
