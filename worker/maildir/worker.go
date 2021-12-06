rjarry/aerc

commit f4d3c8fc77f97c1c01e85329c54c522c2cfd13bb

maildir: watch for external changes
When a maildir is synchronized by an external process while aerc is
running (e.g. mbsync), some emails may be moved out of "new" to "cur" or
completely deleted.

These deletions are ignored and aerc may assume these messages are still
here, leading to errors.

Take file deletions into account. Also, add "cur" to the watched
folders since these can be modified as well.

Signed-off-by: Robin Jarry <robin@jarry.cc>

worker/maildir/worker.go
@@ -71,8 +71,8 @@ func (w *Worker) handleAction(action types.WorkerMessage) {
 }
 
 func (w *Worker) handleFSEvent(ev fsnotify.Event) {
-	// we only care about files being created
-	if ev.Op != fsnotify.Create {
+	// we only care about files being created or removed
+	if ev.Op != fsnotify.Create && ev.Op != fsnotify.Remove {
 		return
 	}
 	// if there's not a selected directory to rescan, ignore
@@ -275,9 +275,13 @@ func (w *Worker) handleOpenDirectory(msg *types.OpenDirectory) error {
 		return err
 	}
 
-	// remove existing watch path
+	// remove existing watch paths
 	if w.selected != nil {
 		prevDir := filepath.Join(string(*w.selected), "new")
+		if err := w.watcher.Remove(prevDir); err != nil {
+			return fmt.Errorf("could not unwatch previous directory: %v", err)
+		}
+		prevDir = filepath.Join(string(*w.selected), "cur")
 		if err := w.watcher.Remove(prevDir); err != nil {
 			return fmt.Errorf("could not unwatch previous directory: %v", err)
 		}
@@ -286,8 +290,12 @@ func (w *Worker) handleOpenDirectory(msg *types.OpenDirectory) error {
 	w.selected = &dir
 	w.selectedName = msg.Directory
 
-	// add watch path
+	// add watch paths
 	newDir := filepath.Join(string(*w.selected), "new")
+	if err := w.watcher.Add(newDir); err != nil {
+		return fmt.Errorf("could not add watch to directory: %v", err)
+	}
+	newDir = filepath.Join(string(*w.selected), "cur")
 	if err := w.watcher.Add(newDir); err != nil {
 		return fmt.Errorf("could not add watch to directory: %v", err)
 	}

commit abcd327359592ca7b552585a0f3837c930b126b4

maildir: watch for message renames
Messages flags can also be changed without changing directories.
Changing flags in maildirs means renaming the message files. Also take
renames into account.

Link: https://cr.yp.to/proto/maildir.html
Fixes: f4d3c8fc77f9 ("maildir: watch for external changes")
Signed-off-by: Robin Jarry <robin@jarry.cc>

worker/maildir/worker.go
@@ -71,8 +71,11 @@ func (w *Worker) handleAction(action types.WorkerMessage) {
 }
 
 func (w *Worker) handleFSEvent(ev fsnotify.Event) {
-	// we only care about files being created or removed
-	if ev.Op != fsnotify.Create && ev.Op != fsnotify.Remove {
+	// we only care about files being created, removed or renamed
+	switch ev.Op {
+	case fsnotify.Create, fsnotify.Remove, fsnotify.Rename:
+		break
+	default:
 		return
 	}
 	// if there's not a selected directory to rescan, ignore