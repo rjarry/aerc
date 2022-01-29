rjarry/aerc

commit cb3090956cfdc0ff4c2f06e4ef3c5eb73b73f9c0

dirlist: skip unnecessary change-folder action
when traversing the directory list, aerc will fetch the contents for
every directory even though the user might just move on to the next.
This causes an unnecessary delay (and load on the worker) and can be
avoided by skipping the old change-folder action when a newer one
arrives within a predefined time interval.

Signed-off-by: Koni Marti <koni.marti@gmail.com>

widgets/dirlist.go
@@ -6,6 +6,7 @@ import (
 	"math"
 	"regexp"
 	"sort"
+	"time"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/mattn/go-runewidth"
@@ -20,27 +21,29 @@ import (
 
 type DirectoryList struct {
 	ui.Invalidatable
-	aercConf  *config.AercConfig
-	acctConf  *config.AccountConfig
-	store     *lib.DirStore
-	dirs      []string
-	logger    *log.Logger
-	selecting string
-	selected  string
-	scroll    int
-	spinner   *Spinner
-	worker    *types.Worker
+	aercConf   *config.AercConfig
+	acctConf   *config.AccountConfig
+	store      *lib.DirStore
+	dirs       []string
+	logger     *log.Logger
+	selecting  string
+	selected   string
+	scroll     int
+	spinner    *Spinner
+	worker     *types.Worker
+	skipSelect chan bool
 }
 
 func NewDirectoryList(conf *config.AercConfig, acctConf *config.AccountConfig,
 	logger *log.Logger, worker *types.Worker) *DirectoryList {
 
 	dirlist := &DirectoryList{
-		aercConf: conf,
-		acctConf: acctConf,
-		logger:   logger,
-		store:    lib.NewDirStore(),
-		worker:   worker,
+		aercConf:   conf,
+		acctConf:   acctConf,
+		logger:     logger,
+		store:      lib.NewDirStore(),
+		worker:     worker,
+		skipSelect: make(chan bool),
 	}
 	uiConf := dirlist.UiConfig()
 	dirlist.spinner = NewSpinner(&uiConf)
@@ -87,32 +90,45 @@ func (dirlist *DirectoryList) UpdateList(done func(dirs []string)) {
 
 func (dirlist *DirectoryList) Select(name string) {
 	dirlist.selecting = name
-	dirlist.worker.PostAction(&types.OpenDirectory{Directory: name},
-		func(msg types.WorkerMessage) {
-			switch msg.(type) {
-			case *types.Error:
-				dirlist.selecting = ""
-			case *types.Done:
-				dirlist.selected = dirlist.selecting
-				dirlist.filterDirsByFoldersConfig()
-				hasSelected := false
-				for _, d := range dirlist.dirs {
-					if d == dirlist.selected {
-						hasSelected = true
-						break
+
+	close(dirlist.skipSelect)
+	dirlist.skipSelect = make(chan bool)
+
+	go func() {
+		select {
+		case <-time.After(1 * time.Second):
+			dirlist.worker.PostAction(&types.OpenDirectory{Directory: name},
+				func(msg types.WorkerMessage) {
+					switch msg.(type) {
+					case *types.Error:
+						dirlist.selecting = ""
+						dirlist.selected = ""
+					case *types.Done:
+						dirlist.selected = dirlist.selecting
+						dirlist.filterDirsByFoldersConfig()
+						hasSelected := false
+						for _, d := range dirlist.dirs {
+							if d == dirlist.selected {
+								hasSelected = true
+								break
+							}
+						}
+						if !hasSelected && dirlist.selected != "" {
+							dirlist.dirs = append(dirlist.dirs, dirlist.selected)
+						}
+						if dirlist.acctConf.EnableFoldersSort {
+							sort.Strings(dirlist.dirs)
+						}
+						dirlist.sortDirsByFoldersSortConfig()
 					}
-				}
-				if !hasSelected && dirlist.selected != "" {
-					dirlist.dirs = append(dirlist.dirs, dirlist.selected)
-				}
-				if dirlist.acctConf.EnableFoldersSort {
-					sort.Strings(dirlist.dirs)
-				}
-				dirlist.sortDirsByFoldersSortConfig()
-			}
+					dirlist.Invalidate()
+				})
 			dirlist.Invalidate()
-		})
-	dirlist.Invalidate()
+		case <-dirlist.skipSelect:
+			dirlist.logger.Println("dirlist: skip", name)
+			return
+		}
+	}()
 }
 
 func (dirlist *DirectoryList) Selected() string {

commit a5c046efe3af8f1b3c94c9d83c214c77fa9f6697

dirlist: use shorter delay before listing directory contents
1 second is a bit excessive. Use 200ms which should cover most quick
folder changes.

Add an option to make that delay configurable by the users.

References: https://todo.sr.ht/~rjarry/aerc/16
Fixes: cb3090956cfd ("dirlist: skip unnecessary change-folder action")
Suggested-by: Koni Marti <koni.marti@gmail.com>
Signed-off-by: Robin Jarry <robin@jarry.cc>

config/aerc.conf.in
@@ -74,6 +74,13 @@ pinned-tab-marker='`'
 # Default: %n %>r
 dirlist-format=%n %>r
 
+# Delay after which the messages are actually listed when entering a directory.
+# This avoids loading messages when skipping over folders and makes the UI more
+# responsive. If you do not want that, set it to 0s.
+#
+# Default: 200ms
+dirlist-delay=200ms
+
 # List of space-separated criteria to sort the messages by, see *sort*
 # command in *aerc*(1) for reference. Prefixing a criterion with "-r "
 # reverses that criterion.

config/config.go
@@ -47,6 +47,7 @@ type UIConfig struct {
 	Spinner             string        `ini:"spinner"`
 	SpinnerDelimiter    string        `ini:"spinner-delimiter"`
 	DirListFormat       string        `ini:"dirlist-format"`
+	DirListDelay        time.Duration `ini:"dirlist-delay"`
 	Sort                []string      `delim:" "`
 	NextMessageOnDelete bool          `ini:"next-message-on-delete"`
 	CompletionDelay     time.Duration `ini:"completion-delay"`
@@ -564,6 +565,7 @@ func LoadConfigFromFile(root *string, sharedir string, logger *log.Logger) (*Aer
 			Spinner:             "[..]    , [..]   ,  [..]  ,   [..] ,    [..],   [..] ,  [..]  , [..]   ",
 			SpinnerDelimiter:    ",",
 			DirListFormat:       "%n %>r",
+			DirListDelay:        200 * time.Millisecond,
 			NextMessageOnDelete: true,
 			CompletionDelay:     250 * time.Millisecond,
 			CompletionPopovers:  true,

doc/aerc-config.5.scd
@@ -177,6 +177,14 @@ These options are configured in the *[ui]* section of aerc.conf.
 |  %>X
 :  make format specifier 'X' be right justified
 
+*dirlist-delay*
+	Delay after which the messages are actually listed when entering
+	a directory. This avoids loading messages when skipping over folders
+	and makes the UI more responsive. If you do not want that, set it to
+	0s.
+
+	Default: 200ms
+
 *next-message-on-delete*
 	Moves to next message when the current message is deleted
 

widgets/dirlist.go
@@ -96,7 +96,7 @@ func (dirlist *DirectoryList) Select(name string) {
 
 	go func() {
 		select {
-		case <-time.After(1 * time.Second):
+		case <-time.After(dirlist.aercConf.Ui.DirListDelay):
 			dirlist.worker.PostAction(&types.OpenDirectory{Directory: name},
 				func(msg types.WorkerMessage) {
 					switch msg.(type) {