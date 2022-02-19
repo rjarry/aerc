rjarry/aerc

commit 5abc3cab96b7ab4f7ea533cd40ffd93c20f37a62

contrib: add xdg desktop entry
Add an XDG desktop file to handle mailto: links, to make it easier to
reply to mailing list threads and compose emails with aerc in general.

Signed-off-by: Moritz Poldrack <git@moritz.sh>
Signed-off-by: Robin Jarry <robin@jarry.cc>

Makefile
@@ -72,7 +72,8 @@ clean:
 
 install: $(DOCS) aerc
 	mkdir -m755 -p $(DESTDIR)$(BINDIR) $(DESTDIR)$(MANDIR)/man1 $(DESTDIR)$(MANDIR)/man5 $(DESTDIR)$(MANDIR)/man7 \
-		$(DESTDIR)$(SHAREDIR) $(DESTDIR)$(SHAREDIR)/filters $(DESTDIR)$(SHAREDIR)/templates $(DESTDIR)$(SHAREDIR)/stylesets
+		$(DESTDIR)$(SHAREDIR) $(DESTDIR)$(SHAREDIR)/filters $(DESTDIR)$(SHAREDIR)/templates $(DESTDIR)$(SHAREDIR)/stylesets \
+		$(DESTDIR)/share/applications
 	install -m755 aerc $(DESTDIR)$(BINDIR)/aerc
 	install -m644 aerc.1 $(DESTDIR)$(MANDIR)/man1/aerc.1
 	install -m644 aerc-search.1 $(DESTDIR)$(MANDIR)/man1/aerc-search.1
@@ -95,6 +96,7 @@ install: $(DOCS) aerc
 	install -m644 templates/quoted_reply $(DESTDIR)$(SHAREDIR)/templates/quoted_reply
 	install -m644 templates/forward_as_body $(DESTDIR)$(SHAREDIR)/templates/forward_as_body
 	install -m644 config/default_styleset $(DESTDIR)$(SHAREDIR)/stylesets/default
+	install -m644 contrib/aerc.desktop $(DESTDIR)/share/applications/aerc.desktop
 
 .PHONY: checkinstall
 checkinstall:
@@ -129,5 +131,7 @@ uninstall:
 	$(RMDIR_IF_EMPTY) $(DESTDIR)$(MANDIR)/man5
 	$(RMDIR_IF_EMPTY) $(DESTDIR)$(MANDIR)/man7
 	$(RMDIR_IF_EMPTY) $(DESTDIR)$(MANDIR)
+	$(RM) $(DESTDIR)/share/applications/aerc.desktop
+	$(RMDIR_IF_EMPTY) $(DESTDIR)/share/applications
 
 .PHONY: all doc clean install uninstall debug

contrib/aerc.desktop
@@ -1,0 +1,26 @@
+[Desktop Entry]
+Version=1.0
+Name=aerc
+
+GenericName=Mail Client
+GenericName[de]=Email Client
+Comment=Launches the aerc email client
+Comment[de]=Startet den aerc Email-Client
+Keywords=Email,Mail,IMAP,SMTP
+Categories=Office;Network;Email;ConsoleOnly
+
+Type=Application
+Icon=utilities-terminal
+Terminal=true
+Exec=aerc %u
+
+[Desktop Action compose]
+Name=Compose mail
+
+# enable as default mailto: handler using
+# xdg-mime default aerc.desktop x-scheme-handler/mailto
+Exec=aerc %u
+MimeType=x-scheme-handler/mailto;
+Type=Application
+Icon=utilities-terminal
+Terminal=true

commit 20cfef95ff715a56b4b708fc57c605a2a8b06c14

contrib: fix aerc.desktop install path
Add missing $(PREFIX).

Note to self: always test install before accepting patches.

Fixes: 5abc3cab96b7 ("contrib: add xdg desktop entry")
Signed-off-by: Robin Jarry <robin@jarry.cc>

Makefile
@@ -73,7 +73,7 @@ clean:
 install: $(DOCS) aerc
 	mkdir -m755 -p $(DESTDIR)$(BINDIR) $(DESTDIR)$(MANDIR)/man1 $(DESTDIR)$(MANDIR)/man5 $(DESTDIR)$(MANDIR)/man7 \
 		$(DESTDIR)$(SHAREDIR) $(DESTDIR)$(SHAREDIR)/filters $(DESTDIR)$(SHAREDIR)/templates $(DESTDIR)$(SHAREDIR)/stylesets \
-		$(DESTDIR)/share/applications
+		$(DESTDIR)$(PREFIX)/share/applications
 	install -m755 aerc $(DESTDIR)$(BINDIR)/aerc
 	install -m644 aerc.1 $(DESTDIR)$(MANDIR)/man1/aerc.1
 	install -m644 aerc-search.1 $(DESTDIR)$(MANDIR)/man1/aerc-search.1
@@ -96,7 +96,7 @@ install: $(DOCS) aerc
 	install -m644 templates/quoted_reply $(DESTDIR)$(SHAREDIR)/templates/quoted_reply
 	install -m644 templates/forward_as_body $(DESTDIR)$(SHAREDIR)/templates/forward_as_body
 	install -m644 config/default_styleset $(DESTDIR)$(SHAREDIR)/stylesets/default
-	install -m644 contrib/aerc.desktop $(DESTDIR)/share/applications/aerc.desktop
+	install -m644 contrib/aerc.desktop $(DESTDIR)$(PREFIX)/share/applications/aerc.desktop
 
 .PHONY: checkinstall
 checkinstall:
@@ -131,7 +131,7 @@ uninstall:
 	$(RMDIR_IF_EMPTY) $(DESTDIR)$(MANDIR)/man5
 	$(RMDIR_IF_EMPTY) $(DESTDIR)$(MANDIR)/man7
 	$(RMDIR_IF_EMPTY) $(DESTDIR)$(MANDIR)
-	$(RM) $(DESTDIR)/share/applications/aerc.desktop
-	$(RMDIR_IF_EMPTY) $(DESTDIR)/share/applications
+	$(RM) $(DESTDIR)$(PREFIX)/share/applications/aerc.desktop
+	$(RMDIR_IF_EMPTY) $(DESTDIR)$(PREFIX)/share/applications
 
 .PHONY: all doc clean install uninstall debug