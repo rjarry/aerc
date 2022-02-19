rjarry/aerc

commit c37f1eaaeaa1067365cff841e5c8298206db78ef

filter/search: display current search/filter arguments
Make the statusbar message after search and filter completion more
informative by showing the full call that was used.

Signed-off-by: inwit <inwit@sindominio.net>

commands/account/search.go
@@ -2,6 +2,7 @@ package account
 
 import (
 	"errors"
+	"fmt"
 
 	"git.sr.ht/~rjarry/aerc/widgets"
 )
@@ -34,14 +35,14 @@ func (SearchFilter) Execute(aerc *widgets.Aerc, args []string) error {
 	if args[0] == "filter" {
 		aerc.SetStatus("Filtering...")
 		cb = func(uids []uint32) {
-			aerc.SetStatus("Filter complete.")
+			aerc.SetStatus(fmt.Sprintf("Filter complete %s", args))
 			acct.Logger().Printf("Filter results: %v", uids)
 			store.ApplyFilter(uids)
 		}
 	} else {
 		aerc.SetStatus("Searching...")
 		cb = func(uids []uint32) {
-			aerc.SetStatus("Search complete.")
+			aerc.SetStatus(fmt.Sprintf("Search complete %s", args))
 			acct.Logger().Printf("Search results: %v", uids)
 			store.ApplySearch(uids)
 			// TODO: Remove when stores have multiple OnUpdate handlers

commit e3842033904af9f4fc0e829b6f23de0579453052

search/filter: remove from status bar after 20 seconds
The status is not cleared. It is annoying.

Fixes: c37f1eaaeaa1 ("filter/search: display current search/filter arguments")
Signed-off-by: Robin Jarry <robin@jarry.cc>

commands/account/search.go
@@ -3,6 +3,7 @@ package account
 import (
 	"errors"
 	"fmt"
+	"time"
 
 	"git.sr.ht/~rjarry/aerc/widgets"
 )
@@ -33,16 +34,16 @@ func (SearchFilter) Execute(aerc *widgets.Aerc, args []string) error {
 
 	var cb func([]uint32)
 	if args[0] == "filter" {
-		aerc.SetStatus("Filtering...")
+		aerc.PushStatus("Filtering...", 10*time.Second)
 		cb = func(uids []uint32) {
-			aerc.SetStatus(fmt.Sprintf("Filter complete %s", args))
+			aerc.PushStatus(fmt.Sprintf("Filter complete %s", args), 20*time.Second)
 			acct.Logger().Printf("Filter results: %v", uids)
 			store.ApplyFilter(uids)
 		}
 	} else {
-		aerc.SetStatus("Searching...")
+		aerc.PushStatus("Searching...", 10*time.Second)
 		cb = func(uids []uint32) {
-			aerc.SetStatus(fmt.Sprintf("Search complete %s", args))
+			aerc.PushStatus(fmt.Sprintf("Search complete %s", args), 20*time.Second)
 			acct.Logger().Printf("Search results: %v", uids)
 			store.ApplySearch(uids)
 			// TODO: Remove when stores have multiple OnUpdate handlers