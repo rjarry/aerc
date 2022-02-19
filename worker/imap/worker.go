rjarry/aerc

commit 05ad96a30cb8ea17970c84ea7c72f8f3def5afa5

imap: improve reconnect stability
improves the robustness of the imap reconnect feature which was
introduced in commit beae17a6da37 ("imap: auto-reconnects on connection
error").

If a connection error is emitted, the message list is cleared and a
corresponding error message is shown in the ui. Status bar is updated as
well. Upon reconnect, the directories and the message list will be
re-fetched (same behavior as the connect command).

Reconnect can be enabled and disabled with the connect and the
disconnect commands.

Signed-off-by: Koni Marti <koni.marti@gmail.com>

worker/imap/worker.go
@@ -85,6 +85,11 @@ func (w *IMAPWorker) handleMessage(msg types.WorkerMessage) error {
 		}
 	}()
 
+	checkConn := func() {
+		w.stopConnectionObserver()
+		w.startConnectionObserver()
+	}
+
 	var reterr error // will be returned at the end, needed to support idle
 
 	switch msg := msg.(type) {
@@ -171,9 +176,15 @@ func (w *IMAPWorker) handleMessage(msg types.WorkerMessage) error {
 		}
 	case *types.Connect:
 		if w.client != nil && w.client.State() == imap.SelectedState {
+			if !w.autoReconnect {
+				w.autoReconnect = true
+				checkConn()
+			}
 			reterr = fmt.Errorf("Already connected")
 			break
 		}
+
+		w.autoReconnect = true
 		c, err := w.connect()
 		if err != nil {
 			reterr = err
@@ -187,7 +198,6 @@ func (w *IMAPWorker) handleMessage(msg types.WorkerMessage) error {
 
 		w.startConnectionObserver()
 
-		w.autoReconnect = true
 		w.worker.PostMessage(&types.Done{types.RespondTo(msg)}, nil)
 	case *types.Reconnect:
 		if !w.autoReconnect {
@@ -196,6 +206,7 @@ func (w *IMAPWorker) handleMessage(msg types.WorkerMessage) error {
 		}
 		c, err := w.connect()
 		if err != nil {
+			checkConn()
 			reterr = err
 			break
 		}
@@ -209,18 +220,17 @@ func (w *IMAPWorker) handleMessage(msg types.WorkerMessage) error {
 
 		w.worker.PostMessage(&types.Done{types.RespondTo(msg)}, nil)
 	case *types.Disconnect:
+		w.autoReconnect = false
+		w.stopConnectionObserver()
 		if w.client == nil || w.client.State() != imap.SelectedState {
 			reterr = fmt.Errorf("Not connected")
 			break
 		}
 
-		w.stopConnectionObserver()
-
 		if err := w.client.Logout(); err != nil {
 			reterr = err
 			break
 		}
-		w.autoReconnect = false
 		w.worker.PostMessage(&types.Done{types.RespondTo(msg)}, nil)
 	case *types.ListDirectories:
 		w.handleListDirectories(msg)
@@ -306,9 +316,11 @@ func (w *IMAPWorker) startConnectionObserver() {
 	go func() {
 		select {
 		case <-w.client.LoggedOut():
-			w.worker.PostMessage(&types.ConnError{
-				Error: fmt.Errorf("Logged Out"),
-			}, nil)
+			if w.autoReconnect {
+				w.worker.PostMessage(&types.ConnError{
+					Error: fmt.Errorf("imap: logged out"),
+				}, nil)
+			}
 		case <-w.done:
 			return
 		}

widgets/account.go
@@ -289,8 +289,10 @@ func (acct *AccountView) onMessage(msg types.WorkerMessage) {
 	case *types.LabelList:
 		acct.labels = msg.Labels
 	case *types.ConnError:
-		acct.logger.Printf("Connection error = %v", msg.Error)
+		acct.logger.Printf("connection error: %v", msg.Error)
+		acct.host.SetStatus("Disconnected.")
 		acct.aerc.PushError(fmt.Sprintf("%v", msg.Error))
+		acct.msglist.SetStore(nil)
 		acct.worker.PostAction(&types.Reconnect{}, nil)
 	case *types.Error:
 		acct.logger.Printf("%v", msg.Error)

commit 11a4d5b71c12ff358916f1e0d0aed2cc26f9ea98

imap: reconnect with exponential backoff
waits an increasing amount of time before attempting a reconnect.
Wait is capped at 16s. Prevents many reconnect attemps in a short time period.

Fixes commit 05ad96a30cb8 ("imap: improve reconnect stability") that
improved the reliability of the reconnect mechanism but did not
implement controls to prevent the triggering of too many reconnects
within a short period of time.

Fixes: 05ad96a30cb8 ("imap: improve reconnect stability")
Signed-off-by: Koni Marti <koni.marti@gmail.com>

worker/imap/worker.go
@@ -3,6 +3,7 @@ package imap
 import (
 	"crypto/tls"
 	"fmt"
+	"math"
 	"net"
 	"net/url"
 	"strconv"
@@ -12,6 +13,7 @@ import (
 	"github.com/emersion/go-imap"
 	sortthread "github.com/emersion/go-imap-sortthread"
 	"github.com/emersion/go-imap/client"
+	"github.com/pkg/errors"
 	"golang.org/x/oauth2"
 
 	"git.sr.ht/~rjarry/aerc/lib"
@@ -58,6 +60,7 @@ type IMAPWorker struct {
 	seqMap        []uint32
 	done          chan struct{}
 	autoReconnect bool
+	retries       int
 }
 
 func NewIMAPWorker(worker *types.Worker) (types.Backend, error) {
@@ -85,7 +88,8 @@ func (w *IMAPWorker) handleMessage(msg types.WorkerMessage) error {
 		}
 	}()
 
-	checkConn := func() {
+	checkConn := func(wait time.Duration) {
+		time.Sleep(wait)
 		w.stopConnectionObserver()
 		w.startConnectionObserver()
 	}
@@ -178,7 +182,7 @@ func (w *IMAPWorker) handleMessage(msg types.WorkerMessage) error {
 		if w.client != nil && w.client.State() == imap.SelectedState {
 			if !w.autoReconnect {
 				w.autoReconnect = true
-				checkConn()
+				checkConn(0)
 			}
 			reterr = fmt.Errorf("Already connected")
 			break
@@ -206,8 +210,10 @@ func (w *IMAPWorker) handleMessage(msg types.WorkerMessage) error {
 		}
 		c, err := w.connect()
 		if err != nil {
-			checkConn()
-			reterr = err
+			wait, msg := w.exponentialBackoff()
+			go checkConn(wait)
+			w.retries++
+			reterr = errors.Wrap(err, msg)
 			break
 		}
 
@@ -312,6 +318,22 @@ func (w *IMAPWorker) handleImapUpdate(update client.Update) {
 	}
 }
 
+func (w *IMAPWorker) exponentialBackoff() (time.Duration, string) {
+	maxWait := 16
+	if w.retries > 0 {
+		backoff := int(math.Pow(2.0, float64(w.retries)))
+		if backoff > maxWait {
+			backoff = maxWait
+		}
+		waitStr := fmt.Sprintf("%ds", backoff)
+		wait, err := time.ParseDuration(waitStr)
+		if err == nil {
+			return wait, fmt.Sprintf("wait %s before reconnect", waitStr)
+		}
+	}
+	return 0 * time.Second, ""
+}
+
 func (w *IMAPWorker) startConnectionObserver() {
 	go func() {
 		select {
@@ -412,6 +434,8 @@ func (w *IMAPWorker) connect() (*client.Client, error) {
 	if _, err := c.Select(imap.InboxName, false); err != nil {
 		return nil, err
 	}
+
+	w.retries = 0
 
 	return c, nil
 }