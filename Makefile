rjarry/aerc

commit e7e22aba60cec373f48ebe21aa6d4898f31f8c66

mk: rebuild if goflags or ldflags have changed
Running make with different values for GOFLAGS or VERSION does not cause
aerc to be rebuilt whereas it should.

Write the go build command line into a file and force aerc to be rebuilt
if the command line has changed.

Use the BSD make compatible != operator to run the command. This
operator is also available in GNU make since version 4.0.

Link: https://git.savannah.gnu.org/cgit/make.git/commit/?id=b34438bee83ee
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Koni Marti <koni.marti@gmail.com>

Makefile
@@ -30,8 +30,19 @@ DOCS := \
 
 all: aerc $(DOCS)
 
-aerc: $(GOSRC)
-	$(GO) build $(GOFLAGS) -ldflags "$(LDFLAGS)" -o $@
+build_cmd:=$(GO) build $(GOFLAGS) -ldflags "$(LDFLAGS)" -o aerc
+
+# the following command outputs nothing, we only want to execute it once
+# and force .aerc.d to be regenerated when build_cmd has changed
+_!=echo '$(build_cmd)' > .aerc.tmp; \
+	cmp -s .aerc.d .aerc.tmp || rm -f .aerc.d; \
+	rm -f .aerc.tmp
+
+.aerc.d:
+	@echo '$(build_cmd)' > $@
+
+aerc: $(GOSRC) .aerc.d
+	$(build_cmd)
 
 .PHONY: fmt
 fmt:

.gitignore
@@ -2,6 +2,7 @@
 /aerc2
 /aerc
 /aerc.debug
+/.aerc.d
 log
 raw.log
 *.1

commit 247c6c74380a9e56527ec0a87965a39142ffc213

mk: rename ambiguous LDFLAGS variable
The LDFLAGS environment variable is usually indented for C the linker
flags which are not compatible with go -ldflags.

Use a more explicit GO_LDFLAGS variable instead. Allow adding extra
flags without overriding the default ones by specifying
GO_EXTRA_LDFLAGS.

This may break the build on some distros that rely on setting LDFLAGS to
change the default shareDir or version. They will have to switch to
GO_EXTRA_LDFLAGS.

Link: https://salsa.debian.org/go-team/packages/aerc/-/commit/e9ed90beae9f
Link: https://src.fedoraproject.org/rpms/aerc/blob/f36/f/aerc.spec#_86
Fixes: e7e22aba60ce ("mk: rebuild if goflags or ldflags have changed")
Signed-off-by: Robin Jarry <robin@jarry.cc>

Makefile
@@ -10,8 +10,11 @@ SHAREDIR?=$(PREFIX)/share/aerc
 MANDIR?=$(PREFIX)/share/man
 GO?=go
 GOFLAGS?=
-LDFLAGS+=-X main.Version=$(VERSION)
-LDFLAGS+=-X git.sr.ht/~rjarry/aerc/config.shareDir=$(SHAREDIR)
+# ignore environment variable
+GO_LDFLAGS:=
+GO_LDFLAGS+=-X main.Version=$(VERSION)
+GO_LDFLAGS+=-X git.sr.ht/~rjarry/aerc/config.shareDir=$(SHAREDIR)
+GO_LDFLAGS+=$(GO_EXTRA_LDFLAGS)
 
 GOSRC!=find * -name '*.go'
 GOSRC+=go.mod go.sum
@@ -31,7 +34,7 @@ DOCS := \
 
 all: aerc $(DOCS)
 
-build_cmd:=$(GO) build $(GOFLAGS) -ldflags "$(LDFLAGS)" -o aerc
+build_cmd:=$(GO) build $(GOFLAGS) -ldflags "$(GO_LDFLAGS)" -o aerc
 
 # the following command outputs nothing, we only want to execute it once
 # and force .aerc.d to be regenerated when build_cmd has changed
@@ -63,7 +66,7 @@ debug: aerc.debug
 	@echo '    dlv attach $$(pidof aerc.debug)'
 
 aerc.debug: $(GOSRC)
-	$(GO) build $(GOFLAGS) -gcflags=*=-N -gcflags=*=-l -ldflags="$(LDFLAGS)" -o aerc.debug
+	$(GO) build $(GOFLAGS) -gcflags=*=-N -gcflags=*=-l -ldflags="$(GO_LDFLAGS)" -o aerc.debug
 
 .1.scd.1:
 	scdoc < $< > $@