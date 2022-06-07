rjarry/aerc

commit 2027223ab302f803952c486ec40f1e859638a273

fix: clear dirlist on disconnect
A message of Done:Disconnect will trigger an update of the dirlist. An
update of the dirlist will issue a ListDirectories message, which causes
an imap: client not ready error by trying to use the imap connection -
which is disconnected. By not issuing the update, we prevent using a
disconnected client.

This patch checks for connection state prior to updating the dirlist. A
disconnected state will clear out the dirlist.

Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>
Acked-by: Robin Jarry <robin@jarry.cc>

widgets/account.go
@@ -246,6 +246,7 @@ func (acct *AccountView) onMessage(msg types.WorkerMessage) {
 		case *types.Connect, *types.Reconnect:
 			acct.SetStatus(statusline.ConnectionActivity("Listing mailboxes..."))
 			acct.logger.Println("Listing mailboxes...")
+			acct.dirlist.SetConnected(true)
 			acct.dirlist.UpdateList(func(dirs []string) {
 				var dir string
 				for _, _dir := range dirs {
@@ -266,6 +267,7 @@ func (acct *AccountView) onMessage(msg types.WorkerMessage) {
 				acct.newConn = true
 			})
 		case *types.Disconnect:
+			acct.dirlist.SetConnected(false)
 			acct.dirlist.UpdateList(nil)
 			acct.msglist.SetStore(nil)
 			acct.logger.Println("Disconnected.")

widgets/dirlist.go
@@ -31,6 +31,7 @@ type DirectoryLister interface {
 
 	UpdateList(func([]string))
 	List() []string
+	SetConnected(bool)
 
 	NextPrev(int)
 
@@ -57,6 +58,7 @@ type DirectoryList struct {
 	spinner    *Spinner
 	worker     *types.Worker
 	skipSelect chan bool
+	connected  bool
 }
 
 func NewDirectoryList(conf *config.AercConfig, acctConf *config.AccountConfig,
@@ -91,11 +93,24 @@ func (dirlist *DirectoryList) UiConfig() config.UIConfig {
 	})
 }
 
+func (dirlist *DirectoryList) SetConnected(c bool) {
+	dirlist.connected = c
+}
+
 func (dirlist *DirectoryList) List() []string {
 	return dirlist.store.List()
 }
 
 func (dirlist *DirectoryList) UpdateList(done func(dirs []string)) {
+	// Clear out dirlist if not connected
+	if !dirlist.connected {
+		// Only dirlist.dirs is used for the UI. No need to update dirstore
+		dirlist.dirs = []string{}
+		dirlist.Invalidate()
+		// Call callback with empty array for dirtree
+		done(dirlist.dirs)
+		return
+	}
 	// TODO: move this logic into dirstore
 	var dirs []string
 	dirlist.worker.PostAction(

commit 115447e57f015b1805d2d58d1ae46beaff2299e5

dirlist: fix panic when disconnecting with dirlist-tree=false
Commit 2027223a created a panic when attempting to clear the dirlist
when the config option dirlist-tree is set to false.

This patch fixes that panic by creating a dirlist.ClearList() function
to prevent needing to check a callback.

Tested with both dirlist-tree=false and true

Fixes: 2027223ab302 ("fix: clear dirlist on disconnect")
Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>
Acked-by: Robin Jarry <robin@jarry.cc>

widgets/account.go
@@ -246,7 +246,6 @@ func (acct *AccountView) onMessage(msg types.WorkerMessage) {
 		case *types.Connect, *types.Reconnect:
 			acct.SetStatus(statusline.ConnectionActivity("Listing mailboxes..."))
 			acct.logger.Println("Listing mailboxes...")
-			acct.dirlist.SetConnected(true)
 			acct.dirlist.UpdateList(func(dirs []string) {
 				var dir string
 				for _, _dir := range dirs {
@@ -267,8 +266,7 @@ func (acct *AccountView) onMessage(msg types.WorkerMessage) {
 				acct.newConn = true
 			})
 		case *types.Disconnect:
-			acct.dirlist.SetConnected(false)
-			acct.dirlist.UpdateList(nil)
+			acct.dirlist.ClearList()
 			acct.msglist.SetStore(nil)
 			acct.logger.Println("Disconnected.")
 			acct.SetStatus(statusline.SetConnected(false))

widgets/dirlist.go
@@ -31,7 +31,7 @@ type DirectoryLister interface {
 
 	UpdateList(func([]string))
 	List() []string
-	SetConnected(bool)
+	ClearList()
 
 	NextPrev(int)
 
@@ -93,24 +93,15 @@ func (dirlist *DirectoryList) UiConfig() config.UIConfig {
 	})
 }
 
-func (dirlist *DirectoryList) SetConnected(c bool) {
-	dirlist.connected = c
-}
-
 func (dirlist *DirectoryList) List() []string {
 	return dirlist.store.List()
 }
 
+func (dirlist *DirectoryList) ClearList() {
+	dirlist.dirs = []string{}
+}
+
 func (dirlist *DirectoryList) UpdateList(done func(dirs []string)) {
-	// Clear out dirlist if not connected
-	if !dirlist.connected {
-		// Only dirlist.dirs is used for the UI. No need to update dirstore
-		dirlist.dirs = []string{}
-		dirlist.Invalidate()
-		// Call callback with empty array for dirtree
-		done(dirlist.dirs)
-		return
-	}
 	// TODO: move this logic into dirstore
 	var dirs []string
 	dirlist.worker.PostAction(

widgets/dirtree.go
@@ -32,6 +32,10 @@ func NewDirectoryTree(dirlist *DirectoryList, pathSeparator string) DirectoryLis
 	return dt
 }
 
+func (dt *DirectoryTree) ClearList() {
+	dt.list = make([]*types.Thread, 0)
+}
+
 func (dt *DirectoryTree) UpdateList(done func([]string)) {
 	dt.DirectoryList.UpdateList(func(dirs []string) {
 		if done != nil {