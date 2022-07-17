rjarry/aerc

commit 3a614e45fce9b505bb7e17a31eabffcc5e2d9022

threading: enable toggle-threads for server-side threads
Enable the :toggle-threads command to work for workers which have Thread
capability. The implementation of that feature has the side effect that
the threading-enabled config option now sets the default view (threaded
or not threaded) for any worker, not just IMAP or notmuch.

Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>
Acked-by: Robin Jarry <robin@jarry.cc>

lib/msgstore.go
@@ -38,7 +38,7 @@ type MessageStore struct {
 
 	sortCriteria []*types.SortCriterion
 
-	thread       bool
+	threadedView bool
 	buildThreads bool
 	builder      *ThreadBuilder
 
@@ -66,6 +66,11 @@ func NewMessageStore(worker *types.Worker,
 
 	dirInfoUpdateDelay := 5 * time.Second
 
+	var clientThreads bool
+	if !dirInfo.Caps.Thread {
+		clientThreads = true
+	}
+
 	return &MessageStore{
 		Deleted:  make(map[uint32]interface{}),
 		DirInfo:  *dirInfo,
@@ -76,7 +81,8 @@ func NewMessageStore(worker *types.Worker,
 		bodyCallbacks:   make(map[uint32][]func(*types.FullMessage)),
 		headerCallbacks: make(map[uint32][]func(*types.MessageInfo)),
 
-		thread: thread,
+		threadedView: thread,
+		buildThreads: clientThreads,
 
 		sortCriteria: defaultSortCriteria,
 
@@ -361,22 +367,22 @@ func (store *MessageStore) update() {
 	}
 }
 
-func (store *MessageStore) SetBuildThreads(buildThreads bool) {
-	// if worker provides threading, don't build our own threads
-	if store.thread {
+func (store *MessageStore) SetThreadedView(thread bool) {
+	store.threadedView = thread
+	if store.buildThreads {
+		if store.threadedView {
+			store.runThreadBuilder()
+		}
 		return
 	}
-	store.buildThreads = buildThreads
-	if store.BuildThreads() {
-		store.runThreadBuilder()
-	}
+	store.Sort(store.sortCriteria, nil)
+}
+
+func (store *MessageStore) ThreadedView() bool {
+	return store.threadedView
 }
 
 func (store *MessageStore) BuildThreads() bool {
-	// if worker provides threading, don't build our own threads
-	if store.thread {
-		return false
-	}
 	return store.buildThreads
 }
 
@@ -486,7 +492,7 @@ func (store *MessageStore) Answered(uids []uint32, answered bool,
 
 func (store *MessageStore) Uids() []uint32 {
 
-	if store.BuildThreads() && store.builder != nil {
+	if store.ThreadedView() && store.builder != nil {
 		if uids := store.builder.Uids(); len(uids) > 0 {
 			return uids
 		}
@@ -802,7 +808,7 @@ func (store *MessageStore) Sort(criteria []*types.SortCriterion, cb func()) {
 		}
 	}
 
-	if store.thread {
+	if store.threadedView && !store.buildThreads {
 		store.worker.PostAction(&types.FetchDirectoryThreaded{
 			SortCriteria: criteria,
 		}, handle_return)

commands/msg/toggle-threads.go
@@ -35,8 +35,8 @@ func (ToggleThreads) Execute(aerc *widgets.Aerc, args []string) error {
 		return err
 	}
 	defer store.Reselect(store.Selected())
-	store.SetBuildThreads(!store.BuildThreads())
-	acct.SetStatus(statusline.Threading(store.BuildThreads()))
+	store.SetThreadedView(!store.ThreadedView())
+	acct.SetStatus(statusline.Threading(store.ThreadedView()))
 	acct.Messages().Invalidate()
 	return nil
 }

config/aerc.conf
@@ -168,10 +168,8 @@ completion-popovers=true
 
 #[ui:account=foo]
 #
-# Enable threading in the ui. Only works with notmuch:// and imap:// accounts
-# (when the server supports it). If this is not supported by the server,
-# you can enable it on the fly by issuing the :toggle-threads command. The
-# threading will be done client side instead of on the server.
+# Enable a threaded view of messages. If this is not supported by the backend
+# (IMAP server or notmuch), threads will be built by the client.
 #
 # Default: false
 #threading-enabled=false

doc/aerc-config.5.scd
@@ -295,15 +295,8 @@ These options are configured in the *[ui]* section of aerc.conf.
 	need not be consecutive characters in the command or option.
 
 *threading-enabled*
-	Enable a threaded viewing of messages, works with IMAP (when there's
-	server support) and NotMuch backends.
-
-	This option should only be set to true for specific accounts
-	accordingly. See *Contextual UI Configuration* below.
-
-	If this is not supported by the server, you can enable it on the fly by
-	issuing the _:toggle-threads_ command. The threading will be done client
-	side instead of on the server.
+	Enable a threaded view of messages. If this is not supported by the
+	backend (IMAP server or notmuch), threads will be built by the client.
 
 	Default: false
 

widgets/msglist.go
@@ -88,7 +88,7 @@ func (ml *MessageList) Draw(ctx *ui.Context) {
 		row          int = 0
 	)
 
-	if uiConfig.ThreadingEnabled || store.BuildThreads() {
+	if store.ThreadedView() {
 		threads := store.Threads
 		counter := len(store.Uids())
 

commit 04097e874388bf30e808a9a9993230c668a36ec4

msgstore: do not build threads when threaded view is off
Commit 3a614e45fce9 ("threading: enable toggle-threads for server-side
threads") changed the behavior of the msgstore.buildThreads variable to
reflect whether the client needs to build threads or the server will.
However, a call to runThreadbuilder was not updated with an extra
conditional. As a result, threads were built regardless of the state of
the threadedView resulting in a large performance penalty for
non-threaded views with client side threading.

Only run thread builder if threaded view is enabled.

Fixes: 3a614e45fce9 ("threading: enable toggle-threads for server-side threads")
Reported-by: akspecs <akspecs@gmail.com>
Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>
Acked-by: Akspecs <akspecs@gmail.com>
Acked-by: Robin Jarry <robin@jarry.cc>

lib/msgstore.go
@@ -356,7 +356,7 @@ func (store *MessageStore) update() {
 	if store.onUpdateDirs != nil {
 		store.onUpdateDirs()
 	}
-	if store.BuildThreads() {
+	if store.BuildThreads() && store.ThreadedView() {
 		store.runThreadBuilder()
 	}
 }