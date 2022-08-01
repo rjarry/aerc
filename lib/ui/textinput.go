rjarry/aerc

commit 27425c15c4b96f1e11db8cc496b48f3d0fc0d3f4

autocompletion: fix slice out of bounds access
Signed-off-by: Moritz Poldrack <git@moritz.sh>
Acked-by: Robin Jarry <robin@jarry.cc>

lib/ui/textinput.go
@@ -70,6 +70,9 @@ func (ti *TextInput) String() string {
 }
 
 func (ti *TextInput) StringLeft() string {
+	for ti.index >= len(ti.text) {
+		ti.index = len(ti.text) - 1
+	}
 	return string(ti.text[:ti.index])
 }
 

commit 5cf81f1cb1e5a1b54dc9421b763b683b6275fb57

autocompletion: fix regression
Commit 27425c15c4b9 ("autocompletion: fix slice out of bounds access")
introduced a regression in the autocompletion that messes up the order
of last entered chars when autocompletion runs.

The ranges for a slice a[low:high] are 0 <= low <= high <= len(a) [0]

To reproduce with the ":cf" command:

1) enter ":c"
2) wait for autocompleteion
3) enter "f"
4) the prompt will now be ":fc"

[0]: https://go.dev/ref/spec#Slice_expressions

Fixes: 27425c15c4b9 ("autocompletion: fix slice out of bounds access")
Signed-off-by: Koni Marti <koni.marti@gmail.com>
Acked-by: Robin Jarry <robin@jarry.cc>

lib/ui/textinput.go
@@ -70,8 +70,8 @@ func (ti *TextInput) String() string {
 }
 
 func (ti *TextInput) StringLeft() string {
-	for ti.index >= len(ti.text) {
-		ti.index = len(ti.text) - 1
+	for ti.index > len(ti.text) {
+		ti.index = len(ti.text)
 	}
 	return string(ti.text[:ti.index])
 }