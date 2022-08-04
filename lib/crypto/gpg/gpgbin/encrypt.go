rjarry/aerc

commit 5ca6022d007bc83daba1e7297480d37f56554455

lint: ensure errors are at least logged (errcheck)
Signed-off-by: Moritz Poldrack <moritz@poldrack.dev>
Acked-by: Robin Jarry <robin@jarry.cc>

lib/crypto/gpg/gpgbin/encrypt.go
@@ -25,15 +25,18 @@ func Encrypt(r io.Reader, to []string, from string) ([]byte, error) {
 	args = append(args, "--encrypt", "-")
 
 	g := newGpg(r, args)
-	g.cmd.Run()
+	err := g.cmd.Run()
+	if err != nil {
+		return nil, fmt.Errorf("gpg: failed to run encryption: %w", err)
+	}
 	outRdr := bytes.NewReader(g.stdout.Bytes())
 	var md models.MessageDetails
-	err := parse(outRdr, &md)
+	err = parse(outRdr, &md)
 	if err != nil {
 		return nil, fmt.Errorf("gpg: failure to encrypt: %v. check public key(s)", err)
 	}
 	var buf bytes.Buffer
-	io.Copy(&buf, md.Body)
+	_, _ = io.Copy(&buf, md.Body)
 
 	return buf.Bytes(), nil
 }

lib/crypto/gpg/gpgbin/sign.go
@@ -17,15 +17,18 @@ func Sign(r io.Reader, from string) ([]byte, string, error) {
 	}
 
 	g := newGpg(r, args)
-	g.cmd.Run()
+	err := g.cmd.Run()
+	if err != nil {
+		return nil, "", fmt.Errorf("failed to run signing: %w", err)
+	}
 
 	outRdr := bytes.NewReader(g.stdout.Bytes())
 	var md models.MessageDetails
-	err := parse(outRdr, &md)
+	err = parse(outRdr, &md)
 	if err != nil {
 		return nil, "", fmt.Errorf("failed to parse messagedetails: %v", err)
 	}
 	var buf bytes.Buffer
-	io.Copy(&buf, md.Body)
+	_, _ = io.Copy(&buf, md.Body)
 	return buf.Bytes(), md.Micalg, nil
 }

lib/crypto/gpg/gpgbin/verify.go
@@ -2,6 +2,7 @@ package gpgbin
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
@@ -19,7 +20,7 @@ func Verify(m io.Reader, s io.Reader) (*models.MessageDetails, error) {
 		if err != nil {
 			return nil, err
 		}
-		io.Copy(sig, s)
+		_, _ = io.Copy(sig, s)
 		sig.Close()
 		defer os.Remove(sig.Name())
 		args = append(args, sig.Name(), "-")
@@ -29,11 +30,17 @@ func Verify(m io.Reader, s io.Reader) (*models.MessageDetails, error) {
 		return nil, err
 	}
 	g := newGpg(bytes.NewReader(orig), args)
-	g.cmd.Run()
+	err = g.cmd.Run()
+	if err != nil {
+		return nil, fmt.Errorf("gpg: failed to run verification: %w", err)
+	}
 
 	out := bytes.NewReader(g.stdout.Bytes())
 	md := new(models.MessageDetails)
-	parse(out, md)
+	err = parse(out, md)
+	if err != nil {
+		return nil, fmt.Errorf("gpg: failed to parse result: %w", err)
+	}
 
 	md.Body = bytes.NewReader(orig)
 

aerc.go
@@ -164,7 +164,10 @@ func main() {
 	deferLoop := make(chan struct{})
 
 	c := crypto.New(conf.General.PgpProvider)
-	c.Init()
+	err = c.Init()
+	if err != nil {
+		logging.Warnf("failed to initialise crypto interface: %v", err)
+	}
 	defer c.Close()
 
 	aerc = widgets.NewAerc(conf, c, func(cmd []string) error {
@@ -205,7 +208,10 @@ func main() {
 		err := lib.ConnectAndExec(arg)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Failed to communicate to aerc: %v\n", err)
-			aerc.CloseBackends()
+			err = aerc.CloseBackends()
+			if err != nil {
+				logging.Warnf("failed to close backends: %v", err)
+			}
 			return
 		}
 	}
@@ -223,5 +229,8 @@ func main() {
 			time.Sleep(16 * time.Millisecond)
 		}
 	}
-	aerc.CloseBackends()
+	err = aerc.CloseBackends()
+	if err != nil {
+		logging.Warnf("failed to close backends: %v", err)
+	}
 }

commands/account/export-mbox.go
@@ -96,7 +96,10 @@ func (ExportMbox) Execute(aerc *widgets.Aerc, args []string) error {
 					done <- false
 				case *types.FullMessage:
 					mu.Lock()
-					mboxer.Write(file, msg.Content.Reader, "", t)
+					err := mboxer.Write(file, msg.Content.Reader, "", t)
+					if err != nil {
+						logging.Warnf("failed to write mbox: %v", err)
+					}
 					for i, uid := range uids {
 						if uid == msg.Content.Uid {
 							uids = append(uids[:i], uids[i+1:]...)

commands/msg/forward.go
@@ -133,14 +133,20 @@ func (forward) Execute(aerc *widgets.Aerc, args []string) error {
 		store.FetchFull([]uint32{msg.Uid}, func(fm *types.FullMessage) {
 			tmpFile, err := os.Create(tmpFileName)
 			if err != nil {
-				println(err)
-				// TODO: Do something with the error
-				addTab()
+				logging.Warnf("failed to create temporary attachment: %v", err)
+				_, err = addTab()
+				if err != nil {
+					logging.Warnf("failed to add tab: %v", err)
+				}
 				return
 			}
 
 			defer tmpFile.Close()
-			io.Copy(tmpFile, fm.Content.Reader)
+			_, err = io.Copy(tmpFile, fm.Content.Reader)
+			if err != nil {
+				logging.Warnf("failed to write to tmpfile: %w", err)
+				return
+			}
 			composer, err := addTab()
 			if err != nil {
 				return

commands/msg/invite.go
@@ -8,6 +8,7 @@ import (
 	"git.sr.ht/~rjarry/aerc/lib"
 	"git.sr.ht/~rjarry/aerc/lib/calendar"
 	"git.sr.ht/~rjarry/aerc/lib/format"
+	"git.sr.ht/~rjarry/aerc/logging"
 	"git.sr.ht/~rjarry/aerc/models"
 	"git.sr.ht/~rjarry/aerc/widgets"
 	"github.com/emersion/go-message/mail"
@@ -153,7 +154,10 @@ func (invite) Execute(aerc *widgets.Aerc, args []string) error {
 		}
 
 		composer.SetContents(cr.PlainText)
-		composer.AppendPart(cr.MimeType, cr.Params, cr.CalendarText)
+		err = composer.AppendPart(cr.MimeType, cr.Params, cr.CalendarText)
+		if err != nil {
+			return fmt.Errorf("failed to write invitation: %w", err)
+		}
 		composer.FocusTerminal()
 
 		tab := aerc.NewTab(composer, subject)
@@ -180,7 +184,10 @@ func (invite) Execute(aerc *widgets.Aerc, args []string) error {
 			aerc.PushError(err.Error())
 			return
 		} else {
-			addTab(cr)
+			err := addTab(cr)
+			if err != nil {
+				logging.Warnf("failed to add tab: %v", err)
+			}
 		}
 	})
 	return nil

commands/msg/pipe.go
@@ -96,7 +96,10 @@ func (Pipe) Execute(aerc *widgets.Aerc, args []string) error {
 			defer logging.PanicHandler()
 
 			defer pipe.Close()
-			io.Copy(pipe, reader)
+			_, err := io.Copy(pipe, reader)
+			if err != nil {
+				logging.Errorf("failed to send data to pipe: %w", err)
+			}
 		}()
 		err = ecmd.Run()
 		if err != nil {
@@ -224,10 +227,14 @@ func newMessagesReader(messages []*types.FullMessage, useMbox bool) io.Reader {
 	go func() {
 		defer pw.Close()
 		for _, msg := range messages {
+			var err error
 			if useMbox {
-				mboxer.Write(pw, msg.Content.Reader, "", time.Now())
+				err = mboxer.Write(pw, msg.Content.Reader, "", time.Now())
 			} else {
-				io.Copy(pw, msg.Content.Reader)
+				_, err = io.Copy(pw, msg.Content.Reader)
+			}
+			if err != nil {
+				logging.Warnf("failed to write data: %v", err)
 			}
 		}
 	}()

commit 6b1afc3ae3d8c4b9fbb46f416edba96b8b4a216f

tests: fix errors after lint series
Fix the following test failures:

 FAIL: TestMessageInfoHandledError (0.00s)
 parse_test.go:53: could not parse envelope: date parsing failed:
 unrecognized date format:

 FAIL: TestReader (0.07s)
 gpg_test.go:27: using GNUPGHOME = /tmp/TestReader2384941142/001
 reader_test.go:108: Test case: Invalid Signature
 reader_test.go:112: gpg.Read() = gpgmail: failed to read PGP
 message: gpg: failed to run verification: exit status 1

Fixes: 5ca6022d007b ("lint: ensure errors are at least logged (errcheck)")
Fixes: 70bfcfef4257 ("lint: work nicely with wrapped errors (errorlint)")
Signed-off-by: Robin Jarry <robin@jarry.cc>
Signed-off-by: Moritz Poldrack <moritz@poldrack.dev>

lib/crypto/gpg/gpgbin/encrypt.go
@@ -25,13 +25,10 @@ func Encrypt(r io.Reader, to []string, from string) ([]byte, error) {
 	args = append(args, "--encrypt", "-")
 
 	g := newGpg(r, args)
-	err := g.cmd.Run()
-	if err != nil {
-		return nil, fmt.Errorf("gpg: failed to run encryption: %w", err)
-	}
+	_ = g.cmd.Run()
 	outRdr := bytes.NewReader(g.stdout.Bytes())
 	var md models.MessageDetails
-	err = parse(outRdr, &md)
+	err := parse(outRdr, &md)
 	if err != nil {
 		return nil, fmt.Errorf("gpg: failure to encrypt: %w. check public key(s)", err)
 	}

lib/crypto/gpg/gpgbin/sign.go
@@ -17,14 +17,11 @@ func Sign(r io.Reader, from string) ([]byte, string, error) {
 	}
 
 	g := newGpg(r, args)
-	err := g.cmd.Run()
-	if err != nil {
-		return nil, "", fmt.Errorf("failed to run signing: %w", err)
-	}
+	_ = g.cmd.Run()
 
 	outRdr := bytes.NewReader(g.stdout.Bytes())
 	var md models.MessageDetails
-	err = parse(outRdr, &md)
+	err := parse(outRdr, &md)
 	if err != nil {
 		return nil, "", fmt.Errorf("failed to parse messagedetails: %w", err)
 	}

lib/crypto/gpg/gpgbin/verify.go
@@ -2,7 +2,6 @@ package gpgbin
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
@@ -30,17 +29,11 @@ func Verify(m io.Reader, s io.Reader) (*models.MessageDetails, error) {
 		return nil, err
 	}
 	g := newGpg(bytes.NewReader(orig), args)
-	err = g.cmd.Run()
-	if err != nil {
-		return nil, fmt.Errorf("gpg: failed to run verification: %w", err)
-	}
+	_ = g.cmd.Run()
 
 	out := bytes.NewReader(g.stdout.Bytes())
 	md := new(models.MessageDetails)
-	err = parse(out, md)
-	if err != nil {
-		return nil, fmt.Errorf("gpg: failed to parse result: %w", err)
-	}
+	_ = parse(out, md)
 
 	md.Body = bytes.NewReader(orig)
 

worker/lib/parse.go
@@ -167,7 +167,7 @@ func parseEnvelope(h *mail.Header) (*models.Envelope, error) {
 	if err != nil {
 		// still return a valid struct plus a sentinel date parsing error
 		// if only the date parsing failed
-		err = fmt.Errorf("%v: %w", DateParseError, err)
+		err = fmt.Errorf("%w: %v", DateParseError, err)
 	}
 	return &models.Envelope{
 		Date:      date,