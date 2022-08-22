rjarry/aerc

commit db39ca181adf61ebb5a89fe5eb7a1e0d6836be56

dirtree: add dirtree-collapse config setting
Adds a setting to the configuration to choose at which level the
folders in the dirtree are collapsed by default.

In my case, this is useful because my organisation has some rather deep
nesting in the folder structure, and a _lot_ of folders, and this way I
can keep my dirtree uncluttered while still having all folders there if
I need them.

Signed-off-by: Sijmen <me@sijman.nl>
Acked-by: Koni Marti <koni.marti@gmail.com>

widgets/dirtree.go
@@ -327,7 +327,7 @@ func (dt *DirectoryTree) buildTree() {
 	copy(dt.treeDirs, dt.dirs)
 
 	root := &types.Thread{Uid: 0}
-	buildTree(root, sTree, 0xFFFFFF)
+	dt.buildTreeNode(root, sTree, 0xFFFFFF, 1)
 
 	threads := make([]*types.Thread, 0)
 
@@ -373,7 +373,7 @@ func (dt *DirectoryTree) buildTree() {
 	}
 }
 
-func buildTree(node *types.Thread, stree [][]string, defaultUid uint32) {
+func (dt *DirectoryTree) buildTreeNode(node *types.Thread, stree [][]string, defaultUid uint32, depth int) {
 	m := make(map[string][][]string)
 	for _, branch := range stree {
 		if len(branch) > 1 {
@@ -398,7 +398,10 @@ func buildTree(node *types.Thread, stree [][]string, defaultUid uint32) {
 		}
 		nextNode := &types.Thread{Uid: uid}
 		node.AddChild(nextNode)
-		buildTree(nextNode, next, defaultUid)
+		if dt.UiConfig().DirListCollapse != 0 {
+			node.Hidden = depth > dt.UiConfig().DirListCollapse
+		}
+		dt.buildTreeNode(nextNode, next, defaultUid, depth+1)
 	}
 }
 

config/aerc.conf
@@ -103,6 +103,12 @@ dirlist-delay=200ms
 # Default: false
 dirlist-tree=false
 
+# If dirlist-tree is enabled, set level at which folders are collapsed by
+# default. Set to 0 to disable.
+#
+# Default: 0
+dirlist-collapse=0
+
 # List of space-separated criteria to sort the messages by, see *sort*
 # command in *aerc*(1) for reference. Prefixing a criterion with "-r "
 # reverses that criterion.

config/config.go
@@ -61,6 +61,7 @@ type UIConfig struct {
 	DirListFormat       string        `ini:"dirlist-format"`
 	DirListDelay        time.Duration `ini:"dirlist-delay"`
 	DirListTree         bool          `ini:"dirlist-tree"`
+	DirListCollapse     int           `ini:"dirlist-collapse"`
 	Sort                []string      `delim:" "`
 	NextMessageOnDelete bool          `ini:"next-message-on-delete"`
 	CompletionDelay     time.Duration `ini:"completion-delay"`

doc/aerc-config.5.scd
@@ -207,6 +207,12 @@ These options are configured in the *[ui]* section of aerc.conf.
 
 	Default: false
 
+*dirlist-collapse*
+	If dirlist-tree is enabled, set level at which folders are collapsed
+	by default. Set to 0 to disable.
+
+	Default: 0
+
 *next-message-on-delete*
 	Moves to next message when the current message is deleted
 

commit d138da0c9fe40ae1adb152da4a22bea2fb86be19

dirtree: fix build errors
Fix the following errors:

widgets/dirtree.go:401:18: not enough arguments in call to dt.UiConfig
	have ()
	want (string) (typecheck)
		if dt.UiConfig().DirListCollapse != 0 {
		              ^
widgets/dirtree.go:402:38: not enough arguments in call to dt.UiConfig
	have ()
	want (string) (typecheck)
			node.Hidden = depth > dt.UiConfig().DirListCollapse
			                                 ^
Since commit e0b62db583c3 ("fix: Set proper UIConfig for msgstores"),
DirectoryTree.UiConfig() takes a path parameter.

Fixes: db39ca181adf ("dirtree: add dirtree-collapse config setting")
Signed-off-by: Robin Jarry <robin@jarry.cc>

widgets/dirtree.go
@@ -386,6 +386,7 @@ func (dt *DirectoryTree) buildTreeNode(node *types.Thread, stree [][]string, def
 		keys = append(keys, key)
 	}
 	sort.Strings(keys)
+	path := dt.getDirectory(node)
 	for _, key := range keys {
 		next := m[key]
 		var uid uint32 = defaultUid
@@ -398,8 +399,8 @@ func (dt *DirectoryTree) buildTreeNode(node *types.Thread, stree [][]string, def
 		}
 		nextNode := &types.Thread{Uid: uid}
 		node.AddChild(nextNode)
-		if dt.UiConfig().DirListCollapse != 0 {
-			node.Hidden = depth > dt.UiConfig().DirListCollapse
+		if dt.UiConfig(path).DirListCollapse != 0 {
+			node.Hidden = depth > dt.UiConfig(path).DirListCollapse
 		}
 		dt.buildTreeNode(nextNode, next, defaultUid, depth+1)
 	}