rjarry/aerc

commit d941960fe175fd89b08b5a3f1bd073e0ecaf2d59

delete: improve find next function
Improve the function to find the next valid message after the delete
operation. This ensures that messages at the end or when marked in the
visual mode are properly dealt with.

Signed-off-by: Koni Marti <koni.marti@gmail.com>
Tested-by: Tim Culverhouse <tim@timculverhouse.com>
Acked-by: Robin Jarry <robin@jarry.cc>

commands/msg/delete.go
@@ -89,16 +89,24 @@ func (Delete) Execute(aerc *widgets.Aerc, args []string) error {
 }
 
 func findNextNonDeleted(deleted []uint32, store *lib.MessageStore) *models.MessageInfo {
-	selected := store.Selected()
-	if !contains(deleted, selected.Uid) {
-		return selected
+	var next, previous *models.MessageInfo
+	stepper := []func(){store.Next, store.Prev}
+	for _, stepFn := range stepper {
+		for {
+			next = store.Selected()
+			if next != nil && !contains(deleted, next.Uid) {
+				return next
+			}
+			if next == nil || previous == next {
+				break
+			}
+			stepFn()
+			previous = next
+		}
 	}
 
-	store.Next()
-	next := store.Selected()
-	if next == selected || next == nil {
-		// the last message is in the deleted state or doesn't exist
-		return nil
+	if next != nil {
+		store.Select(next.Uid)
 	}
 	return next
 }

commit 45e506e6aebdfdcb743d2d861eaddb85a01b3d5b

delete: fix find-next function
Fixes the find-next-loop when deleting the last message. The reverse
loop with store.Prev() will break too early because the value of
'previous' was not reset correctly.

Fixes: d941960f "delete: improve find next function"
References: https://todo.sr.ht/~rjarry/aerc/59
Signed-off-by: Koni Marti <koni.marti@gmail.com>
Tested-by: Tim Culverhouse <tim@timculverhouse.com>

commands/msg/delete.go
@@ -59,7 +59,7 @@ func (Delete) Execute(aerc *widgets.Aerc, args []string) error {
 					// no more messages in the list
 					if next == nil {
 						aerc.RemoveTab(h.msgProvider)
-						acct.Messages().Select(0)
+						acct.Messages().Select(-1)
 						acct.Messages().Invalidate()
 						return
 					}
@@ -77,7 +77,7 @@ func (Delete) Execute(aerc *widgets.Aerc, args []string) error {
 				if next == nil {
 					// We deleted the last message, select the new last message
 					// instead of the first message
-					acct.Messages().Select(0)
+					acct.Messages().Select(-1)
 				}
 			}
 		case *types.Error:
@@ -98,10 +98,13 @@ func findNextNonDeleted(deleted []uint32, store *lib.MessageStore) *models.Messa
 	var next, previous *models.MessageInfo
 	stepper := []func(){store.Next, store.Prev}
 	for _, stepFn := range stepper {
+		previous = nil
 		for {
 			next = store.Selected()
 			if next != nil && !contains(deleted, next.Uid) {
-				return next
+				if _, deleted := store.Deleted[next.Uid]; !deleted {
+					return next
+				}
 			}
 			if next == nil || previous == next {
 				break