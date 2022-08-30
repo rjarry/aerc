rjarry/aerc

commit 54a0a377e03074a27d7d9e84092487c55761b510

threads: debounce client-side thread building
Debounce client-side thread building in the message store. Debouncing is
useful when multiple messages are loaded, i.e. when scrolling with
PgUp/PgDown.

Without the debouncing, all client-side threads will be built everytime
the message store is updated which creates a noticable lag in the
message list ui when client-side threading is activated.

The default debouncing delay can be changed by changing
'client-threads-delay' in the UI config section.

Signed-off-by: Koni Marti <koni.marti@gmail.com>
Acked-by: Robin Jarry <robin@jarry.cc>

lib/msgstore.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"git.sr.ht/~rjarry/aerc/lib/sort"
+	"git.sr.ht/~rjarry/aerc/logging"
 	"git.sr.ht/~rjarry/aerc/models"
 	"git.sr.ht/~rjarry/aerc/worker/types"
 )
@@ -53,8 +54,8 @@ type MessageStore struct {
 	triggerNewEmail        func(*models.MessageInfo)
 	triggerDirectoryChange func()
 
-	dirInfoUpdateDebounce *time.Timer
-	dirInfoUpdateDelay    time.Duration
+	threadBuilderDebounce *time.Timer
+	threadBuilderDelay    time.Duration
 }
 
 const MagicUid = 0xFFFFFFFF
@@ -62,11 +63,9 @@ const MagicUid = 0xFFFFFFFF
 func NewMessageStore(worker *types.Worker,
 	dirInfo *models.DirectoryInfo,
 	defaultSortCriteria []*types.SortCriterion,
-	thread bool, clientThreads bool,
+	thread bool, clientThreads bool, clientThreadsDelay time.Duration,
 	triggerNewEmail func(*models.MessageInfo),
 	triggerDirectoryChange func()) *MessageStore {
-
-	dirInfoUpdateDelay := 5 * time.Second
 
 	if !dirInfo.Caps.Thread {
 		clientThreads = true
@@ -95,8 +94,7 @@ func NewMessageStore(worker *types.Worker,
 		triggerNewEmail:        triggerNewEmail,
 		triggerDirectoryChange: triggerDirectoryChange,
 
-		dirInfoUpdateDelay:    dirInfoUpdateDelay,
-		dirInfoUpdateDebounce: time.NewTimer(dirInfoUpdateDelay),
+		threadBuilderDelay: clientThreadsDelay,
 	}
 }
 
@@ -386,7 +384,17 @@ func (store *MessageStore) runThreadBuilder() {
 			store.builder.Update(msg)
 		}
 	}
-	store.Threads = store.builder.Threads(store.uids)
+	if store.threadBuilderDebounce != nil {
+		if store.threadBuilderDebounce.Stop() {
+			logging.Infof("thread builder debounced")
+		}
+	}
+	store.threadBuilderDebounce = time.AfterFunc(store.threadBuilderDelay, func() {
+		store.Threads = store.builder.Threads(store.uids)
+		if store.onUpdate != nil {
+			store.onUpdate(store)
+		}
+	})
 }
 
 func (store *MessageStore) Delete(uids []uint32,

config/aerc.conf
@@ -179,6 +179,11 @@ completion-popovers=true
 # Default: false
 #force-client-threads=false
 
+# Debounce client-side thread building
+#
+# Default: 50ms
+#client-threads-delay=50ms
+
 [statusline]
 # Describes the format string for the statusline.
 #

config/config.go
@@ -48,6 +48,7 @@ type UIConfig struct {
 	MouseEnabled        bool          `ini:"mouse-enabled"`
 	ThreadingEnabled    bool          `ini:"threading-enabled"`
 	ForceClientThreads  bool          `ini:"force-client-threads"`
+	ClientThreadsDelay  time.Duration `ini:"client-threads-delay"`
 	FuzzyComplete       bool          `ini:"fuzzy-complete"`
 	NewMessageBell      bool          `ini:"new-message-bell"`
 	Spinner             string        `ini:"spinner"`
@@ -712,6 +713,7 @@ func LoadConfigFromFile(root *string) (*AercConfig, error) {
 			EmptyMessage:        "(no messages)",
 			EmptyDirlist:        "(no folders)",
 			MouseEnabled:        false,
+			ClientThreadsDelay:  50 * time.Millisecond,
 			NewMessageBell:      true,
 			FuzzyComplete:       false,
 			Spinner:             "[..]    , [..]   ,  [..]  ,   [..] ,    [..],   [..] ,  [..]  , [..]   ",

lib/threadbuilder.go
@@ -1,6 +1,7 @@
 package lib
 
 import (
+	"sync"
 	"time"
 
 	"git.sr.ht/~rjarry/aerc/logging"
@@ -10,6 +11,7 @@ import (
 )
 
 type ThreadBuilder struct {
+	sync.Mutex
 	threadBlocks   map[uint32]jwz.Threadable
 	messageidToUid map[string]uint32
 	seen           map[uint32]bool
@@ -27,6 +29,9 @@ func NewThreadBuilder() *ThreadBuilder {
 
 // Uids returns the uids in threading order
 func (builder *ThreadBuilder) Uids() []uint32 {
+	builder.Lock()
+	defer builder.Unlock()
+
 	if builder.threadedUids == nil {
 		return []uint32{}
 	}
@@ -35,6 +40,9 @@ func (builder *ThreadBuilder) Uids() []uint32 {
 
 // Update updates the thread builder with a new message header
 func (builder *ThreadBuilder) Update(msg *models.MessageInfo) {
+	builder.Lock()
+	defer builder.Unlock()
+
 	if msg != nil {
 		if threadable := newThreadable(msg); threadable != nil {
 			builder.messageidToUid[threadable.MessageThreadID()] = msg.Uid
@@ -45,6 +53,9 @@ func (builder *ThreadBuilder) Update(msg *models.MessageInfo) {
 
 // Threads returns a slice of threads for the given list of uids
 func (builder *ThreadBuilder) Threads(uids []uint32) []*types.Thread {
+	builder.Lock()
+	defer builder.Unlock()
+
 	start := time.Now()
 
 	threads := builder.buildAercThreads(builder.generateStructure(uids), uids)

widgets/account.go
@@ -288,6 +288,7 @@ func (acct *AccountView) onMessage(msg types.WorkerMessage) {
 				acct.GetSortCriteria(),
 				acct.UiConfig().ThreadingEnabled,
 				acct.UiConfig().ForceClientThreads,
+				acct.UiConfig().ClientThreadsDelay,
 				func(msg *models.MessageInfo) {
 					acct.conf.Triggers.ExecNewEmail(acct.acct,
 						acct.conf, msg)

widgets/msglist.go
@@ -344,6 +344,7 @@ func (ml *MessageList) storeUpdate(store *lib.MessageStore) {
 	if ml.Store() != store {
 		return
 	}
+	ml.Invalidate()
 }
 
 func (ml *MessageList) SetStore(store *lib.MessageStore) {

commit 380cf13cff8e1f44f7f9705d17abb4a54d478033

msgstore: run threadBuilder with no debounce on DirectoryContents msg
Commit 54a0a377e030 ("threads: debounce client-side thread building")
introduced the option to debounce threadbuilding to improve performance
of client side threads. This caused a UI regression during filtering of
mailboxes:

1. The mailbox contains the full set of message UIDs
2. User filters mailbox
3. A DirectoryContents message is received from worker with the proper
   UIDs to display
4. Debounce is started (in a separate go routine)
5. The value of msgStore.Uids() is used to show pending headers, but is
   not updated until the threadbuilder runs. The full set of UIDs is
   shown briefly
6. Debounce is over, threadbuilder runs, updates msgStore.Uids(), proper
   messages show.

Run the thread builder immediately upon receipt of a DirectoryContents
message, bypassing any debounce. Performance will remain the same: the
debounce is meant to prevent multiple sequential calls to the thread
builder during scrolling. This is unlikely to occur in rapid succession
from filtering or sorting.

Fixes: https://todo.sr.ht/~rjarry/aerc/76
Fixes: 54a0a377e030 ("threads: debounce client-side thread building")
Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>
Acked-by: Robin Jarry <robin@jarry.cc>

lib/msgstore.go
@@ -208,7 +208,7 @@ func (store *MessageStore) Update(msg types.WorkerMessage) {
 		}
 		store.Messages = newMap
 		store.uids = msg.Uids
-		update = true
+		store.runThreadBuilderNow()
 	case *types.DirectoryThreaded:
 		var uids []uint32
 		newMap := make(map[uint32]*models.MessageInfo)
@@ -368,35 +368,40 @@ func (store *MessageStore) BuildThreads() bool {
 }
 
 func (store *MessageStore) runThreadBuilder() {
-	if store.builder == nil {
-		store.builder = NewThreadBuilder()
-		for _, msg := range store.Messages {
-			store.builder.Update(msg)
-		}
-	}
 	if store.threadBuilderDebounce != nil {
 		if store.threadBuilderDebounce.Stop() {
 			logging.Infof("thread builder debounced")
 		}
 	}
 	store.threadBuilderDebounce = time.AfterFunc(store.threadBuilderDelay, func() {
-		// build new threads
-		th := store.builder.Threads(store.uids)
-
-		// save local threads to the message store variable and
-		// run callback if defined (callback should reposition cursor)
-		store.threadsMutex.Lock()
-		store.threads = th
-		if store.threadCallback != nil {
-			store.threadCallback()
-		}
-		store.threadsMutex.Unlock()
-
-		// invalidate message list
-		if store.onUpdate != nil {
-			store.onUpdate(store)
-		}
+		store.runThreadBuilderNow()
 	})
+}
+
+// runThreadBuilderNow runs the threadbuilder without any debounce logic
+func (store *MessageStore) runThreadBuilderNow() {
+	if store.builder == nil {
+		store.builder = NewThreadBuilder()
+		for _, msg := range store.Messages {
+			store.builder.Update(msg)
+		}
+	}
+	// build new threads
+	th := store.builder.Threads(store.uids)
+
+	// save local threads to the message store variable and
+	// run callback if defined (callback should reposition cursor)
+	store.threadsMutex.Lock()
+	store.threads = th
+	if store.threadCallback != nil {
+		store.threadCallback()
+	}
+	store.threadsMutex.Unlock()
+
+	// invalidate message list
+	if store.onUpdate != nil {
+		store.onUpdate(store)
+	}
 }
 
 // SelectedThread returns the thread with the UID from the selected message