rjarry/aerc

commit 70bfcfef42578079f211d87cddc49519ee3503dc

lint: work nicely with wrapped errors (errorlint)
Error wrapping as introduced in Go 1.13 adds some additional logic to
use for comparing errors and adding information to it.

Signed-off-by: Moritz Poldrack <moritz@poldrack.dev>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/notmuch/lib/database.go
@@ -4,6 +4,7 @@
 package lib
 
 import (
+	"errors"
 	"fmt"
 	"time"
 
@@ -55,7 +56,7 @@ func (db *DB) connect(writable bool) error {
 	var err error
 	db.db, err = notmuch.Open(db.path, mode)
 	if err != nil {
-		return fmt.Errorf("could not connect to notmuch db: %v", err)
+		return fmt.Errorf("could not connect to notmuch db: %w", err)
 	}
 	db.lastOpenTime = time.Now()
 	return nil
@@ -112,7 +113,7 @@ func (db *DB) newQuery(ndb *notmuch.DB, query string) (*notmuch.Query, error) {
 	q.SetSortScheme(notmuch.SORT_OLDEST_FIRST)
 	for _, t := range db.excludedTags {
 		err := q.AddTagExclude(t)
-		if err != nil && err != notmuch.ErrIgnored {
+		if errors.Is(err, notmuch.ErrIgnored) {
 			return nil, err
 		}
 	}

commands/move-tab.go
@@ -31,7 +31,7 @@ func (MoveTab) Execute(aerc *widgets.Aerc, args []string) error {
 
 	n, err := strconv.Atoi(joinedArgs)
 	if err != nil {
-		return fmt.Errorf("failed to parse index argument: %v", err)
+		return fmt.Errorf("failed to parse index argument: %w", err)
 	}
 
 	var relative bool

commands/msg/forward.go
@@ -85,7 +85,7 @@ func (forward) Execute(aerc *widgets.Aerc, args []string) error {
 	if strings.Contains(to, "@") {
 		tolist, err = mail.ParseAddressList(to)
 		if err != nil {
-			return fmt.Errorf("invalid to address(es): %v", err)
+			return fmt.Errorf("invalid to address(es): %w", err)
 		}
 	}
 	if len(tolist) > 0 {

completer/completer.go
@@ -2,6 +2,7 @@ package completer
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -82,14 +83,14 @@ func (c *Completer) completeAddress(s string) ([]string, string, error) {
 	}
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		return nil, "", fmt.Errorf("stdout: %v", err)
+		return nil, "", fmt.Errorf("stdout: %w", err)
 	}
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
-		return nil, "", fmt.Errorf("stderr: %v", err)
+		return nil, "", fmt.Errorf("stderr: %w", err)
 	}
 	if err := cmd.Start(); err != nil {
-		return nil, "", fmt.Errorf("cmd start: %v", err)
+		return nil, "", fmt.Errorf("cmd start: %w", err)
 	}
 	// Wait returns an error if the exit status != 0, which some completion
 	// programs will do to signal no matches. We don't want to spam the user with
@@ -104,7 +105,7 @@ func (c *Completer) completeAddress(s string) ([]string, string, error) {
 		if msg != "" {
 			msg = ": " + msg
 		}
-		return nil, "", fmt.Errorf("read completions%s: %v", msg, err)
+		return nil, "", fmt.Errorf("read completions%s: %w", msg, err)
 	}
 
 	return completions, prefix, nil
@@ -150,7 +151,7 @@ func readCompletions(r io.Reader) ([]string, error) {
 	completions := []string{}
 	for {
 		line, err := buf.ReadString('\n')
-		if err == io.EOF {
+		if errors.Is(err, io.EOF) {
 			return completions, nil
 		} else if err != nil {
 			return nil, err

config/config.go
@@ -128,7 +128,7 @@ func (c RemoteConfig) ConnectionString() (string, error) {
 	cmd.Stdin = os.Stdin
 	output, err := cmd.Output()
 	if err != nil {
-		return "", fmt.Errorf("failed to read password: %s", err)
+		return "", fmt.Errorf("failed to read password: %w", err)
 	}
 
 	pw := strings.TrimSpace(string(output))
@@ -344,13 +344,13 @@ func loadAccountConfig(path string) ([]AccountConfig, error) {
 
 		source, err := sourceRemoteConfig.ConnectionString()
 		if err != nil {
-			return nil, fmt.Errorf("Invalid source credentials for %s: %s", _sec, err)
+			return nil, fmt.Errorf("Invalid source credentials for %s: %w", _sec, err)
 		}
 		account.Source = source
 
 		_, err = account.Outgoing.parseValue()
 		if err != nil {
-			return nil, fmt.Errorf("Invalid outgoing credentials for %s: %s", _sec, err)
+			return nil, fmt.Errorf("Invalid outgoing credentials for %s: %w", _sec, err)
 		}
 
 		accounts = append(accounts, account)
@@ -1027,7 +1027,7 @@ func (ui *UIConfig) loadStyleSet(styleSetDirs []string) error {
 	ui.style = NewStyleSet()
 	err := ui.style.LoadStyleSet(ui.StyleSetName, styleSetDirs)
 	if err != nil {
-		return fmt.Errorf("Unable to load default styleset: %s", err)
+		return fmt.Errorf("Unable to load default styleset: %w", err)
 	}
 
 	return nil

lib/crypto/gpg/gpgbin/encrypt.go
@@ -33,7 +33,7 @@ func Encrypt(r io.Reader, to []string, from string) ([]byte, error) {
 	var md models.MessageDetails
 	err = parse(outRdr, &md)
 	if err != nil {
-		return nil, fmt.Errorf("gpg: failure to encrypt: %v. check public key(s)", err)
+		return nil, fmt.Errorf("gpg: failure to encrypt: %w. check public key(s)", err)
 	}
 	var buf bytes.Buffer
 	_, _ = io.Copy(&buf, md.Body)

lib/crypto/gpg/gpgbin/sign.go
@@ -26,7 +26,7 @@ func Sign(r io.Reader, from string) ([]byte, string, error) {
 	var md models.MessageDetails
 	err = parse(outRdr, &md)
 	if err != nil {
-		return nil, "", fmt.Errorf("failed to parse messagedetails: %v", err)
+		return nil, "", fmt.Errorf("failed to parse messagedetails: %w", err)
 	}
 	var buf bytes.Buffer
 	_, _ = io.Copy(&buf, md.Body)

lib/crypto/gpg/reader.go
@@ -61,12 +61,12 @@ func Read(r io.Reader) (*Reader, error) {
 func newEncryptedReader(h textproto.Header, mr *textproto.MultipartReader) (*Reader, error) {
 	p, err := mr.NextPart()
 	if err != nil {
-		return nil, fmt.Errorf("gpgmail: failed to read first part in multipart/encrypted message: %v", err)
+		return nil, fmt.Errorf("gpgmail: failed to read first part in multipart/encrypted message: %w", err)
 	}
 
 	t, _, err := mime.ParseMediaType(p.Header.Get("Content-Type"))
 	if err != nil {
-		return nil, fmt.Errorf("gpgmail: failed to parse Content-Type of first part in multipart/encrypted message: %v", err)
+		return nil, fmt.Errorf("gpgmail: failed to parse Content-Type of first part in multipart/encrypted message: %w", err)
 	}
 	if !strings.EqualFold(t, "application/pgp-encrypted") {
 		return nil, fmt.Errorf("gpgmail: first part in multipart/encrypted message has type %q, not application/pgp-encrypted", t)
@@ -74,7 +74,7 @@ func newEncryptedReader(h textproto.Header, mr *textproto.MultipartReader) (*Rea
 
 	metadata, err := textproto.ReadHeader(bufio.NewReader(p))
 	if err != nil {
-		return nil, fmt.Errorf("gpgmail: failed to parse application/pgp-encrypted part: %v", err)
+		return nil, fmt.Errorf("gpgmail: failed to parse application/pgp-encrypted part: %w", err)
 	}
 	if s := metadata.Get("Version"); s != "1" {
 		return nil, fmt.Errorf("gpgmail: unsupported PGP/MIME version: %q", s)
@@ -82,11 +82,11 @@ func newEncryptedReader(h textproto.Header, mr *textproto.MultipartReader) (*Rea
 
 	p, err = mr.NextPart()
 	if err != nil {
-		return nil, fmt.Errorf("gpgmail: failed to read second part in multipart/encrypted message: %v", err)
+		return nil, fmt.Errorf("gpgmail: failed to read second part in multipart/encrypted message: %w", err)
 	}
 	t, _, err = mime.ParseMediaType(p.Header.Get("Content-Type"))
 	if err != nil {
-		return nil, fmt.Errorf("gpgmail: failed to parse Content-Type of second part in multipart/encrypted message: %v", err)
+		return nil, fmt.Errorf("gpgmail: failed to parse Content-Type of second part in multipart/encrypted message: %w", err)
 	}
 	if !strings.EqualFold(t, "application/octet-stream") {
 		return nil, fmt.Errorf("gpgmail: second part in multipart/encrypted message has type %q, not application/octet-stream", t)
@@ -94,13 +94,13 @@ func newEncryptedReader(h textproto.Header, mr *textproto.MultipartReader) (*Rea
 
 	md, err := gpgbin.Decrypt(p)
 	if err != nil {
-		return nil, fmt.Errorf("gpgmail: failed to read PGP message: %v", err)
+		return nil, fmt.Errorf("gpgmail: failed to read PGP message: %w", err)
 	}
 
 	cleartext := bufio.NewReader(md.Body)
 	cleartextHeader, err := textproto.ReadHeader(cleartext)
 	if err != nil {
-		return nil, fmt.Errorf("gpgmail: failed to read encrypted header: %v", err)
+		return nil, fmt.Errorf("gpgmail: failed to read encrypted header: %w", err)
 	}
 
 	t, params, err := mime.ParseMediaType(cleartextHeader.Get("Content-Type"))
@@ -114,7 +114,7 @@ func newEncryptedReader(h textproto.Header, mr *textproto.MultipartReader) (*Rea
 		mr := textproto.NewMultipartReader(cleartext, params["boundary"])
 		mds, err := newSignedReader(cleartextHeader, mr, micalg)
 		if err != nil {
-			return nil, fmt.Errorf("gpgmail: failed to read encapsulated multipart/signed message: %v", err)
+			return nil, fmt.Errorf("gpgmail: failed to read encapsulated multipart/signed message: %w", err)
 		}
 		mds.MessageDetails.IsEncrypted = md.IsEncrypted
 		mds.MessageDetails.DecryptedWith = md.DecryptedWith
@@ -136,7 +136,7 @@ func newSignedReader(h textproto.Header, mr *textproto.MultipartReader, micalg s
 	micalg = strings.ToLower(micalg)
 	p, err := mr.NextPart()
 	if err != nil {
-		return nil, fmt.Errorf("gpgmail: failed to read signed part in multipart/signed message: %v", err)
+		return nil, fmt.Errorf("gpgmail: failed to read signed part in multipart/signed message: %w", err)
 	}
 	var headerBuf bytes.Buffer
 	_ = textproto.WriteHeader(&headerBuf, p.Header)
@@ -147,12 +147,12 @@ func newSignedReader(h textproto.Header, mr *textproto.MultipartReader, micalg s
 
 	sig, err := mr.NextPart()
 	if err != nil {
-		return nil, fmt.Errorf("gpgmail: failed to read pgp part in multipart/signed message: %v", err)
+		return nil, fmt.Errorf("gpgmail: failed to read pgp part in multipart/signed message: %w", err)
 	}
 
 	md, err := gpgbin.Verify(&msg, sig)
 	if err != nil {
-		return nil, fmt.Errorf("gpgmail: failed to read PGP message: %v", err)
+		return nil, fmt.Errorf("gpgmail: failed to read PGP message: %w", err)
 	}
 	if md.Micalg != micalg && md.SignatureError == "" {
 		md.SignatureError = "gpg: header hash does not match actual sig hash"

commit 8ada3260ab8c3e9b4981c4bdc2de01aff2fae88c

notmuch: fix regression in error handling
Fix reggression introduced by 70bfcfef4257 ("lint: work nicely with
wrapped errors (errorlint)").

Discovered this because it broke my arec-notmuch config where I have
`exclude-tags=deleted`.  Queries with `tag:deleted` would now fail with
error message saying "Argument was ignored".

Fixes: 70bfcfef4257 ("lint: work nicely with wrapped errors (errorlint)")
Signed-off-by: Jose Lombera <jose@lombera.dev>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/notmuch/lib/database.go
@@ -113,7 +113,7 @@ func (db *DB) newQuery(ndb *notmuch.DB, query string) (*notmuch.Query, error) {
 	q.SetSortScheme(notmuch.SORT_OLDEST_FIRST)
 	for _, t := range db.excludedTags {
 		err := q.AddTagExclude(t)
-		if errors.Is(err, notmuch.ErrIgnored) {
+		if err != nil && !errors.Is(err, notmuch.ErrIgnored) {
 			return nil, err
 		}
 	}