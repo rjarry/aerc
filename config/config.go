rjarry/aerc

commit de24d2d5909adee29ea7adea9b22fbf1d02b7502

config: fix setting of zero-value time.Duration config values
When using section.MapTo(struct) (go-ini), if the struct has a default
value for a time.Duration type, a zero-value in the config will not
overwrite the default. If the type is *time.Duration, it will be
overwritten properly. One consideration was to change all
time.Duration types to *time.Duration. This method was chosen for ease
of implementation.

For example, if you set dirlist-delay = 0s, the delay will be 200ms.
This can be observed by logging the value just after mapping the ui
section in config.go. A config value of 0.1ms will have a delay of
0.1ms.

Currently, aerc has 4 time.Duration config values:
1. DirlistDelay - default 200 ms
2. CompletionDelay - default 250 ms
3. CheckMail - default unset (0)
4. CheckMailTimeout - default 10 s

1, 2, and 4 have a non-zero default value and are subject to this bug.
Only 1 and 2 are fixed in this patch. Number 4 would not make sense to
have a 0 second timeout, therefore we can prevent the user from doing
this by keeping it as it is.

Another option could be to set these to 0 in config.go. The default
config (aerc.conf) has these keys in it with their default values.
Presumably, we don't need to set them again in config.go. If a user
deletes the config values out of aerc.conf, the UI will function but
with 0s delays.

Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>
Tested-by: Moritz Poldrack <moritz@poldrack.dev>

config/config.go
@@ -467,6 +467,25 @@ func (config *AercConfig) LoadConfig(file *ini.File) error {
 		if err := validateBorderChars(ui, &config.Ui); err != nil {
 			return err
 		}
+		// Values with type=time.Duration must be explicitly set. If these
+		// values are given a default in the struct passed to ui.MapTo, which
+		// they are, a zero-value in the config won't overwrite the default.
+		for key, val := range ui.KeysHash() {
+			switch key {
+			case "dirlist-delay":
+				dur, err := time.ParseDuration(val)
+				if err != nil {
+					return err
+				}
+				config.Ui.DirListDelay = dur
+			case "completion-delay":
+				dur, err := time.ParseDuration(val)
+				if err != nil {
+					return err
+				}
+				config.Ui.CompletionDelay = dur
+			}
+		}
 	}
 
 	for _, sectionName := range file.SectionStrings() {

commit 1aa4fb9bff14e6857362f22e668369a47e43bc24

config: move ui config parse in function
Avoid repetition. Also, commit de24d2d5909a ("config: fix setting of
zero-value time.Duration config values") did not apply to contextual ui
config sections. Fix that.

Fixes: de24d2d5909a ("config: fix setting of zero-value time.Duration config values")
Signed-off-by: Robin Jarry <robin@jarry.cc>
Acked-by: Tim Culverhouse <tim@timculverhouse.com>

config/config.go
@@ -499,30 +499,8 @@ func (config *AercConfig) LoadConfig(file *ini.File) error {
 	}
 
 	if ui, err := file.GetSection("ui"); err == nil {
-		if err := ui.MapTo(&config.Ui); err != nil {
+		if err := parseUiConfig(ui, &config.Ui); err != nil {
 			return err
-		}
-		if err := validateBorderChars(ui, &config.Ui); err != nil {
-			return err
-		}
-		// Values with type=time.Duration must be explicitly set. If these
-		// values are given a default in the struct passed to ui.MapTo, which
-		// they are, a zero-value in the config won't overwrite the default.
-		for key, val := range ui.KeysHash() {
-			switch key {
-			case "dirlist-delay":
-				dur, err := time.ParseDuration(val)
-				if err != nil {
-					return err
-				}
-				config.Ui.DirListDelay = dur
-			case "completion-delay":
-				dur, err := time.ParseDuration(val)
-				if err != nil {
-					return err
-				}
-				config.Ui.CompletionDelay = dur
-			}
 		}
 	}
 
@@ -536,10 +514,7 @@ func (config *AercConfig) LoadConfig(file *ini.File) error {
 			return err
 		}
 		uiSubConfig := UIConfig{}
-		if err := uiSection.MapTo(&uiSubConfig); err != nil {
-			return err
-		}
-		if err := validateBorderChars(uiSection, &uiSubConfig); err != nil {
+		if err := parseUiConfig(uiSection, &uiSubConfig); err != nil {
 			return err
 		}
 		contextualUi := UIConfigContext{
@@ -641,23 +616,44 @@ func (config *AercConfig) LoadConfig(file *ini.File) error {
 	return nil
 }
 
-func validateBorderChars(section *ini.Section, config *UIConfig) error {
-	for key, val := range section.KeysHash() {
-		switch key {
-		case "border-char-vertical":
-			char := []rune(val)
-			if len(char) != 1 {
-				return fmt.Errorf("%v must be one and only one character", key)
-			}
-			config.BorderCharVertical = char[0]
-		case "border-char-horizontal":
-			char := []rune(val)
-			if len(char) != 1 {
-				return fmt.Errorf("%v must be one and only one character", key)
-			}
-			config.BorderCharHorizontal = char[0]
-		}
+func parseUiConfig(section *ini.Section, config *UIConfig) error {
+	if err := section.MapTo(config); err != nil {
+		return err
 	}
+
+	if key, err := section.GetKey("border-char-vertical"); err == nil {
+		chars := []rune(key.String())
+		if len(chars) != 1 {
+			return fmt.Errorf("%v must be one and only one character", key)
+		}
+		config.BorderCharVertical = chars[0]
+	}
+	if key, err := section.GetKey("border-char-horizontal"); err == nil {
+		chars := []rune(key.String())
+		if len(chars) != 1 {
+			return fmt.Errorf("%v must be one and only one character", key)
+		}
+		config.BorderCharHorizontal = chars[0]
+	}
+
+	// Values with type=time.Duration must be explicitly set. If these
+	// values are given a default in the struct passed to ui.MapTo, which
+	// they are, a zero-value in the config won't overwrite the default.
+	if key, err := section.GetKey("dirlist-delay"); err == nil {
+		dur, err := key.Duration()
+		if err != nil {
+			return err
+		}
+		config.DirListDelay = dur
+	}
+	if key, err := section.GetKey("completion-delay"); err == nil {
+		dur, err := key.Duration()
+		if err != nil {
+			return err
+		}
+		config.CompletionDelay = dur
+	}
+
 	return nil
 }
 