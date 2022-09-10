rjarry/aerc

commit ca90343850290c256430ad64daa3d809a7016299

outgoing-cred-cmd: delay execution until an email needs to be sent
This can be useful in cases when:

1. outgoing-cred-cmd requires a user action or confirmation (e.g. when
   using pass with a Yubikey or similar smart card that requires a user
   to enter a pin or touch the device when decrypting the password)

2. A user starts aerc frequently, but not all the sessions end up with
   sending emails

3. So the user only wants to execute outgoing-cred-cmd when the password
   is really used, so the user doesn't have to enter pin or touch their
   Yubikey each time aerc starts

Signed-off-by: Stas Rudakou <stas@garage22.net>
Acked-by: Robin Jarry <robin@jarry.cc>

config/config.go
@@ -95,6 +95,50 @@ const (
 	FILTER_HEADER
 )
 
+type RemoteConfig struct {
+	Value       string
+	PasswordCmd string
+}
+
+func (c RemoteConfig) parseValue() (*url.URL, error) {
+	return url.Parse(c.Value)
+}
+
+func (c RemoteConfig) ConnectionString() (string, error) {
+	if c.Value == "" || c.PasswordCmd == "" {
+		return c.Value, nil
+	}
+
+	u, err := c.parseValue()
+	if err != nil {
+		return "", err
+	}
+
+	// ignore the command if a password is specified
+	if _, exists := u.User.Password(); exists {
+		return c.Value, nil
+	}
+
+	// don't attempt to parse the command if the url is a path (ie /usr/bin/sendmail)
+	if !u.IsAbs() {
+		return c.Value, nil
+	}
+
+	cmd := exec.Command("sh", "-c", c.PasswordCmd)
+	cmd.Stdin = os.Stdin
+	output, err := cmd.Output()
+	if err != nil {
+		return "", fmt.Errorf("failed to read password: %s", err)
+	}
+
+	pw := strings.TrimSpace(string(output))
+	u.User = url.UserPassword(u.User.Username(), pw)
+	c.Value = u.String()
+	c.PasswordCmd = ""
+
+	return c.Value, nil
+}
+
 type AccountConfig struct {
 	Archive           string
 	CopyTo            string
@@ -104,12 +148,10 @@ type AccountConfig struct {
 	Aliases           string
 	Name              string
 	Source            string
-	SourceCredCmd     string
 	Folders           []string
 	FoldersExclude    []string
 	Params            map[string]string
-	Outgoing          string
-	OutgoingCredCmd   string
+	Outgoing          RemoteConfig
 	SignatureFile     string
 	SignatureCmd      string
 	EnableFoldersSort bool     `ini:"enable-folders-sort"`
@@ -239,6 +281,7 @@ func loadAccountConfig(path string) ([]AccountConfig, error) {
 			continue
 		}
 		sec := file.Section(_sec)
+		sourceRemoteConfig := RemoteConfig{}
 		account := AccountConfig{
 			Archive:           "Archive",
 			Default:           "INBOX",
@@ -260,12 +303,14 @@ func loadAccountConfig(path string) ([]AccountConfig, error) {
 				folders := strings.Split(val, ",")
 				sort.Strings(folders)
 				account.FoldersExclude = folders
+			} else if key == "source" {
+				sourceRemoteConfig.Value = val
 			} else if key == "source-cred-cmd" {
-				account.SourceCredCmd = val
+				sourceRemoteConfig.PasswordCmd = val
 			} else if key == "outgoing" {
-				account.Outgoing = val
+				account.Outgoing.Value = val
 			} else if key == "outgoing-cred-cmd" {
-				account.OutgoingCredCmd = val
+				account.Outgoing.PasswordCmd = val
 			} else if key == "from" {
 				account.From = val
 			} else if key == "aliases" {
@@ -295,54 +340,20 @@ func loadAccountConfig(path string) ([]AccountConfig, error) {
 			return nil, fmt.Errorf("Expected from for account %s", _sec)
 		}
 
-		source, err := parseCredential(account.Source, account.SourceCredCmd)
+		source, err := sourceRemoteConfig.ConnectionString()
 		if err != nil {
 			return nil, fmt.Errorf("Invalid source credentials for %s: %s", _sec, err)
 		}
 		account.Source = source
 
-		outgoing, err := parseCredential(account.Outgoing, account.OutgoingCredCmd)
+		_, err = account.Outgoing.parseValue()
 		if err != nil {
 			return nil, fmt.Errorf("Invalid outgoing credentials for %s: %s", _sec, err)
 		}
-		account.Outgoing = outgoing
 
 		accounts = append(accounts, account)
 	}
 	return accounts, nil
-}
-
-func parseCredential(cred, command string) (string, error) {
-	if cred == "" || command == "" {
-		return cred, nil
-	}
-
-	u, err := url.Parse(cred)
-	if err != nil {
-		return "", err
-	}
-
-	// ignore the command if a password is specified
-	if _, exists := u.User.Password(); exists {
-		return cred, nil
-	}
-
-	// don't attempt to parse the command if the url is a path (ie /usr/bin/sendmail)
-	if !u.IsAbs() {
-		return cred, nil
-	}
-
-	cmd := exec.Command("sh", "-c", command)
-	cmd.Stdin = os.Stdin
-	output, err := cmd.Output()
-	if err != nil {
-		return "", fmt.Errorf("failed to read password: %s", err)
-	}
-
-	pw := strings.TrimSpace(string(output))
-	u.User = url.UserPassword(u.User.Username(), pw)
-
-	return u.String(), nil
 }
 
 // Set at build time

commands/compose/send.go
@@ -51,7 +51,11 @@ func (Send) Execute(aerc *widgets.Aerc, args []string) error {
 	tabName := tab.Name
 	config := composer.Config()
 
-	if config.Outgoing == "" {
+	outgoing, err := config.Outgoing.ConnectionString()
+	if err != nil {
+		return errors.Wrap(err, "ReadCredentials(outgoing)")
+	}
+	if outgoing == "" {
 		return errors.New(
 			"No outgoing mail transport configured for this account")
 	}
@@ -74,7 +78,7 @@ func (Send) Execute(aerc *widgets.Aerc, args []string) error {
 		return errors.Wrap(err, "ParseAddress(config.From)")
 	}
 
-	uri, err := url.Parse(config.Outgoing)
+	uri, err := url.Parse(outgoing)
 	if err != nil {
 		return errors.Wrap(err, "url.Parse(outgoing)")
 	}

widgets/account-wizard.go
@@ -522,7 +522,7 @@ func (wizard *AccountWizard) finish(tutorial bool) {
 		Default:  "INBOX",
 		From:     sec.Key("from").String(),
 		Source:   sec.Key("source").String(),
-		Outgoing: sec.Key("outgoing").String(),
+		Outgoing: config.RemoteConfig{Value: sec.Key("outgoing").String()},
 	}
 	if wizard.smtpMode == SMTP_STARTTLS {
 		account.Params = map[string]string{

commit abf6ec7f02edbf8721fb32e4e760ebda2c20bc50

config: keep cache of resolved credential commands
outgoing-cred-cmd is used to retrieve the password from a password
manager such as UNIX pass or bitwarden CLI. These tools often prompt for
a passphrase to secure the passwords and it is annoying having to enter
it every time sending an email with aerc.

Add a new option outgoing-cred-cmd-cache (default to true) to control
whether aerc will keep a cache of the password or run outgoing-cred-cmd
every time an email needs to be sent.

NB: If the cached password is incorrect, the only way to change it is to
restart aerc.

Fixes: ca9034385029 ("outgoing-cred-cmd: delay execution until an email needs to be sent")
Signed-off-by: Robin Jarry <robin@jarry.cc>
Acked-by: Moritz Poldrack <moritz@poldrack.dev>

config/config.go
@@ -98,13 +98,15 @@ const (
 type RemoteConfig struct {
 	Value       string
 	PasswordCmd string
+	CacheCmd    bool
+	cache       string
 }
 
-func (c RemoteConfig) parseValue() (*url.URL, error) {
+func (c *RemoteConfig) parseValue() (*url.URL, error) {
 	return url.Parse(c.Value)
 }
 
-func (c RemoteConfig) ConnectionString() (string, error) {
+func (c *RemoteConfig) ConnectionString() (string, error) {
 	if c.Value == "" || c.PasswordCmd == "" {
 		return c.Value, nil
 	}
@@ -124,18 +126,23 @@ func (c RemoteConfig) ConnectionString() (string, error) {
 		return c.Value, nil
 	}
 
-	cmd := exec.Command("sh", "-c", c.PasswordCmd)
-	cmd.Stdin = os.Stdin
-	output, err := cmd.Output()
-	if err != nil {
-		return "", fmt.Errorf("failed to read password: %w", err)
+	pw := c.cache
+
+	if pw == "" {
+		cmd := exec.Command("sh", "-c", c.PasswordCmd)
+		cmd.Stdin = os.Stdin
+		output, err := cmd.Output()
+		if err != nil {
+			return "", fmt.Errorf("failed to read password: %w", err)
+		}
+		pw = strings.TrimSpace(string(output))
+	}
+	u.User = url.UserPassword(u.User.Username(), pw)
+	if c.CacheCmd {
+		c.cache = pw
 	}
 
-	pw := strings.TrimSpace(string(output))
-	u.User = url.UserPassword(u.User.Username(), pw)
-	c.Value = u.String()
-
-	return c.Value, nil
+	return u.String(), nil
 }
 
 type AccountConfig struct {
@@ -314,6 +321,13 @@ func loadAccountConfig(path string, accts []string) ([]AccountConfig, error) {
 				account.Outgoing.Value = val
 			case "outgoing-cred-cmd":
 				account.Outgoing.PasswordCmd = val
+			case "outgoing-cred-cmd-cache":
+				cache, err := strconv.ParseBool(val)
+				if err != nil {
+					return nil, fmt.Errorf(
+						"%s=%s %w", key, val, err)
+				}
+				account.Outgoing.CacheCmd = cache
 			case "from":
 				account.From = val
 			case "aliases":

doc/aerc-config.5.scd
@@ -657,6 +657,13 @@ Note that many of these configuration options are written for you, such as
 
 	Default: none
 
+*outgoing-cred-cmd-cache*
+	By default, the credentials returned by the command will be cached until
+	aerc is shut down. If set to false, *outgoing-cred-cmd* will be executed
+	every time an email is to be sent.
+
+	Default: true
+
 *pgp-auto-sign*
 	If true, all outgoing emails from this account will be signed (if a signing
 	key is available)