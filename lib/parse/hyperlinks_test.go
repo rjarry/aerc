rjarry/aerc

commit e1d8bc4d17cb7bd79e19fdf866d1da2fedda4de5

msgviewer: open http links from messages
Parse http links from a message and display them as completions in the
:open-link command.

Add the following binds to the [view] section in your binds.conf:
<C-l> = :open-link <space>

Parsing can be disabled in aerc.conf by setting parse-http-links to
false in the viewer section.

Thanks to Moritz for the help with the regular expression.

Signed-off-by: Koni Marti <koni.marti@gmail.com>
Reviewed-by: Moritz Poldrack <git@moritz.sh>
Acked-by: Robin Jarry <robin@jarry.cc>

lib/parse/hyperlinks.go
@@ -1,0 +1,44 @@
+package parse
+
+import (
+	"bufio"
+	"bytes"
+	"io"
+	"regexp"
+	"strings"
+)
+
+var submatch = `(https?:\/\/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,10}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*))`
+var httpRe = regexp.MustCompile("\"" + submatch + "\"" + "|" + "\\(" + submatch + "\\)" + "|" + "<" + submatch + ">" + "|" + submatch)
+
+// HttpLinks searches a reader for a http link and returns a copy of the
+// reader and a slice with links.
+func HttpLinks(r io.Reader) (io.Reader, []string) {
+	var buf bytes.Buffer
+	tr := io.TeeReader(r, &buf)
+
+	scanner := bufio.NewScanner(tr)
+	linkMap := make(map[string]struct{})
+	for scanner.Scan() {
+		line := scanner.Text()
+		if !strings.Contains(line, "http") {
+			continue
+		}
+		for _, word := range strings.Fields(line) {
+			if links := httpRe.FindStringSubmatch(word); len(links) > 0 {
+				for _, l := range links[1:] {
+					if l != "" {
+						linkMap[strings.TrimSpace(l)] = struct{}{}
+					}
+				}
+			}
+		}
+	}
+
+	results := []string{}
+	for link, _ := range linkMap {
+		results = append(results, link)
+	}
+
+	return &buf, results
+}

lib/parse/hyperlinks_test.go
@@ -1,0 +1,99 @@
+package parse_test
+
+import (
+	"io/ioutil"
+	"strings"
+	"testing"
+
+	"git.sr.ht/~rjarry/aerc/lib/parse"
+)
+
+func TestHyperlinks(t *testing.T) {
+	tests := []struct {
+		text  string
+		links []string
+	}{
+		{
+			text:  "http://aerc-mail.org",
+			links: []string{"http://aerc-mail.org"},
+		},
+		{
+			text:  "https://aerc-mail.org",
+			links: []string{"https://aerc-mail.org"},
+		},
+		{
+			text:  "text https://aerc-mail.org more text",
+			links: []string{"https://aerc-mail.org"},
+		},
+		{
+			text:  "text (https://aerc-mail.org) more text",
+			links: []string{"https://aerc-mail.org"},
+		},
+		{
+			text:  "text \"https://aerc-mail.org\" more text",
+			links: []string{"https://aerc-mail.org"},
+		},
+		{
+			text:  "text <https://aerc-mail.org> more text",
+			links: []string{"https://aerc-mail.org"},
+		},
+		{
+			text:  "<a href=\"https://aerc-mail.org\">",
+			links: []string{"https://aerc-mail.org"},
+		},
+		{
+			text:  "text https://aerc-mail.org more text https://aerc-mail.org more text",
+			links: []string{"https://aerc-mail.org"},
+		},
+		{
+			text:  "text https://aerc-mail.org more text http://git.sr.ht/~rjarry/aerc more text",
+			links: []string{"https://aerc-mail.org", "http://git.sr.ht/~rjarry/aerc"},
+		},
+		{
+			text:  "text http://www.ietf.org/rfc/rfc2396.txt more text",
+			links: []string{"http://www.ietf.org/rfc/rfc2396.txt"},
+		},
+		{
+			text:  "text <http://example.com:8042/over/there?name=ferret#nose> more text",
+			links: []string{"http://example.com:8042/over/there?name=ferret#nose"},
+		},
+		{
+			text:  "text http://cnn.example.com&story=breaking_news@10.0.0.1/top_story.htm more text",
+			links: []string{"http://cnn.example.com&story=breaking_news@10.0.0.1/top_story.htm"},
+		},
+		{
+			text:  "text https://www.ics.uci.edu/pub/ietf/uri/#Related more text",
+			links: []string{"https://www.ics.uci.edu/pub/ietf/uri/#Related"},
+		},
+		{
+			text:  "text https://www.example.com/index.php?id_sezione=360&sid=3a5ebc944f41daa6f849f730f1 more text",
+			links: []string{"https://www.example.com/index.php?id_sezione=360&sid=3a5ebc944f41daa6f849f730f1"},
+		},
+	}
+
+	for _, test := range tests {
+
+		// make sure reader is exact copy of input reader
+		reader, links := parse.HttpLinks(strings.NewReader(test.text))
+		if data, err := ioutil.ReadAll(reader); err != nil {
+			t.Errorf("could not read text: %v", err)
+		} else if string(data) != test.text {
+			t.Errorf("did not copy input reader correctly")
+		}
+
+		// check correct parsed links
+		if len(links) != len(test.links) {
+			t.Errorf("different number of links: got %d but expected %d", len(links), len(test.links))
+		}
+		linkMap := make(map[string]struct{})
+		for _, got := range links {
+			linkMap[got] = struct{}{}
+		}
+		for _, expected := range test.links {
+			if _, ok := linkMap[expected]; !ok {
+				t.Errorf("link not parsed: %s", expected)
+			}
+		}
+
+	}
+}

commands/msgview/open.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"time"
 
+	"git.sr.ht/~rjarry/aerc/commands"
 	"git.sr.ht/~rjarry/aerc/lib"
 	"git.sr.ht/~rjarry/aerc/logging"
 	"git.sr.ht/~rjarry/aerc/widgets"
@@ -20,16 +21,33 @@ func init() {
 }
 
 func (Open) Aliases() []string {
-	return []string{"open"}
+	return []string{"open", "open-link"}
 }
 
 func (Open) Complete(aerc *widgets.Aerc, args []string) []string {
+	mv := aerc.SelectedTab().(*widgets.MessageViewer)
+	if mv != nil {
+		if p := mv.SelectedMessagePart(); p != nil {
+			return commands.CompletionFromList(aerc, p.Links, args)
+		}
+	}
 	return nil
 }
 
 func (Open) Execute(aerc *widgets.Aerc, args []string) error {
 	mv := aerc.SelectedTab().(*widgets.MessageViewer)
 	p := mv.SelectedMessagePart()
+
+	if args[0] == "open-link" && len(args) > 1 {
+		if link := args[1]; link != "" {
+			go func() {
+				if err := lib.NewXDGOpen(link).Start(); err != nil {
+					aerc.PushError(fmt.Sprintf("%s: %s", args[0], err.Error()))
+				}
+			}()
+		}
+		return nil
+	}
 
 	store := mv.Store()
 	store.FetchBodyPart(p.Msg.Uid, p.Index, func(reader io.Reader) {

config/aerc.conf
@@ -209,6 +209,12 @@ header-layout=From|To,Cc|Bcc,Date,Subject
 # Default: false
 always-show-mime=false
 
+# Parses and extracts http links when viewing a message. Links can then be
+# accessed with the open-link command.
+#
+# Default: true
+parse-http-links=true
+
 # How long to wait after the last input before auto-completion is triggered.
 #
 # Default: 250ms

config/binds.conf
@@ -64,6 +64,8 @@ S = :save<space>
 D = :delete<Enter>
 A = :archive flat<Enter>
 
+<C-l> = :open-link <space>
+
 f = :forward<Enter>
 rr = :reply -a<Enter>
 rq = :reply -aq<Enter>

config/config.go
@@ -160,6 +160,7 @@ type ViewerConfig struct {
 	Alternatives   []string
 	ShowHeaders    bool       `ini:"show-headers"`
 	AlwaysShowMime bool       `ini:"always-show-mime"`
+	ParseHttpLinks bool       `ini:"parse-http-links"`
 	HeaderLayout   [][]string `ini:"-"`
 	KeyPassthrough bool       `ini:"-"`
 }
@@ -705,6 +706,7 @@ func LoadConfigFromFile(root *string, logger *log.Logger) (*AercConfig, error) {
 				{"Date"},
 				{"Subject"},
 			},
+			ParseHttpLinks: true,
 		},
 
 		Statusline: StatuslineConfig{

doc/aerc-config.5.scd
@@ -401,6 +401,12 @@ These options are configured in the *[viewer]* section of aerc.conf.
 
 	Default: false
 
+*parse-http-links*
+	Parses and extracts http links when viewing a message. Links can then be
+	accessed with the open-link command.
+
+	Default: true
+
 ## COMPOSE
 
 These options are configured in the *[compose]* section of aerc.conf.

widgets/msgviewer.go
@@ -19,6 +19,7 @@ import (
 	"git.sr.ht/~rjarry/aerc/lib"
 	"git.sr.ht/~rjarry/aerc/lib/auth"
 	"git.sr.ht/~rjarry/aerc/lib/format"
+	"git.sr.ht/~rjarry/aerc/lib/parse"
 	"git.sr.ht/~rjarry/aerc/lib/ui"
 	"git.sr.ht/~rjarry/aerc/logging"
 	"git.sr.ht/~rjarry/aerc/models"
@@ -325,6 +326,7 @@ func (mv *MessageViewer) SelectedMessagePart() *PartInfo {
 		Index: part.index,
 		Msg:   part.msg.MessageInfo(),
 		Part:  part.part,
+		Links: part.links,
 	}
 }
 
@@ -518,6 +520,8 @@ type PartViewer struct {
 	term        *Terminal
 	grid        *ui.Grid
 	uiConfig    config.UIConfig
+
+	links []string
 }
 
 func NewPartViewer(acct *AccountView, conf *config.AercConfig,
@@ -670,6 +674,14 @@ func (pv *PartViewer) writeMailHeaders() {
 	}
 }
 
+func (pv *PartViewer) hyperlinks(r io.Reader) (reader io.Reader) {
+	if !pv.conf.Viewer.ParseHttpLinks {
+		return r
+	}
+	reader, pv.links = parse.HttpLinks(r)
+	return reader
+}
+
 func (pv *PartViewer) copyFilterOutToPager() {
 	stdout, _ := pv.filter.StdoutPipe()
 	stderr, _ := pv.filter.StderrPipe()
@@ -708,7 +720,7 @@ func (pv *PartViewer) copyFilterOutToPager() {
 }
 
 func (pv *PartViewer) copySourceToSinkStripAnsi() {
-	scanner := bufio.NewScanner(pv.source)
+	scanner := bufio.NewScanner(pv.hyperlinks(pv.source))
 	// some people send around huge html without any newline in between
 	// this did overflow the default 64KB buffer of bufio.Scanner.
 	// If something can't fit in a GB there's no hope left

commit fad90c2956c6e57fd8da83da9c218b35cf2a2f93

open-link: make URL parsing more lenient
URLs are extremely loosely defined and can take many shapes which may
not be parsed at all if unusual characters like the exclamation mark are
present. To ensure lists and odd use of spaces are not parsed as links
some sanity-checks are in place:
	- the URL's schema must be at least two characters long
	- the URL's authority, path, and fragment must have a combined
	  length of 8 characters or longer
	- the URL must not contain a whitespace character, >, ), or "
	- the URL may only contain a ] when followed by a different allowed
	  character or at the end of the line (necessary for IPv6
	  authorities)

The tests for this function now include links with an exclamation point
and IPv6 addresses. The tests are given names to be easier identifiable.

Link: https://www.iana.org/assignments/uri-schemes/uri-schemes.xhtml
Reported-by: "Bence Ferdinandy" <bence@ferdinandy.com>
Cc: "Koni Marti" <koni.marti@gmail.com>
Fixes: e1d8bc4d17cb ("msgviewer: open http links from messages")
Signed-off-by: Moritz Poldrack <git@moritz.sh>
Acked-by: Tim Culverhouse <tim@timculverhouse.com>

lib/parse/hyperlinks.go
@@ -4,14 +4,12 @@ import (
 	"bufio"
 	"bytes"
 	"io"
+	"net/url"
 	"regexp"
 	"strings"
 )
 
-var (
-	submatch = `(https?:\/\/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,10}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*))`
-	httpRe   = regexp.MustCompile("\"" + submatch + "\"" + "|" + "\\(" + submatch + "\\)" + "|" + "<" + submatch + ">" + "|" + submatch)
-)
+var urlRe = regexp.MustCompile(`([\w\d]{2,}:([^\s>\]\)"]|\][^\s>\)"]|\]$){8,})`)
 
 // HttpLinks searches a reader for a http link and returns a copy of the
 // reader and a slice with links.
@@ -23,16 +21,12 @@ func HttpLinks(r io.Reader) (io.Reader, []string) {
 	linkMap := make(map[string]struct{})
 	for scanner.Scan() {
 		line := scanner.Text()
-		if !strings.Contains(line, "http") {
-			continue
-		}
 		for _, word := range strings.Fields(line) {
-			if links := httpRe.FindStringSubmatch(word); len(links) > 0 {
-				for _, l := range links[1:] {
-					if l != "" {
-						linkMap[strings.TrimSpace(l)] = struct{}{}
-					}
+			if links := urlRe.FindStringSubmatch(word); len(links) > 0 {
+				if _, err := url.Parse(links[0]); err != nil {
+					continue
 				}
+				linkMap[strings.TrimSpace(links[0])] = struct{}{}
 			}
 		}
 	}

lib/parse/hyperlinks_test.go
@@ -10,90 +10,118 @@ import (
 
 func TestHyperlinks(t *testing.T) {
 	tests := []struct {
+		name  string
 		text  string
 		links []string
 	}{
 		{
+			name:  "http-link",
 			text:  "http://aerc-mail.org",
 			links: []string{"http://aerc-mail.org"},
 		},
 		{
+			name:  "https-link",
 			text:  "https://aerc-mail.org",
 			links: []string{"https://aerc-mail.org"},
 		},
 		{
+			name:  "https-link-in-text",
 			text:  "text https://aerc-mail.org more text",
 			links: []string{"https://aerc-mail.org"},
 		},
 		{
+			name:  "https-link-in-parenthesis",
 			text:  "text (https://aerc-mail.org) more text",
 			links: []string{"https://aerc-mail.org"},
 		},
 		{
+			name:  "https-link-in-quotes",
 			text:  "text \"https://aerc-mail.org\" more text",
 			links: []string{"https://aerc-mail.org"},
 		},
 		{
+			name:  "https-link-in-angle-brackets",
 			text:  "text <https://aerc-mail.org> more text",
 			links: []string{"https://aerc-mail.org"},
 		},
 		{
+			name:  "https-link-in-html",
 			text:  "<a href=\"https://aerc-mail.org\">",
 			links: []string{"https://aerc-mail.org"},
 		},
 		{
+			name:  "https-link-twice",
 			text:  "text https://aerc-mail.org more text https://aerc-mail.org more text",
 			links: []string{"https://aerc-mail.org"},
 		},
 		{
+			name:  "multiple-links",
 			text:  "text https://aerc-mail.org more text http://git.sr.ht/~rjarry/aerc more text",
 			links: []string{"https://aerc-mail.org", "http://git.sr.ht/~rjarry/aerc"},
 		},
 		{
+			name:  "rfc",
 			text:  "text http://www.ietf.org/rfc/rfc2396.txt more text",
 			links: []string{"http://www.ietf.org/rfc/rfc2396.txt"},
 		},
 		{
+			name:  "http-with-query-and-fragment",
 			text:  "text <http://example.com:8042/over/there?name=ferret#nose> more text",
 			links: []string{"http://example.com:8042/over/there?name=ferret#nose"},
 		},
 		{
+			name:  "http-with-at",
 			text:  "text http://cnn.example.com&story=breaking_news@10.0.0.1/top_story.htm more text",
 			links: []string{"http://cnn.example.com&story=breaking_news@10.0.0.1/top_story.htm"},
 		},
 		{
+			name:  "https-with-fragment",
 			text:  "text https://www.ics.uci.edu/pub/ietf/uri/#Related more text",
 			links: []string{"https://www.ics.uci.edu/pub/ietf/uri/#Related"},
 		},
 		{
+			name:  "https-with-query",
 			text:  "text https://www.example.com/index.php?id_sezione=360&sid=3a5ebc944f41daa6f849f730f1 more text",
 			links: []string{"https://www.example.com/index.php?id_sezione=360&sid=3a5ebc944f41daa6f849f730f1"},
 		},
+		{
+			name:  "https-onedrive",
+			text:  "I have a link like this in an email (I deleted a few characters here-and-there for privacy) https://1drv.ms/w/s!Ap-KLfhNxS4fRt6tIvw?e=dW8WLO",
+			links: []string{"https://1drv.ms/w/s!Ap-KLfhNxS4fRt6tIvw?e=dW8WLO"},
+		},
+		{
+			name:  "mailto-ipv6",
+			text:  "You can reach me via the somewhat strange, but nonetheless valid, email mailto:~mpldr/list@[2001:db8::7]",
+			links: []string{"mailto:~mpldr/list@[2001:db8::7]"},
+		},
+		{
+			name:  "mailto-ipv6-query",
+			text:  "You can reach me via the somewhat strange, but nonetheless valid, email mailto:~mpldr/list@[2001:db8::7]?subject=whazzup%3F",
+			links: []string{"mailto:~mpldr/list@[2001:db8::7]?subject=whazzup%3F"},
+		},
 	}
 
-	for _, test := range tests {
-
-		// make sure reader is exact copy of input reader
-		reader, links := parse.HttpLinks(strings.NewReader(test.text))
-		if data, err := io.ReadAll(reader); err != nil {
-			t.Errorf("could not read text: %v", err)
-		} else if string(data) != test.text {
-			t.Errorf("did not copy input reader correctly")
-		}
-
-		// check correct parsed links
-		if len(links) != len(test.links) {
-			t.Errorf("different number of links: got %d but expected %d", len(links), len(test.links))
-		}
-		linkMap := make(map[string]struct{})
-		for _, got := range links {
-			linkMap[got] = struct{}{}
-		}
-		for _, expected := range test.links {
-			if _, ok := linkMap[expected]; !ok {
-				t.Errorf("link not parsed: %s", expected)
+	for i, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			// make sure reader is exact copy of input reader
+			reader, parsedLinks := parse.HttpLinks(strings.NewReader(test.text))
+			if _, err := io.ReadAll(reader); err != nil {
+				t.Skipf("could not read text: %v", err)
 			}
-		}
 
+			// check correct parsed links
+			if len(parsedLinks) != len(test.links) {
+				t.Errorf("different number of links: got %d but expected %d", len(parsedLinks), len(test.links))
+			}
+			linkMap := make(map[string]struct{})
+			for _, got := range parsedLinks {
+				linkMap[got] = struct{}{}
+			}
+			for _, expected := range test.links {
+				if _, ok := linkMap[expected]; !ok {
+					t.Errorf("link[%d] not parsed: %s", i, expected)
+				}
+			}
+		})
 	}
 }