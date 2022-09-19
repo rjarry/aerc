rjarry/aerc

commit 77f69501d6483bf94e85cd8b9cb520c3d9175a54

msgviewer: properly close embedded terminal
The terminal widget already handles most boring stuff: unwatch terminal
events, kill the underlying process, wait for it to exit, etc. Call the
Close() method and be done with it.

This avoids issues where the embedded terminal widget is destroyed but
the pager process does not know about it and dies in agony, writing over
aerc's UI:

  Vim: Caught deadly signal HUP

Also, it may avoid leaving child processes as zombies without giving
them a proper burial.

Reported-by: skejg
Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>
Signed-off-by: Robin Jarry <robin@jarry.cc>

widgets/msgviewer.go
@@ -863,12 +863,9 @@ func (pv *PartViewer) Draw(ctx *ui.Context) {
 }
 
 func (pv *PartViewer) Cleanup() {
-	if pv.pager != nil && pv.pager.Process != nil {
-		err := pv.pager.Process.Kill()
-		if err != nil {
-			logging.Warnf("failed to kill pager process: %v", err)
-		}
-		pv.pager = nil
+	if pv.term != nil {
+		pv.term.Close(nil)
+		pv.term = nil
 	}
 }
 

commit b31bb1876bee90434496d852f55a820fa3a26ca5

msgviewer: fix segfault when closing viewer
There is a race between PartViewer.Cleanup and PartViewer.Draw. pv.term
may be not nil in Draw and Cleanup() may set it to nil before
pv.term.Draw() is called, causing an invalid memory access:

[signal SIGSEGV: segmentation violation code=0x1 addr=0x29 pc=0x9413b8]

git.sr.ht/~rjarry/aerc/widgets.(*Terminal).Draw(0x0?, 0x0?)
	git.sr.ht/~rjarry/aerc/widgets/terminal.go:97 +0x18
git.sr.ht/~rjarry/aerc/widgets.(*PartViewer).Draw(0xc00012a540, 0xc0026ea690)
	git.sr.ht/~rjarry/aerc/widgets/msgviewer.go:862 +0x2fd

There is no need to reset term to nil.

Fixes: 77f69501d648 ("msgviewer: properly close embedded terminal")
Signed-off-by: Robin Jarry <robin@jarry.cc>
Acked-by: Tim Culverhouse <tim@timculverhouse.com>

widgets/msgviewer.go
@@ -867,7 +867,6 @@ func (pv *PartViewer) Draw(ctx *ui.Context) {
 func (pv *PartViewer) Cleanup() {
 	if pv.term != nil {
 		pv.term.Close(nil)
-		pv.term = nil
 	}
 }
 