rjarry/aerc

commit 9fdc7acf5b4842b95ab2b53c9baf69ab085b9e79

cache: fetch flags from UI
When cached headers are fetched, an action is posted back to the Worker
to immediately fetch the flags for the message from the server (we can't
know the flags state, therefore it's not cached). When scrolling, a lag
occurs when loading cached headers because the n+1 message has to wait
for the flag request to return before the cached headers are retrieved.

Collect the message UIDs in the UI that need flags, and fetch them based
off a debounce timer in a single request. Post the action from the UI to
eliminate an (ugly) go routine in the worker.

Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>
Acked-by: Robin Jarry <robin@jarry.cc>

lib/msgstore.go
@@ -48,6 +48,10 @@ type MessageStore struct {
 	pendingHeaders map[uint32]interface{}
 	worker         *types.Worker
 
+	needsFlags         []uint32
+	fetchFlagsDebounce *time.Timer
+	fetchFlagsDelay    time.Duration
+
 	triggerNewEmail        func(*models.MessageInfo)
 	triggerDirectoryChange func()
 
@@ -90,6 +94,9 @@ func NewMessageStore(worker *types.Worker,
 		pendingBodies:  make(map[uint32]interface{}),
 		pendingHeaders: make(map[uint32]interface{}),
 		worker:         worker,
+
+		needsFlags:      []uint32{},
+		fetchFlagsDelay: 50 * time.Millisecond,
 
 		triggerNewEmail:        triggerNewEmail,
 		triggerDirectoryChange: triggerDirectoryChange,
@@ -250,6 +257,10 @@ func (store *MessageStore) Update(msg types.WorkerMessage) {
 			merge(existing, msg.Info)
 		} else if msg.Info.Envelope != nil {
 			store.Messages[msg.Info.Uid] = msg.Info
+		}
+		if msg.NeedsFlags {
+			store.needsFlags = append(store.needsFlags, msg.Info.Uid)
+			store.fetchFlags()
 		}
 		seen := false
 		recent := false
@@ -751,4 +762,16 @@ func (store *MessageStore) Capabilities() *models.Capabilities {
 // -1 if not found
 func (store *MessageStore) SelectedIndex() int {
 	return store.FindIndexByUid(store.selectedUid)
+}
+
+func (store *MessageStore) fetchFlags() {
+	if store.fetchFlagsDebounce != nil {
+		store.fetchFlagsDebounce.Stop()
+	}
+	store.fetchFlagsDebounce = time.AfterFunc(store.fetchFlagsDelay, func() {
+		store.worker.PostAction(&types.FetchMessageFlags{
+			Uids: store.needsFlags,
+		}, nil)
+		store.needsFlags = []uint32{}
+	})
 }

worker/imap/cache.go
@@ -85,7 +85,7 @@ func (w *IMAPWorker) cacheHeader(mi *models.MessageInfo) {
 
 func (w *IMAPWorker) getCachedHeaders(msg *types.FetchMessageHeaders) []uint32 {
 	logging.Debugf("Retrieving headers from cache: %v", msg.Uids)
-	var need, found []uint32
+	var need []uint32
 	uv := fmt.Sprintf("%d", w.selected.UidValidity)
 	for _, uid := range msg.Uids {
 		u := fmt.Sprintf("%d", uid)
@@ -118,17 +118,11 @@ func (w *IMAPWorker) getCachedHeaders(msg *types.FetchMessageHeaders) []uint32 {
 			Uid:           ch.Uid,
 			RFC822Headers: hdr,
 		}
-		found = append(found, uid)
 		logging.Debugf("located cached header %s.%s", uv, u)
 		w.worker.PostMessage(&types.MessageInfo{
-			Message: types.RespondTo(msg),
-			Info:    mi,
-		}, nil)
-	}
-	if len(found) > 0 {
-		// Post in a separate goroutine to prevent deadlocking
-		go w.worker.PostAction(&types.FetchMessageFlags{
-			Uids: found,
+			Message:    types.RespondTo(msg),
+			Info:       mi,
+			NeedsFlags: true,
 		}, nil)
 	}
 	return need

worker/types/messages.go
@@ -212,7 +212,8 @@ type SearchResults struct {
 
 type MessageInfo struct {
 	Message
-	Info *models.MessageInfo
+	Info       *models.MessageInfo
+	NeedsFlags bool
 }
 
 type FullMessage struct {

commit a9af5635bc16bf994489f521006ab5151cddf609

msgstore: revert 9fdc7acf5b48 "post messageInfo on erroneous fetch"
Commit 9fdc7acf5b48 ("cache: fetch flags from UI") introduced a
regression where all messages were marked as erroneous if a single one
in the fetch request had an error.

Reported-by: Jose Lombera <jose@lombera.dev>
Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>
Acked-by: Robin Jarry <robin@jarry.cc>

lib/msgstore.go
@@ -119,9 +119,8 @@ func (store *MessageStore) FetchHeaders(uids []uint32,
 	}
 	if len(toFetch) > 0 {
 		store.worker.PostAction(&types.FetchMessageHeaders{Uids: toFetch}, func(msg types.WorkerMessage) {
-			if msg, ok := msg.(*types.Error); ok {
+			if _, ok := msg.(*types.Error); ok {
 				for _, uid := range toFetch {
-					store.postInvalidMessageInfo(uid, msg.Error)
 					delete(store.pendingHeaders, uid)
 				}
 			}
@@ -130,17 +129,6 @@ func (store *MessageStore) FetchHeaders(uids []uint32,
 			}
 		})
 	}
-}
-
-func (store *MessageStore) postInvalidMessageInfo(uid uint32, err error) {
-	logging.Errorf("Unable to fetch header %d: %w", uid, err)
-	info := &models.MessageInfo{
-		Envelope: &models.Envelope{},
-		Flags:    []models.Flag{models.SeenFlag},
-		Uid:      uid,
-		Error:    err,
-	}
-	store.Update(&types.MessageInfo{Info: info})
 }
 
 func (store *MessageStore) FetchFull(uids []uint32, cb func(*types.FullMessage)) {