rjarry/aerc

commit aae29324fdf558c8ee878ddefdd278758b67f479

config: fix default time format values
Adjust default values in config.go to follow what is set in the default
aerc.conf file.

Signed-off-by: Robin Jarry <robin@jarry.cc>
Acked-by: Tim Culverhouse <tim@timculverhouse.com>

config/aerc.conf
@@ -22,7 +22,7 @@ unsafe-accounts-conf=false
 # Describes the format for each row in a mailbox view. This field is compatible
 # with mutt's printf-like syntax.
 #
-# Default: %D %-17.17n %Z %s
+# Default: %-20.20D %-17.17n %Z %s
 index-format=%-20.20D %-17.17n %Z %s
 
 #

config/config.go
@@ -723,11 +723,11 @@ func LoadConfigFromFile(root *string, accts []string) (*AercConfig, error) {
 		},
 
 		Ui: UIConfig{
-			IndexFormat:        "%D %-17.17n %s",
+			IndexFormat:        "%-20.20D %-17.17n %Z %s",
 			TimestampFormat:    "2006-01-02 03:04 PM",
-			ThisDayTimeFormat:  "",
-			ThisWeekTimeFormat: "",
-			ThisYearTimeFormat: "",
+			ThisDayTimeFormat:  "03:04 PM",
+			ThisWeekTimeFormat: "Monday 03:04 PM",
+			ThisYearTimeFormat: "January 02",
 			ShowHeaders: []string{
 				"From", "To", "Cc", "Bcc", "Subject", "Date",
 			},

commit 24e30f7f1fd4d4f35a46b613b941c83d84dea8e8

config: remove default values for this-*-time-format
Having a default value is confusing because to disable the dynamic time
format, the users need to explicitly configure these settings to the
empty string.

Do not set default values for these settings when they are unset in the
configuration. Comment the default config file values to serve as
examples.

Fixes: aae29324fdf5 ("config: fix default time format values")
Reported-by: Nicolai Dagestad <nicolai@dagestad.fr>
Signed-off-by: Robin Jarry <robin@jarry.cc>
Acked-by: Moritz Poldrack <moritz@poldrack.dev>

config/aerc.conf
@@ -35,22 +35,22 @@ timestamp-format=2006-01-02 03:04 PM
 # Index-only time format for messages that were received/sent today.
 # If this is not specified, timestamp-format is used instead.
 #
-# Default: "03:04 PM" (12 hour time)
-this-day-time-format=03:04 PM
+# Default: ""
+#this-day-time-format=03:04 PM
 
 #
 # Index-only time format for messages that were received/sent within the last
 # 7 days. If this is not specified, timestamp-format is used instead.
 #
-# Default: "Monday 03:04 PM" (Week day + 12 hour time)
-this-week-time-format=Monday 03:04 PM
+# Default: ""
+#this-week-time-format=Monday 03:04 PM
 
 #
 # Index-only time format for messages that were received/sent this year.
 # If this is not specified, timestamp-format is used instead.
 #
-# Default: "January 02" (Month + month day)
-this-year-time-format=January 02
+# Default: ""
+#this-year-time-format=January 02
 
 #
 # Width of the sidebar, including the border.

config/config.go
@@ -736,9 +736,9 @@ func LoadConfigFromFile(root *string, accts []string) (*AercConfig, error) {
 		Ui: UIConfig{
 			IndexFormat:        "%-20.20D %-17.17n %Z %s",
 			TimestampFormat:    "2006-01-02 03:04 PM",
-			ThisDayTimeFormat:  "03:04 PM",
-			ThisWeekTimeFormat: "Monday 03:04 PM",
-			ThisYearTimeFormat: "January 02",
+			ThisDayTimeFormat:  "",
+			ThisWeekTimeFormat: "",
+			ThisYearTimeFormat: "",
 			ShowHeaders: []string{
 				"From", "To", "Cc", "Bcc", "Subject", "Date",
 			},

doc/aerc-config.5.scd
@@ -106,20 +106,20 @@ These options are configured in the *[ui]* section of aerc.conf.
 	Index-only time format for messages that were received/sent today.
 	If this is not specified, *timestamp-format* is used instead.
 
-	Default: "03:04 PM" (12 hour time)
+	Default: ""
 
 *this-week-time-format*
 	Index-only time format for messages that were received/sent within the
 	last 7 days. If this is not specified, *timestamp-format* is used
 	instead.
 
-	Default: "Monday 03:04 PM" (Week day + 12 hour time)
+	Default: ""
 
 *this-year-time-format*
 	Index-only time format for messages that were received/sent this year.
 	If this is not specified, *timestamp-format* is used instead.
 
-	Default: "January 02" (Month + month day)
+	Default: ""
 
 *sidebar-width*
 	Width of the sidebar, including the border. Set to zero to disable the