rjarry/aerc

commit 716ade89687150daadbb41bdec4a00d6d6e34193

worker: lock access to callback maps
Worker callbacks are inherently set and called from different
goroutines. Protect access to all callback maps with a mutex.

Signed-off-by: Moritz Poldrack <moritz@poldrack.dev>
Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/types/worker.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"sync"
 	"sync/atomic"
 
 	"git.sr.ht/~rjarry/aerc/logging"
@@ -20,6 +21,8 @@ type Worker struct {
 
 	actionCallbacks  map[int64]func(msg WorkerMessage)
 	messageCallbacks map[int64]func(msg WorkerMessage)
+
+	sync.Mutex
 }
 
 func NewWorker() *Worker {
@@ -49,7 +52,9 @@ func (worker *Worker) PostAction(msg WorkerMessage, cb func(msg WorkerMessage)) 
 	worker.Actions <- msg
 
 	if cb != nil {
+		worker.Lock()
 		worker.actionCallbacks[msg.getId()] = cb
+		worker.Unlock()
 	}
 }
 
@@ -68,7 +73,9 @@ func (worker *Worker) PostMessage(msg WorkerMessage,
 	worker.Messages <- msg
 
 	if cb != nil {
+		worker.Lock()
 		worker.messageCallbacks[msg.getId()] = cb
+		worker.Unlock()
 	}
 }
 
@@ -79,12 +86,14 @@ func (worker *Worker) ProcessMessage(msg WorkerMessage) WorkerMessage {
 		logging.Debugf("ProcessMessage %T(%d)", msg, msg.getId())
 	}
 	if inResponseTo := msg.InResponseTo(); inResponseTo != nil {
+		worker.Lock()
 		if f, ok := worker.actionCallbacks[inResponseTo.getId()]; ok {
 			f(msg)
 			if _, ok := msg.(*Done); ok {
 				delete(worker.actionCallbacks, inResponseTo.getId())
 			}
 		}
+		worker.Unlock()
 	}
 	return msg
 }
@@ -96,12 +105,14 @@ func (worker *Worker) ProcessAction(msg WorkerMessage) WorkerMessage {
 		logging.Debugf("ProcessAction %T(%d)", msg, msg.getId())
 	}
 	if inResponseTo := msg.InResponseTo(); inResponseTo != nil {
+		worker.Lock()
 		if f, ok := worker.messageCallbacks[inResponseTo.getId()]; ok {
 			f(msg)
 			if _, ok := msg.(*Done); ok {
 				delete(worker.messageCallbacks, inResponseTo.getId())
 			}
 		}
+		worker.Unlock()
 	}
 	return msg
 }

commit ea58e76332cbe854ab3b812678bbf68165795908

worker: do not lock while callbacks are running
Commit 716ade896871 ("worker: lock access to callback maps") introduced
locks to the worker callback maps. The locks also locked the processing
of the callback, which had the unintended side effect of deadlocking the
worker if any callbacks attempted to post a new action or message.

Refactor the locks to only lock the worker while accessing the maps.

Fixes: 716ade896871 ("worker: lock access to callback maps")
Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/types/worker.go
@@ -87,13 +87,16 @@ func (worker *Worker) ProcessMessage(msg WorkerMessage) WorkerMessage {
 	}
 	if inResponseTo := msg.InResponseTo(); inResponseTo != nil {
 		worker.Lock()
-		if f, ok := worker.actionCallbacks[inResponseTo.getId()]; ok {
+		f, ok := worker.actionCallbacks[inResponseTo.getId()]
+		worker.Unlock()
+		if ok {
 			f(msg)
 			if _, ok := msg.(*Done); ok {
+				worker.Lock()
 				delete(worker.actionCallbacks, inResponseTo.getId())
+				worker.Unlock()
 			}
 		}
-		worker.Unlock()
 	}
 	return msg
 }
@@ -106,13 +109,16 @@ func (worker *Worker) ProcessAction(msg WorkerMessage) WorkerMessage {
 	}
 	if inResponseTo := msg.InResponseTo(); inResponseTo != nil {
 		worker.Lock()
-		if f, ok := worker.messageCallbacks[inResponseTo.getId()]; ok {
+		f, ok := worker.messageCallbacks[inResponseTo.getId()]
+		worker.Unlock()
+		if ok {
 			f(msg)
 			if _, ok := msg.(*Done); ok {
+				worker.Lock()
 				delete(worker.messageCallbacks, inResponseTo.getId())
+				worker.Unlock()
 			}
 		}
-		worker.Unlock()
 	}
 	return msg
 }