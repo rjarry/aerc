rjarry/aerc

commit 150aa0f498b9780a40fdab815552c4c4230b5178

maildir: hide invalid folders
The maildir worker currently populates the list of mail folders by
listing all the filesystem subdirectories in the maildir directory.
Although there's no official specification for maildir subfolders,
they should all have cur/ new/ and tmp/ subdirectories to be valid.

This patch prevents directories that don't have those subdirectories
present on the filesystem from appearing in the account folder list.

This is useful for example to prevent ".notmuch" and ".notmuch/xapian"
from showing up in the folder list if using notmuch to index emails
while using aerc's maildir backend.

Signed-off-by: Julian Pidancet <julian.pidancet@oracle.com>
Acked-by: Tim Culverhouse <tim@timculverhouse.com>

worker/maildir/container.go
@@ -78,6 +78,13 @@ func (c *Container) ListFolders() ([]string, error) {
 			return nil
 		}
 
+		// Drop dirs that lack {new,tmp,cur} subdirs
+		for _, sub := range []string{"new", "tmp", "cur"} {
+			if _, err := os.Stat(filepath.Join(path, sub)); os.IsNotExist(err) {
+				return filepath.SkipDir
+			}
+		}
+
 		if c.maildirpp {
 			// In Maildir++ layout, mailboxes are stored in a single directory
 			// and prefixed with a dot, and subfolders are separated by dots.

commit 8153f59188bfb62a944a731fcffc78670085b675

maildir: fix maildir folder listing
Previous change "maildir: hide invalid folders" prevents filepath.Walk
from descending into directories that don't contain new/, tmp/ or cur/
because the WalkDirFunc returns filepath.SkipDir.

We have to assume that these directories can be the parent of valid
maildir folders even if they're not themselves valid maildir folders, so
it is a mistake not to recurse into these subfolders. Fix it by
returning nil instead of filepath.SkipDir in the WalkDirFunc.

Fixes: 150aa0f498b9 ("maildir: hide invalid folders")
Signed-off-by: Julian Pidancet <julian.pidancet@oracle.com>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/maildir/container.go
@@ -81,7 +81,7 @@ func (c *Container) ListFolders() ([]string, error) {
 		// Drop dirs that lack {new,tmp,cur} subdirs
 		for _, sub := range []string{"new", "tmp", "cur"} {
 			if _, err := os.Stat(filepath.Join(path, sub)); os.IsNotExist(err) {
-				return filepath.SkipDir
+				return nil
 			}
 		}
 