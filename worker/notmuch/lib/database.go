rjarry/aerc

commit 5ca6022d007bc83daba1e7297480d37f56554455

lint: ensure errors are at least logged (errcheck)
Signed-off-by: Moritz Poldrack <moritz@poldrack.dev>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/notmuch/lib/database.go
@@ -234,7 +234,11 @@ func (db *DB) msgModify(key string,
 		}
 		defer msg.Close()
 
-		cb(msg)
+		err = cb(msg)
+		if err != nil {
+			logging.Warnf("callback failed: %v", err)
+		}
+
 		err = msg.TagsToMaildirFlags()
 		if err != nil {
 			logging.Errorf("could not sync maildir flags: %v", err)
@@ -248,10 +252,16 @@ func (db *DB) MsgModifyTags(key string, add, remove []string) error {
 	err := db.msgModify(key, func(msg *notmuch.Message) error {
 		ierr := msg.Atomic(func(msg *notmuch.Message) {
 			for _, t := range add {
-				msg.AddTag(t)
+				err := msg.AddTag(t)
+				if err != nil {
+					logging.Warnf("failed to add tag: %v", err)
+				}
 			}
 			for _, t := range remove {
-				msg.RemoveTag(t)
+				err := msg.RemoveTag(t)
+				if err != nil {
+					logging.Warnf("failed to add tag: %v", err)
+				}
 			}
 		})
 		return ierr

aerc.go
@@ -164,7 +164,10 @@ func main() {
 	deferLoop := make(chan struct{})
 
 	c := crypto.New(conf.General.PgpProvider)
-	c.Init()
+	err = c.Init()
+	if err != nil {
+		logging.Warnf("failed to initialise crypto interface: %v", err)
+	}
 	defer c.Close()
 
 	aerc = widgets.NewAerc(conf, c, func(cmd []string) error {
@@ -205,7 +208,10 @@ func main() {
 		err := lib.ConnectAndExec(arg)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Failed to communicate to aerc: %v\n", err)
-			aerc.CloseBackends()
+			err = aerc.CloseBackends()
+			if err != nil {
+				logging.Warnf("failed to close backends: %v", err)
+			}
 			return
 		}
 	}
@@ -223,5 +229,8 @@ func main() {
 			time.Sleep(16 * time.Millisecond)
 		}
 	}
-	aerc.CloseBackends()
+	err = aerc.CloseBackends()
+	if err != nil {
+		logging.Warnf("failed to close backends: %v", err)
+	}
 }

commands/account/export-mbox.go
@@ -96,7 +96,10 @@ func (ExportMbox) Execute(aerc *widgets.Aerc, args []string) error {
 					done <- false
 				case *types.FullMessage:
 					mu.Lock()
-					mboxer.Write(file, msg.Content.Reader, "", t)
+					err := mboxer.Write(file, msg.Content.Reader, "", t)
+					if err != nil {
+						logging.Warnf("failed to write mbox: %v", err)
+					}
 					for i, uid := range uids {
 						if uid == msg.Content.Uid {
 							uids = append(uids[:i], uids[i+1:]...)

commands/msg/forward.go
@@ -133,14 +133,20 @@ func (forward) Execute(aerc *widgets.Aerc, args []string) error {
 		store.FetchFull([]uint32{msg.Uid}, func(fm *types.FullMessage) {
 			tmpFile, err := os.Create(tmpFileName)
 			if err != nil {
-				println(err)
-				// TODO: Do something with the error
-				addTab()
+				logging.Warnf("failed to create temporary attachment: %v", err)
+				_, err = addTab()
+				if err != nil {
+					logging.Warnf("failed to add tab: %v", err)
+				}
 				return
 			}
 
 			defer tmpFile.Close()
-			io.Copy(tmpFile, fm.Content.Reader)
+			_, err = io.Copy(tmpFile, fm.Content.Reader)
+			if err != nil {
+				logging.Warnf("failed to write to tmpfile: %w", err)
+				return
+			}
 			composer, err := addTab()
 			if err != nil {
 				return

commands/msg/invite.go
@@ -8,6 +8,7 @@ import (
 	"git.sr.ht/~rjarry/aerc/lib"
 	"git.sr.ht/~rjarry/aerc/lib/calendar"
 	"git.sr.ht/~rjarry/aerc/lib/format"
+	"git.sr.ht/~rjarry/aerc/logging"
 	"git.sr.ht/~rjarry/aerc/models"
 	"git.sr.ht/~rjarry/aerc/widgets"
 	"github.com/emersion/go-message/mail"
@@ -153,7 +154,10 @@ func (invite) Execute(aerc *widgets.Aerc, args []string) error {
 		}
 
 		composer.SetContents(cr.PlainText)
-		composer.AppendPart(cr.MimeType, cr.Params, cr.CalendarText)
+		err = composer.AppendPart(cr.MimeType, cr.Params, cr.CalendarText)
+		if err != nil {
+			return fmt.Errorf("failed to write invitation: %w", err)
+		}
 		composer.FocusTerminal()
 
 		tab := aerc.NewTab(composer, subject)
@@ -180,7 +184,10 @@ func (invite) Execute(aerc *widgets.Aerc, args []string) error {
 			aerc.PushError(err.Error())
 			return
 		} else {
-			addTab(cr)
+			err := addTab(cr)
+			if err != nil {
+				logging.Warnf("failed to add tab: %v", err)
+			}
 		}
 	})
 	return nil

commands/msg/pipe.go
@@ -96,7 +96,10 @@ func (Pipe) Execute(aerc *widgets.Aerc, args []string) error {
 			defer logging.PanicHandler()
 
 			defer pipe.Close()
-			io.Copy(pipe, reader)
+			_, err := io.Copy(pipe, reader)
+			if err != nil {
+				logging.Errorf("failed to send data to pipe: %w", err)
+			}
 		}()
 		err = ecmd.Run()
 		if err != nil {
@@ -224,10 +227,14 @@ func newMessagesReader(messages []*types.FullMessage, useMbox bool) io.Reader {
 	go func() {
 		defer pw.Close()
 		for _, msg := range messages {
+			var err error
 			if useMbox {
-				mboxer.Write(pw, msg.Content.Reader, "", time.Now())
+				err = mboxer.Write(pw, msg.Content.Reader, "", time.Now())
 			} else {
-				io.Copy(pw, msg.Content.Reader)
+				_, err = io.Copy(pw, msg.Content.Reader)
+			}
+			if err != nil {
+				logging.Warnf("failed to write data: %v", err)
 			}
 		}
 	}()

commands/msg/recall.go
@@ -182,7 +182,10 @@ func (Recall) Execute(aerc *widgets.Aerc, args []string) error {
 						composer.SetEncrypt(md.IsEncrypted)
 					}
 					if md.IsSigned {
-						composer.SetSign(md.IsSigned)
+						err = composer.SetSign(md.IsSigned)
+						if err != nil {
+							logging.Warnf("failed to set signed state: %v", err)
+						}
 					}
 				}
 				addTab()

commands/msg/reply.go
@@ -12,6 +12,7 @@ import (
 
 	"git.sr.ht/~rjarry/aerc/lib"
 	"git.sr.ht/~rjarry/aerc/lib/format"
+	"git.sr.ht/~rjarry/aerc/logging"
 	"git.sr.ht/~rjarry/aerc/models"
 	"git.sr.ht/~rjarry/aerc/widgets"
 	"github.com/emersion/go-message/mail"
@@ -224,9 +225,15 @@ func (reply) Execute(aerc *widgets.Aerc, args []string) error {
 
 		store.FetchBodyPart(msg.Uid, part, func(reader io.Reader) {
 			buf := new(bytes.Buffer)
-			buf.ReadFrom(reader)
+			_, err := buf.ReadFrom(reader)
+			if err != nil {
+				logging.Warnf("failed to fetch bodypart: %v", err)
+			}
 			original.Text = buf.String()
-			addTab()
+			err = addTab()
+			if err != nil {
+				logging.Warnf("failed to add tab: %v", err)
+			}
 		})
 		return nil
 	} else {

commit 135439b83fc5087e7067a040db2fe3ed2da8bb57

notmuch: fix bad copy-paste in MsgModifyTags logging
Print "failed to remove tag" instead of "failed to add tag" when
removing tag failed.

Fixes: 5ca6022d007b ("lint: ensure errors are at least logged (errcheck)")
Signed-off-by: Julian Pidancet <julian.pidancet@oracle.com>
Acked-by: Robin Jarry <robin@jarry.cc>
Acked-by: Tim Culverhouse <tim@timculverhouse.com>

worker/notmuch/lib/database.go
@@ -315,7 +315,7 @@ func (db *DB) MsgModifyTags(key string, add, remove []string) error {
 			for _, t := range remove {
 				err := msg.RemoveTag(t)
 				if err != nil {
-					logging.Warnf("failed to add tag: %v", err)
+					logging.Warnf("failed to remove tag: %v", err)
 				}
 			}
 		})