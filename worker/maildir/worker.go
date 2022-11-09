rjarry/aerc

commit 155f0c3f28a1d7bdb5c317f83a195b2fe9594b0d

maildir/sort: get MessageInfos in parallel
Sorting in the maildir worker requires reading each file in the
directory. Use waitgroups and goroutines to read files in parallel.

Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/maildir/worker.go
@@ -10,8 +10,10 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"runtime"
 	"sort"
 	"strings"
+	"sync"
 
 	"github.com/emersion/go-maildir"
 	"github.com/fsnotify/fsnotify"
@@ -436,14 +438,28 @@ func (w *Worker) sort(uids []uint32, criteria []*types.SortCriterion) ([]uint32,
 		return uids, nil
 	}
 	var msgInfos []*models.MessageInfo
+	mu := sync.Mutex{}
+	wg := sync.WaitGroup{}
+	// Hard limit at 2x CPU cores
+	max := runtime.NumCPU() * 2
+	limit := make(chan struct{}, max)
 	for _, uid := range uids {
-		info, err := w.msgInfoFromUid(uid)
-		if err != nil {
-			logging.Errorf("could not get message info: %v", err)
-			continue
-		}
-		msgInfos = append(msgInfos, info)
+		limit <- struct{}{}
+		wg.Add(1)
+		go func(uid uint32) {
+			defer wg.Done()
+			info, err := w.msgInfoFromUid(uid)
+			if err != nil {
+				logging.Errorf("could not get message info: %v", err)
+				return
+			}
+			mu.Lock()
+			msgInfos = append(msgInfos, info)
+			mu.Unlock()
+			<-limit
+		}(uid)
 	}
+	wg.Wait()
 	sortedUids, err := lib.Sort(msgInfos, criteria)
 	if err != nil {
 		logging.Errorf("could not sort the messages: %v", err)

commit 650e00972d036014530594bbd781579166889a5e

maildir: drain limit channel on parse error when sorting
Commit 155f0c3f28a1 ("maildir/sort: get MessageInfos in parallel") added
parallel parsing of maildir messages. The parsing is limitted to 2x the
number of CPUs using a buffered channel. The channel is drained at the
end of the parsing, however it is not drained if a parse error is
encountered. With enough errors, this can lead to a deadlock.

Drain the channel when an error occurs.

Fixes: 155f0c3f28a1 ("maildir/sort: get MessageInfos in parallel")
Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/maildir/worker.go
@@ -461,6 +461,7 @@ func (w *Worker) sort(uids []uint32, criteria []*types.SortCriterion) ([]uint32,
 			info, err := w.msgHeadersFromUid(uid)
 			if err != nil {
 				logging.Errorf("could not get message info: %v", err)
+				<-limit
 				return
 			}
 			mu.Lock()