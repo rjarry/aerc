rjarry/aerc

commit ca903d4228265272a0f6a780f5ed2280772eceec

envelope: add InReplyTo field
A standard IMAP envelope response includes the In-Reply-To header field.
Add this field to the aerc model of Envelope. Update envelope parser to
set this value. Update imap worker to set this value.

Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>
Tested-by: Inwit <inwit@sindominio.net>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/lib/parse.go
@@ -164,6 +164,14 @@ func parseEnvelope(h *mail.Header) (*models.Envelope, error) {
 			return nil, err
 		}
 	}
+	irtList, err := h.MsgIDList("in-reply-to")
+	if err != nil {
+		return nil, err
+	}
+	irt := ""
+	if len(irtList) > 0 {
+		irt = irtList[0]
+	}
 	date, err := parseDate(h)
 	if err != nil {
 		// still return a valid struct plus a sentinel date parsing error
@@ -179,6 +187,7 @@ func parseEnvelope(h *mail.Header) (*models.Envelope, error) {
 		To:        to,
 		Cc:        cc,
 		Bcc:       bcc,
+		InReplyTo: irt,
 	}, err
 }
 

models/models.go
@@ -90,6 +90,9 @@ func (mi *MessageInfo) InReplyTo() (msgid string, err error) {
 	if mi == nil {
 		return "", errors.New("msg is nil")
 	}
+	if mi.Envelope != nil && mi.Envelope.InReplyTo != "" {
+		return mi.Envelope.InReplyTo, nil
+	}
 	if mi.RFC822Headers == nil {
 		return "", errors.New("header is nil")
 	}
@@ -200,6 +203,7 @@ type Envelope struct {
 	Cc        []*mail.Address
 	Bcc       []*mail.Address
 	MessageId string
+	InReplyTo string
 }
 
 // OriginalMail is helper struct used for reply/forward

worker/imap/imap.go
@@ -1,6 +1,8 @@
 package imap
 
 import (
+	"strings"
+
 	"github.com/emersion/go-imap"
 
 	"git.sr.ht/~rjarry/aerc/models"
@@ -50,11 +52,8 @@ func translateEnvelope(e *imap.Envelope) *models.Envelope {
 
 	// we strip the msgid of "<>" in order to be more compatible with go-message
 	// which wants to handle msgids without the markers
-	// note this is a very naive way of doing it but probably good enough
-	msgID := e.MessageId
-	if len(msgID) > 1 && msgID[0] == '<' && msgID[len(msgID)-1] == '>' {
-		msgID = msgID[1 : len(msgID)-1]
-	}
+	msgID := strings.TrimSuffix(strings.TrimPrefix(e.MessageId, "<"), ">")
+	inReplyTo := strings.TrimSuffix(strings.TrimPrefix(e.InReplyTo, "<"), ">")
 
 	return &models.Envelope{
 		Date:      e.Date,
@@ -65,6 +64,7 @@ func translateEnvelope(e *imap.Envelope) *models.Envelope {
 		Cc:        translateAddresses(e.Cc),
 		Bcc:       translateAddresses(e.Bcc),
 		MessageId: msgID,
+		InReplyTo: inReplyTo,
 	}
 }
 

commit 1352942056940afb58d4e25d8094c08251fd54de

maildir: fallback when parsing for in-reply-to fails
Fallback to just using the raw In-Reply-To header content when it cannot
be properly parsed as a message-id.

Fixes: ca903d422826 ("envelope: add InReplyTo field")
Reported-by: Bence Ferdinandy <bence@ferdinandy.com>
Signed-off-by: Koni Marti <koni.marti@gmail.com>
Tested-by: Bence Ferdinandy <bence@ferdinandy.com>

worker/lib/parse.go
@@ -164,12 +164,15 @@ func parseEnvelope(h *mail.Header) (*models.Envelope, error) {
 			return nil, err
 		}
 	}
+	var irt string
 	irtList, err := h.MsgIDList("in-reply-to")
 	if err != nil {
-		return nil, err
-	}
-	irt := ""
-	if len(irtList) > 0 {
+		// proper parsing failed, so fall back to whatever is there
+		irt, err = h.Text("in-reply-to")
+		if err != nil {
+			return nil, err
+		}
+	} else if len(irtList) > 0 {
 		irt = irtList[0]
 	}
 	date, err := parseDate(h)