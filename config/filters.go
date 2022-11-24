rjarry/aerc

commit 17bb9387c4a3ecab17ace3dc78694c2236a1024f

config: move [filters] parsing to separate file
The config.go file is getting too big. Move the aerc.conf [filters]
section parsing logic into a dedicated filters.go file.

No functional change.

Signed-off-by: Robin Jarry <robin@jarry.cc>
Acked-by: Moritz Poldrack <moritz@poldrack.dev>

config/filters.go
@@ -1,0 +1,68 @@
+package config
+
+import (
+	"regexp"
+	"strings"
+
+	"git.sr.ht/~rjarry/aerc/logging"
+	"github.com/go-ini/ini"
+)
+
+type FilterType int
+
+const (
+	FILTER_MIMETYPE FilterType = iota
+	FILTER_HEADER
+)
+
+type FilterConfig struct {
+	Type    FilterType
+	Filter  string
+	Command string
+	Header  string
+	Regex   *regexp.Regexp
+}
+
+func (config *AercConfig) parseFilters(file *ini.File) error {
+	filters, err := file.GetSection("filters")
+	if err != nil {
+		goto end
+	}
+
+	// TODO: Parse the filter more finely, e.g. parse the regex
+	for match, cmd := range filters.KeysHash() {
+		filter := FilterConfig{
+			Command: cmd,
+			Filter:  match,
+		}
+		switch {
+		case strings.Contains(match, ",~"):
+			filter.Type = FILTER_HEADER
+			//nolint:gocritic // guarded by strings.Contains
+			header := filter.Filter[:strings.Index(filter.Filter, ",")]
+			regex := filter.Filter[strings.Index(filter.Filter, "~")+1:]
+			filter.Header = strings.ToLower(header)
+			filter.Regex, err = regexp.Compile(regex)
+			if err != nil {
+				return err
+			}
+		case strings.ContainsRune(match, ','):
+			filter.Type = FILTER_HEADER
+			//nolint:gocritic // guarded by strings.Contains
+			header := filter.Filter[:strings.Index(filter.Filter, ",")]
+			value := filter.Filter[strings.Index(filter.Filter, ",")+1:]
+			filter.Header = strings.ToLower(header)
+			filter.Regex, err = regexp.Compile(regexp.QuoteMeta(value))
+			if err != nil {
+				return err
+			}
+		default:
+			filter.Type = FILTER_MIMETYPE
+		}
+		config.Filters = append(config.Filters, filter)
+	}
+
+end:
+	logging.Debugf("aerc.conf: [filters] %#v", config.Filters)
+	return nil
+}

config/config.go
@@ -18,25 +18,12 @@ import (
 	"git.sr.ht/~rjarry/aerc/logging"
 )
 
-const (
-	FILTER_MIMETYPE = iota
-	FILTER_HEADER
-)
-
 type ComposeConfig struct {
 	Editor              string         `ini:"editor"`
 	HeaderLayout        [][]string     `ini:"-"`
 	AddressBookCmd      string         `ini:"address-book-cmd"`
 	ReplyToSelf         bool           `ini:"reply-to-self"`
 	NoAttachmentWarning *regexp.Regexp `ini:"-"`
-}
-
-type FilterConfig struct {
-	FilterType int
-	Filter     string
-	Command    string
-	Header     string
-	Regex      *regexp.Regexp
 }
 
 type ViewerConfig struct {
@@ -156,39 +143,6 @@ func installTemplate(root, name string) error {
 }
 
 func (config *AercConfig) LoadConfig(file *ini.File) error {
-	if filters, err := file.GetSection("filters"); err == nil {
-		// TODO: Parse the filter more finely, e.g. parse the regex
-		for _, match := range filters.KeyStrings() {
-			cmd := filters.KeysHash()[match]
-			filter := FilterConfig{
-				Command: cmd,
-				Filter:  match,
-			}
-			switch {
-			case strings.Contains(match, ",~"):
-				filter.FilterType = FILTER_HEADER
-				header := filter.Filter[:strings.Index(filter.Filter, ",")] //nolint:gocritic // guarded by strings.Contains
-				regex := filter.Filter[strings.Index(filter.Filter, "~")+1:]
-				filter.Header = strings.ToLower(header)
-				filter.Regex, err = regexp.Compile(regex)
-				if err != nil {
-					return err
-				}
-			case strings.ContainsRune(match, ','):
-				filter.FilterType = FILTER_HEADER
-				header := filter.Filter[:strings.Index(filter.Filter, ",")] //nolint:gocritic // guarded by strings.Contains
-				value := filter.Filter[strings.Index(filter.Filter, ",")+1:]
-				filter.Header = strings.ToLower(header)
-				filter.Regex, err = regexp.Compile(regexp.QuoteMeta(value))
-				if err != nil {
-					return err
-				}
-			default:
-				filter.FilterType = FILTER_MIMETYPE
-			}
-			config.Filters = append(config.Filters, filter)
-		}
-	}
 	if openers, err := file.GetSection("openers"); err == nil {
 		for mimeType, command := range openers.KeysHash() {
 			mimeType = strings.ToLower(mimeType)
@@ -313,6 +267,9 @@ func LoadConfigFromFile(root *string, accts []string) (*AercConfig, error) {
 		Openers:   make(map[string][]string),
 	}
 
+	if err := config.parseFilters(file); err != nil {
+		return nil, err
+	}
 	if err = config.LoadConfig(file); err != nil {
 		return nil, err
 	}
@@ -326,7 +283,6 @@ func LoadConfigFromFile(root *string, accts []string) (*AercConfig, error) {
 	logging.Debugf("aerc.conf: [statusline] %#v", config.Statusline)
 	logging.Debugf("aerc.conf: [viewer] %#v", config.Viewer)
 	logging.Debugf("aerc.conf: [compose] %#v", config.Compose)
-	logging.Debugf("aerc.conf: [filters] %#v", config.Filters)
 	logging.Debugf("aerc.conf: [openers] %#v", config.Openers)
 	logging.Debugf("aerc.conf: [triggers] %#v", config.Triggers)
 

widgets/msgviewer.go
@@ -557,7 +557,7 @@ func NewPartViewer(acct *AccountView, conf *config.AercConfig,
 	mime := part.FullMIMEType()
 
 	for _, f := range conf.Filters {
-		switch f.FilterType {
+		switch f.Type {
 		case config.FILTER_MIMETYPE:
 			if fnmatch.Match(f.Filter, mime, 0) {
 				filter = exec.Command("sh", "-c", f.Command)

commit eebb85a9d5bd35b460d5cf89a0cadcfce5b68137

config: fix [filters] ordering
Using KeysHash() does not guarantee any stable ordering of elements.
This causes random weirdness when some filters are using wildcards:

  [filters]
  text/plain=colorize
  text/html=html
  text/*=bat -fP --file-name="$AERC_FILENAME" --style=plain

When the source order is not preserved, the text/* filter may be matched
first against text/plain or text/html parts.

Use Keys() which does not create a map and preserve original ordering.

Fixes: 17bb9387c4a3 ("config: move [filters] parsing to separate file")
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Bence Ferdinandy <bence@ferdinandy.com>

config/filters.go
@@ -29,14 +29,13 @@ func (config *AercConfig) parseFilters(file *ini.File) error {
 		goto end
 	}
 
-	// TODO: Parse the filter more finely, e.g. parse the regex
-	for match, cmd := range filters.KeysHash() {
+	for _, key := range filters.Keys() {
 		filter := FilterConfig{
-			Command: cmd,
-			Filter:  match,
+			Command: key.Value(),
+			Filter:  key.Name(),
 		}
 		switch {
-		case strings.Contains(match, ",~"):
+		case strings.Contains(filter.Filter, ",~"):
 			filter.Type = FILTER_HEADER
 			//nolint:gocritic // guarded by strings.Contains
 			header := filter.Filter[:strings.Index(filter.Filter, ",")]
@@ -46,7 +45,7 @@ func (config *AercConfig) parseFilters(file *ini.File) error {
 			if err != nil {
 				return err
 			}
-		case strings.ContainsRune(match, ','):
+		case strings.ContainsRune(filter.Filter, ','):
 			filter.Type = FILTER_HEADER
 			//nolint:gocritic // guarded by strings.Contains
 			header := filter.Filter[:strings.Index(filter.Filter, ",")]