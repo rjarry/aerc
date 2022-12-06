rjarry/aerc

commit b46e57324394dd1ca106d210463190ae80f743c8

store: fix server-side threads toggling
Fix server-side threads toggling that can sometimes cause the uids to be
out-of-sync with the threads. This patch ensures a consistent way of
handling the uids and threads in the store.

Fixes: https://todo.sr.ht/~rjarry/aerc/102
Signed-off-by: Koni Marti <koni.marti@gmail.com>
Tested-by: Inwit <inwit@sindominio.net>
Acked-by: Robin Jarry <robin@jarry.cc>

lib/msgstore.go
@@ -236,9 +236,8 @@ func (store *MessageStore) Update(msg types.WorkerMessage) {
 	case *types.DirectoryThreaded:
 		newMap := make(map[uint32]*models.MessageInfo)
 
-		builder := NewThreadBuilder(store.iterFactory)
-		builder.RebuildUids(msg.Threads, store.reverseThreadOrder)
-		store.uids = builder.Uids()
+		store.builder = NewThreadBuilder(store.iterFactory)
+		store.builder.RebuildUids(msg.Threads, store.reverseThreadOrder)
 		store.threads = msg.Threads
 
 		for _, uid := range store.uids {

commit c4df1eea3ef4ac89d52a9d80a8151d3999215a6a

msgstore: set store.uids in threaded directories
Commit b46e57324394 ("store: fix server-side threads toggling")
introduced a regression where the uids of the store were not set after
rebuilding the threads. This would result in a flash of pending messages
as they were repopulated. This also would not properly remove messages
which were moved out of the store (deleted, moved, archived) with server
side threading.

Re-add the line that sets the store.uids.

Fixes: b46e57324394 ("store: fix server-side threads toggling")
Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>
Acked-by: Bence Ferdinandy <bence@ferdinandy.com>

lib/msgstore.go
@@ -238,6 +238,7 @@ func (store *MessageStore) Update(msg types.WorkerMessage) {
 
 		store.builder = NewThreadBuilder(store.iterFactory)
 		store.builder.RebuildUids(msg.Threads, store.reverseThreadOrder)
+		store.uids = store.builder.Uids()
 		store.threads = msg.Threads
 
 		for _, uid := range store.uids {