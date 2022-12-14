rjarry/aerc

commit adf74be4b5c55faa724d35ca7bea2603c3ef42f9

msglist: add attachment indicator
Add indicator of an attachment to the flags and make the character used
to be configurable.

Signed-off-by: Bence Ferdinandy <bence@ferdinandy.com>
Tested-by: Jens Grassel <jens@wegtam.com>

config/ui.go
@@ -45,6 +45,7 @@ type UIConfig struct {
 	IconSignedEncrypted           string        `ini:"icon-signed-encrypted"`
 	IconUnknown                   string        `ini:"icon-unknown"`
 	IconInvalid                   string        `ini:"icon-invalid"`
+	IconAttachment                string        `ini:"icon-attachment"`
 	DirListFormat                 string        `ini:"dirlist-format"`
 	DirListDelay                  time.Duration `ini:"dirlist-delay"`
 	DirListTree                   bool          `ini:"dirlist-tree"`

config/triggers.go
@@ -64,6 +64,7 @@ func (trig *TriggersConfig) ExecNewEmail(
 				Ui.ThisDayTimeFormat,
 				Ui.ThisWeekTimeFormat,
 				Ui.ThisYearTimeFormat,
+				Ui.IconAttachment,
 				format.Ctx{
 					FromAddress: account.From,
 					AccountName: account.Name,

doc/aerc-config.5.scd
@@ -112,7 +112,7 @@ These options are configured in the *[ui]* section of _aerc.conf_.
 |  _%v_
 :  sender first name (e.g. "Alex" in "Alex Smith <smith@example.net>")
 |  _%Z_
-:  flags (O=old, N=new, r=answered, D=deleted, !=flagged, \*=marked)
+:  flags (O=old, N=new, r=answered, D=deleted, !=flagged, \*=marked, a=attachment)
 
 *timestamp-format* = _<timeformat>_
 	See time.Time#Format at https://godoc.org/time#Time.Format
@@ -322,6 +322,11 @@ These options are configured in the *[ui]* section of _aerc.conf_.
 	The icon to display for signed mails where verification failed.
 
 	Default: _[s!]_
+
+*icon-attachment* = _<string>_
+	The icon to display index-format when the message has an attachment.
+
+	Default: _a_
 
 *fuzzy-complete* = _true_|_false_
 	When typing a command or option, the popover will now show not only the

lib/format/format.go
@@ -80,7 +80,7 @@ type Ctx struct {
 }
 
 func ParseMessageFormat(format string, timeFmt string, thisDayTimeFmt string,
-	thisWeekTimeFmt string, thisYearTimeFmt string, ctx Ctx) (
+	thisWeekTimeFmt string, thisYearTimeFmt string, iconAttachment string, ctx Ctx) (
 	string, []interface{}, error,
 ) {
 	if ctx.MsgInfo.Error != nil {
@@ -298,6 +298,7 @@ func ParseMessageFormat(format string, timeFmt string, thisDayTimeFmt string,
 			delFlag := ""
 			flaggedFlag := ""
 			markedFlag := ""
+			hasattachment := ""
 			seen := false
 			recent := false
 			answered := false
@@ -333,8 +334,13 @@ func ParseMessageFormat(format string, timeFmt string, thisDayTimeFmt string,
 			if ctx.MsgIsMarked {
 				markedFlag = "*"
 			}
+			for _, bS := range ctx.MsgInfo.BodyStructure.Parts {
+				if strings.ToLower(bS.Disposition) == "attachment" {
+					hasattachment = iconAttachment
+				}
+			}
 			retval = append(retval, '4', 's')
-			args = append(args, readReplyFlag+delFlag+flaggedFlag+markedFlag)
+			args = append(args, readReplyFlag+delFlag+flaggedFlag+markedFlag+hasattachment)
 
 		// Move the below cases to proper alphabetical positions once
 		// implemented

widgets/msglist.go
@@ -257,6 +257,7 @@ func (ml *MessageList) drawRow(textWidth int, ctx *ui.Context, uid uint32, row i
 		uiConfig.ThisDayTimeFormat,
 		uiConfig.ThisWeekTimeFormat,
 		uiConfig.ThisYearTimeFormat,
+		uiConfig.IconAttachment,
 		fmtCtx)
 	if err != nil {
 		ctx.Printf(0, row, style, "%v", err)

commit 4fe27144e3e78108b9ead03a256d1f7cf3e85557

config: add a default flag to icon-attachment
Set a default flag character for icon-attachment to match the man page.

Fixes: adf74be4b5c5 ("msglist: add attachment indicator")
Signed-off-by: Ben Cohen <ben@bencohen.net>
Acked-by: Robin Jarry <robin@jarry.cc>

config/ui.go
@@ -120,6 +120,7 @@ func defaultUiConfig() *UIConfig {
 		IconSignedEncrypted: "",
 		IconUnknown:         "[s?]",
 		IconInvalid:         "[s!]",
+		IconAttachment:      "a",
 		DirListFormat:       "%n %>r",
 		DirListDelay:        200 * time.Millisecond,
 		NextMessageOnDelete: true,