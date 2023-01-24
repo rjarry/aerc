rjarry/aerc

commit 5677f93ff8e0f212be112a110fcfe09663c84f98

model: change flags array to bitmask
Using a list of integers is not optimal. Use a bit mask instead.

Signed-off-by: Robin Jarry <robin@jarry.cc>
Acked-by: Tim Culverhouse <tim@timculverhouse.com>

lib/msgstore.go
@@ -269,15 +269,8 @@ func (store *MessageStore) Update(msg types.WorkerMessage) {
 			store.Unlock()
 			store.fetchFlags()
 		}
-		seen := false
-		recent := false
-		for _, flag := range msg.Info.Flags {
-			if flag == models.RecentFlag {
-				recent = true
-			} else if flag == models.SeenFlag {
-				seen = true
-			}
-		}
+		seen := msg.Info.Flags.Has(models.RecentFlag)
+		recent := msg.Info.Flags.Has(models.SeenFlag)
 		if !seen && recent {
 			store.triggerNewEmail(msg.Info)
 		}
@@ -563,12 +556,12 @@ func (store *MessageStore) Move(uids []uint32, dest string, createDest bool,
 	})
 }
 
-func (store *MessageStore) Flag(uids []uint32, flag models.Flag,
+func (store *MessageStore) Flag(uids []uint32, flags models.Flags,
 	enable bool, cb func(msg types.WorkerMessage),
 ) {
 	store.worker.PostAction(&types.FlagMessages{
 		Enable: enable,
-		Flag:   flag,
+		Flags:  flags,
 		Uids:   uids,
 	}, cb)
 }

commands/account/import-mbox.go
@@ -84,7 +84,7 @@ func (ImportMbox) Execute(aerc *widgets.Aerc, args []string) error {
 				nbytes, _ := io.Copy(&buf, r)
 				worker.PostAction(&types.AppendMessage{
 					Destination: folder,
-					Flags:       []models.Flag{models.SeenFlag},
+					Flags:       models.SeenFlag,
 					Date:        time.Now(),
 					Reader:      &buf,
 					Length:      int(nbytes),

commands/compose/postpone.go
@@ -95,7 +95,7 @@ func (Postpone) Execute(aerc *widgets.Aerc, args []string) error {
 		nbytes := int(ctr.Count())
 		worker.PostAction(&types.AppendMessage{
 			Destination: config.Postpone,
-			Flags:       []models.Flag{models.SeenFlag},
+			Flags:       models.SeenFlag,
 			Date:        time.Now(),
 			Reader:      &buf,
 			Length:      int(nbytes),

commands/compose/send.go
@@ -502,7 +502,7 @@ func copyToSent(worker *types.Worker, dest string,
 	errCh := make(chan error)
 	worker.PostAction(&types.AppendMessage{
 		Destination: dest,
-		Flags:       []models.Flag{models.SeenFlag},
+		Flags:       models.SeenFlag,
 		Date:        time.Now(),
 		Reader:      msg,
 		Length:      n,

commands/msg/read.go
@@ -34,7 +34,7 @@ func (FlagMsg) Complete(aerc *widgets.Aerc, args []string) []string {
 // 'flag' or 'unflag', respectively, but the 'Seen' flag is affected.
 func (FlagMsg) Execute(aerc *widgets.Aerc, args []string) error {
 	// The flag to change
-	var flag models.Flag
+	var flag models.Flags
 	// User-readable name of the flag to change
 	var flagName string
 	// Whether to toggle the flag (true) or to enable/disable it (false)
@@ -130,14 +130,7 @@ func (FlagMsg) Execute(aerc *widgets.Aerc, args []string) error {
 			return err
 		}
 		for _, m := range msgs {
-			var enabled bool
-			for _, mFlag := range m.Flags {
-				if mFlag == flag {
-					enabled = true
-					break
-				}
-			}
-			if enabled {
+			if m.Flags.Has(flag) {
 				toDisable = append(toDisable, m.Uid)
 			} else {
 				toEnable = append(toEnable, m.Uid)

lib/emlview.go
@@ -27,8 +27,8 @@ func (fm *EmlMessage) Labels() ([]string, error) {
 	return nil, nil
 }
 
-func (fm *EmlMessage) ModelFlags() ([]models.Flag, error) {
-	return []models.Flag{models.SeenFlag}, nil
+func (fm *EmlMessage) ModelFlags() (models.Flags, error) {
+	return models.SeenFlag, nil
 }
 
 // NewEmlMessageView provides a MessageView for a full message that is not

lib/format/format.go
@@ -294,53 +294,36 @@ func ParseMessageFormat(format string, timeFmt string, thisDayTimeFmt string,
 			}
 		case 'Z':
 			// calculate all flags
-			readReplyFlag := ""
-			delFlag := ""
-			flaggedFlag := ""
-			markedFlag := ""
-			hasattachment := ""
-			seen := false
-			recent := false
-			answered := false
-			for _, flag := range ctx.MsgInfo.Flags {
-				switch flag {
-				case models.SeenFlag:
-					seen = true
-				case models.RecentFlag:
-					recent = true
-				case models.AnsweredFlag:
-					answered = true
-				}
-				if flag == models.DeletedFlag {
-					delFlag = "D"
-					// TODO: check if attachments
-				}
-				if flag == models.FlaggedFlag {
-					flaggedFlag = "!"
-				}
-				// TODO: check gpg stuff
+			flags := ctx.MsgInfo.Flags
+			f := ""
+
+			switch {
+			case flags.Has(models.SeenFlag | models.AnsweredFlag):
+				f += "r" // message has been replied to
+			case flags.Has(models.SeenFlag):
+				break
+			case flags.Has(models.RecentFlag):
+				f += "N" // message is new
+			default:
+				f += "O" // message is old
 			}
-			if seen {
-				if answered {
-					readReplyFlag = "r" // message has been replied to
-				}
-			} else {
-				if recent {
-					readReplyFlag = "N" // message is new
-				} else {
-					readReplyFlag = "O" // message is old
-				}
-			}
-			if ctx.MsgIsMarked {
-				markedFlag = "*"
+			if flags.Has(models.DeletedFlag) {
+				f += "D"
 			}
 			for _, bS := range ctx.MsgInfo.BodyStructure.Parts {
 				if strings.ToLower(bS.Disposition) == "attachment" {
-					hasattachment = iconAttachment
+					f += iconAttachment
+					break
 				}
 			}
+			if flags.Has(models.FlaggedFlag) {
+				f += "!"
+			}
+			if ctx.MsgIsMarked {
+				f += "*"
+			}
 			retval = append(retval, '4', 's')
-			args = append(args, readReplyFlag+delFlag+flaggedFlag+markedFlag+hasattachment)
+			args = append(args, f)
 
 		// Move the below cases to proper alphabetical positions once
 		// implemented

models/models.go
@@ -10,13 +10,13 @@ import (
 	"github.com/emersion/go-message/mail"
 )
 
-// Flag is an abstraction around the different flags which can be present in
+// Flags is an abstraction around the different flags which can be present in
 // different email backends and represents a flag that we use in the UI.
-type Flag int
+type Flags uint32
 
 const (
 	// SeenFlag marks a message as having been seen previously
-	SeenFlag Flag = iota
+	SeenFlag Flags = 1 << iota
 
 	// RecentFlag marks a message as being recent
 	RecentFlag
@@ -30,6 +30,10 @@ const (
 	// FlaggedFlag marks a message with a user flag
 	FlaggedFlag
 )
+
+func (f Flags) Has(flags Flags) bool {
+	return f&flags == flags
+}
 
 type Directory struct {
 	Name       string
@@ -67,7 +71,7 @@ type Capabilities struct {
 type MessageInfo struct {
 	BodyStructure *BodyStructure
 	Envelope      *Envelope
-	Flags         []Flag
+	Flags         Flags
 	Labels        []string
 	InternalDate  time.Time
 	RFC822Headers *mail.Header

commit 3000c8262f69e2b04266be6ee08f1f5cb7c3f473

msgstore: fix new-email trigger firing for all unread messages
Unfortunate typo.

Fixes: 5677f93ff8e0 ("model: change flags array to bitmask")
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Ben Cohen <ben@bencohen.net>

lib/msgstore.go
@@ -269,8 +269,8 @@ func (store *MessageStore) Update(msg types.WorkerMessage) {
 			store.Unlock()
 			store.fetchFlags()
 		}
-		seen := msg.Info.Flags.Has(models.RecentFlag)
-		recent := msg.Info.Flags.Has(models.SeenFlag)
+		seen := msg.Info.Flags.Has(models.SeenFlag)
+		recent := msg.Info.Flags.Has(models.RecentFlag)
 		if !seen && recent {
 			store.triggerNewEmail(msg.Info)
 		}