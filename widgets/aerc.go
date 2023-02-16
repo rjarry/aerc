rjarry/aerc

commit 7a489cb0011a34a68d3e77d0174076857cc37902

Add Unix socket for communicating with aerc

widgets/aerc.go
@@ -1,7 +1,10 @@
 package widgets
 
 import (
+	"errors"
 	"log"
+	"net/url"
+	"strings"
 	"time"
 
 	"github.com/gdamore/tcell"
@@ -301,4 +304,41 @@ func (aerc *Aerc) BeginExCommand() {
 	})
 	aerc.statusbar.Push(exline)
 	aerc.focus(exline)
+}
+
+func (aerc *Aerc) Mailto(addr *url.URL) error {
+	acct := aerc.SelectedAccount()
+	if acct == nil {
+		return errors.New("No account selected")
+	}
+	defaults := make(map[string]string)
+	defaults["To"] = addr.Opaque
+	headerMap := map[string]string{
+		"cc":          "Cc",
+		"in-reply-to": "In-Reply-To",
+		"subject":     "Subject",
+	}
+	for key, vals := range addr.Query() {
+		if header, ok := headerMap[strings.ToLower(key)]; ok {
+			defaults[header] = strings.Join(vals, ",")
+		}
+	}
+	composer := NewComposer(aerc.Config(),
+		acct.AccountConfig(), acct.Worker()).Defaults(defaults)
+	composer.FocusSubject()
+	title := "New email"
+	if subj, ok := defaults["Subject"]; ok {
+		title = subj
+		composer.FocusTerminal()
+	}
+	tab := aerc.NewTab(composer, title)
+	composer.OnSubjectChange(func(subject string) {
+		if subject == "" {
+			tab.Name = "New email"
+		} else {
+			tab.Name = subject
+		}
+		tab.Content.Invalidate()
+	})
+	return nil
 }

aerc.go
@@ -18,6 +18,7 @@ import (
 	"git.sr.ht/~sircmpwn/aerc/commands/msgview"
 	"git.sr.ht/~sircmpwn/aerc/commands/terminal"
 	"git.sr.ht/~sircmpwn/aerc/config"
+	"git.sr.ht/~sircmpwn/aerc/lib"
 	libui "git.sr.ht/~sircmpwn/aerc/lib/ui"
 	"git.sr.ht/~sircmpwn/aerc/widgets"
 )
@@ -148,6 +149,15 @@ func main() {
 		panic(err)
 	}
 	defer ui.Close()
+
+	logger.Println("Starting Unix server")
+	as, err := lib.StartServer(logger)
+	if err != nil {
+		logger.Printf("Failed to start Unix server: %v (non-fatal)", err)
+	} else {
+		defer as.Close()
+		as.OnMailto = aerc.Mailto
+	}
 
 	for !ui.ShouldExit() {
 		for aerc.Tick() {

lib/socket.go
@@ -1,0 +1,82 @@
+package lib
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"net/url"
+	"path"
+	"strings"
+	"sync/atomic"
+	"time"
+
+	"github.com/kyoh86/xdg"
+)
+
+type AercServer struct {
+	logger   *log.Logger
+	listener net.Listener
+	OnMailto func(addr *url.URL) error
+}
+
+func StartServer(logger *log.Logger) (*AercServer, error) {
+	sockpath := path.Join(xdg.RuntimeDir(), "aerc.sock")
+	l, err := net.Listen("unix", sockpath)
+	if err != nil {
+		return nil, err
+	}
+	as := &AercServer{
+		logger:   logger,
+		listener: l,
+	}
+	// TODO: stash clients and close them on exit... bleh racey
+	go func() {
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				// TODO: Something more useful, in some cases, on wednesdays,
+				// after 2 PM, I guess?
+				as.logger.Println("Closing Unix server: %v", err)
+				return
+			}
+			go as.handleClient(conn)
+		}
+	}()
+	return as, nil
+}
+
+func (as *AercServer) Close() {
+	as.listener.Close()
+}
+
+var lastId int64 = 0 // access via atomic
+
+func (as *AercServer) handleClient(conn net.Conn) {
+	clientId := atomic.AddInt64(&lastId, 1)
+	as.logger.Printf("Accepted Unix connection %d", clientId)
+	scanner := bufio.NewScanner(conn)
+	conn.SetDeadline(time.Now().Add(1 * time.Minute))
+	for scanner.Scan() {
+		conn.SetDeadline(time.Now().Add(1 * time.Minute))
+		msg := scanner.Text()
+		if !strings.ContainsRune(msg, ':') {
+			conn.Write([]byte("error: invalid command\n"))
+		}
+		as.logger.Printf("unix:%d: got message %s", clientId, msg)
+		prefix := msg[:strings.IndexRune(msg, ':')]
+		switch prefix {
+		case "mailto":
+			mailto, err := url.Parse(msg)
+			if err != nil {
+				conn.Write([]byte(fmt.Sprintf("error: %v\n", err)))
+				break
+			}
+			if as.OnMailto != nil {
+				err = as.OnMailto(mailto)
+			}
+			conn.Write([]byte(fmt.Sprintf("result: %v\n", err)))
+		}
+	}
+	as.logger.Printf("Closed Unix connection %d", clientId)
+}

widgets/compose.go
@@ -138,6 +138,13 @@ func (c *Composer) FocusTerminal() *Composer {
 	return c
 }
 
+func (c *Composer) FocusSubject() *Composer {
+	c.focusable[c.focused].Focus(false)
+	c.focused = 2
+	c.focusable[c.focused].Focus(true)
+	return c
+}
+
 func (c *Composer) OnSubjectChange(fn func(subject string)) {
 	c.headers.subject.OnChange(func() {
 		fn(c.headers.subject.input.String())

commit 4ef76f0a95fc1aba7d92fd1a84308286280856e7

aerc: show effects of mailto: and mbox: immediately
When handling a mailto-Link or opening an mbox file, another update is
required before the composer is shown. This is caused by the view not
being invalidated and no redraw being queued.

Invalidate the UI and queue a redraw when an IPC command is received.

Fixes: 7a489cb0011a ("Add Unix socket for communicating with aerc")
Signed-off-by: Moritz Poldrack <git@moritz.sh>
Acked-by: Robin Jarry <robin@jarry.cc>

widgets/aerc.go
@@ -733,6 +733,9 @@ func (aerc *Aerc) Mailto(addr *url.URL) error {
 		return errors.New("No account selected")
 	}
 
+	defer ui.Invalidate()
+	defer ui.QueueRedraw()
+
 	composer, err := NewComposer(aerc, acct,
 		acct.AccountConfig(), acct.Worker(), "", h, nil)
 	if err != nil {
@@ -772,6 +775,9 @@ func (aerc *Aerc) Mbox(source string) error {
 	acctConf.Archive = "Archive"
 	acctConf.Postpone = "Drafts"
 	acctConf.CopyTo = "Sent"
+
+	defer ui.Invalidate()
+	defer ui.QueueRedraw()
 
 	mboxView, err := NewAccountView(aerc, &acctConf, aerc, nil)
 	if err != nil {