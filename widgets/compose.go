rjarry/aerc

commit a553b33ebcbdf8b12ac1546ec45f4437dac1f8b0

compose: ensure signature uses standard delimiter
Since it has recently been a topic on IRC, and to guide users new to
"raw" email, add a note on how signatures are detected and what they
should look like.

Prepend signature-file and signature-cmd with the standard delimiter if
missing.

Signed-off-by: Moritz Poldrack <git@moritz.sh>
Signed-off-by: Robin Jarry <robin@jarry.cc>

widgets/compose.go
@@ -1,6 +1,7 @@
 package widgets
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"io"
@@ -550,6 +551,7 @@ func (c *Composer) AddSignature() {
 	} else {
 		signature = c.readSignatureFromFile()
 	}
+	signature = ensureSignatureDelimiter(signature)
 	c.AppendContents(bytes.NewReader(signature))
 }
 
@@ -579,6 +581,21 @@ func (c *Composer) readSignatureFromFile() []byte {
 		return nil
 	}
 	return signature
+}
+
+func ensureSignatureDelimiter(signature []byte) []byte {
+	buf := bytes.NewBuffer(signature)
+	scanner := bufio.NewScanner(buf)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "-- " {
+			// signature contains standard delimiter, we're good
+			return signature
+		}
+	}
+	// signature does not contain standard delimiter, prepend one
+	sig := "\n\n-- \n" + strings.TrimLeft(string(signature), " \t\r\n")
+	return []byte(sig)
 }
 
 func (c *Composer) FocusTerminal() *Composer {

doc/aerc-accounts.5.scd
@@ -160,6 +160,11 @@ Note that many of these configuration options are written for you, such as
 	Specifies the file to read in order to obtain the signature to be added
 	to emails sent from this account.
 
+	Please note that by convention the Usenet signature style of two dashes,
+	followed by a space ("-- ") should be placed at the top of the signature
+	to separate content and signature. Aerc will add that delimiter if it is
+	not already present.
+
 *signature-cmd* = _<command>_
 	Specifies the command to execute with _sh -c_ in order to obtain the
 	signature to be added to emails sent from this account. If the command

commit bba7159756908f092655bdf6db091daf9eb74f76

compose: only add delimiter when a signature is defined
Inserting a solitary "-- " delimiter without any signature after it
makes no sense. Skip the whole delimiter check if the signature is
empty. Trim leading and trailing white space along the way.

Fixes: a553b33ebcbd ("compose: ensure signature uses standard delimiter")
Reported-by: Ben Cohen <ben@bencohen.net>
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Ben Cohen <ben@bencohen.net>

widgets/compose.go
@@ -542,6 +542,10 @@ func (c *Composer) AddSignature() {
 	} else {
 		signature = c.readSignatureFromFile()
 	}
+	signature = bytes.TrimSpace(signature)
+	if len(signature) == 0 {
+		return
+	}
 	signature = ensureSignatureDelimiter(signature)
 	c.AppendContents(bytes.NewReader(signature))
 }