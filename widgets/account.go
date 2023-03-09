rjarry/aerc

commit 6b39c0dae1e1c42445a2d8557e48135b02fc729b

tabs: use template for account tab name
Use a go template to render the account tab display. Add config option
for setting a specific template for the account. Add a method on Tab to
allow setting a title, which may be different than the tab Name.

The default template is {{.Account}}.

Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>
Acked-by: Robin Jarry <robin@jarry.cc>

widgets/account.go
@@ -1,6 +1,7 @@
 package widgets
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"sync"
@@ -30,6 +31,7 @@ type AccountView struct {
 	labels  []string
 	grid    *ui.Grid
 	host    TabHost
+	tab     *ui.Tab
 	msglist *MessageList
 	worker  *types.Worker
 	state   *statusline.State
@@ -351,6 +353,7 @@ func (acct *AccountView) onMessage(msg types.WorkerMessage) {
 		acct.PushError(msg.Error)
 	}
 	acct.UpdateStatus()
+	acct.setTitle()
 }
 
 func (acct *AccountView) updateDirCounts(destination string, uids []uint32) {
@@ -587,4 +590,30 @@ func (acct *AccountView) Vsplit(n int) error {
 	acct.grid.AddChild(acct.split).At(0, 2)
 	acct.updateSplitView(acct.msglist.Selected())
 	return nil
+}
+
+// setTitle executes the title template and sets the tab title
+func (acct *AccountView) setTitle() {
+	data := struct {
+		Account string
+		Recent  int
+		Unread  int
+		Exists  int
+		Folder  string
+	}{}
+	data.Account = acct.Name()
+	data.Folder = acct.SelectedDirectory()
+	for _, name := range acct.dirlist.List() {
+		r, u, e := acct.dirlist.GetRUECount(name)
+		data.Recent += r
+		data.Unread += u
+		data.Exists += e
+	}
+	buf := bytes.NewBuffer(nil)
+	err := acct.uiConf.TabTitleAccount.Execute(buf, data)
+	if err != nil {
+		acct.PushError(err)
+		return
+	}
+	acct.tab.SetTitle(buf.String())
 }

config/aerc.conf
@@ -125,6 +125,12 @@
 # Default: true
 #new-message-bell=true
 
+#
+# Template to use for Account tab titles
+#
+# Default: {{.Account}}
+#tab-title-account={{.Account}}
+
 # Marker to show before a pinned tab's name.
 #
 # Default: `

config/ui.go
@@ -6,6 +6,7 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"text/template"
 	"time"
 
 	"git.sr.ht/~rjarry/aerc/lib/templates"
@@ -72,6 +73,9 @@ type UIConfig struct {
 	ReverseThreadOrder bool `ini:"reverse-thread-order"`
 	SortThreadSiblings bool `ini:"sort-thread-siblings"`
 
+	// Tab Templates
+	TabTitleAccount *template.Template `ini:"-"`
+
 	// private
 	contextualUis    []*UiConfigContext
 	contextualCounts map[uiContextType]int
@@ -102,6 +106,7 @@ func defaultUiConfig() *UIConfig {
 	name, _ := templates.ParseTemplate("column-name", "{{index (.From | names) 0}}")
 	flags, _ := templates.ParseTemplate("column-flags", `{{.Flags | join ""}}`)
 	subject, _ := templates.ParseTemplate("column-subject", "{{.Subject}}")
+	tabTitleAccount, _ := templates.ParseTemplate("tab-title-account", "{{.Account}}")
 	return &UIConfig{
 		IndexFormat: "", // deprecated
 		IndexColumns: []*ColumnDef{
@@ -144,6 +149,7 @@ func defaultUiConfig() *UIConfig {
 		MouseEnabled:        false,
 		ClientThreadsDelay:  50 * time.Millisecond,
 		NewMessageBell:      true,
+		TabTitleAccount:     tabTitleAccount,
 		FuzzyComplete:       false,
 		Spinner:             "[..]    , [..]   ,  [..]  ,   [..] ,    [..],   [..] ,  [..]  , [..]   ",
 		SpinnerDelimiter:    ",",
@@ -335,6 +341,14 @@ func (config *UIConfig) parse(section *ini.Section) error {
 			return err
 		}
 		config.IndexColumns = columns
+	}
+	if key, err := section.GetKey("tab-title-account"); err == nil {
+		val := key.Value()
+		tmpl, err := templates.ParseTemplate("tab-title-account", val)
+		if err != nil {
+			return err
+		}
+		config.TabTitleAccount = tmpl
 	}
 
 	return nil

doc/aerc-config.5.scd
@@ -163,6 +163,15 @@ These options are configured in the *[ui]* section of _aerc.conf_.
 
 	Default: _true_
 
+*tab-title-account* = _<go_template>_
+	The template to use for account tab titles. See *aerc-templates*(7) for
+	available field names. To conditionally show the unread count next to
+	the account name, set to:
+
+		*tab-title-account* = {{.Account}} {{if .Unread}}({{.Unread}}){{end}}
+
+	Default: _{{.Account}}_
+
 *pinned-tab-marker* = _"<string>"_
 	Marker to show before a pinned tab's name.
 

doc/aerc-templates.7.scd
@@ -148,6 +148,11 @@ available always.
 	{{.Folder}}
 	```
 
+	Current message counts
+	```
+	{{.Recent}} {{.Unread}} {{.Exists}}
+	```
+
 # TEMPLATE FUNCTIONS
 
 Besides the standard functions described in go's text/template documentation,

lib/ui/tab.go
@@ -30,6 +30,11 @@ type Tab struct {
 	pinned         bool
 	indexBeforePin int
 	uiConf         *config.UIConfig
+	title          string
+}
+
+func (t *Tab) SetTitle(s string) {
+	t.title = s
 }
 
 type (
@@ -351,6 +356,9 @@ func (strip *TabStrip) Draw(ctx *Context) {
 			tabWidth = ctx.Width() - x - 2
 		}
 		name := tab.Name
+		if tab.title != "" {
+			name = tab.title
+		}
 		if tab.pinned {
 			name = uiConfig.PinnedTabMarker + name
 		}

widgets/aerc.go
@@ -94,7 +94,7 @@ func NewAerc(
 			tabs.Add(errorScreen(err.Error()), acct.Name, nil)
 		} else {
 			aerc.accounts[acct.Name] = view
-			tabs.Add(view, acct.Name, view.UiConfig())
+			view.tab = tabs.Add(view, acct.Name, view.UiConfig())
 		}
 	}
 

widgets/dirlist.go
@@ -42,6 +42,7 @@ type DirectoryLister interface {
 	SetMsgStore(string, *lib.MessageStore)
 
 	FilterDirs([]string, []string, bool) []string
+	GetRUECount(string) (int, int, int)
 
 	UiConfig(string) *config.UIConfig
 }
@@ -252,6 +253,19 @@ func (dirlist *DirectoryList) getRUEString(name string) string {
 		rueString = fmt.Sprintf("%d", di.Exists)
 	}
 	return rueString
+}
+
+// Returns the Recent, Unread, and Exist counts for the named directory
+func (dirlist *DirectoryList) GetRUECount(name string) (int, int, int) {
+	msgStore, ok := dirlist.MsgStore(name)
+	if !ok {
+		return 0, 0, 0
+	}
+	if !msgStore.DirInfo.AccurateCounts {
+		msgStore.DirInfo.Recent, msgStore.DirInfo.Unseen = countRUE(msgStore)
+	}
+	di := msgStore.DirInfo
+	return di.Recent, di.Unseen, di.Exists
 }
 
 func (dirlist *DirectoryList) Draw(ctx *ui.Context) {

commit 5ae7a23e1ec99f1e552967ed9d058133b6c860c6

account: fix crash/race on init
When an account is initialized too quickly, setTitle may be called
before acct.tab is initialized, leading to a crash:

runtime error: invalid memory address or nil pointer dereference

goroutine 1 [running]:
panic({0xa49820, 0x1059510})
	runtime/panic.go:890 +0x262
git.sr.ht/~rjarry/aerc/lib/ui.(*Tab).SetTitle(...)
	git.sr.ht/~rjarry/aerc/lib/ui/tab.go:37
git.sr.ht/~rjarry/aerc/widgets.(*AccountView).setTitle(0xc000838000)
	git.sr.ht/~rjarry/aerc/widgets/account.go:617 +0x2a0
git.sr.ht/~rjarry/aerc/widgets.(*AccountView).onMessage(0xc000838000, {0xbffc60?, 0xc0000f1080?})
	git.sr.ht/~rjarry/aerc/widgets/account.go:364 +0x13e8
git.sr.ht/~rjarry/aerc/widgets.(*Aerc).HandleMessage(0xc0000f4000, {0xbffc60, 0xc0000f1080})
	git.sr.ht/~rjarry/aerc/widgets/aerc.go:174 +0x5f
main.main()
	git.sr.ht/~rjarry/aerc/aerc.go:252 +0x965

Avoid this.

Fixes: 6b39c0dae1e1 ("tabs: use template for account tab name")
Signed-off-by: Robin Jarry <robin@jarry.cc>

widgets/account.go
@@ -604,6 +604,9 @@ func (acct *AccountView) Vsplit(n int) error {
 func (acct *AccountView) setTitle() {
 	var data state.TemplateData
 
+	if acct.tab == nil {
+		return
+	}
 	data.SetAccount(acct.acct)
 	data.SetFolder(acct.SelectedDirectory())
 	data.SetRUE(acct.dirlist.List(), acct.dirlist.GetRUECount)