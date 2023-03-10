rjarry/aerc

commit a0935a3de0cec483a605df5017307f50a186f5cb

worker/lib: implement an fswatcher interface
Implement an FSWatcher interface. The interface is used to abstract away
file system watchers, which have implementation specific backends. The
initial interface has one implementation: inotify for linux. Subsequent
commits will add a macOS watcher.

Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>
Tested-by: Ben Lee-Cohen <ben@lee-cohen.com>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/handlers/register.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"fmt"
+	"runtime"
 
 	"git.sr.ht/~rjarry/aerc/worker/types"
 )
@@ -24,4 +25,19 @@ func GetHandlerForScheme(scheme string, worker *types.Worker) (types.Backend, er
 		return nil, err
 	}
 	return backend, nil
+}
+
+type WatcherFactoryFunc func() (types.FSWatcher, error)
+
+var watcherFactories map[string]WatcherFactoryFunc = make(map[string]WatcherFactoryFunc)
+
+func RegisterWatcherFactory(os string, fn WatcherFactoryFunc) {
+	watcherFactories[os] = fn
+}
+
+func NewWatcher() (types.FSWatcher, error) {
+	if fn, ok := watcherFactories[runtime.GOOS]; ok {
+		return fn()
+	}
+	return nil, fmt.Errorf("Unsupported OS: %s", runtime.GOOS)
 }

worker/lib/watchers/linux/linux.go
@@ -1,0 +1,73 @@
+package linux
+
+import (
+	"git.sr.ht/~rjarry/aerc/log"
+	"git.sr.ht/~rjarry/aerc/worker/handlers"
+	"git.sr.ht/~rjarry/aerc/worker/types"
+	"github.com/fsnotify/fsnotify"
+)
+
+func init() {
+	handlers.RegisterWatcherFactory("linux", newInotifyWatcher)
+}
+
+type inotifyWatcher struct {
+	w  *fsnotify.Watcher
+	ch chan *types.FSEvent
+}
+
+func newInotifyWatcher() (types.FSWatcher, error) {
+	watcher := &inotifyWatcher{
+		ch: make(chan *types.FSEvent),
+	}
+	w, err := fsnotify.NewWatcher()
+	if err != nil {
+		return nil, err
+	}
+	watcher.w = w
+
+	go watcher.watch()
+	return watcher, nil
+}
+
+func (w *inotifyWatcher) watch() {
+	defer log.PanicHandler()
+	for ev := range w.w.Events {
+		// we only care about files being created, removed or renamed
+		switch ev.Op {
+		case fsnotify.Create:
+			w.ch <- &types.FSEvent{
+				Operation: types.FSCreate,
+				Path:      ev.Name,
+			}
+		case fsnotify.Remove:
+			w.ch <- &types.FSEvent{
+				Operation: types.FSRemove,
+				Path:      ev.Name,
+			}
+		case fsnotify.Rename:
+			w.ch <- &types.FSEvent{
+				Operation: types.FSRename,
+				Path:      ev.Name,
+			}
+		default:
+			continue
+		}
+	}
+}
+
+func (w *inotifyWatcher) Configure(root string) error {
+	return w.w.Add(root)
+}
+
+func (w *inotifyWatcher) Events() chan *types.FSEvent {
+	return w.ch
+}
+
+func (w *inotifyWatcher) Add(p string) error {
+	return w.w.Add(p)
+}
+
+func (w *inotifyWatcher) Remove(p string) error {
+	return w.w.Remove(p)
+}

worker/types/watcher.go
@@ -1,0 +1,24 @@
+package types
+
+// FSWatcher is a file system watcher
+type FSWatcher interface {
+	Configure(string) error
+	Events() chan *FSEvent
+	// Adds a directory or file to the watcher
+	Add(string) error
+	// Removes a directory or file from the watcher
+	Remove(string) error
+}
+
+type FSOperation int
+
+const (
+	FSCreate FSOperation = iota
+	FSRemove
+	FSRename
+)
+
+type FSEvent struct {
+	Operation FSOperation
+	Path      string
+}

commit 60078b8452867c9ab40c9623ef63169bdb090c83

fswatcher: fix bsd support
Fix the following error when running maildir on freebsd:

 could not create file system watcher: Unsupported OS: freebsd

Do not register based on os type. Register based on supported API.
Rename linux -> inotify and darwin -> fsevents. Only build fsevents on
darwin, and inotify on all other platforms.

Fixes: a0935a3de0ce ("worker/lib: implement an fswatcher interface")
Reported-by: Jens Grassel <jens@wegtam.com>
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Jens Grassel <jens@wegtam.com>

worker/handlers/register.go
@@ -29,15 +29,15 @@ func GetHandlerForScheme(scheme string, worker *types.Worker) (types.Backend, er
 
 type WatcherFactoryFunc func() (types.FSWatcher, error)
 
-var watcherFactories map[string]WatcherFactoryFunc = make(map[string]WatcherFactoryFunc)
+var watcherFactory WatcherFactoryFunc
 
-func RegisterWatcherFactory(os string, fn WatcherFactoryFunc) {
-	watcherFactories[os] = fn
+func RegisterWatcherFactory(fn WatcherFactoryFunc) {
+	watcherFactory = fn
 }
 
 func NewWatcher() (types.FSWatcher, error) {
-	if fn, ok := watcherFactories[runtime.GOOS]; ok {
-		return fn()
+	if watcherFactory == nil {
+		return nil, fmt.Errorf("Unsupported OS: %s", runtime.GOOS)
 	}
-	return nil, fmt.Errorf("Unsupported OS: %s", runtime.GOOS)
+	return watcherFactory()
 }

worker/lib/watchers/fsevents.go
@@ -1,7 +1,7 @@
 //go:build darwin
 // +build darwin
 
-package darwin
+package watchers
 
 import (
 	"time"
@@ -13,7 +13,7 @@ import (
 )
 
 func init() {
-	handlers.RegisterWatcherFactory("darwin", newDarwinWatcher)
+	handlers.RegisterWatcherFactory(newDarwinWatcher)
 }
 
 type darwinWatcher struct {

worker/lib/watchers/inotify.go
@@ -1,4 +1,7 @@
-package linux
+//go:build !darwin
+// +build !darwin
+
+package watchers
 
 import (
 	"git.sr.ht/~rjarry/aerc/log"
@@ -8,7 +11,7 @@ import (
 )
 
 func init() {
-	handlers.RegisterWatcherFactory("linux", newInotifyWatcher)
+	handlers.RegisterWatcherFactory(newInotifyWatcher)
 }
 
 type inotifyWatcher struct {

worker/watcher_darwin.go
@@ -1,6 +1,0 @@
-//go:build darwin
-// +build darwin
-
-package worker
-
-import _ "git.sr.ht/~rjarry/aerc/worker/lib/watchers/darwin"

worker/watcher_enabled.go
@@ -1,3 +1,0 @@
-package worker
-
-import _ "git.sr.ht/~rjarry/aerc/worker/lib/watchers/linux"

worker/worker_enabled.go
@@ -3,7 +3,7 @@ package worker
 // the following workers are always enabled
 import (
 	_ "git.sr.ht/~rjarry/aerc/worker/imap"
+	_ "git.sr.ht/~rjarry/aerc/worker/lib/watchers"
 	_ "git.sr.ht/~rjarry/aerc/worker/maildir"
-
 	_ "git.sr.ht/~rjarry/aerc/worker/mbox"
 )