rjarry/aerc

commit b63c93563c622e70cda7006c1816dc6b59e75844

config: use reflection to map ini keys to struct fields
The default ini.Section.MapTo() function only handles basic types.
Implement a more complete mapping solution that allows:

* parsing templates, regexps, email addresses
* defining a custom parsing method via the `parse:"MethodName"` tag
* defining default values via the `default:"value"` tag
* parsing rune values with the `type:"rune"` tag

The field name must be specified in the `ini:"field-name"` tag as it was
before. It is no longer optional.

The `delim:"<separator>"` tag remains but can only be used to parse
string arrays.

It is now possible to override default values with "zero" values. For
example:

	[ui]
	dirlist-delay = 0

Will override the default "200ms" value. Also:

	[statusline]
	status-columns =

Will override the default "left<*,center>=,right>*" value.

Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Tim Culverhouse <tim@timculverhouse.com>

config/ui.go
@@ -17,17 +17,14 @@ import (
 )
 
 type UIConfig struct {
-	IndexColumns    []*ColumnDef `ini:"-"`
-	ColumnSeparator string       `ini:"column-separator"`
-	// deprecated
-	IndexFormat string `ini:"index-format"`
+	IndexColumns    []*ColumnDef `ini:"index-columns" parse:"ParseIndexColumns" default:"date<20,name<17,flags>4,subject<*"`
+	ColumnSeparator string       `ini:"column-separator" default:"  "`
 
-	DirListFormat string             `ini:"dirlist-format"` // deprecated
-	DirListLeft   *template.Template `ini:"-"`
-	DirListRight  *template.Template `ini:"-"`
+	DirListLeft  *template.Template `ini:"dirlist-left" default:"{{.Folder}}"`
+	DirListRight *template.Template `ini:"dirlist-right" default:"{{if .Unread}}{{humanReadable .Unread}}/{{end}}{{if .Exists}}{{humanReadable .Exists}}{{end}}"`
 
-	AutoMarkRead                  bool          `ini:"auto-mark-read"`
-	TimestampFormat               string        `ini:"timestamp-format"`
+	AutoMarkRead                  bool          `ini:"auto-mark-read" default:"true"`
+	TimestampFormat               string        `ini:"timestamp-format" default:"2006-01-02 03:04 PM"`
 	ThisDayTimeFormat             string        `ini:"this-day-time-format"`
 	ThisWeekTimeFormat            string        `ini:"this-week-time-format"`
 	ThisYearTimeFormat            string        `ini:"this-year-time-format"`
@@ -35,49 +32,48 @@ type UIConfig struct {
 	MessageViewThisDayTimeFormat  string        `ini:"message-view-this-day-time-format"`
 	MessageViewThisWeekTimeFormat string        `ini:"message-view-this-week-time-format"`
 	MessageViewThisYearTimeFormat string        `ini:"message-view-this-year-time-format"`
-	PinnedTabMarker               string        `ini:"pinned-tab-marker"`
-	SidebarWidth                  int           `ini:"sidebar-width"`
-	PreviewHeight                 int           `ini:"preview-height"`
-	EmptyMessage                  string        `ini:"empty-message"`
-	EmptyDirlist                  string        `ini:"empty-dirlist"`
+	PinnedTabMarker               string        "ini:\"pinned-tab-marker\" default:\"`\""
+	SidebarWidth                  int           `ini:"sidebar-width" default:"20"`
+	EmptyMessage                  string        `ini:"empty-message" default:"(no messages)"`
+	EmptyDirlist                  string        `ini:"empty-dirlist" default:"(no folders)"`
 	MouseEnabled                  bool          `ini:"mouse-enabled"`
 	ThreadingEnabled              bool          `ini:"threading-enabled"`
 	ForceClientThreads            bool          `ini:"force-client-threads"`
-	ClientThreadsDelay            time.Duration `ini:"client-threads-delay"`
+	ClientThreadsDelay            time.Duration `ini:"client-threads-delay" default:"50ms"`
 	FuzzyComplete                 bool          `ini:"fuzzy-complete"`
-	NewMessageBell                bool          `ini:"new-message-bell"`
-	Spinner                       string        `ini:"spinner"`
-	SpinnerDelimiter              string        `ini:"spinner-delimiter"`
-	SpinnerInterval               time.Duration `ini:"spinner-interval"`
+	NewMessageBell                bool          `ini:"new-message-bell" default:"true"`
+	Spinner                       string        `ini:"spinner" default:"[..]    , [..]   ,  [..]  ,   [..] ,    [..],   [..] ,  [..]  , [..]   "`
+	SpinnerDelimiter              string        `ini:"spinner-delimiter" default:","`
+	SpinnerInterval               time.Duration `ini:"spinner-interval" default:"200ms"`
 	IconUnencrypted               string        `ini:"icon-unencrypted"`
-	IconEncrypted                 string        `ini:"icon-encrypted"`
-	IconSigned                    string        `ini:"icon-signed"`
+	IconEncrypted                 string        `ini:"icon-encrypted" default:"[e]"`
+	IconSigned                    string        `ini:"icon-signed" default:"[s]"`
 	IconSignedEncrypted           string        `ini:"icon-signed-encrypted"`
-	IconUnknown                   string        `ini:"icon-unknown"`
-	IconInvalid                   string        `ini:"icon-invalid"`
-	IconAttachment                string        `ini:"icon-attachment"`
-	DirListDelay                  time.Duration `ini:"dirlist-delay"`
+	IconUnknown                   string        `ini:"icon-unknown" default:"[s?]"`
+	IconInvalid                   string        `ini:"icon-invalid" default:"[s!]"`
+	IconAttachment                string        `ini:"icon-attachment" default:"a"`
+	DirListDelay                  time.Duration `ini:"dirlist-delay" default:"200ms"`
 	DirListTree                   bool          `ini:"dirlist-tree"`
 	DirListCollapse               int           `ini:"dirlist-collapse"`
-	Sort                          []string      `delim:" "`
+	Sort                          []string      `ini:"sort" delim:" "`
 	NextMessageOnDelete           bool          `ini:"next-message-on-delete"`
-	CompletionDelay               time.Duration `ini:"completion-delay"`
-	CompletionMinChars            int           `ini:"completion-min-chars"`
-	CompletionPopovers            bool          `ini:"completion-popovers"`
+	CompletionDelay               time.Duration `ini:"completion-delay" default:"250ms"`
+	CompletionMinChars            int           `ini:"completion-min-chars" default:"1"`
+	CompletionPopovers            bool          `ini:"completion-popovers" default:"true"`
 	StyleSetDirs                  []string      `ini:"stylesets-dirs" delim:":"`
-	StyleSetName                  string        `ini:"styleset-name"`
+	StyleSetName                  string        `ini:"styleset-name" default:"default"`
 	style                         StyleSet
 	// customize border appearance
-	BorderCharVertical   rune `ini:"-"`
-	BorderCharHorizontal rune `ini:"-"`
+	BorderCharVertical   rune `ini:"border-char-vertical" default:" " type:"rune"`
+	BorderCharHorizontal rune `ini:"border-char-horizontal" default:" " type:"rune"`
 
 	ReverseOrder       bool `ini:"reverse-msglist-order"`
 	ReverseThreadOrder bool `ini:"reverse-thread-order"`
 	SortThreadSiblings bool `ini:"sort-thread-siblings"`
 
 	// Tab Templates
-	TabTitleAccount  *template.Template `ini:"-"`
-	TabTitleComposer *template.Template `ini:"-"`
+	TabTitleAccount  *template.Template `ini:"tab-title-account" default:"{{.Account}}"`
+	TabTitleComposer *template.Template `ini:"tab-title-composer" default:"{{.Subject}}"`
 
 	// private
 	contextualUis    []*UiConfigContext
@@ -106,95 +102,14 @@ type uiContextKey struct {
 
 const unreadExists string = `{{if .Unread}}{{humanReadable .Unread}}/{{end}}{{if .Exists}}{{humanReadable .Exists}}{{end}}`
 
-func defaultUiConfig() *UIConfig {
-	date, _ := templates.ParseTemplate("column-date", "{{.DateAutoFormat .Date.Local}}")
-	name, _ := templates.ParseTemplate("column-name", "{{index (.From | names) 0}}")
-	flags, _ := templates.ParseTemplate("column-flags", `{{.Flags | join ""}}`)
-	subject, _ := templates.ParseTemplate("column-subject", "{{.Subject}}")
-	left, _ := templates.ParseTemplate("folder", "{{.Folder}}")
-	right, _ := templates.ParseTemplate("ue", unreadExists)
-	tabTitleAccount, _ := templates.ParseTemplate("tab-title-account", "{{.Account}}")
-	tabTitleComposer, _ := templates.ParseTemplate("tab-title-composer", "{{.Subject}}")
-	return &UIConfig{
-		IndexFormat:   "", // deprecated
-		DirListFormat: "", // deprecated
-		IndexColumns: []*ColumnDef{
-			{
-				Name:     "date",
-				Width:    20,
-				Flags:    ALIGN_LEFT | WIDTH_EXACT,
-				Template: date,
-			},
-			{
-				Name:     "name",
-				Width:    17,
-				Flags:    ALIGN_LEFT | WIDTH_EXACT,
-				Template: name,
-			},
-			{
-				Name:     "flags",
-				Width:    4,
-				Flags:    ALIGN_RIGHT | WIDTH_EXACT,
-				Template: flags,
-			},
-			{
-				Name:     "subject",
-				Flags:    ALIGN_LEFT | WIDTH_AUTO,
-				Template: subject,
-			},
-		},
-		DirListLeft:         left,
-		DirListRight:        right,
-		ColumnSeparator:     "  ",
-		AutoMarkRead:        true,
-		TimestampFormat:     "2006-01-02 03:04 PM",
-		ThisDayTimeFormat:   "",
-		ThisWeekTimeFormat:  "",
-		ThisYearTimeFormat:  "",
-		PinnedTabMarker:     "`",
-		SidebarWidth:        20,
-		PreviewHeight:       12,
-		EmptyMessage:        "(no messages)",
-		EmptyDirlist:        "(no folders)",
-		MouseEnabled:        false,
-		ClientThreadsDelay:  50 * time.Millisecond,
-		NewMessageBell:      true,
-		TabTitleAccount:     tabTitleAccount,
-		TabTitleComposer:    tabTitleComposer,
-		FuzzyComplete:       false,
-		Spinner:             "[..]    , [..]   ,  [..]  ,   [..] ,    [..],   [..] ,  [..]  , [..]   ",
-		SpinnerDelimiter:    ",",
-		SpinnerInterval:     200 * time.Millisecond,
-		IconUnencrypted:     "",
-		IconSigned:          "[s]",
-		IconEncrypted:       "[e]",
-		IconSignedEncrypted: "",
-		IconUnknown:         "[s?]",
-		IconInvalid:         "[s!]",
-		IconAttachment:      "a",
-		DirListDelay:        200 * time.Millisecond,
-		NextMessageOnDelete: true,
-		CompletionDelay:     250 * time.Millisecond,
-		CompletionMinChars:  1,
-		CompletionPopovers:  true,
-		StyleSetDirs:        []string{},
-		StyleSetName:        "default",
-		// border defaults
-		BorderCharVertical:   ' ',
-		BorderCharHorizontal: ' ',
-		// private
-		contextualCache:  make(map[uiContextKey]*UIConfig),
-		contextualCounts: make(map[uiContextType]int),
-	}
+var Ui = &UIConfig{
+	contextualCounts: make(map[uiContextType]int),
+	contextualCache:  make(map[uiContextKey]*UIConfig),
 }
 
-var Ui = defaultUiConfig()
-
 func parseUi(file *ini.File) error {
-	if ui, err := file.GetSection("ui"); err == nil {
-		if err := Ui.parse(ui); err != nil {
-			return err
-		}
+	if err := Ui.parse(file.Section("ui")); err != nil {
+		return err
 	}
 
 	for _, sectionName := range file.SectionStrings() {
@@ -283,64 +198,16 @@ func parseUi(file *ini.File) error {
 }
 
 func (config *UIConfig) parse(section *ini.Section) error {
-	if err := section.MapTo(config); err != nil {
+	if err := MapToStruct(section, config, section.Name() == "ui"); err != nil {
 		return err
-	}
-
-	if key, err := section.GetKey("border-char-vertical"); err == nil {
-		chars := []rune(key.String())
-		if len(chars) != 1 {
-			return fmt.Errorf("%v must be one and only one character", key)
-		}
-		config.BorderCharVertical = chars[0]
-	}
-	if key, err := section.GetKey("border-char-horizontal"); err == nil {
-		chars := []rune(key.String())
-		if len(chars) != 1 {
-			return fmt.Errorf("%v must be one and only one character", key)
-		}
-		config.BorderCharHorizontal = chars[0]
-	}
-
-	// Values with type=time.Duration must be explicitly set. If these
-	// values are given a default in the struct passed to ui.MapTo, which
-	// they are, a zero-value in the config won't overwrite the default.
-	if key, err := section.GetKey("dirlist-delay"); err == nil {
-		dur, err := key.Duration()
-		if err != nil {
-			return err
-		}
-		config.DirListDelay = dur
-	}
-	if key, err := section.GetKey("completion-delay"); err == nil {
-		dur, err := key.Duration()
-		if err != nil {
-			return err
-		}
-		config.CompletionDelay = dur
 	}
 
 	if config.MessageViewTimestampFormat == "" {
 		config.MessageViewTimestampFormat = config.TimestampFormat
 	}
-	if config.MessageViewThisDayTimeFormat == "" {
-		config.MessageViewThisDayTimeFormat = config.TimestampFormat
-	}
-	if config.MessageViewThisWeekTimeFormat == "" {
-		config.MessageViewThisWeekTimeFormat = config.TimestampFormat
-	}
-	if config.MessageViewThisDayTimeFormat == "" {
-		config.MessageViewThisDayTimeFormat = config.TimestampFormat
-	}
 
-	if key, err := section.GetKey("index-columns"); err == nil {
-		columns, err := ParseColumnDefs(key, section)
-		if err != nil {
-			return err
-		}
-		config.IndexColumns = columns
-	} else if config.IndexFormat != "" {
-		columns, err := convertIndexFormat(config.IndexFormat)
+	if key, err := section.GetKey("index-format"); err == nil {
+		columns, err := convertIndexFormat(key.String())
 		if err != nil {
 			return err
 		}
@@ -366,24 +233,8 @@ index-format will be removed in aerc 0.17.
 		}
 		Warnings = append(Warnings, w)
 	}
-	left, _ := section.GetKey("dirlist-left")
-	if left != nil {
-		t, err := templates.ParseTemplate(left.String(), left.String())
-		if err != nil {
-			return err
-		}
-		config.DirListLeft = t
-	}
-	right, _ := section.GetKey("dirlist-right")
-	if right != nil {
-		t, err := templates.ParseTemplate(right.String(), right.String())
-		if err != nil {
-			return err
-		}
-		config.DirListRight = t
-	}
-	if left == nil && right == nil && config.DirListFormat != "" {
-		left, right := convertDirlistFormat(config.DirListFormat)
+	if key, err := section.GetKey("dirlist-format"); err == nil {
+		left, right := convertDirlistFormat(key.String())
 		l, err := templates.ParseTemplate(left, left)
 		if err != nil {
 			return err
@@ -418,24 +269,24 @@ dirlist-format will be removed in aerc 0.17.
 		}
 		Warnings = append(Warnings, w)
 	}
-	if key, err := section.GetKey("tab-title-account"); err == nil {
-		val := key.Value()
-		tmpl, err := templates.ParseTemplate("tab-title-account", val)
-		if err != nil {
-			return err
-		}
-		config.TabTitleAccount = tmpl
-	}
-	if key, err := section.GetKey("tab-title-composer"); err == nil {
-		val := key.Value()
-		tmpl, err := templates.ParseTemplate("tab-title-composer", val)
-		if err != nil {
-			return err
-		}
-		config.TabTitleComposer = tmpl
-	}
 
 	return nil
+}
+
+func (*UIConfig) ParseIndexColumns(section *ini.Section, key *ini.Key) ([]*ColumnDef, error) {
+	if !section.HasKey("column-date") {
+		_, _ = section.NewKey("column-date", `{{.DateAutoFormat .Date.Local}}`)
+	}
+	if !section.HasKey("column-name") {
+		_, _ = section.NewKey("column-name", `{{index (.From | names) 0}}`)
+	}
+	if !section.HasKey("column-flags") {
+		_, _ = section.NewKey("column-flags", `{{.Flags | join ""}}`)
+	}
+	if !section.HasKey("column-subject") {
+		_, _ = section.NewKey("column-subject", `{{.Subject}}`)
+	}
+	return ParseColumnDefs(key, section)
 }
 
 var indexFmtRegexp = regexp.MustCompile(`%(-?\d+)?(\.\d+)?([ACDFRTZadfgilnrstuv])`)

config/accounts.go
@@ -10,7 +10,6 @@ import (
 	"reflect"
 	"regexp"
 	"sort"
-	"strconv"
 	"strings"
 	"time"
 
@@ -70,30 +69,32 @@ func (c *RemoteConfig) ConnectionString() (string, error) {
 }
 
 type AccountConfig struct {
-	Archive           string            `ini:"archive"`
-	CopyTo            string            `ini:"copy-to"`
-	Default           string            `ini:"default"`
-	Postpone          string            `ini:"postpone"`
-	From              *mail.Address     `ini:"-"`
-	Aliases           []*mail.Address   `ini:"-"`
-	Name              string            `ini:"-"`
-	Source            string            `ini:"-"`
-	Folders           []string          `ini:"folders" delim:","`
-	FoldersExclude    []string          `ini:"folders-exclude" delim:","`
-	Params            map[string]string `ini:"-"`
-	Outgoing          RemoteConfig      `ini:"-"`
-	SignatureFile     string            `ini:"signature-file"`
-	SignatureCmd      string            `ini:"signature-cmd"`
-	EnableFoldersSort bool              `ini:"enable-folders-sort"`
-	FoldersSort       []string          `ini:"folders-sort" delim:","`
-	AddressBookCmd    string            `ini:"address-book-cmd"`
-	SendAsUTC         bool              `ini:"send-as-utc"`
-	LocalizedRe       *regexp.Regexp    `ini:"-"`
+	Name string
+	// backend specific
+	Params map[string]string
+
+	Archive           string          `ini:"archive" default:"Archive"`
+	CopyTo            string          `ini:"copy-to"`
+	Default           string          `ini:"default" default:"INBOX"`
+	Postpone          string          `ini:"postpone" default:"Drafts"`
+	From              *mail.Address   `ini:"from"`
+	Aliases           []*mail.Address `ini:"aliases"`
+	Source            string          `ini:"source" parse:"ParseSource"`
+	Folders           []string        `ini:"folders" delim:","`
+	FoldersExclude    []string        `ini:"folders-exclude" delim:","`
+	Outgoing          RemoteConfig    `ini:"outgoing" parse:"ParseOutgoing"`
+	SignatureFile     string          `ini:"signature-file"`
+	SignatureCmd      string          `ini:"signature-cmd"`
+	EnableFoldersSort bool            `ini:"enable-folders-sort" default:"true"`
+	FoldersSort       []string        `ini:"folders-sort" delim:","`
+	AddressBookCmd    string          `ini:"address-book-cmd"`
+	SendAsUTC         bool            `ini:"send-as-utc" default:"false"`
+	LocalizedRe       *regexp.Regexp  `ini:"subject-re-pattern" default:"(?i)^((AW|RE|SV|VS|ODP|R): ?)+"`
 
 	// CheckMail
 	CheckMail        time.Duration `ini:"check-mail"`
 	CheckMailCmd     string        `ini:"check-mail-cmd"`
-	CheckMailTimeout time.Duration `ini:"check-mail-timeout"`
+	CheckMailTimeout time.Duration `ini:"check-mail-timeout" default:"10s"`
 	CheckMailInclude []string      `ini:"check-mail-include"`
 	CheckMailExclude []string      `ini:"check-mail-exclude"`
 
@@ -101,7 +102,7 @@ type AccountConfig struct {
 	PgpKeyId                string `ini:"pgp-key-id"`
 	PgpAutoSign             bool   `ini:"pgp-auto-sign"`
 	PgpOpportunisticEncrypt bool   `ini:"pgp-opportunistic-encrypt"`
-	PgpErrorLevel           int    `ini:"-"`
+	PgpErrorLevel           int    `ini:"pgp-error-level" parse:"ParsePgpErrorLevel" default:"warn"`
 
 	// AuthRes
 	TrustedAuthRes []string `ini:"trusted-authres" delim:","`
@@ -130,7 +131,6 @@ func parseAccounts(root string, accts []string) error {
 		// No config triggers account configuration wizard
 		return nil
 	}
-	file.NameMapper = mapName
 
 	for _, _sec := range file.SectionStrings() {
 		if _sec == "DEFAULT" {
@@ -140,98 +140,32 @@ func parseAccounts(root string, accts []string) error {
 			continue
 		}
 		sec := file.Section(_sec)
-		sourceRemoteConfig := RemoteConfig{}
 		account := AccountConfig{
-			Archive:           "Archive",
-			Default:           "INBOX",
-			Postpone:          "Drafts",
-			Name:              _sec,
-			Params:            make(map[string]string),
-			EnableFoldersSort: true,
-			CheckMailTimeout:  10 * time.Second,
-			PgpErrorLevel:     PgpErrorLevelWarn,
-			// localizedRe contains a list of known translations for the common Re:
-			LocalizedRe: regexp.MustCompile(`(?i)^((AW|RE|SV|VS|ODP|R): ?)+`),
+			Name:   _sec,
+			Params: make(map[string]string),
 		}
-		if err = sec.MapTo(&account); err != nil {
+		if err = MapToStruct(sec, &account, true); err != nil {
 			return err
 		}
 		for key, val := range sec.KeysHash() {
-			switch key {
-			case "source":
-				sourceRemoteConfig.Value = val
-			case "source-cred-cmd":
-				sourceRemoteConfig.PasswordCmd = val
-			case "outgoing":
-				account.Outgoing.Value = val
-			case "outgoing-cred-cmd":
-				account.Outgoing.PasswordCmd = val
-			case "outgoing-cred-cmd-cache":
-				cache, err := strconv.ParseBool(val)
-				if err != nil {
-					return fmt.Errorf("%s=%s %w", key, val, err)
-				}
-				account.Outgoing.CacheCmd = cache
-			case "from":
-				addr, err := mail.ParseAddress(val)
-				if err != nil {
-					return fmt.Errorf("%s=%s %w", key, val, err)
-				}
-				account.From = addr
-			case "aliases":
-				addrs, err := mail.ParseAddressList(val)
-				if err != nil {
-					return fmt.Errorf("%s=%s %w", key, val, err)
-				}
-				account.Aliases = addrs
-			case "subject-re-pattern":
-				re, err := regexp.Compile(val)
-				if err != nil {
-					return fmt.Errorf("%s=%s %w", key, val, err)
-				}
-				account.LocalizedRe = re
-			case "pgp-error-level":
-				switch strings.ToLower(val) {
-				case "none":
-					account.PgpErrorLevel = PgpErrorLevelNone
-				case "warn":
-					account.PgpErrorLevel = PgpErrorLevelWarn
-				case "error":
-					account.PgpErrorLevel = PgpErrorLevelError
-				default:
-					return fmt.Errorf("unknown pgp-error-level: %s", val)
-				}
-			default:
-				backendSpecific := true
-				typ := reflect.TypeOf(account)
-				for i := 0; i < typ.NumField(); i++ {
-					field := typ.Field(i)
-					if field.Tag.Get("ini") == key {
-						backendSpecific = false
-						break
-					}
-				}
-				if backendSpecific {
-					account.Params[key] = val
+			backendSpecific := true
+			typ := reflect.TypeOf(account)
+			for i := 0; i < typ.NumField(); i++ {
+				field := typ.Field(i)
+				if field.Tag.Get("ini") == key {
+					backendSpecific = false
+					break
 				}
 			}
+			if backendSpecific {
+				account.Params[key] = val
+			}
 		}
-		source, err := sourceRemoteConfig.ConnectionString()
-		if err != nil {
-			return fmt.Errorf("Invalid source credentials for %s: %w", _sec, err)
-		}
-		account.Source = source
-
 		if account.Source == "" {
 			return fmt.Errorf("Expected source for account %s", _sec)
 		}
 		if account.From == nil {
 			return fmt.Errorf("Expected from for account %s", _sec)
-		}
-
-		_, err = account.Outgoing.parseValue()
-		if err != nil {
-			return fmt.Errorf("Invalid outgoing credentials for %s: %w", _sec, err)
 		}
 
 		log.Debugf("accounts.conf: [%s] from = %s", account.Name, account.From)
@@ -249,6 +183,48 @@ func parseAccounts(root string, accts []string) error {
 	}
 
 	return nil
+}
+
+func (a *AccountConfig) ParseSource(sec *ini.Section, key *ini.Key) (string, error) {
+	var remote RemoteConfig
+	remote.Value = key.String()
+	if k, err := sec.GetKey("source-cred-cmd"); err == nil {
+		remote.PasswordCmd = k.String()
+	}
+	return remote.ConnectionString()
+}
+
+func (a *AccountConfig) ParseOutgoing(sec *ini.Section, key *ini.Key) (RemoteConfig, error) {
+	var remote RemoteConfig
+	remote.Value = key.String()
+	if k, err := sec.GetKey("outgoing-cred-cmd"); err == nil {
+		remote.PasswordCmd = k.String()
+	}
+	if k, err := sec.GetKey("outgoing-cred-cmd-cache"); err == nil {
+		cache, err := k.Bool()
+		if err != nil {
+			return remote, err
+		}
+		remote.CacheCmd = cache
+	}
+	_, err := remote.parseValue()
+	return remote, err
+}
+
+func (a *AccountConfig) ParsePgpErrorLevel(sec *ini.Section, key *ini.Key) (int, error) {
+	var level int
+	var err error
+	switch strings.ToLower(key.String()) {
+	case "none":
+		level = PgpErrorLevelNone
+	case "warn":
+		level = PgpErrorLevelWarn
+	case "error":
+		level = PgpErrorLevelError
+	default:
+		err = fmt.Errorf("unknown level: %s", key.String())
+	}
+	return level, err
 }
 
 // checkConfigPerms checks for too open permissions

config/compose.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"fmt"
 	"regexp"
 
 	"git.sr.ht/~rjarry/aerc/log"
@@ -10,54 +9,29 @@ import (
 
 type ComposeConfig struct {
 	Editor              string         `ini:"editor"`
-	HeaderLayout        [][]string     `ini:"-"`
+	HeaderLayout        [][]string     `ini:"header-layout" parse:"ParseLayout" default:"To|From,Subject"`
 	AddressBookCmd      string         `ini:"address-book-cmd"`
-	ReplyToSelf         bool           `ini:"reply-to-self"`
-	NoAttachmentWarning *regexp.Regexp `ini:"-"`
+	ReplyToSelf         bool           `ini:"reply-to-self" default:"true"`
+	NoAttachmentWarning *regexp.Regexp `ini:"no-attachment-warning" parse:"ParseNoAttachmentWarning"`
 	FilePickerCmd       string         `ini:"file-picker-cmd"`
 	FormatFlowed        bool           `ini:"format-flowed"`
 }
 
-func defaultComposeConfig() *ComposeConfig {
-	return &ComposeConfig{
-		HeaderLayout: [][]string{
-			{"To", "From"},
-			{"Subject"},
-		},
-		ReplyToSelf: true,
-	}
-}
-
-var Compose = defaultComposeConfig()
+var Compose = new(ComposeConfig)
 
 func parseCompose(file *ini.File) error {
-	compose, err := file.GetSection("compose")
-	if err != nil {
-		goto end
-	}
-
-	if err := compose.MapTo(&Compose); err != nil {
+	if err := MapToStruct(file.Section("compose"), Compose, true); err != nil {
 		return err
 	}
-	for key, val := range compose.KeysHash() {
-		if key == "header-layout" {
-			Compose.HeaderLayout = parseLayout(val)
-		}
-
-		if key == "no-attachment-warning" && len(val) > 0 {
-			re, err := regexp.Compile("(?im)" + val)
-			if err != nil {
-				return fmt.Errorf(
-					"Invalid no-attachment-warning '%s': %w",
-					val, err,
-				)
-			}
-
-			Compose.NoAttachmentWarning = re
-		}
-	}
-
-end:
 	log.Debugf("aerc.conf: [compose] %#v", Compose)
 	return nil
+}
+
+func (c *ComposeConfig) ParseLayout(sec *ini.Section, key *ini.Key) ([][]string, error) {
+	layout := parseLayout(key.String())
+	return layout, nil
+}
+
+func (c *ComposeConfig) ParseNoAttachmentWarning(sec *ini.Section, key *ini.Key) (*regexp.Regexp, error) {
+	return regexp.Compile(`(?im)` + key.String())
 }

config/config.go
@@ -7,27 +7,11 @@ import (
 	"os"
 	"path"
 	"strings"
-	"unicode"
 
 	"github.com/go-ini/ini"
 	"github.com/kyoh86/xdg"
 	"github.com/mitchellh/go-homedir"
 )
-
-// Input: TimestampFormat
-// Output: timestamp-format
-func mapName(raw string) string {
-	newstr := make([]rune, 0, len(raw))
-	for i, chr := range raw {
-		if isUpper := 'A' <= chr && chr <= 'Z'; isUpper {
-			if i > 0 {
-				newstr = append(newstr, '-')
-			}
-		}
-		newstr = append(newstr, unicode.ToLower(chr))
-	}
-	return string(newstr)
-}
 
 // Set at build time
 var (
@@ -126,7 +110,6 @@ func LoadConfigFromFile(root *string, accts []string) error {
 	if err != nil {
 		return err
 	}
-	file.NameMapper = mapName
 
 	if err := parseGeneral(file); err != nil {
 		return err

config/general.go
@@ -12,45 +12,20 @@ import (
 
 type GeneralConfig struct {
 	DefaultSavePath    string       `ini:"default-save-path"`
-	PgpProvider        string       `ini:"pgp-provider"`
+	PgpProvider        string       `ini:"pgp-provider" default:"auto" parse:"ParsePgpProvider"`
 	UnsafeAccountsConf bool         `ini:"unsafe-accounts-conf"`
 	LogFile            string       `ini:"log-file"`
-	LogLevel           log.LogLevel `ini:"-"`
+	LogLevel           log.LogLevel `ini:"log-level" default:"info" parse:"ParseLogLevel"`
 }
 
-func defaultGeneralConfig() *GeneralConfig {
-	return &GeneralConfig{
-		PgpProvider:        "auto",
-		UnsafeAccountsConf: false,
-		LogLevel:           log.INFO,
-	}
-}
-
-var General = defaultGeneralConfig()
+var General = new(GeneralConfig)
 
 func parseGeneral(file *ini.File) error {
-	var level *ini.Key
 	var logFile *os.File
 
-	gen, err := file.GetSection("general")
-	if err != nil {
-		goto end
-	}
-	if err := gen.MapTo(&General); err != nil {
+	if err := MapToStruct(file.Section("general"), General, true); err != nil {
 		return err
 	}
-	level, err = gen.GetKey("log-level")
-	if err == nil {
-		l, err := log.ParseLevel(level.String())
-		if err != nil {
-			return err
-		}
-		General.LogLevel = l
-	}
-	if err := General.validatePgpProvider(); err != nil {
-		return err
-	}
-end:
 	if !isatty.IsTerminal(os.Stdout.Fd()) {
 		logFile = os.Stdout
 		// redirected to file, force TRACE level
@@ -71,11 +46,14 @@ end:
 	return nil
 }
 
-func (gen *GeneralConfig) validatePgpProvider() error {
-	switch gen.PgpProvider {
+func (gen *GeneralConfig) ParseLogLevel(sec *ini.Section, key *ini.Key) (log.LogLevel, error) {
+	return log.ParseLevel(key.String())
+}
+
+func (gen *GeneralConfig) ParsePgpProvider(sec *ini.Section, key *ini.Key) (string, error) {
+	switch key.String() {
 	case "gpg", "internal", "auto":
-		return nil
-	default:
-		return fmt.Errorf("pgp-provider must be either auto, gpg or internal")
+		return key.String(), nil
 	}
+	return "", fmt.Errorf("must be either auto, gpg or internal")
 }

commit 6cd8441c0d23d04007020b9f5908c03db7138f69

config: fix default value of next-message-on-delete
The default value should be true as stated in the docs.

Fixes: b63c93563c62 ("config: use reflection to map ini keys to struct fields")
Reported-by: Kirill Chibisov <contact@kchibisov.com>
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Kirill Chibisov <contact@kchibisov.com>

config/ui.go
@@ -56,7 +56,7 @@ type UIConfig struct {
 	DirListTree                   bool          `ini:"dirlist-tree"`
 	DirListCollapse               int           `ini:"dirlist-collapse"`
 	Sort                          []string      `ini:"sort" delim:" "`
-	NextMessageOnDelete           bool          `ini:"next-message-on-delete"`
+	NextMessageOnDelete           bool          `ini:"next-message-on-delete" default:"true"`
 	CompletionDelay               time.Duration `ini:"completion-delay" default:"250ms"`
 	CompletionMinChars            int           `ini:"completion-min-chars" default:"1"`
 	CompletionPopovers            bool          `ini:"completion-popovers" default:"true"`