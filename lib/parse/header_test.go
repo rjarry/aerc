rjarry/aerc

commit 31d2f5be3cec88d82924816c3482253557999e63

message-info: add explicit References field
Add an explicit References field to message info. This is useful for
storing information needed for threading without storing all of the
header values, keeping system RAM usage lower.

Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>
Tested-by: Inwit <inwit@sindominio.net>
Acked-by: Robin Jarry <robin@jarry.cc>

models/models.go
@@ -71,6 +71,7 @@ type MessageInfo struct {
 	Labels        []string
 	InternalDate  time.Time
 	RFC822Headers *mail.Header
+	Refs          []string
 	Size          uint32
 	Uid           uint32
 	Error         error
@@ -109,6 +110,9 @@ func (mi *MessageInfo) InReplyTo() (msgid string, err error) {
 func (mi *MessageInfo) References() ([]string, error) {
 	if mi == nil {
 		return []string{}, errors.New("msg is nil")
+	}
+	if mi.Refs != nil {
+		return mi.Refs, nil
 	}
 	if mi.RFC822Headers == nil {
 		return []string{}, errors.New("header is nil")

worker/imap/cache.go
@@ -118,6 +118,10 @@ func (w *IMAPWorker) getCachedHeaders(msg *types.FetchMessageHeaders) []uint32 {
 			Uid:           ch.Uid,
 			RFC822Headers: hdr,
 		}
+		refs, err := hdr.MsgIDList("references")
+		if err != nil {
+			mi.Refs = refs
+		}
 		logging.Debugf("located cached header %s.%s", uv, u)
 		w.worker.PostMessage(&types.MessageInfo{
 			Message:    types.RespondTo(msg),

worker/imap/fetch.go
@@ -62,6 +62,10 @@ func (imapw *IMAPWorker) handleFetchMessageHeaders(
 				RFC822Headers: header,
 				Uid:           _msg.Uid,
 			}
+			refs, err := header.MsgIDList("references")
+			if err != nil {
+				info.Refs = refs
+			}
 			imapw.worker.PostMessage(&types.MessageInfo{
 				Message: types.RespondTo(msg),
 				Info:    info,

worker/lib/parse.go
@@ -348,11 +348,16 @@ func MessageHeaders(raw RawMessage) (*models.MessageInfo, error) {
 	if err != nil {
 		return nil, err
 	}
+	refs, err := h.MsgIDList("references")
+	if err != nil {
+		return nil, err
+	}
 	return &models.MessageInfo{
 		Envelope:     env,
 		Flags:        flags,
 		Labels:       labels,
 		InternalDate: recDate,
+		Refs:         refs,
 		Size:         0,
 		Uid:          raw.UID(),
 		Error:        parseErr,

commit 94a763920f5ee8b54b8ba7f819be373d80a4805d

parse msg-id lists more liberally
Some user agents deliberately generate non-standard
message identifier lists in In-Reply-To and References headers.

Instead of failing silently, aerc now falls back to a desperate parser
scavaging whatever looking like <id-left@id-right>.

As the more liberal parser being substituted with, References header
are now stored for IMAP not only when there's a parsing error.

Fixes: 31d2f5be3cec ("message-info: add explicit References field")
Signed-off-by: Nguyễn Gia Phong <mcsinyx@disroot.org>
Acked-by: Robin Jarry <robin@jarry.cc>

commands/msg/reply.go
@@ -16,6 +16,7 @@ import (
 	"git.sr.ht/~rjarry/aerc/lib"
 	"git.sr.ht/~rjarry/aerc/lib/crypto"
 	"git.sr.ht/~rjarry/aerc/lib/format"
+	"git.sr.ht/~rjarry/aerc/lib/parse"
 	"git.sr.ht/~rjarry/aerc/log"
 	"git.sr.ht/~rjarry/aerc/models"
 	"git.sr.ht/~rjarry/aerc/widgets"
@@ -299,17 +300,11 @@ func (s addrSet) Contains(a *mail.Address) bool {
 // setReferencesHeader adds the references header to target based on parent
 // according to RFC2822
 func setReferencesHeader(target, parent *mail.Header) error {
-	refs, err := parent.MsgIDList("references")
-	if err != nil {
-		return err
-	}
+	refs := parse.MsgIDList(parent, "references")
 	if len(refs) == 0 {
 		// according to the RFC we need to fall back to in-reply-to only if
 		// References is not set
-		refs, err = parent.MsgIDList("in-reply-to")
-		if err != nil {
-			return err
-		}
+		refs = parse.MsgIDList(parent, "in-reply-to")
 	}
 	msgID, err := parent.MessageID()
 	if err != nil {

lib/parse/header.go
@@ -1,0 +1,42 @@
+package parse
+
+import (
+	"strings"
+
+	"git.sr.ht/~rjarry/aerc/log"
+	"github.com/emersion/go-message/mail"
+)
+
+// MsgIDList parses a list of message identifiers.  It returns message
+// identifiers without angle brackets.  If the header field is missing,
+// it returns nil.
+//
+// This can be used on In-Reply-To and References header fields.
+// If the field does not conform to RFC 5322, fall back
+// to greedily parsing a subsequence of the original field.
+func MsgIDList(h *mail.Header, key string) []string {
+	l, err := h.MsgIDList(key)
+	if err == nil {
+		return l
+	}
+	log.Errorf("%s: %s", err, h.Get(key))
+
+	// Expensive, fix your peer's MUA instead!
+	var list []string
+	header := &mail.Header{Header: h.Header.Copy()}
+	value := header.Get(key)
+	for err != nil && len(value) > 0 {
+		// Skip parsed IDs
+		if len(l) > 0 {
+			last := "<" + l[len(l)-1] + ">"
+			value = value[strings.Index(value, last)+len(last):]
+			list = append(list, l...)
+		}
+
+		// Skip a character until some IDs can be parsed
+		value = value[1:]
+		header.Set(key, value)
+		l, err = header.MsgIDList(key)
+	}
+	return append(list, l...)
+}

models/models.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"time"
 
+	"git.sr.ht/~rjarry/aerc/lib/parse"
 	"github.com/emersion/go-message/mail"
 )
 
@@ -101,10 +102,7 @@ func (mi *MessageInfo) InReplyTo() (msgid string, err error) {
 	if mi.RFC822Headers == nil {
 		return "", errors.New("header is nil")
 	}
-	list, err := mi.RFC822Headers.MsgIDList("In-Reply-To")
-	if err != nil {
-		return "", err
-	}
+	list := parse.MsgIDList(mi.RFC822Headers, "In-Reply-To")
 	if len(list) == 0 {
 		return "", errors.New("no results")
 	}
@@ -121,14 +119,11 @@ func (mi *MessageInfo) References() ([]string, error) {
 	if mi.RFC822Headers == nil {
 		return []string{}, errors.New("header is nil")
 	}
-	list, err := mi.RFC822Headers.MsgIDList("References")
-	if err != nil {
-		return []string{}, err
-	}
+	list := parse.MsgIDList(mi.RFC822Headers, "References")
 	if len(list) == 0 {
 		return []string{}, errors.New("no results")
 	}
-	return list, err
+	return list, nil
 }
 
 // A MessageBodyPart can be displayed in the message viewer

worker/imap/cache.go
@@ -9,6 +9,7 @@ import (
 	"path"
 	"time"
 
+	"git.sr.ht/~rjarry/aerc/lib/parse"
 	"git.sr.ht/~rjarry/aerc/log"
 	"git.sr.ht/~rjarry/aerc/models"
 	"git.sr.ht/~rjarry/aerc/worker/types"
@@ -117,10 +118,7 @@ func (w *IMAPWorker) getCachedHeaders(msg *types.FetchMessageHeaders) []uint32 {
 			Flags:         models.SeenFlag, // Always return a SEEN flag
 			Uid:           ch.Uid,
 			RFC822Headers: hdr,
-		}
-		refs, err := hdr.MsgIDList("references")
-		if err != nil {
-			mi.Refs = refs
+			Refs:          parse.MsgIDList(hdr, "references"),
 		}
 		log.Tracef("located cached header %s.%s", uv, u)
 		w.worker.PostMessage(&types.MessageInfo{

worker/imap/fetch.go
@@ -10,6 +10,7 @@ import (
 	"github.com/emersion/go-message/mail"
 	"github.com/emersion/go-message/textproto"
 
+	"git.sr.ht/~rjarry/aerc/lib/parse"
 	"git.sr.ht/~rjarry/aerc/log"
 	"git.sr.ht/~rjarry/aerc/models"
 	"git.sr.ht/~rjarry/aerc/worker/types"
@@ -64,11 +65,8 @@ func (imapw *IMAPWorker) handleFetchMessageHeaders(
 				Flags:         translateImapFlags(_msg.Flags),
 				InternalDate:  _msg.InternalDate,
 				RFC822Headers: header,
+				Refs:          parse.MsgIDList(header, "references"),
 				Uid:           _msg.Uid,
-			}
-			refs, err := header.MsgIDList("references")
-			if err != nil {
-				info.Refs = refs
 			}
 			imapw.worker.PostMessage(&types.MessageInfo{
 				Message: types.RespondTo(msg),

worker/lib/parse.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	"time"
 
+	"git.sr.ht/~rjarry/aerc/lib/parse"
 	"git.sr.ht/~rjarry/aerc/log"
 	"git.sr.ht/~rjarry/aerc/models"
 	"github.com/emersion/go-message"
@@ -165,14 +166,8 @@ func parseEnvelope(h *mail.Header) (*models.Envelope, error) {
 		}
 	}
 	var irt string
-	irtList, err := h.MsgIDList("in-reply-to")
-	if err != nil {
-		// proper parsing failed, so fall back to whatever is there
-		irt, err = h.Text("in-reply-to")
-		if err != nil {
-			return nil, err
-		}
-	} else if len(irtList) > 0 {
+	irtList := parse.MsgIDList(h, "in-reply-to")
+	if len(irtList) > 0 {
 		irt = irtList[0]
 	}
 	date, err := parseDate(h)
@@ -351,16 +346,12 @@ func MessageHeaders(raw RawMessage) (*models.MessageInfo, error) {
 	if err != nil {
 		return nil, err
 	}
-	refs, err := h.MsgIDList("references")
-	if err != nil {
-		return nil, err
-	}
 	return &models.MessageInfo{
 		Envelope:     env,
 		Flags:        flags,
 		Labels:       labels,
 		InternalDate: recDate,
-		Refs:         refs,
+		Refs:         parse.MsgIDList(h, "references"),
 		Size:         0,
 		Uid:          raw.UID(),
 		Error:        parseErr,

lib/parse/header_test.go
@@ -1,0 +1,42 @@
+package parse_test
+
+import (
+	"testing"
+
+	"git.sr.ht/~rjarry/aerc/lib/parse"
+	"github.com/emersion/go-message/mail"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMsgIDList(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected []string
+	}{
+		{
+			name:     "valid",
+			input:    "<1q@az> (cmt)\r\n <2w@sx> (khld)",
+			expected: []string{"1q@az", "2w@sx"},
+		},
+		{
+			name:     "comma",
+			input:    "<3e@dc>, <4r@fv>,\t<5t@gb>",
+			expected: []string{"3e@dc", "4r@fv", "5t@gb"},
+		},
+		{
+			name:     "other non-CFWS separators",
+			input:    "<6y@>, <hn@7u>\n <> <jm@8i>",
+			expected: []string{"hn@7u", "jm@8i"},
+		},
+	}
+
+	for _, test := range tests {
+		var h mail.Header
+		h.Set("References", test.input)
+		t.Run(test.name, func(t *testing.T) {
+			actual := parse.MsgIDList(&h, "References")
+			assert.Equal(t, test.expected, actual)
+		})
+	}
+}