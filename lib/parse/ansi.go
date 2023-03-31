rjarry/aerc

commit ab8f433e1739f0c823809ddab192693e875d75df

templates: allow layered fg & bg color for inline styles
If a user style has no fg and/or no bg color defined, use the color from
the underlying style.

Signed-off-by: Robin Jarry <robin@jarry.cc>
Acked-by: Tim Culverhouse <tim@timculverhouse.com>

lib/parse/ansi.go
@@ -242,13 +242,15 @@ func (rb *RuneBuffer) ApplyStyle(style tcell.Style) {
 // attributes
 func (rb *RuneBuffer) ApplyAttrs(style tcell.Style) {
 	for _, sr := range rb.buf {
-		if sr.Style == tcell.StyleDefault {
-			sr.Style = style
-			continue
-		}
 		_, _, srAttrs := sr.Style.Decompose()
-		_, _, attrs := style.Decompose()
+		fg, bg, attrs := style.Decompose()
 		sr.Style = sr.Style.Attributes(srAttrs | attrs)
+		if fg != tcell.ColorDefault {
+			sr.Style = sr.Style.Foreground(fg)
+		}
+		if bg != tcell.ColorDefault {
+			sr.Style = sr.Style.Background(bg)
+		}
 	}
 }
 

commit ae4d742c5a90091c1229639a8480830805ac105d

templates: fix layered fg & bg color for inline styles
The logic was inverted. The style argument is actually the "under" layer
style. Each character in the RuneBuffer style must be applied on *top*
of it, not the other way around.

Use colors from style and override them with each character's colors if
they are set. Keep attributes handling unchanged.

Fixes: ab8f433e1739 ("templates: allow layered fg & bg color for inline styles")
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Bence Ferdinandy <bence@ferdinandy.com>

lib/parse/ansi.go
@@ -241,16 +241,17 @@ func (rb *RuneBuffer) ApplyStyle(style tcell.Style) {
 // ApplyAttrs applies the style, and if another style is present ORs the
 // attributes
 func (rb *RuneBuffer) ApplyAttrs(style tcell.Style) {
+	fg, bg, attrs := style.Decompose()
 	for _, sr := range rb.buf {
-		_, _, srAttrs := sr.Style.Decompose()
-		fg, bg, attrs := style.Decompose()
-		sr.Style = sr.Style.Attributes(srAttrs | attrs)
-		if fg != tcell.ColorDefault {
-			sr.Style = sr.Style.Foreground(fg)
+		srFg, srBg, srAttrs := sr.Style.Decompose()
+		if srFg == tcell.ColorDefault {
+			srFg = fg
 		}
-		if bg != tcell.ColorDefault {
-			sr.Style = sr.Style.Background(bg)
+		if srBg == tcell.ColorDefault {
+			srBg = bg
 		}
+		sr.Style = sr.Style.Attributes(attrs | srAttrs).
+			Foreground(srFg).Background(srBg)
 	}
 }
 