rjarry/aerc

commit 382aea4a942600935a5c2d0e4d42cbe129e9be9a

terminal: avoid high cpu usage when not focused
When running dynamic tui applications (such as btop, htop, etc.) in the
embedded :terminal and focusing an account tab, every tui application
change triggers a full redraw of the whole window. When the message list
is focused, this leads to high cpu usage because of the computationally
intensive templates parsing and ansi sequence handling.

Only redraw when the terminal is focused. Do not use tcell Watch and
Unwatch functions since these completely disable all widget updates,
including title changes and terminal close events.

Reported-by: John Mcenroy <handplanet@outlook.com>
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: John Mcenroy <handplanet@outlook.com>

widgets/terminal.go
@@ -149,7 +149,9 @@ func (term *Terminal) HandleEvent(ev tcell.Event) bool {
 	}
 	switch ev := ev.(type) {
 	case *views.EventWidgetContent:
-		ui.QueueRedraw()
+		if term.focus {
+			ui.QueueRedraw()
+		}
 		return true
 	case *tcellterm.EventTitle:
 		if term.OnTitle != nil {

commit e641da83471bc3e70b31bfceac21933836a26eac

term: ignore redraw events when not visible
Another attempt at fixing the high CPU usage when terminal tabs are
running interactive tui applications and the message list tab is
selected.

There are cases where a terminal widget can be visible but not focused
(composer, message viewer, attachment selector, etc.). Define a new
Visible interface with a single Show() method. Call that method when
changing tabs to make sure that terminal widget content events only
trigger redraws when they are visible.

Fixes: 382aea4a9426 ("terminal: avoid high cpu usage when not focused")
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Koni Marti <koni.marti@gmail.com>

lib/ui/interfaces.go
@@ -20,6 +20,11 @@ type Closeable interface {
 	Close()
 }
 
+type Visible interface {
+	// Indicate that this component is visible or not
+	Show(bool)
+}
+
 type RootDrawable interface {
 	Initialize(ui *UI)
 }

lib/ui/tab.go
@@ -153,9 +153,17 @@ func (tabs *Tabs) selectPriv(index int) bool {
 	if tabs.curIndex != index {
 		// only push valid tabs onto the history
 		if tabs.curIndex < len(tabs.tabs) {
+			prev := tabs.tabs[tabs.curIndex]
+			if vis, ok := prev.Content.(Visible); ok {
+				vis.Show(false)
+			}
 			tabs.pushHistory(tabs.curIndex)
 		}
 		tabs.curIndex = index
+		next := tabs.tabs[tabs.curIndex]
+		if vis, ok := next.Content.(Visible); ok {
+			vis.Show(true)
+		}
 		Invalidate()
 	}
 	return true

widgets/compose.go
@@ -700,6 +700,14 @@ func (c *Composer) Focus(focus bool) {
 	c.Unlock()
 }
 
+func (c *Composer) Show(visible bool) {
+	c.Lock()
+	if vis, ok := c.focusable[c.focused].(ui.Visible); ok {
+		vis.Show(visible)
+	}
+	c.Unlock()
+}
+
 func (c *Composer) Config() *config.AccountConfig {
 	return c.acctConfig
 }

widgets/msgviewer.go
@@ -406,6 +406,12 @@ func (ps *PartSwitcher) Focus(focus bool) {
 	}
 }
 
+func (ps *PartSwitcher) Show(visible bool) {
+	if ps.parts[ps.selected].term != nil {
+		ps.parts[ps.selected].term.Show(visible)
+	}
+}
+
 func (ps *PartSwitcher) Event(event tcell.Event) bool {
 	return ps.parts[ps.selected].Event(event)
 }
@@ -503,6 +509,10 @@ func (mv *MessageViewer) Event(event tcell.Event) bool {
 
 func (mv *MessageViewer) Focus(focus bool) {
 	mv.switcher.Focus(focus)
+}
+
+func (mv *MessageViewer) Show(visible bool) {
+	mv.switcher.Show(visible)
 }
 
 type PartViewer struct {

widgets/terminal.go
@@ -18,6 +18,7 @@ type Terminal struct {
 	ctx       *ui.Context
 	destroyed bool
 	focus     bool
+	visible   bool
 	vterm     *tcellterm.Terminal
 	running   bool
 
@@ -29,8 +30,9 @@ type Terminal struct {
 
 func NewTerminal(cmd *exec.Cmd) (*Terminal, error) {
 	term := &Terminal{
-		cmd:   cmd,
-		vterm: tcellterm.New(),
+		cmd:     cmd,
+		vterm:   tcellterm.New(),
+		visible: true,
 	}
 	return term, nil
 }
@@ -97,6 +99,10 @@ func (term *Terminal) Draw(ctx *ui.Context) {
 	term.draw()
 }
 
+func (term *Terminal) Show(visible bool) {
+	term.visible = visible
+}
+
 func (term *Terminal) draw() {
 	term.vterm.Draw()
 	if term.focus && !term.closed && term.ctx != nil {
@@ -149,7 +155,7 @@ func (term *Terminal) HandleEvent(ev tcell.Event) bool {
 	}
 	switch ev := ev.(type) {
 	case *views.EventWidgetContent:
-		if term.focus {
+		if term.visible {
 			ui.QueueRedraw()
 		}
 		return true