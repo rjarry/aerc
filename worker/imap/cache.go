rjarry/aerc

commit 6ea0f18635a8be20b89e11af53333cc9fee5d04f

imap: clear cache on tag mismatch
Tag the imap cache and clear it when the cache tag does not match the
current tag in the code. This ensures that the cache structure is always
consitent with our code base.

Signed-off-by: Koni Marti <koni.marti@gmail.com>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/imap/cache.go
@@ -4,9 +4,11 @@ import (
 	"bufio"
 	"bytes"
 	"encoding/gob"
+	"errors"
 	"fmt"
 	"os"
 	"path"
+	"reflect"
 	"time"
 
 	"git.sr.ht/~rjarry/aerc/lib/parse"
@@ -29,6 +31,14 @@ type CachedHeader struct {
 	Created       time.Time
 }
 
+var (
+	// cacheTag should be updated when changing the cache
+	// structure; this will ensure that the user's cache is cleared and
+	// reloaded when the underlying cache structure changes
+	cacheTag    = []byte("0000")
+	cacheTagKey = []byte("cache.tag")
+)
+
 // initCacheDb opens (or creates) the database for the cache. One database is
 // created per account
 func (w *IMAPWorker) initCacheDb(acct string) {
@@ -47,8 +57,26 @@ func (w *IMAPWorker) initCacheDb(acct string) {
 	}
 	w.cache = db
 	log.Debugf("cache db opened: %s", p)
-	if w.config.cacheMaxAge.Hours() > 0 {
-		go w.cleanCache(p)
+
+	tag, err := w.cache.Get(cacheTagKey, nil)
+	clearCache := errors.Is(err, leveldb.ErrNotFound) ||
+		!reflect.DeepEqual(tag, cacheTag)
+	switch {
+	case clearCache:
+		log.Infof("current cache tag is '%s' but found '%s'",
+			cacheTag, tag)
+		log.Warnf("tag mismatch: clear cache")
+		w.clearCache()
+		if err = w.cache.Put(cacheTagKey, cacheTag, nil); err != nil {
+			log.Errorf("could not set the current cache tag")
+		}
+	case err != nil:
+		log.Errorf("could not get the cache tag from db")
+	default:
+		log.Tracef("cache version match")
+		if w.config.cacheMaxAge.Hours() > 0 {
+			go w.cleanCache(p)
+		}
 	}
 }
 
@@ -171,4 +199,15 @@ func (w *IMAPWorker) cleanCache(path string) {
 	elapsed := time.Since(start)
 	log.Debugf("%s: removed %d/%d expired entries in %s",
 		path, removed, scanned, elapsed)
+}
+
+// clearCache clears the entire cache
+func (w *IMAPWorker) clearCache() {
+	iter := w.cache.NewIterator(nil, nil)
+	for iter.Next() {
+		if err := w.cache.Delete(iter.Key(), nil); err != nil {
+			log.Errorf("error clearing cache: %v", err)
+		}
+	}
+	iter.Release()
 }

commit c65f7dca01872ce17491ffb3240993d3b60e1e96

imap: avoid error log when pruning cache entries
Avoid such errors in the logs:

  ERROR cache.go:187: cannot clean database 0: unexpected EOF

The cache now contains a tag mapped to a special key. This is not
a gob serialized cached header. Ignore it when pruning old cache
entries.

Fixes: 6ea0f18635a8 ("imap: clear cache on tag mismatch")
Signed-off-by: Robin Jarry <robin@jarry.cc>
Acked-by: Tim Culverhouse <tim@timculverhouse.com>

worker/imap/cache.go
@@ -8,7 +8,6 @@ import (
 	"fmt"
 	"os"
 	"path"
-	"reflect"
 	"time"
 
 	"git.sr.ht/~rjarry/aerc/lib/parse"
@@ -61,7 +60,7 @@ func (w *IMAPWorker) initCacheDb(acct string) {
 
 	tag, err := w.cache.Get(cacheTagKey, nil)
 	clearCache := errors.Is(err, leveldb.ErrNotFound) ||
-		!reflect.DeepEqual(tag, cacheTag)
+		!bytes.Equal(tag, cacheTag)
 	switch {
 	case clearCache:
 		log.Infof("current cache tag is '%s' but found '%s'",
@@ -179,6 +178,9 @@ func (w *IMAPWorker) cleanCache(path string) {
 	var scanned, removed int
 	iter := w.cache.NewIterator(nil, nil)
 	for iter.Next() {
+		if bytes.Equal(iter.Key(), cacheTagKey) {
+			continue
+		}
 		data := iter.Value()
 		ch := &CachedHeader{}
 		dec := gob.NewDecoder(bytes.NewReader(data))