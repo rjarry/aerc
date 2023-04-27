rjarry/aerc

commit bba7159756908f092655bdf6db091daf9eb74f76

compose: only add delimiter when a signature is defined
Inserting a solitary "-- " delimiter without any signature after it
makes no sense. Skip the whole delimiter check if the signature is
empty. Trim leading and trailing white space along the way.

Fixes: a553b33ebcbd ("compose: ensure signature uses standard delimiter")
Reported-by: Ben Cohen <ben@bencohen.net>
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Ben Cohen <ben@bencohen.net>

widgets/compose.go
@@ -542,6 +542,10 @@ func (c *Composer) AddSignature() {
 	} else {
 		signature = c.readSignatureFromFile()
 	}
+	signature = bytes.TrimSpace(signature)
+	if len(signature) == 0 {
+		return
+	}
 	signature = ensureSignatureDelimiter(signature)
 	c.AppendContents(bytes.NewReader(signature))
 }

commit b3c1b693184d0d03171339c883bb2ecb4d983abc

compose: do not strip leading space from signature
Only strip the leading whitespace when a delimiter needs to be
prepended.

Fixes: bba715975690 ("compose: only add delimiter when a signature is defined")
Reported-by: Jason Cox <dev@jasoncarloscox.com>
Signed-off-by: Robin Jarry <robin@jarry.cc>
Acked-by: Jason Cox <dev@jasoncarloscox.com>

widgets/compose.go
@@ -538,8 +538,7 @@ func (c *Composer) AddSignature() {
 	} else {
 		signature = c.readSignatureFromFile()
 	}
-	signature = bytes.TrimSpace(signature)
-	if len(signature) == 0 {
+	if len(bytes.TrimSpace(signature)) == 0 {
 		return
 	}
 	signature = ensureSignatureDelimiter(signature)