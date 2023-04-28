rjarry/aerc

commit 91ac21ac615582e2dd5e36f4b36bde1bc0bf38d8

msgstore: fetch message list based on OpenDirectory msg
Fetching the message list is done in a convoluted way. The UI receives a
DirectoryInfo message, which creates a message store. It then receives a
second DirectoryInfo (an oddity from the IMAP worker), and this
DirectoryInfo is passed to the message store which then requests a fetch
of the message list via store.Sort.

Use the OpenDirectory done response to tell the message store to fetch
messages. This makes the code easier to follow, and does not rely on
quirks from the IMAP worker.

Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/maildir/worker.go
@@ -38,6 +38,7 @@ type Worker struct {
 	c                   *Container
 	selected            *maildir.Dir
 	selectedName        string
+	selectedInfo        *models.DirectoryInfo
 	worker              *types.Worker
 	watcher             types.FSWatcher
 	currentSortCriteria []*types.SortCriterion
@@ -112,8 +113,13 @@ func (w *Worker) handleFSEvent(ev *types.FSEvent) {
 	}
 
 	dirInfo := w.getDirectoryInfo(w.selectedName)
+	var refetch bool
+	if dirInfo.Exists > w.selectedInfo.Exists {
+		refetch = true
+	}
 	w.worker.PostMessage(&types.DirectoryInfo{
-		Info: dirInfo,
+		Info:    dirInfo,
+		Refetch: refetch,
 	}, nil)
 }
 
@@ -394,6 +400,7 @@ func (w *Worker) handleOpenDirectory(msg *types.OpenDirectory) error {
 	info := &types.DirectoryInfo{
 		Info: w.getDirectoryInfo(msg.Directory),
 	}
+	w.selectedInfo = info.Info
 	w.worker.PostMessage(info, nil)
 	return nil
 }
@@ -865,8 +872,7 @@ func (w *Worker) handleCheckMail(msg *types.CheckMail) {
 				}
 				dirInfo := w.getDirectoryInfo(name)
 				w.worker.PostMessage(&types.DirectoryInfo{
-					Info:     dirInfo,
-					SkipSort: true,
+					Info: dirInfo,
 				}, nil)
 			}
 			w.done(msg)

lib/msgstore.go
@@ -214,12 +214,15 @@ func (store *MessageStore) Update(msg types.WorkerMessage) {
 	updateThreads := false
 	directoryChange := false
 	switch msg := msg.(type) {
+	case *types.OpenDirectory:
+		store.Sort(store.sortCriteria, nil)
+		update = true
 	case *types.DirectoryInfo:
 		store.DirInfo = *msg.Info
-		if !msg.SkipSort {
+		if msg.Refetch {
 			store.Sort(store.sortCriteria, nil)
+			update = true
 		}
-		update = true
 	case *types.DirectoryContents:
 		newMap := make(map[uint32]*models.MessageInfo)
 		for _, uid := range msg.Uids {

widgets/account.go
@@ -263,6 +263,7 @@ func (acct *AccountView) onMessage(msg types.WorkerMessage) {
 				// snappier. If not, we'll unset the store and show the spinner
 				// while we download the UID list.
 				acct.msglist.SetStore(store)
+				acct.Store().Update(msg.InResponseTo())
 			} else {
 				acct.msglist.SetStore(nil)
 			}

worker/imap/checkmail.go
@@ -60,7 +60,6 @@ func (w *IMAPWorker) handleCheckMailMessage(msg *types.CheckMail) {
 				Unseen: int(status.Unseen),
 				Caps:   w.caps,
 			},
-			SkipSort: true,
 		}, nil)
 	}
 	if len(remaining) > 0 {

worker/imap/list.go
@@ -67,7 +67,6 @@ func (imapw *IMAPWorker) handleListDirectories(msg *types.ListDirectories) {
 					Unseen: int(status.Unseen),
 					Caps:   imapw.caps,
 				},
-				SkipSort: true,
 			}, nil)
 		}
 	default:

worker/notmuch/eventhandlers.go
@@ -30,16 +30,14 @@ func (w *worker) handleUpdateDirCounts() error {
 		for name := range folders {
 			query := fmt.Sprintf("folder:%s", strconv.Quote(name))
 			w.w.PostMessage(&types.DirectoryInfo{
-				Info:     w.getDirectoryInfo(name, query),
-				SkipSort: true,
+				Info: w.getDirectoryInfo(name, query),
 			}, nil)
 		}
 	}
 
 	for name, query := range w.nameQueryMap {
 		w.w.PostMessage(&types.DirectoryInfo{
-			Info:     w.getDirectoryInfo(name, query),
-			SkipSort: true,
+			Info: w.getDirectoryInfo(name, query),
 		}, nil)
 	}
 	return nil

worker/types/messages.go
@@ -207,8 +207,8 @@ type Directory struct {
 
 type DirectoryInfo struct {
 	Message
-	Info     *models.DirectoryInfo
-	SkipSort bool
+	Info    *models.DirectoryInfo
+	Refetch bool
 }
 
 type DirectoryContents struct {

commit ef4504e6baf513713779b1e1af40848e746e704b

maildir: fix handling of FSEvents
Maildir uses a filesystem watcher to watch the currently selected
directory for changes, updating the UI as necessary. Not all changes get
updated based on the current logic. Send a MessageInfo if the event was
a Rename. Send a MessagesDeleted if the event was a Remove. Tell the UI
to refetch the list if the event was a Create.

Note: This leads to some unnecessary fetching in the UI, as renames also
come with a Create event in most cases. Koni suggested sending the
MessageInfo and having the UI perform a binary search and inserting the
message in the proper position. This is optimization is left out of this
series, with a TODO left in the code.

Fixes: https://todo.sr.ht/~rjarry/aerc/171
Fixes: 91ac21ac6155 ("msgstore: fetch message list based on OpenDirectory msg")
Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/maildir/container.go
@@ -93,6 +93,15 @@ func (c *Container) Message(d maildir.Dir, uid uint32) (*Message, error) {
 		uid, d)
 }
 
+func (c *Container) MessageFromKey(d maildir.Dir, key string) *Message {
+	uid := c.uids.GetOrInsert(key)
+	return &Message{
+		dir: d,
+		uid: uid,
+		key: key,
+	}
+}
+
 // DeleteAll deletes a set of messages by UID and returns the subset of UIDs
 // which were successfully deleted, stopping upon the first error.
 func (c *Container) DeleteAll(d maildir.Dir, uids []uint32) ([]uint32, error) {

worker/maildir/worker.go
@@ -132,15 +132,42 @@ func (w *Worker) handleFSEvent(ev *types.FSEvent) {
 		return
 	}
 
-	dirInfo := w.getDirectoryInfo(w.selectedName)
-	var refetch bool
-	if dirInfo.Exists > w.selectedInfo.Exists {
-		refetch = true
+	w.selectedInfo = w.getDirectoryInfo(w.selectedName)
+	dirInfoMsg := &types.DirectoryInfo{
+		Info: w.selectedInfo,
 	}
-	w.worker.PostMessage(&types.DirectoryInfo{
-		Info:    dirInfo,
-		Refetch: refetch,
-	}, nil)
+
+	base := filepath.Base(ev.Path)
+	parts := strings.SplitN(base, ":", 2)
+	if len(parts) != 2 {
+		log.Errorf("Couldn't parse key from file: %s", ev.Path)
+		return
+	}
+	msg := w.c.MessageFromKey(*w.selected, parts[0])
+
+	switch ev.Operation {
+	case types.FSCreate:
+		// TODO for FSCreate we should send a new message type that
+		// creates the message in the UI, does a binary search based on
+		// current sort criteria and inserts message at proper index
+		// For now, we just refetch the list.
+		dirInfoMsg.Refetch = true
+	case types.FSRename:
+		msgInfo, err := msg.MessageInfo()
+		if err != nil {
+			log.Errorf(err.Error())
+			return
+		}
+		w.worker.PostMessage(&types.MessageInfo{
+			Info: msgInfo,
+		}, nil)
+	case types.FSRemove:
+		w.worker.PostMessage(&types.MessagesDeleted{
+			Uids: []uint32{msg.uid},
+		}, nil)
+	}
+
+	w.worker.PostMessage(dirInfoMsg, nil)
 }
 
 func (w *Worker) done(msg types.WorkerMessage) {