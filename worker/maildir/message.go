rjarry/aerc

commit f04d83e80af70f9d29a1c515e93238eeb356ad2d

messageinfo: report message sizes
Report sizes of the message across all backends.

Signed-off-by: Koni Marti <koni.marti@gmail.com>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/maildir/message.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/emersion/go-maildir"
 
+	"git.sr.ht/~rjarry/aerc/log"
 	"git.sr.ht/~rjarry/aerc/models"
 	"git.sr.ht/~rjarry/aerc/worker/lib"
 )
@@ -73,7 +74,19 @@ func (m Message) Remove() error {
 
 // MessageInfo populates a models.MessageInfo struct for the message.
 func (m Message) MessageInfo() (*models.MessageInfo, error) {
-	return lib.MessageInfo(m)
+	info, err := lib.MessageInfo(m)
+	if err != nil {
+		return nil, err
+	}
+	// if size retrieval fails, just return info and log error
+	if name, err := m.dir.Filename(m.key); err != nil {
+		log.Errorf("failed to obtain filename: %v", err)
+	} else {
+		if info.Size, err = lib.FileSize(name); err != nil {
+			log.Errorf("failed to obtain file size: %v", err)
+		}
+	}
+	return info, nil
 }
 
 // MessageHeaders populates a models.MessageInfo struct for the message with

worker/notmuch/message.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/emersion/go-maildir"
 
+	"git.sr.ht/~rjarry/aerc/log"
 	"git.sr.ht/~rjarry/aerc/models"
 	"git.sr.ht/~rjarry/aerc/worker/lib"
 	notmuch "git.sr.ht/~rjarry/aerc/worker/notmuch/lib"
@@ -34,7 +35,19 @@ func (m *Message) NewReader() (io.ReadCloser, error) {
 
 // MessageInfo populates a models.MessageInfo struct for the message.
 func (m *Message) MessageInfo() (*models.MessageInfo, error) {
-	return lib.MessageInfo(m)
+	info, err := lib.MessageInfo(m)
+	if err != nil {
+		return nil, err
+	}
+	// if size retrieval fails, just return info and log error
+	if name, err := m.Filename(); err != nil {
+		log.Errorf("failed to obtain filename: %v", err)
+	} else {
+		if info.Size, err = lib.FileSize(name); err != nil {
+			log.Errorf("failed to obtain file size: %v", err)
+		}
+	}
+	return info, nil
 }
 
 // NewBodyPartReader creates a new io.Reader for the requested body part(s) of

worker/imap/fetch.go
@@ -42,6 +42,7 @@ func (imapw *IMAPWorker) handleFetchMessageHeaders(
 		imap.FetchInternalDate,
 		imap.FetchFlags,
 		imap.FetchUid,
+		imap.FetchRFC822Size,
 		section.FetchItem(),
 	}
 	imapw.handleFetchMessages(msg, toFetch, items,
@@ -66,6 +67,7 @@ func (imapw *IMAPWorker) handleFetchMessageHeaders(
 				InternalDate:  _msg.InternalDate,
 				RFC822Headers: header,
 				Refs:          parse.MsgIDList(header, "references"),
+				Size:          _msg.Size,
 				Uid:           _msg.Uid,
 			}
 			imapw.worker.PostMessage(&types.MessageInfo{

worker/lib/parse.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"os"
 	"regexp"
 	"strings"
 	"time"
@@ -380,4 +381,13 @@ func ReadMessage(r io.Reader) (*message.Entity, error) {
 		return nil, fmt.Errorf("could not read message: %w", err)
 	}
 	return entity, nil
+}
+
+// FileSize returns the size of the file specified by name
+func FileSize(name string) (uint32, error) {
+	fileInfo, err := os.Stat(name)
+	if err != nil {
+		return 0, fmt.Errorf("failed to obtain fileinfo: %w", err)
+	}
+	return uint32(fileInfo.Size()), nil
 }

worker/mbox/worker.go
@@ -15,6 +15,7 @@ import (
 	"git.sr.ht/~rjarry/aerc/worker/handlers"
 	"git.sr.ht/~rjarry/aerc/worker/lib"
 	"git.sr.ht/~rjarry/aerc/worker/types"
+	"github.com/miolini/datacounter"
 )
 
 func init() {
@@ -155,7 +156,7 @@ func (w *mboxWorker) handleMessage(msg types.WorkerMessage) error {
 				reterr = err
 				break
 			}
-			msgInfo, err := lib.MessageInfo(m)
+			msgInfo, err := messageInfo(m, true)
 			if err != nil {
 				w.worker.PostMessageInfoError(msg, uid, err)
 				break
@@ -404,13 +405,19 @@ func sortUids(folder *container, uids []uint32,
 	criteria []*types.SortCriterion,
 ) ([]uint32, error) {
 	var infos []*models.MessageInfo
+	needSize := false
+	for _, item := range criteria {
+		if item.Field == types.SortSize {
+			needSize = true
+		}
+	}
 	for _, uid := range uids {
 		m, err := folder.Message(uid)
 		if err != nil {
 			log.Errorf("could not get message %v", err)
 			continue
 		}
-		info, err := lib.MessageInfo(m)
+		info, err := messageInfo(m, needSize)
 		if err != nil {
 			log.Errorf("could not get message info %v", err)
 			continue
@@ -418,4 +425,21 @@ func sortUids(folder *container, uids []uint32,
 		infos = append(infos, info)
 	}
 	return lib.Sort(infos, criteria)
+}
+
+func messageInfo(m lib.RawMessage, needSize bool) (*models.MessageInfo, error) {
+	info, err := lib.MessageInfo(m)
+	if err != nil {
+		return nil, err
+	}
+	if needSize {
+		if r, err := m.NewReader(); err == nil {
+			var buf bytes.Buffer
+			ctr := datacounter.NewWriterCounter(&buf)
+			if _, err := io.Copy(ctr, r); err == nil {
+				info.Size = uint32(ctr.Count())
+			}
+		}
+	}
+	return info, nil
 }

commit cd68adc4630ed834eeac33f09ef58891c18c2dee

maildir: fix sorting by size
Retrieve message size not only in the MessageInfo but also in the
MessageHeaders function. The MessageHeaders function is used for an
memory-optimized maildir sorting. This fixes sorting by size in the
maildir backend.

Fixes: f04d83e8 ("messageinfo: report message sizes")
Signed-off-by: Koni Marti <koni.marti@gmail.com>
Acked-by: Tim Culverhouse <tim@timculverhouse.com>

worker/maildir/message.go
@@ -78,21 +78,39 @@ func (m Message) MessageInfo() (*models.MessageInfo, error) {
 	if err != nil {
 		return nil, err
 	}
-	// if size retrieval fails, just return info and log error
-	if name, err := m.dir.Filename(m.key); err != nil {
-		log.Errorf("failed to obtain filename: %v", err)
-	} else {
-		if info.Size, err = lib.FileSize(name); err != nil {
-			log.Errorf("failed to obtain file size: %v", err)
-		}
+	info.Size, err = m.Size()
+	if err != nil {
+		// don't care if size retrieval fails
+		log.Debugf("message size: %v", err)
 	}
 	return info, nil
+}
+
+func (m Message) Size() (uint32, error) {
+	name, err := m.dir.Filename(m.key)
+	if err != nil {
+		return 0, fmt.Errorf("failed to get filename: %w", err)
+	}
+	size, err := lib.FileSize(name)
+	if err != nil {
+		return 0, fmt.Errorf("failed to get filesize: %w", err)
+	}
+	return size, nil
 }
 
 // MessageHeaders populates a models.MessageInfo struct for the message with
 // minimal information, used for sorting and threading.
 func (m Message) MessageHeaders() (*models.MessageInfo, error) {
-	return lib.MessageHeaders(m)
+	info, err := lib.MessageHeaders(m)
+	if err != nil {
+		return nil, err
+	}
+	info.Size, err = m.Size()
+	if err != nil {
+		// don't care if size retrieval fails
+		log.Debugf("message size failed: %v", err)
+	}
+	return info, nil
 }
 
 // NewBodyPartReader creates a new io.Reader for the requested body part(s) of