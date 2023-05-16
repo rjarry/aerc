rjarry/aerc

commit 6e038c7fd510563efc35780e293e25e34ffe3077

dirtree: modify drawing of the dirtree
Update the dirtree drawing structure to be a bit more compact. The
dirtree is drawn with one column left for a flag, which indicates if a
parent is collapsed or expanded. Only draw the "flag" when the parent is
collapsed. Change the flag to a '+' character to match common UI
patterns of expanding lists having a '+' box.

Don't draw the '>' character in the dirtree. This character is nice in
the threaded message view, but clutters up the dirtree. I know this is a
matter of preference, but this approach is similar to most other UI tree
view and gives the dirtree an extra column of space.

The original dirtree looks like this:
	┌Inbox
	├─>Sub
	└─>Sub

And collapsed:
	-Inbox

This patch changes it to:
	Inbox
	├─Sub
	└─Sub

And collapsed:
	+Inbox

Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>
Acked-by: Moritz Poldrack <moritz@poldrack.dev>

widgets/msglist.go
@@ -165,7 +165,7 @@ func (ml *MessageList) Draw(ctx *ui.Context) {
 
 				baseSubject := threadSubject(store, thread)
 				data.SetThreading(
-					threadPrefix(thread, store.ReverseThreadOrder()),
+					threadPrefix(thread, store.ReverseThreadOrder(), true),
 					baseSubject == lastSubject && sameParent(thread, prevThread) && !isParent(thread),
 				)
 				lastSubject = baseSubject
@@ -427,17 +427,19 @@ func (ml *MessageList) drawEmptyMessage(ctx *ui.Context) {
 		uiConfig.GetStyle(config.STYLE_MSGLIST_DEFAULT), "%s", msg)
 }
 
-func threadPrefix(t *types.Thread, reverse bool) string {
+func threadPrefix(t *types.Thread, reverse bool, point bool) string {
 	var arrow string
 	if t.Parent != nil {
-		if t.NextSibling != nil {
-			arrow = "├─>"
-		} else {
-			if reverse {
-				arrow = "┌─>"
-			} else {
-				arrow = "└─>"
-			}
+		switch {
+		case t.NextSibling != nil:
+			arrow = "├─"
+		case reverse:
+			arrow = "┌─"
+		default:
+			arrow = "└─"
+		}
+		if point {
+			arrow += ">"
 		}
 	}
 	var prefix []string

widgets/dirtree.go
@@ -275,7 +275,7 @@ func (dt *DirectoryTree) countVisible(list []*types.Thread) (n int) {
 
 func (dt *DirectoryTree) displayText(node *types.Thread) string {
 	elems := strings.Split(dt.treeDirs[getAnyUid(node)], dt.pathSeparator)
-	return fmt.Sprintf("%s%s%s", threadPrefix(node, false), getFlag(node), elems[countLevels(node)])
+	return fmt.Sprintf("%s%s%s", threadPrefix(node, false, false), getFlag(node), elems[countLevels(node)])
 }
 
 func (dt *DirectoryTree) getDirectory(node *types.Thread) string {
@@ -471,13 +471,12 @@ func countLevels(node *types.Thread) (level int) {
 	return
 }
 
-func getFlag(node *types.Thread) (flag string) {
-	if node != nil && node.FirstChild != nil {
-		if node.Hidden {
-			flag = "─"
-		} else {
-			flag = "┌"
-		}
+func getFlag(node *types.Thread) string {
+	if node == nil && node.FirstChild == nil {
+		return ""
 	}
-	return
+	if node.Hidden {
+		return "+"
+	}
+	return ""
 }

commit e8089e7d8f6065031814bf7d80be043536e09fe7

dirtree: fixup to apply correct patchset version
I had applied v2 instead of v4... It is time to automate these things
a bit better.

Fixes: 6e038c7fd510 ("dirtree: modify drawing of the dirtree")
Signed-off-by: Robin Jarry <robin@jarry.cc>

widgets/msglist.go
@@ -444,10 +444,15 @@ func threadPrefix(t *types.Thread, reverse bool, point bool) string {
 	}
 	var prefix []string
 	for n := t; n.Parent != nil; n = n.Parent {
-		if n.Parent.NextSibling != nil {
+		switch {
+		case n.Parent.NextSibling != nil && point:
 			prefix = append(prefix, "│  ")
-		} else {
+		case n.Parent.NextSibling != nil:
+			prefix = append(prefix, "│ ")
+		case point:
 			prefix = append(prefix, "   ")
+		default:
+			prefix = append(prefix, "  ")
 		}
 	}
 	// prefix is now in a reverse order (inside --> outside), so turn it