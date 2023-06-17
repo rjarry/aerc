rjarry/aerc

commit 8ced001d82b59f353c33ad34ca0c79313eef41af

listDirectories: refactor listdirectories handling
ListDirectories is called when connecting, reconnecting, and
creation/deletion of a directory. The code is not in the same style as
other areas of aerc. Refactor to match coding style of the rest of aerc
by creating an Update function which handles necessary updates in the
dirlist. This style does not use a callback, making it clearer what is
happening in the message flow, and operates similar to how the msgstore
receives updates.

Use a map in the dirstore to reduce duplicate storage of directory
names. Directly add or remove directories from the map when created /
deleted to prevent a new ListDirectories message, and a flash of the UI.

Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>
Acked-by: Robin Jarry <robin@jarry.cc>

widgets/account.go
@@ -231,26 +231,10 @@ func (acct *AccountView) onMessage(msg types.WorkerMessage) {
 		switch msg.InResponseTo().(type) {
 		case *types.Connect, *types.Reconnect:
 			acct.SetStatus(state.ConnectionActivity("Listing mailboxes..."))
+			log.Infof("[%s] connected.", acct.acct.Name)
+			acct.SetStatus(state.SetConnected(true))
 			log.Tracef("Listing mailboxes...")
-			acct.dirlist.UpdateList(func(dirs []string) {
-				var dir string
-				for _, _dir := range dirs {
-					if _dir == acct.acct.Default {
-						dir = _dir
-						break
-					}
-				}
-				if dir == "" && len(dirs) > 0 {
-					dir = dirs[0]
-				}
-				if dir != "" {
-					acct.dirlist.Select(dir)
-				}
-				acct.msglist.SetInitDone()
-				log.Infof("[%s] connected.", acct.acct.Name)
-				acct.SetStatus(state.SetConnected(true))
-				acct.newConn = true
-			})
+			acct.worker.PostAction(&types.ListDirectories{}, nil)
 		case *types.Disconnect:
 			acct.dirlist.ClearList()
 			acct.msglist.SetStore(nil)
@@ -268,13 +252,35 @@ func (acct *AccountView) onMessage(msg types.WorkerMessage) {
 				acct.msglist.SetStore(nil)
 			}
 		case *types.CreateDirectory:
-			acct.dirlist.UpdateList(nil)
+			acct.dirlist.Update(msg)
 		case *types.RemoveDirectory:
-			acct.dirlist.UpdateList(nil)
+			acct.dirlist.Update(msg)
 		case *types.FetchMessageHeaders:
 			if acct.newConn {
 				acct.checkMailOnStartup()
 			}
+		case *types.ListDirectories:
+			acct.dirlist.Update(msg)
+			if acct.dirlist.Selected() != "" {
+				return
+			}
+			// Nothing selected, select based on config
+			dirs := acct.dirlist.List()
+			var dir string
+			for _, _dir := range dirs {
+				if _dir == acct.acct.Default {
+					dir = _dir
+					break
+				}
+			}
+			if dir == "" && len(dirs) > 0 {
+				dir = dirs[0]
+			}
+			if dir != "" {
+				acct.dirlist.Select(dir)
+			}
+			acct.msglist.SetInitDone()
+			acct.newConn = true
 		}
 	case *types.Directory:
 		name := msg.Dir.Name

widgets/dirlist.go
@@ -28,7 +28,7 @@ type DirectoryLister interface {
 	Selected() string
 	Select(string)
 
-	UpdateList(func([]string))
+	Update(types.WorkerMessage)
 	List() []string
 	ClearList()
 
@@ -91,33 +91,30 @@ func (dirlist *DirectoryList) UiConfig(dir string) *config.UIConfig {
 }
 
 func (dirlist *DirectoryList) List() []string {
-	return dirlist.store.List()
+	return dirlist.dirs
 }
 
 func (dirlist *DirectoryList) ClearList() {
 	dirlist.dirs = []string{}
 }
 
-func (dirlist *DirectoryList) UpdateList(done func(dirs []string)) {
-	// TODO: move this logic into dirstore
-	var dirs []string
-	dirlist.worker.PostAction(
-		&types.ListDirectories{}, func(msg types.WorkerMessage) {
-			switch msg := msg.(type) {
-			case *types.Directory:
-				dirs = append(dirs, msg.Dir.Name)
-			case *types.Done:
-				dirlist.store.Update(dirs)
-				dirlist.filterDirsByFoldersConfig()
-				dirlist.sortDirsByFoldersSortConfig()
-				dirlist.store.Update(dirlist.dirs)
-				dirlist.spinner.Stop()
-				dirlist.Invalidate()
-				if done != nil {
-					done(dirlist.dirs)
-				}
-			}
-		})
+func (dirlist *DirectoryList) Update(msg types.WorkerMessage) {
+	switch msg := msg.(type) {
+	case *types.Done:
+		switch msg := msg.InResponseTo().(type) {
+		case *types.ListDirectories:
+			dirlist.filterDirsByFoldersConfig()
+			dirlist.sortDirsByFoldersSortConfig()
+			dirlist.spinner.Stop()
+			dirlist.Invalidate()
+		case *types.RemoveDirectory:
+			dirlist.store.Remove(msg.Directory)
+			dirlist.filterDirsByFoldersConfig()
+			dirlist.sortDirsByFoldersSortConfig()
+		}
+	default:
+		return
+	}
 }
 
 func (dirlist *DirectoryList) CollapseFolder() {

widgets/dirtree.go
@@ -37,18 +37,23 @@ func NewDirectoryTree(dirlist *DirectoryList, pathSeparator string) DirectoryLis
 
 func (dt *DirectoryTree) ClearList() {
 	dt.list = make([]*types.Thread, 0)
+	dt.selected = ""
 }
 
-func (dt *DirectoryTree) UpdateList(done func([]string)) {
-	dt.DirectoryList.UpdateList(func(dirs []string) {
-		if done != nil {
-			done(dirs)
+func (dt *DirectoryTree) Update(msg types.WorkerMessage) {
+	switch msg := msg.(type) {
+
+	case *types.Done:
+		switch msg.InResponseTo().(type) {
+		case *types.RemoveDirectory, *types.ListDirectories, *types.CreateDirectory:
+			dt.DirectoryList.Update(msg)
+			dt.buildTree()
+		default:
+			dt.DirectoryList.Update(msg)
 		}
-		dt.buildTree()
-		dt.listIdx = findString(dt.dirs, dt.selecting)
-		dt.Select(dt.selecting)
-		dt.Scrollable = Scrollable{}
-	})
+	default:
+		dt.DirectoryList.Update(msg)
+	}
 }
 
 func (dt *DirectoryTree) Draw(ctx *ui.Context) {

lib/dirstore.go
@@ -1,7 +1,6 @@
 package lib
 
 type DirStore struct {
-	dirs      []string
 	msgStores map[string]*MessageStore
 }
 
@@ -10,13 +9,12 @@ func NewDirStore() *DirStore {
 	return &DirStore{msgStores: msgStores}
 }
 
-func (store *DirStore) Update(dirs []string) {
-	store.dirs = make([]string, len(dirs))
-	copy(store.dirs, dirs)
-}
-
 func (store *DirStore) List() []string {
-	return store.dirs
+	dirs := []string{}
+	for dir := range store.msgStores {
+		dirs = append(dirs, dir)
+	}
+	return dirs
 }
 
 func (store *DirStore) MessageStore(dirname string) (*MessageStore, bool) {
@@ -26,4 +24,8 @@ func (store *DirStore) MessageStore(dirname string) (*MessageStore, bool) {
 
 func (store *DirStore) SetMessageStore(name string, msgStore *MessageStore) {
 	store.msgStores[name] = msgStore
+}
+
+func (store *DirStore) Remove(name string) {
+	delete(store.msgStores, name)
 }

commit b13c45797a1ea2d27f162c0fde0b640d7b24471e

account: create new store when creating folders
Add a correct message store entry to the DirStore for newly created
directories. This is currently not done, so the directory name does
either not show up in the directory list or contains a nil message
store. Either way, such a directory cannot be used in the current
session and aerc needs to be restarted/reconnected. This affects the
:mkdir and :archive commands (archive when a new directory is created).

To reproduce on imap: create a new directory (:mkdir testdir), try to
move a message into it (:move testdir). It will not show up because
there is not message store.

Fixes: 8ced001d ("listDirectories: refactor listdirectories handling")
Signed-off-by: Koni Marti <koni.marti@gmail.com>
Acked-by: Robin Jarry <robin@jarry.cc>

widgets/account.go
@@ -229,11 +229,40 @@ func (acct *AccountView) isSelected() bool {
 	return acct == acct.aerc.SelectedAccount()
 }
 
+func (acct *AccountView) newStore(name string) *lib.MessageStore {
+	uiConf := acct.dirlist.UiConfig(name)
+	store := lib.NewMessageStore(acct.worker,
+		acct.GetSortCriteria(),
+		uiConf.ThreadingEnabled,
+		uiConf.ForceClientThreads,
+		uiConf.ClientThreadsDelay,
+		uiConf.ReverseOrder,
+		uiConf.ReverseThreadOrder,
+		uiConf.SortThreadSiblings,
+		func(msg *models.MessageInfo) {
+			err := hooks.RunHook(&hooks.MailReceived{
+				MsgInfo: msg,
+			})
+			if err != nil {
+				msg := fmt.Sprintf("mail-received hook: %s", err)
+				acct.aerc.PushError(msg)
+			}
+		}, func() {
+			if uiConf.NewMessageBell {
+				acct.host.Beep()
+			}
+		},
+		acct.updateSplitView,
+	)
+	store.SetMarker(marker.New(store))
+	return store
+}
+
 func (acct *AccountView) onMessage(msg types.WorkerMessage) {
 	msg = acct.worker.ProcessMessage(msg)
 	switch msg := msg.(type) {
 	case *types.Done:
-		switch msg.InResponseTo().(type) {
+		switch resp := msg.InResponseTo().(type) {
 		case *types.Connect, *types.Reconnect:
 			acct.SetStatus(state.ConnectionActivity("Listing mailboxes..."))
 			log.Infof("[%s] connected.", acct.acct.Name)
@@ -257,6 +286,10 @@ func (acct *AccountView) onMessage(msg types.WorkerMessage) {
 				acct.msglist.SetStore(nil)
 			}
 		case *types.CreateDirectory:
+			store := acct.newStore(resp.Directory)
+			acct.dirlist.SetMsgStore(&models.Directory{
+				Name: resp.Directory,
+			}, store)
 			acct.dirlist.Update(msg)
 		case *types.RemoveDirectory:
 			acct.dirlist.Update(msg)
@@ -288,31 +321,7 @@ func (acct *AccountView) onMessage(msg types.WorkerMessage) {
 			acct.newConn = true
 		}
 	case *types.Directory:
-		name := msg.Dir.Name
-		store := lib.NewMessageStore(acct.worker,
-			acct.GetSortCriteria(),
-			acct.dirlist.UiConfig(name).ThreadingEnabled,
-			acct.dirlist.UiConfig(name).ForceClientThreads,
-			acct.dirlist.UiConfig(name).ClientThreadsDelay,
-			acct.dirlist.UiConfig(name).ReverseOrder,
-			acct.dirlist.UiConfig(name).ReverseThreadOrder,
-			acct.dirlist.UiConfig(name).SortThreadSiblings,
-			func(msg *models.MessageInfo) {
-				err := hooks.RunHook(&hooks.MailReceived{
-					MsgInfo: msg,
-				})
-				if err != nil {
-					msg := fmt.Sprintf("mail-received hook: %s", err)
-					acct.aerc.PushError(msg)
-				}
-			}, func() {
-				if acct.dirlist.UiConfig(name).NewMessageBell {
-					acct.host.Beep()
-				}
-			},
-			acct.updateSplitView,
-		)
-		store.SetMarker(marker.New(store))
+		store := acct.newStore(msg.Dir.Name)
 		acct.dirlist.SetMsgStore(msg.Dir, store)
 	case *types.DirectoryInfo:
 		acct.dirlist.Update(msg)

widgets/dirlist.go
@@ -117,6 +117,10 @@ func (dirlist *DirectoryList) Update(msg types.WorkerMessage) {
 			dirlist.store.Remove(msg.Directory)
 			dirlist.filterDirsByFoldersConfig()
 			dirlist.sortDirsByFoldersSortConfig()
+		case *types.CreateDirectory:
+			dirlist.filterDirsByFoldersConfig()
+			dirlist.sortDirsByFoldersSortConfig()
+			dirlist.Invalidate()
 		}
 	case *types.DirectoryInfo:
 		dir := dirlist.Directory(msg.Info.Name)

widgets/dirtree.go
@@ -55,6 +55,7 @@ func (dt *DirectoryTree) Update(msg types.WorkerMessage) {
 		case *types.RemoveDirectory, *types.ListDirectories, *types.CreateDirectory:
 			dt.DirectoryList.Update(msg)
 			dt.buildTree()
+			dt.Invalidate()
 		default:
 			dt.DirectoryList.Update(msg)
 		}