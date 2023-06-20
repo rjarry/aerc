rjarry/aerc

commit 42cd415756bff13acd306db21eff5f4439665270

commands: execute commands with templates
Execute template code before running a command.

With this, we can use templates in our keybinds like:

[messages]
E = :'{{if match (.Flags|join "") "O"}}envelope{{else}}view{{end}}'<Enter>

Signed-off-by: Koni Marti <koni.marti@gmail.com>
Acked-by: Robin Jarry <robin@jarry.cc>

commands/commands.go
@@ -103,42 +103,76 @@ func templateData(
 	data.SetAccount(cfg)
 	data.SetFolder(folder)
 	data.SetInfo(msg, 0, false)
+	if acct != nil {
+		acct.SetStatus(func(s *state.AccountState, _ string) {
+			data.SetState(s)
+		})
+	}
 
 	return &data
 }
 
 func (cmds *Commands) ExecuteCommand(
 	aerc *widgets.Aerc,
-	args []string,
+	origArgs []string,
 	account *config.AccountConfig,
 	msg *models.MessageInfo,
 ) error {
-	if len(args) == 0 {
+	if len(origArgs) == 0 {
 		return errors.New("Expected a command.")
+	}
+	data := templateData(aerc, account, msg)
+	args, err := expand(data, origArgs)
+	if err != nil {
+		return err
 	}
 	if cmd, ok := cmds.dict()[args[0]]; ok {
 		log.Tracef("executing command %v", args)
-		var buf bytes.Buffer
-		data := templateData(aerc, account, msg)
-
-		processedArgs := make([]string, len(args))
-		for i, arg := range args {
-			t, err := templates.ParseTemplate(arg, arg)
-			if err != nil {
-				return err
-			}
-			err = templates.Render(t, &buf, data)
-			if err != nil {
-				return err
-			}
-			arg = buf.String()
-			buf.Reset()
-			processedArgs[i] = arg
-		}
-
-		return cmd.Execute(aerc, processedArgs)
+		return cmd.Execute(aerc, args)
 	}
 	return NoSuchCommand(args[0])
+}
+
+// expand expands template expressions and returns a new slice of arguments
+func expand(data models.TemplateData, origArgs []string) ([]string, error) {
+	args := make([]string, len(origArgs))
+	copy(args, origArgs)
+
+	c := strings.Join(origArgs, "")
+	isTemplate := strings.Contains(c, "{{") || strings.Contains(c, "}}")
+
+	if isTemplate {
+		for i := range args {
+			if strings.Contains(args[i], " ") {
+				q := "\""
+				if strings.ContainsAny(args[i], "\"") {
+					q = "'"
+				}
+				args[i] = q + args[i] + q
+			}
+		}
+
+		cmdline := strings.Join(args, " ")
+		log.Tracef("template data found in: %v", cmdline)
+
+		t, err := templates.ParseTemplate("execute", cmdline)
+		if err != nil {
+			return nil, err
+		}
+
+		var buf bytes.Buffer
+		err = templates.Render(t, &buf, data)
+		if err != nil {
+			return nil, err
+		}
+
+		args, err = splitCmd(buf.String())
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return args, nil
 }
 
 func GetTemplateCompletion(

commands/commands_test.go
@@ -1,0 +1,54 @@
+package commands
+
+import (
+	"reflect"
+	"testing"
+
+	"git.sr.ht/~rjarry/aerc/lib/state"
+)
+
+func TestExecuteCommand_expand(t *testing.T) {
+	tests := []struct {
+		args []string
+		want []string
+	}{
+		{
+			args: []string{"prompt", "Really quit? ", "quit"},
+			want: []string{"prompt", "Really quit? ", "quit"},
+		},
+		{
+			args: []string{"{{", "print", "\"hello\"", "}}"},
+			want: []string{"hello"},
+		},
+		{
+			args: []string{"prompt", "Really quit  ? ", "  quit "},
+			want: []string{"prompt", "Really quit  ? ", "  quit "},
+		},
+		{
+			args: []string{
+				"prompt", "Really quit? ", "{{",
+				"print", "\"quit\"", "}}",
+			},
+			want: []string{"prompt", "Really quit? ", "quit"},
+		},
+		{
+			args: []string{
+				"prompt", "Really quit? ", "{{",
+				"if", "1", "}}", "quit", "{{end}}",
+			},
+			want: []string{"prompt", "Really quit? ", "quit"},
+		},
+	}
+
+	data := state.TemplateData{}
+
+	for i, test := range tests {
+		got, err := expand(&data, test.args)
+		if err != nil {
+			t.Errorf("test %d failed with err: %v", i, err)
+		} else if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("test %d failed: "+
+				"got: %v, but want: %v", i, got, test.want)
+		}
+	}
+}

commit 709eafda087f1f4779265e54484906c92b23b3be

commands: fix panic in template execution
Fix a panic in the template execution when the template evalutes to nil.
Check for length of returned arguments after the template code is
expanded.

To reproduce, run :{{exec "cat > /dev/null" .MessageId}}

Fixes: 42cd4157 ("commands: execute commands with templates")
Signed-off-by: Koni Marti <koni.marti@gmail.com>
Acked-by: Robin Jarry <robin@jarry.cc>

commands/commands.go
@@ -125,6 +125,9 @@ func (cmds *Commands) ExecuteCommand(
 	if err != nil {
 		return err
 	}
+	if len(args) == 0 {
+		return errors.New("Expected a command after template evaluation.")
+	}
 	if cmd, ok := cmds.dict()[args[0]]; ok {
 		log.Tracef("executing command %v", args)
 		return cmd.Execute(aerc, args)