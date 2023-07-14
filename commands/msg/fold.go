94b1c778dbe691a75b0d918e9c40cc34f8523768
Koni Marti <koni.marti@gmail.com>
2023-07-14 20:37:31 +0200

    commands: add :fold and :unfold for thread folding
    Add the :fold and :unfold commands to perform thread folding. They only
    work on the selected message. Add vim-like bindings.
    
    Fixes: https://todo.sr.ht/~rjarry/aerc/148
    Signed-off-by: Koni Marti <koni.marti@gmail.com>
    Signed-off-by: Robin Jarry <robin@jarry.cc>
    Tested-by: inwit <inwit@sindominio.net>

---
+++ commands/msg/fold.go
@@ -0,0 +1,49 @@
+package msg
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"git.sr.ht/~rjarry/aerc/lib/ui"
+	"git.sr.ht/~rjarry/aerc/widgets"
+)
+
+type Fold struct{}
+
+func init() {
+	register(Fold{})
+}
+
+func (Fold) Aliases() []string {
+	return []string{"fold", "unfold"}
+}
+
+func (Fold) Complete(aerc *widgets.Aerc, args []string) []string {
+	return nil
+}
+
+func (Fold) Execute(aerc *widgets.Aerc, args []string) error {
+	if len(args) != 1 {
+		return fmt.Errorf("Usage: %s", args[0])
+	}
+	h := newHelper(aerc)
+	store, err := h.store()
+	if err != nil {
+		return err
+	}
+
+	msg := store.Selected()
+	if msg == nil {
+		return errors.New("No message selected")
+	}
+
+	switch strings.ToLower(args[0]) {
+	case "fold":
+		err = store.Fold(msg.Uid)
+	case "unfold":
+		err = store.Unfold(msg.Uid)
+	}
+	ui.Invalidate()
+	return err
+}
