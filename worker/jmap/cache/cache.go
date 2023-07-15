rjarry/aerc

commit be0bfc1ae28b49be6546626ff9eaadce5464a6c8

worker: add jmap support
Add support for JMAP backends. This is on par with IMAP features with
some additions specific to JMAP:

* tagging
* sending emails

This makes use of git.sr.ht/~rockorager/go-jmap for the low level
interaction with the JMAP server. The transport is JSON over HTTPS.

For now, only oauthbearer with token is supported. If this proves
useful, we may need to file for an official three-legged oauth support
at JMAP providers.

I have tested most features and this seems to be reliable. There are
some quirks with the use-labels option. Especially when moving and
deleting messages from the "All mail" virtual folder (see aerc-jmap(5)).

Overall, the user experience is nice and there are a lot less background
updates issues than with IMAP (damn IDLE mode hanging after restoring
from sleep).

I know that not everyone has access to a JMAP provider. For those
interested, there are at least these two commercial offerings:

  https://www.fastmail.com/
  https://www.topicbox.com/

And, if you host your own mail, you can use a JMAP capable server:

  https://stalw.art/jmap/
  https://www.cyrusimap.org/imap/download/installation/http/jmap.html

Link: https://www.rfc-editor.org/rfc/rfc8620.html
Link: https://www.rfc-editor.org/rfc/rfc8621.html
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Tim Culverhouse <tim@timculverhouse.com>

worker/jmap/cache/cache.go
@@ -1,0 +1,79 @@
+package cache
+
+import (
+	"errors"
+	"os"
+	"path"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/syndtr/goleveldb/leveldb"
+)
+
+type JMAPCache struct {
+	mem      map[string][]byte
+	file     *leveldb.DB
+	blobsDir string
+}
+
+func NewJMAPCache(state, blobs bool, accountName string) (*JMAPCache, error) {
+	c := new(JMAPCache)
+	cacheDir, err := os.UserCacheDir()
+	if err != nil {
+		cacheDir, err = homedir.Expand("~/.cache")
+		if err != nil {
+			return nil, err
+		}
+	}
+	if state {
+		dir := path.Join(cacheDir, "aerc", accountName, "state")
+		_ = os.MkdirAll(dir, 0o700)
+		c.file, err = leveldb.OpenFile(dir, nil)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		c.mem = make(map[string][]byte)
+	}
+	if blobs {
+		c.blobsDir = path.Join(cacheDir, "aerc", accountName, "blobs")
+	}
+	return c, nil
+}
+
+var notfound = errors.New("key not found")
+
+func (c *JMAPCache) get(key string) ([]byte, error) {
+	switch {
+	case c.file != nil:
+		return c.file.Get([]byte(key), nil)
+	case c.mem != nil:
+		value, ok := c.mem[key]
+		if !ok {
+			return nil, notfound
+		}
+		return value, nil
+	}
+	panic("jmap cache with no backend")
+}
+
+func (c *JMAPCache) put(key string, value []byte) error {
+	switch {
+	case c.file != nil:
+		return c.file.Put([]byte(key), value, nil)
+	case c.mem != nil:
+		c.mem[key] = value
+		return nil
+	}
+	panic("jmap cache with no backend")
+}
+
+func (c *JMAPCache) delete(key string) error {
+	switch {
+	case c.file != nil:
+		return c.file.Delete([]byte(key), nil)
+	case c.mem != nil:
+		delete(c.mem, key)
+		return nil
+	}
+	panic("jmap cache with no backend")
+}

worker/jmap/configure.go
@@ -1,0 +1,70 @@
+package jmap
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+	"time"
+
+	"git.sr.ht/~rjarry/aerc/worker/jmap/cache"
+	"git.sr.ht/~rjarry/aerc/worker/types"
+)
+
+func (w *JMAPWorker) handleConfigure(msg *types.Configure) error {
+	u, err := url.Parse(msg.Config.Source)
+	if err != nil {
+		return err
+	}
+
+	if strings.HasSuffix(u.Scheme, "+oauthbearer") {
+		w.config.oauth = true
+	} else {
+		if u.User == nil {
+			return fmt.Errorf("user:password not specified")
+		} else if u.User.Username() == "" {
+			return fmt.Errorf("username not specified")
+		} else if _, ok := u.User.Password(); !ok {
+			return fmt.Errorf("password not specified")
+		}
+	}
+
+	u.RawQuery = ""
+	u.Fragment = ""
+	w.config.user = u.User
+	u.User = nil
+	u.Scheme = "https"
+
+	w.config.endpoint = u.String()
+	w.config.account = msg.Config
+	w.config.cacheState = parseBool(msg.Config.Params["cache-state"])
+	w.config.cacheBlobs = parseBool(msg.Config.Params["cache-blobs"])
+	w.config.useLabels = parseBool(msg.Config.Params["use-labels"])
+	w.config.allMail = msg.Config.Params["all-mail"]
+	if w.config.allMail == "" {
+		w.config.allMail = "All mail"
+	}
+	if ping, ok := msg.Config.Params["server-ping"]; ok {
+		dur, err := time.ParseDuration(ping)
+		if err != nil {
+			return fmt.Errorf("server-ping: %w", err)
+		}
+		w.config.serverPing = dur
+	}
+
+	c, err := cache.NewJMAPCache(
+		w.config.cacheState, w.config.cacheBlobs, msg.Config.Name)
+	if err != nil {
+		return err
+	}
+	w.cache = c
+
+	return nil
+}
+
+func parseBool(val string) bool {
+	switch strings.ToLower(val) {
+	case "1", "t", "true", "yes", "y", "on":
+		return true
+	}
+	return false
+}

Makefile
@@ -30,6 +30,7 @@ DOCS := \
 	aerc-binds.5 \
 	aerc-config.5 \
 	aerc-imap.5 \
+	aerc-jmap.5 \
 	aerc-maildir.5 \
 	aerc-sendmail.5 \
 	aerc-notmuch.5 \
@@ -127,6 +128,7 @@ install: $(DOCS) aerc wrap colorize
 	install -m644 aerc-binds.5 $(DESTDIR)$(MANDIR)/man5/aerc-binds.5
 	install -m644 aerc-config.5 $(DESTDIR)$(MANDIR)/man5/aerc-config.5
 	install -m644 aerc-imap.5 $(DESTDIR)$(MANDIR)/man5/aerc-imap.5
+	install -m644 aerc-jmap.5 $(DESTDIR)$(MANDIR)/man5/aerc-jmap.5
 	install -m644 aerc-maildir.5 $(DESTDIR)$(MANDIR)/man5/aerc-maildir.5
 	install -m644 aerc-sendmail.5 $(DESTDIR)$(MANDIR)/man5/aerc-sendmail.5
 	install -m644 aerc-notmuch.5 $(DESTDIR)$(MANDIR)/man5/aerc-notmuch.5
@@ -164,6 +166,7 @@ checkinstall:
 	test -e $(DESTDIR)$(MANDIR)/man5/aerc-binds.5
 	test -e $(DESTDIR)$(MANDIR)/man5/aerc-config.5
 	test -e $(DESTDIR)$(MANDIR)/man5/aerc-imap.5
+	test -e $(DESTDIR)$(MANDIR)/man5/aerc-jmap.5
 	test -e $(DESTDIR)$(MANDIR)/man5/aerc-notmuch.5
 	test -e $(DESTDIR)$(MANDIR)/man5/aerc-sendmail.5
 	test -e $(DESTDIR)$(MANDIR)/man5/aerc-smtp.5
@@ -181,6 +184,7 @@ uninstall:
 	$(RM) $(DESTDIR)$(MANDIR)/man5/aerc-binds.5
 	$(RM) $(DESTDIR)$(MANDIR)/man5/aerc-config.5
 	$(RM) $(DESTDIR)$(MANDIR)/man5/aerc-imap.5
+	$(RM) $(DESTDIR)$(MANDIR)/man5/aerc-jmap.5
 	$(RM) $(DESTDIR)$(MANDIR)/man5/aerc-maildir.5
 	$(RM) $(DESTDIR)$(MANDIR)/man5/aerc-sendmail.5
 	$(RM) $(DESTDIR)$(MANDIR)/man5/aerc-notmuch.5

commands/account/rmdir.go
@@ -80,6 +80,7 @@ func (RemoveDir) Execute(aerc *widgets.Aerc, args []string) error {
 
 	acct.Worker().PostAction(&types.RemoveDirectory{
 		Directory: curDir,
+		Quiet:     force,
 	}, func(msg types.WorkerMessage) {
 		switch msg := msg.(type) {
 		case *types.Done:

commands/compose/send.go
@@ -174,6 +174,8 @@ func send(aerc *widgets.Aerc, composer *widgets.Composer, ctx sendCtx,
 			fallthrough
 		case "smtps":
 			sender, err = newSmtpSender(ctx)
+		case "jmap":
+			sender, err = newJmapSender(composer, header, ctx)
 		case "":
 			sender, err = newSendmailSender(ctx)
 		default:
@@ -186,7 +188,7 @@ func send(aerc *widgets.Aerc, composer *widgets.Composer, ctx sendCtx,
 
 		var writer io.Writer = sender
 
-		if config.CopyTo != "" {
+		if config.CopyTo != "" && ctx.scheme != "jmap" {
 			writer = io.MultiWriter(writer, &copyBuf)
 		}
 		err = composer.WriteMessage(header, writer)
@@ -210,7 +212,7 @@ func send(aerc *widgets.Aerc, composer *widgets.Composer, ctx sendCtx,
 			aerc.NewTab(composer, tabName)
 			return
 		}
-		if config.CopyTo != "" {
+		if config.CopyTo != "" && ctx.scheme != "jmap" {
 			aerc.PushStatus("Copying to "+config.CopyTo, 10*time.Second)
 			errch := copyToSent(composer.Worker(), config.CopyTo,
 				copyBuf.Len(), &copyBuf)
@@ -510,6 +512,36 @@ func connectSmtps(host string) (*smtp.Client, error) {
 		return nil, errors.Wrap(err, "smtp.DialTLS")
 	}
 	return conn, nil
+}
+
+func newJmapSender(
+	composer *widgets.Composer, header *mail.Header, ctx sendCtx,
+) (io.WriteCloser, error) {
+	var writer io.WriteCloser
+	done := make(chan error)
+
+	composer.Worker().PostAction(
+		&types.StartSendingMessage{Header: header},
+		func(msg types.WorkerMessage) {
+			switch msg := msg.(type) {
+			case *types.Done:
+				return
+			case *types.Unsupported:
+				done <- fmt.Errorf("unsupported by worker")
+			case *types.Error:
+				done <- msg.Error
+			case *types.MessageWriter:
+				writer = msg.Writer
+			default:
+				done <- fmt.Errorf("unexpected worker message: %#v", msg)
+			}
+			close(done)
+		},
+	)
+
+	err := <-done
+
+	return writer, err
 }
 
 func copyToSent(worker *types.Worker, dest string,

commands/help.go
@@ -14,6 +14,7 @@ var pages = []string{
 	"binds",
 	"config",
 	"imap",
+	"jmap",
 	"notmuch",
 	"search",
 	"sendmail",

doc/aerc-accounts.5.scd
@@ -173,6 +173,7 @@ Note that many of these configuration options are written for you, such as
 	See each protocol's man page for more details:
 
 	- *aerc-imap*(5)
+	- *aerc-jmap*(5)
 	- *aerc-maildir*(5)
 	- *aerc-notmuch*(5)
 
@@ -212,8 +213,8 @@ Note that many of these configuration options are written for you, such as
 
 # SEE ALSO
 
-*aerc*(1) *aerc-config*(5) *aerc-imap*(5) *aerc-maildir*(5) *aerc-notmuch*(5)
-*aerc-sendmail*(5) *aerc-smtp*(5)
+*aerc*(1) *aerc-config*(5) *aerc-imap*(5) *aerc-jmap*(5) *aerc-maildir*(5)
+*aerc-notmuch*(5) *aerc-sendmail*(5) *aerc-smtp*(5)
 
 # AUTHORS
 

doc/aerc-config.5.scd
@@ -924,9 +924,9 @@ These options are configured in the *[templates]* section of _aerc.conf_.
 
 # SEE ALSO
 
-*aerc*(1) *aerc-accounts*(5) *aerc-binds*(5) *aerc-imap*(5) *aerc-maildir*(5)
-*aerc-notmuch*(5) *aerc-templates*(7) *aerc-sendmail*(5) *aerc-smtp*(5)
-*aerc-stylesets*(7) *carddav-query*(1)
+*aerc*(1) *aerc-accounts*(5) *aerc-binds*(5) *aerc-imap*(5) *aerc-jmap*(5)
+*aerc-maildir*(5) *aerc-notmuch*(5) *aerc-templates*(7) *aerc-sendmail*(5)
+*aerc-smtp*(5) *aerc-stylesets*(7) *carddav-query*(1)
 
 # AUTHORS
 

commit 7a674312b6f006be7c1241b10cf9063841aa1d9c

jmap: fix crash when opening multiple instances
Fix the following error when opening another aerc instance with the same
jmap account:

panic: runtime error: invalid memory address or nil pointer dereference
[signal SIGSEGV: segmentation violation code=0x1 addr=0x8 pc=0x9a1ffd]

git.sr.ht/~rjarry/aerc/worker/jmap/cache.(*JMAPCache).get(0x99d08e?, {0xbc3c1a?, 0xc00003a160?})
	git.sr.ht/~rjarry/aerc/worker/jmap/cache/cache.go:47 +0x1d
git.sr.ht/~rjarry/aerc/worker/jmap/cache.(*JMAPCache).GetSession(0xc00052a030?)
	git.sr.ht/~rjarry/aerc/worker/jmap/cache/session.go:8 +0x29
git.sr.ht/~rjarry/aerc/worker/jmap.(*JMAPWorker).handleConnect(0xc00055e180, 0x0?)
	git.sr.ht/~rjarry/aerc/worker/jmap/connect.go:29 +0xd3
git.sr.ht/~rjarry/aerc/worker/jmap.(*JMAPWorker).handleMessage(0xc000311500?, {0xcc8b00?, 0xc0001fcff0?})
	git.sr.ht/~rjarry/aerc/worker/jmap/worker.go:114 +0x9f
git.sr.ht/~rjarry/aerc/worker/jmap.(*JMAPWorker).Run(0xc00055e180)
	git.sr.ht/~rjarry/aerc/worker/jmap/worker.go:177 +0x10c
git.sr.ht/~rjarry/aerc/widgets.NewAccountView.func3()
	git.sr.ht/~rjarry/aerc/widgets/account.go:110 +0x65
created by git.sr.ht/~rjarry/aerc/widgets.NewAccountView
	git.sr.ht/~rjarry/aerc/widgets/account.go:103 +0x518

Do not return an error if the leveldb cannot be opened, log a message
and fallback on the in-memory cache.

Fixes: be0bfc1ae28b ("worker: add jmap support")
Reported-by: Tim Culverhouse <tim@timculverhouse.com>
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Tim Culverhouse <tim@timculverhouse.com>

worker/jmap/cache/cache.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path"
 
+	"git.sr.ht/~rjarry/aerc/log"
 	"github.com/mitchellh/go-homedir"
 	"github.com/syndtr/goleveldb/leveldb"
 )
@@ -15,29 +16,31 @@ type JMAPCache struct {
 	blobsDir string
 }
 
-func NewJMAPCache(state, blobs bool, accountName string) (*JMAPCache, error) {
+func NewJMAPCache(state, blobs bool, accountName string) *JMAPCache {
 	c := new(JMAPCache)
 	cacheDir, err := os.UserCacheDir()
 	if err != nil {
 		cacheDir, err = homedir.Expand("~/.cache")
 		if err != nil {
-			return nil, err
+			log.Errorf("homedir.Expand: %s", err)
+			cacheDir = ""
 		}
 	}
-	if state {
+	if state && cacheDir != "" {
 		dir := path.Join(cacheDir, "aerc", accountName, "state")
 		_ = os.MkdirAll(dir, 0o700)
 		c.file, err = leveldb.OpenFile(dir, nil)
 		if err != nil {
-			return nil, err
+			log.Errorf("failed to open goleveldb: %s", err)
+			c.mem = make(map[string][]byte)
 		}
 	} else {
 		c.mem = make(map[string][]byte)
 	}
-	if blobs {
+	if blobs && cacheDir != "" {
 		c.blobsDir = path.Join(cacheDir, "aerc", accountName, "blobs")
 	}
-	return c, nil
+	return c
 }
 
 var notfound = errors.New("key not found")

worker/jmap/configure.go
@@ -11,6 +11,12 @@ import (
 )
 
 func (w *JMAPWorker) handleConfigure(msg *types.Configure) error {
+	w.config.cacheState = parseBool(msg.Config.Params["cache-state"])
+	w.config.cacheBlobs = parseBool(msg.Config.Params["cache-blobs"])
+	w.config.useLabels = parseBool(msg.Config.Params["use-labels"])
+	w.cache = cache.NewJMAPCache(
+		w.config.cacheState, w.config.cacheBlobs, msg.Config.Name)
+
 	u, err := url.Parse(msg.Config.Source)
 	if err != nil {
 		return err
@@ -36,9 +42,6 @@ func (w *JMAPWorker) handleConfigure(msg *types.Configure) error {
 
 	w.config.endpoint = u.String()
 	w.config.account = msg.Config
-	w.config.cacheState = parseBool(msg.Config.Params["cache-state"])
-	w.config.cacheBlobs = parseBool(msg.Config.Params["cache-blobs"])
-	w.config.useLabels = parseBool(msg.Config.Params["use-labels"])
 	w.config.allMail = msg.Config.Params["all-mail"]
 	if w.config.allMail == "" {
 		w.config.allMail = "All mail"
@@ -50,13 +53,6 @@ func (w *JMAPWorker) handleConfigure(msg *types.Configure) error {
 		}
 		w.config.serverPing = dur
 	}
-
-	c, err := cache.NewJMAPCache(
-		w.config.cacheState, w.config.cacheBlobs, msg.Config.Name)
-	if err != nil {
-		return err
-	}
-	w.cache = c
 
 	return nil
 }