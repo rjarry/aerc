rjarry/aerc

commit 40cc540357d92aee07fba2819801e168d3bf0f33

reply: allow to override localized Re regexp in configuration
My corporate email server annoyingly adds an "[External] " prefix when
delivering emails from outside my organization. I'd like to be able to
automatically strip it from the subject line when replying to external
emails.

With this patch, I can achieve it by setting this line in my account
configuration:

subject-re-pattern = ^(\[External\] : )?((?i)((AW|RE|SV|VS|ODP|R): ?)+)

Signed-off-by: jp39 <jp39@gmx.com>
Acked-by: Moritz Poldrack <moritz@poldrack.dev>

config/config.go
@@ -175,6 +175,7 @@ type AccountConfig struct {
 	FoldersSort       []string `ini:"folders-sort" delim:","`
 	AddressBookCmd    string   `ini:"address-book-cmd"`
 	SendAsUTC         bool     `ini:"send-as-utc"`
+	LocalizedRe       *regexp.Regexp
 
 	// CheckMail
 	CheckMail        time.Duration `ini:"check-mail"`
@@ -313,6 +314,8 @@ func loadAccountConfig(path string, accts []string) ([]AccountConfig, error) {
 			Params:            make(map[string]string),
 			EnableFoldersSort: true,
 			CheckMailTimeout:  10 * time.Second,
+			// localizedRe contains a list of known translations for the common Re:
+			LocalizedRe: regexp.MustCompile(`(?i)^((AW|RE|SV|VS|ODP|R): ?)+`),
 		}
 		if err = sec.MapTo(&account); err != nil {
 			return nil, err
@@ -360,6 +363,13 @@ func loadAccountConfig(path string, accts []string) ([]AccountConfig, error) {
 				account.PgpOpportunisticEncrypt, _ = strconv.ParseBool(val)
 			case "address-book-cmd":
 				account.AddressBookCmd = val
+			case "subject-re-pattern":
+				re, err := regexp.Compile(val)
+				if err != nil {
+					return nil, fmt.Errorf(
+						"%s=%s %w", key, val, err)
+				}
+				account.LocalizedRe = re
 			default:
 				if key != "name" {
 					account.Params[key] = val

commands/msg/invite.go
@@ -48,7 +48,7 @@ func (invite) Execute(aerc *widgets.Aerc, args []string) error {
 		return fmt.Errorf("no invitation found (missing text/calendar)")
 	}
 
-	subject := trimLocalizedRe(msg.Envelope.Subject)
+	subject := trimLocalizedRe(msg.Envelope.Subject, acct.AccountConfig().LocalizedRe)
 	switch args[0] {
 	case "accept":
 		subject = "Accepted: " + subject

commands/msg/reply.go
@@ -158,7 +158,7 @@ func (reply) Execute(aerc *widgets.Aerc, args []string) error {
 		recSet.AddList(cc)
 	}
 
-	subject := "Re: " + trimLocalizedRe(msg.Envelope.Subject)
+	subject := "Re: " + trimLocalizedRe(msg.Envelope.Subject, conf.LocalizedRe)
 
 	h := &mail.Header{}
 	h.SetAddressList("to", to)
@@ -331,9 +331,6 @@ func addMimeType(msg *models.MessageInfo, part []int,
 }
 
 // trimLocalizedRe removes known localizations of Re: commonly used by Outlook.
-func trimLocalizedRe(subject string) string {
+func trimLocalizedRe(subject string, localizedRe *regexp.Regexp) string {
 	return strings.TrimPrefix(subject, localizedRe.FindString(subject))
 }
-
-// localizedRe contains a list of known translations for the common Re:
-var localizedRe = regexp.MustCompile(`(?i)^((AW|RE|SV|VS|ODP|R): ?)+`)

doc/aerc-config.5.scd
@@ -838,6 +838,16 @@ Note that many of these configuration options are written for you, such as
 	expressions. If you want to trust any host (e.g. for debugging),
 	use the wildcard \*.
 
+*subject-re-pattern*
+	When replying to a message, this is the regular expression that will
+	be used to match the prefix of the original message's subject that has
+	to be removed, to create the subject line of the new message.
+	Typically, this will be used to avoid a repetition of the "Re: "
+	prefix in the subject header. The default will match known
+	translations for the common "Re: ".
+
+	Default: (?i)^((AW|RE|SV|VS|ODP|R): ?)+
+
 # BINDS.CONF
 
 This file is used for configuring keybindings used in the aerc interactive

commit 8dcb58c81782a30f06794227cf9a1b54928134e3

wizard: properly initialize configuration
The wizard constructs an AccountConfig object by hand without
initializing default values. This causes a crash when replying to an
email (rr) just after completing the account creation:

~/.config/aerc/aerc.conf not found, installing the system default~/.config/aerc/binds.conf not found, installing the system defaultpanic: runtime error: invalid memory address or nil pointer dereference [recovered]
         panic: runtime error: invalid memory address or nil pointer dereference
[signal SIGSEGV: segmentation violation code=0x1 addr=0x90 pc=0x5d1550]

...
regexp.(*Regexp).doExecute(0x7fc1089aa108?, {0x0?, 0x0?}, {0x0?, 0x0?, 0xc000fe5408?}, {0xc000698120?, 0x10?}, 0xad3a40?, 0x2, ...)
         regexp/exec.go:527 +0x90
regexp.(*Regexp).FindString(0x44e372?, {0xc000698120, 0x27})
         regexp/regexp.go:852 +0x6c
git.sr.ht/~rjarry/aerc/commands/msg.trimLocalizedRe({0xc000698120, 0x27}, 0xc00003e420?)
         git.sr.ht/~rjarry/aerc/commands/msg/reply.go:332 +0x36
git.sr.ht/~rjarry/aerc/commands/msg.reply.Execute({}, 0xc0002ba180, {0xc0002f8800?, 0x2, 0x2})
         git.sr.ht/~rjarry/aerc/commands/msg/reply.go:157 +0x765
...

Extract the account parsing and initialization into a function. Reuse
that function in both accounts.conf parsing and the wizard.

Fixes: 40cc540357d9 ("reply: allow to override localized Re regexp in configuration")
Reported-by: Mechiel Lukkien <mechiel@ueber.net>
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Andrew Yu <andrew@andrewyu.org>

config/accounts.go
@@ -146,26 +146,10 @@ func parseAccounts(root string, accts []string) error {
 			continue
 		}
 		sec := file.Section(_sec)
-		account := AccountConfig{
-			Name:   _sec,
-			Params: make(map[string]string),
-		}
-		if err = MapToStruct(sec, &account, true); err != nil {
+
+		account, err := ParseAccountConfig(_sec, sec)
+		if err != nil {
 			return err
-		}
-		for key, val := range sec.KeysHash() {
-			backendSpecific := true
-			typ := reflect.TypeOf(account)
-			for i := 0; i < typ.NumField(); i++ {
-				field := typ.Field(i)
-				if field.Tag.Get("ini") == key {
-					backendSpecific = false
-					break
-				}
-			}
-			if backendSpecific {
-				account.Params[key] = val
-			}
 		}
 		if _, ok := account.Params["smtp-starttls"]; ok && !starttls_warned {
 			Warnings = append(Warnings, Warning{
@@ -178,31 +162,9 @@ If you want to disable STARTTLS, append +insecure to the schema.
 			})
 			starttls_warned = true
 		}
-		if account.Source == "" {
-			return fmt.Errorf("Expected source for account %s", _sec)
-		}
-		if account.From == nil {
-			return fmt.Errorf("Expected from for account %s", _sec)
-		}
-		if len(account.Headers) > 0 {
-			defaults := []string{
-				"date",
-				"subject",
-				"from",
-				"sender",
-				"reply-to",
-				"to",
-				"cc",
-				"bcc",
-				"in-reply-to",
-				"message-id",
-				"references",
-			}
-			account.Headers = append(account.Headers, defaults...)
-		}
 
 		log.Debugf("accounts.conf: [%s] from = %s", account.Name, account.From)
-		Accounts = append(Accounts, &account)
+		Accounts = append(Accounts, account)
 	}
 	if len(accts) > 0 {
 		// Sort accounts struct to match the specified order, if we
@@ -216,6 +178,53 @@ If you want to disable STARTTLS, append +insecure to the schema.
 	}
 
 	return nil
+}
+
+func ParseAccountConfig(name string, section *ini.Section) (*AccountConfig, error) {
+	account := AccountConfig{
+		Name:   name,
+		Params: make(map[string]string),
+	}
+	if err := MapToStruct(section, &account, true); err != nil {
+		return nil, err
+	}
+	for key, val := range section.KeysHash() {
+		backendSpecific := true
+		typ := reflect.TypeOf(account)
+		for i := 0; i < typ.NumField(); i++ {
+			field := typ.Field(i)
+			if field.Tag.Get("ini") == key {
+				backendSpecific = false
+				break
+			}
+		}
+		if backendSpecific {
+			account.Params[key] = val
+		}
+	}
+	if account.Source == "" {
+		return nil, fmt.Errorf("Expected source for account %s", name)
+	}
+	if account.From == nil {
+		return nil, fmt.Errorf("Expected from for account %s", name)
+	}
+	if len(account.Headers) > 0 {
+		defaults := []string{
+			"date",
+			"subject",
+			"from",
+			"sender",
+			"reply-to",
+			"to",
+			"cc",
+			"bcc",
+			"in-reply-to",
+			"message-id",
+			"references",
+		}
+		account.Headers = append(account.Headers, defaults...)
+	}
+	return &account, nil
 }
 
 func (a *AccountConfig) ParseSource(sec *ini.Section, key *ini.Key) (string, error) {

config/binds.go
@@ -101,7 +101,7 @@ var Binds = defaultBindsConfig()
 func parseBinds(root string) error {
 	filename := path.Join(root, "binds.conf")
 	if _, err := os.Stat(filename); errors.Is(err, os.ErrNotExist) {
-		fmt.Printf("%s not found, installing the system default", filename)
+		fmt.Printf("%s not found, installing the system default\n", filename)
 		if err := installTemplate(root, "binds.conf"); err != nil {
 			return err
 		}

config/config.go
@@ -98,7 +98,7 @@ func LoadConfigFromFile(root *string, accts []string) error {
 
 	// if it doesn't exist copy over the template, then load
 	if _, err := os.Stat(filename); errors.Is(err, os.ErrNotExist) {
-		fmt.Printf("%s not found, installing the system default", filename)
+		fmt.Printf("%s not found, installing the system default\n", filename)
 		if err := installTemplate(*root, "aerc.conf"); err != nil {
 			return err
 		}

widgets/account-wizard.go
@@ -13,7 +13,6 @@ import (
 	"sync"
 	"time"
 
-	"github.com/emersion/go-message/mail"
 	"github.com/gdamore/tcell/v2"
 	"github.com/go-ini/ini"
 	"github.com/kyoh86/xdg"
@@ -531,25 +530,14 @@ func (wizard *AccountWizard) finish(tutorial bool) {
 		}
 	}
 
-	from, err := mail.ParseAddress(sec.Key("from").String())
+	account, err := config.ParseAccountConfig(sec.Name(), sec)
 	if err != nil {
 		wizard.errorFor(nil, err)
 		return
 	}
+	config.Accounts = append(config.Accounts, account)
 
-	account := config.AccountConfig{
-		Name:     sec.Name(),
-		Default:  "INBOX",
-		From:     from,
-		Source:   sec.Key("source").String(),
-		Outgoing: config.RemoteConfig{Value: sec.Key("outgoing").String()},
-	}
-	if wizard.copySent {
-		account.CopyTo = "Sent"
-	}
-	config.Accounts = append(config.Accounts, &account)
-
-	view, err := NewAccountView(wizard.aerc, &account, wizard.aerc, nil)
+	view, err := NewAccountView(wizard.aerc, account, wizard.aerc, nil)
 	if err != nil {
 		wizard.aerc.NewTab(errorScreen(err.Error()), account.Name)
 		return