rjarry/aerc

commit 801caf812377a5f818aed1b5677c0d667db6b6df

compose: factorize body read in a method
In preparation for allowing headers in the text editor, put code related
to reading the email body in a method and use it where needed.

Never close the email temp file, always keep it opened in read-write
mode. This will be also required to update embedded header values when
running :edit after modifying email headers from the compose::review
screen.

Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Bence Ferdinandy <bence@ferdinandy.com>
Tested-by: Koni Marti <koni.marti@gmail.com>

widgets/compose.go
@@ -604,6 +604,19 @@ func ensureSignatureDelimiter(signature []byte) []byte {
 	return []byte(sig)
 }
 
+func (c *Composer) GetBody() (*bytes.Buffer, error) {
+	_, err := c.email.Seek(0, io.SeekStart)
+	if err != nil {
+		return nil, err
+	}
+	buf := new(bytes.Buffer)
+	_, err = io.Copy(buf, c.email)
+	if err != nil {
+		return nil, err
+	}
+	return buf, nil
+}
+
 func (c *Composer) FocusTerminal() *Composer {
 	c.Lock()
 	defer c.Unlock()
@@ -813,10 +826,6 @@ func (c *Composer) Signer() (string, error) {
 }
 
 func (c *Composer) WriteMessage(header *mail.Header, writer io.Writer) error {
-	if err := c.reloadEmail(); err != nil {
-		return err
-	}
-
 	if c.sign || c.encrypt {
 
 		var signedHeader mail.Header
@@ -876,19 +885,13 @@ func (c *Composer) ShouldWarnAttachment() bool {
 		return false
 	}
 
-	err := c.reloadEmail()
+	body, err := c.GetBody()
 	if err != nil {
-		log.Warnf("failed to check for a forgotten attachment (reloadEmail): %v", err)
+		log.Warnf("failed to check for a forgotten attachment: %v", err)
 		return true
 	}
 
-	body, err := io.ReadAll(c.email)
-	if err != nil {
-		log.Warnf("failed to check for a forgotten attachment (io.ReadAll): %v", err)
-		return true
-	}
-
-	return regex.Match(body)
+	return regex.Match(body.Bytes())
 }
 
 func (c *Composer) ShouldWarnSubject() bool {
@@ -906,16 +909,20 @@ func writeMsgImpl(c *Composer, header *mail.Header, writer io.Writer) error {
 	if config.Compose.FormatFlowed {
 		mimeParams["Format"] = "Flowed"
 	}
+	body, err := c.GetBody()
+	if err != nil {
+		return err
+	}
 	if len(c.attachments) == 0 && len(c.textParts) == 0 {
 		// no attachments
-		return writeInlineBody(header, c.email, writer, mimeParams)
+		return writeInlineBody(header, body, writer, mimeParams)
 	} else {
 		// with attachments
 		w, err := mail.CreateWriter(writer, *header)
 		if err != nil {
 			return errors.Wrap(err, "CreateWriter")
 		}
-		newPart, err := lib.NewPart("text/plain", mimeParams, c.email)
+		newPart, err := lib.NewPart("text/plain", mimeParams, body)
 		if err != nil {
 			return err
 		}
@@ -1200,17 +1207,6 @@ func (c *Composer) updateGrid() {
 	c.grid.AddChild(ui.NewFill(borderChar, borderStyle)).At(2, 0)
 }
 
-func (c *Composer) reloadEmail() error {
-	name := c.email.Name()
-	c.email.Close()
-	file, err := os.Open(name)
-	if err != nil {
-		return errors.Wrap(err, "ReloadEmail")
-	}
-	c.email = file
-	return nil
-}
-
 type headerEditor struct {
 	name     string
 	header   *mail.Header
@@ -1485,16 +1481,12 @@ func (c *Composer) updateMultipart(p *lib.Part) error {
 	}
 	// reset part body to avoid it leaving outdated if the command fails
 	p.Data = nil
-	err := c.reloadEmail()
+	body, err := c.GetBody()
 	if err != nil {
-		return errors.Wrap(err, "reloadEmail")
-	}
-	body, err := io.ReadAll(c.email)
-	if err != nil {
-		return errors.Wrap(err, "io.ReadAll")
+		return errors.Wrap(err, "GetBody")
 	}
 	cmd := exec.Command("sh", "-c", command)
-	cmd.Stdin = bytes.NewReader(body)
+	cmd.Stdin = body
 	out, err := cmd.Output()
 	if err != nil {
 		var stderr string

commit e32cf3d478e746e1a83a1289e47399087fe1fda3

composer: reopen the email file when editor is closed
Some text editors save changes in a temp file before moving it over to
the destination instead of rewriting the original file in place. This
protects against all sorts of corruption, like writing partial files in
disk-full scenarios for example. When this is the case, aerc's open file
descriptor points to a different file, one that was never written to by
the editor.

For text editors that *do* overwrite the file (i.e. the same filesystem
inode id), depending on timing, kernel version, operating system,
filesystem options, caching, etc. Reading from aerc's file descriptor
after the text editor has modified the same file on disk may not return
the updated data. This behaviour can be easily observed with the
following C program:

	#include <fcntl.h>
	#include <stdio.h>
	#include <stdlib.h>
	#include <string.h>
	#include <unistd.h>

	int main(void) {
		int fd;
		ssize_t n;
		char buf[BUFSIZ];

		unlink("test.txt");
		fd = open("test.txt", O_CREAT|O_RDWR, 0644);
		write(fd, "Bonjour\n", strlen("Bonjour\n"));
		fsync(fd);

		lseek(fd, 0, SEEK_SET);
		n = read(fd, buf, sizeof(buf));
		buf[n] = '\0';
		printf("before: %s\n", buf);

		system("vim test.txt");

		lseek(fd, 0, SEEK_SET);
		n = read(fd, buf, sizeof(buf));
		buf[n] = '\0';
		printf("after: %s\n", buf);

		return 0;
	}

Most of the time, before and after texts will be identical even if you
modified the test.txt file contents in the editor.

The only reliable way to ensure to read the latest contents from the
disk is to close and reopen the file after the text editor has exited.

Do exactly that in termClosed before anything else.

Fixes: 801caf812377 ("compose: factorize body read in a method")
Fixes: https://todo.sr.ht/~rjarry/aerc/184
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Ronald Evers <ronald@ch10.nl>

widgets/compose.go
@@ -1144,11 +1144,25 @@ func (c *Composer) termEvent(event tcell.Event) bool {
 	return false
 }
 
+func (c *Composer) reopenEmailFile() error {
+	name := c.email.Name()
+	f, err := os.OpenFile(name, os.O_RDWR, 0o600)
+	if err != nil {
+		return err
+	}
+	err = c.email.Close()
+	c.email = f
+	return err
+}
+
 func (c *Composer) termClosed(err error) {
 	c.Lock()
 	defer c.Unlock()
 	if c.editor == nil {
 		return
+	}
+	if e := c.reopenEmailFile(); e != nil {
+		c.aerc.PushError("Failed to reopen email file: " + e.Error())
 	}
 	editor := c.editor
 	defer editor.Destroy()