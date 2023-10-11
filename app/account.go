rjarry/aerc

commit bc176bd61ba726351a489cabf4da16a47dc5ec3b

app: export global functions
The single Aerc object is passed around in almost all command functions.
This hinders readability.

Store the single Aerc instance as a global variable. Export public
functions from the app package to access methods of that object. Remove
all explicit references to *app.Aerc and replace them with calls to
these functions. For references to private/unexported fields and
functions from within the app package, directly access the global aerc
object.

Signed-off-by: Robin Jarry <robin@jarry.cc>
Acked-by: Moritz Poldrack <moritz@poldrack.dev>

app/account.go
@@ -28,7 +28,6 @@ var _ ProvidesMessages = (*AccountView)(nil)
 type AccountView struct {
 	sync.Mutex
 	acct    *config.AccountConfig
-	aerc    *Aerc
 	dirlist DirectoryLister
 	labels  []string
 	grid    *ui.Grid
@@ -58,15 +57,12 @@ func (acct *AccountView) UiConfig() *config.UIConfig {
 }
 
 func NewAccountView(
-	aerc *Aerc, acct *config.AccountConfig,
-	host TabHost, deferLoop chan struct{},
+	acct *config.AccountConfig, deferLoop chan struct{},
 ) (*AccountView, error) {
 	acctUiConf := config.Ui.ForAccount(acct.Name)
 
 	view := &AccountView{
 		acct:   acct,
-		aerc:   aerc,
-		host:   host,
 		uiConf: acctUiConf,
 	}
 
@@ -81,7 +77,7 @@ func NewAccountView(
 
 	worker, err := worker.NewWorker(acct.Source, acct.Name)
 	if err != nil {
-		host.SetError(fmt.Sprintf("%s: %s", acct.Name, err))
+		SetError(fmt.Sprintf("%s: %s", acct.Name, err))
 		log.Errorf("%s: %v", acct.Name, err)
 		return view, err
 	}
@@ -92,7 +88,7 @@ func NewAccountView(
 		view.grid.AddChild(ui.NewBordered(view.dirlist, ui.BORDER_RIGHT, acctUiConf))
 	}
 
-	view.msglist = NewMessageList(aerc, view)
+	view.msglist = NewMessageList(view)
 	view.grid.AddChild(view.msglist).At(0, 1)
 
 	view.dirlist.OnVirtualNode(func() {
@@ -134,15 +130,15 @@ func (acct *AccountView) UpdateStatus() {
 }
 
 func (acct *AccountView) PushStatus(status string, expiry time.Duration) {
-	acct.aerc.PushStatus(fmt.Sprintf("%s: %s", acct.acct.Name, status), expiry)
+	PushStatus(fmt.Sprintf("%s: %s", acct.acct.Name, status), expiry)
 }
 
 func (acct *AccountView) PushError(err error) {
-	acct.aerc.PushError(fmt.Sprintf("%s: %v", acct.acct.Name, err))
+	PushError(fmt.Sprintf("%s: %v", acct.acct.Name, err))
 }
 
 func (acct *AccountView) PushWarning(warning string) {
-	acct.aerc.PushWarning(fmt.Sprintf("%s: %s", acct.acct.Name, warning))
+	PushWarning(fmt.Sprintf("%s: %s", acct.acct.Name, warning))
 }
 
 func (acct *AccountView) AccountConfig() *config.AccountConfig {
@@ -226,7 +222,7 @@ func (acct *AccountView) SelectedMessagePart() *PartInfo {
 }
 
 func (acct *AccountView) isSelected() bool {
-	return acct == acct.aerc.SelectedAccount()
+	return acct == SelectedAccount()
 }
 
 func (acct *AccountView) newStore(name string) *lib.MessageStore {
@@ -247,7 +243,7 @@ func (acct *AccountView) newStore(name string) *lib.MessageStore {
 			})
 			if err != nil {
 				msg := fmt.Sprintf("mail-received hook: %s", err)
-				acct.aerc.PushError(msg)
+				PushError(msg)
 			}
 		}, func() {
 			if uiConf.NewMessageBell {
@@ -544,10 +540,10 @@ func (acct *AccountView) updateSplitView(msg *models.MessageInfo) {
 			acct.grid.RemoveChild(acct.split)
 			acct.split.Close()
 		}
-		lib.NewMessageStoreView(msg, false, acct.Store(), acct.aerc.Crypto, acct.aerc.DecryptKeys,
+		lib.NewMessageStoreView(msg, false, acct.Store(), CryptoProvider(), DecryptKeys,
 			func(view lib.MessageView, err error) {
 				if err != nil {
-					acct.aerc.PushError(err.Error())
+					PushError(err.Error())
 					return
 				}
 				acct.split = NewMessageViewer(acct, view)

app/aerc.go
@@ -87,10 +87,8 @@ func NewAerc(
 		Crypto:     crypto,
 	}
 
-	statusline.SetAerc(aerc)
-
 	for _, acct := range config.Accounts {
-		view, err := NewAccountView(aerc, acct, aerc, deferLoop)
+		view, err := NewAccountView(acct, deferLoop)
 		if err != nil {
 			tabs.Add(errorScreen(err.Error()), acct.Name, nil)
 		} else {
@@ -100,7 +98,7 @@ func NewAerc(
 	}
 
 	if len(config.Accounts) == 0 {
-		wizard := NewAccountWizard(aerc)
+		wizard := NewAccountWizard()
 		wizard.Focus(true)
 		aerc.NewTab(wizard, "New account")
 	}
@@ -732,7 +730,7 @@ func (aerc *Aerc) Mailto(addr *url.URL) error {
 
 	defer ui.Invalidate()
 
-	composer, err := NewComposer(aerc, acct,
+	composer, err := NewComposer(acct,
 		acct.AccountConfig(), acct.Worker(),
 		config.Compose.EditHeaders, template, h, nil,
 		strings.NewReader(body))
@@ -775,7 +773,7 @@ func (aerc *Aerc) Mbox(source string) error {
 
 	defer ui.Invalidate()
 
-	mboxView, err := NewAccountView(aerc, &acctConf, aerc, nil)
+	mboxView, err := NewAccountView(&acctConf, nil)
 	if err != nil {
 		aerc.NewTab(errorScreen(err.Error()), acctConf.Name)
 	} else {
@@ -886,9 +884,9 @@ func (aerc *Aerc) isExKey(event *tcell.EventKey, exKey config.KeyStroke) bool {
 	return event.Modifiers() == exKey.Modifiers && event.Key() == exKey.Key
 }
 
-// CmdFallbackSearch checks cmds for the first executable availabe in PATH. An error is
+// cmdFallbackSearch checks cmds for the first executable availabe in PATH. An error is
 // returned if none are found
-func (aerc *Aerc) CmdFallbackSearch(cmds []string) (string, error) {
+func cmdFallbackSearch(cmds []string) (string, error) {
 	var tried []string
 	for _, cmd := range cmds {
 		if cmd == "" {
@@ -899,7 +897,7 @@ func (aerc *Aerc) CmdFallbackSearch(cmds []string) (string, error) {
 		if err != nil {
 			tried = append(tried, cmd)
 			warn := fmt.Sprintf("cmd '%s' not found in PATH, using fallback", cmd)
-			aerc.PushWarning(warn)
+			PushWarning(warn)
 			continue
 		}
 		return cmd, nil

app/app.go
@@ -1,0 +1,80 @@
+package app
+
+import (
+	"time"
+
+	"git.sr.ht/~rjarry/aerc/config"
+	"git.sr.ht/~rjarry/aerc/lib"
+	"git.sr.ht/~rjarry/aerc/lib/crypto"
+	"git.sr.ht/~rjarry/aerc/lib/ipc"
+	"git.sr.ht/~rjarry/aerc/lib/ui"
+	"git.sr.ht/~rjarry/aerc/models"
+	"git.sr.ht/~rjarry/aerc/worker/types"
+	"github.com/ProtonMail/go-crypto/openpgp"
+)
+
+var aerc *Aerc
+
+func Init(
+	crypto crypto.Provider,
+	cmd func([]string, *config.AccountConfig, *models.MessageInfo) error,
+	complete func(cmd string) ([]string, string), history lib.History,
+	deferLoop chan struct{},
+) {
+	aerc = NewAerc(crypto, cmd, complete, history, deferLoop)
+}
+
+func Drawable() ui.DrawableInteractive      { return aerc }
+func IPCHandler() ipc.Handler               { return aerc }
+func HandleMessage(msg types.WorkerMessage) { aerc.HandleMessage(msg) }
+
+func CloseBackends() error { return aerc.CloseBackends() }
+
+func AddDialog(d ui.DrawableInteractive) { aerc.AddDialog(d) }
+func CloseDialog()                       { aerc.CloseDialog() }
+
+func HumanReadableBindings() []string {
+	return aerc.HumanReadableBindings()
+}
+
+func Account(name string) (*AccountView, error) { return aerc.Account(name) }
+func AccountNames() []string                    { return aerc.AccountNames() }
+func NextAccount() (*AccountView, error)        { return aerc.NextAccount() }
+func PrevAccount() (*AccountView, error)        { return aerc.PrevAccount() }
+func SelectedAccount() *AccountView             { return aerc.SelectedAccount() }
+func SelectedAccountUiConfig() *config.UIConfig { return aerc.SelectedAccountUiConfig() }
+
+func NextTab()                                          { aerc.NextTab() }
+func PrevTab()                                          { aerc.PrevTab() }
+func PinTab()                                           { aerc.PinTab() }
+func UnpinTab()                                         { aerc.UnpinTab() }
+func MoveTab(i int, relative bool)                      { aerc.MoveTab(i, relative) }
+func TabNames() []string                                { return aerc.TabNames() }
+func SelectTab(name string) bool                        { return aerc.SelectTab(name) }
+func SelectPreviousTab() bool                           { return aerc.SelectPreviousTab() }
+func SelectedTab() *ui.Tab                              { return aerc.SelectedTab() }
+func SelectedTabContent() ui.Drawable                   { return aerc.SelectedTabContent() }
+func SelectTabIndex(index int) bool                     { return aerc.SelectTabIndex(index) }
+func RemoveTab(tab ui.Drawable, closeContent bool)      { aerc.RemoveTab(tab, closeContent) }
+func NewTab(clickable ui.Drawable, name string) *ui.Tab { return aerc.NewTab(clickable, name) }
+func ReplaceTab(tabSrc ui.Drawable, tabTarget ui.Drawable, name string, closeSrc bool) {
+	aerc.ReplaceTab(tabSrc, tabTarget, name, closeSrc)
+}
+
+func UpdateStatus()                          { aerc.UpdateStatus() }
+func PushPrompt(prompt *ExLine)              { aerc.PushPrompt(prompt) }
+func SetError(text string)                   { aerc.SetError(text) }
+func PushError(text string) *StatusMessage   { return aerc.PushError(text) }
+func PushWarning(text string) *StatusMessage { return aerc.PushWarning(text) }
+func PushSuccess(text string) *StatusMessage { return aerc.PushSuccess(text) }
+func PushStatus(text string, expiry time.Duration) *StatusMessage {
+	return aerc.PushStatus(text, expiry)
+}
+
+func RegisterChoices(choices []Choice)           { aerc.RegisterChoices(choices) }
+func RegisterPrompt(prompt string, cmd []string) { aerc.RegisterPrompt(prompt, cmd) }
+
+func CryptoProvider() crypto.Provider { return aerc.Crypto }
+func DecryptKeys(keys []openpgp.Key, symmetric bool) (b []byte, err error) {
+	return aerc.DecryptKeys(keys, symmetric)
+}

app/msglist.go
@@ -27,14 +27,12 @@ type MessageList struct {
 	spinner       *Spinner
 	store         *lib.MessageStore
 	isInitalizing bool
-	aerc          *Aerc
 }
 
-func NewMessageList(aerc *Aerc, account *AccountView) *MessageList {
+func NewMessageList(account *AccountView) *MessageList {
 	ml := &MessageList{
 		spinner:       NewSpinner(account.uiConf),
 		isInitalizing: true,
-		aerc:          aerc,
 	}
 	// TODO: stop spinner, probably
 	ml.spinner.Start()
@@ -56,11 +54,11 @@ type messageRowParams struct {
 func (ml *MessageList) Draw(ctx *ui.Context) {
 	ml.height = ctx.Height()
 	ml.width = ctx.Width()
-	uiConfig := ml.aerc.SelectedAccountUiConfig()
+	uiConfig := SelectedAccountUiConfig()
 	ctx.Fill(0, 0, ctx.Width(), ctx.Height(), ' ',
 		uiConfig.GetStyle(config.STYLE_MSGLIST_DEFAULT))
 
-	acct := ml.aerc.SelectedAccount()
+	acct := SelectedAccount()
 	store := ml.Store()
 	if store == nil || acct == nil || len(store.Uids()) == 0 {
 		if ml.isInitalizing {
@@ -242,7 +240,7 @@ func addMessage(
 }
 
 func (ml *MessageList) drawScrollbar(ctx *ui.Context) {
-	uiConfig := ml.aerc.SelectedAccountUiConfig()
+	uiConfig := SelectedAccountUiConfig()
 	gutterStyle := uiConfig.GetStyle(config.STYLE_MSGLIST_GUTTER)
 	pillStyle := uiConfig.GetStyle(config.STYLE_MSGLIST_PILL)
 
@@ -259,13 +257,13 @@ func (ml *MessageList) MouseEvent(localX int, localY int, event tcell.Event) {
 	if event, ok := event.(*tcell.EventMouse); ok {
 		switch event.Buttons() {
 		case tcell.Button1:
-			if ml.aerc == nil {
+			if aerc == nil {
 				return
 			}
 			selectedMsg, ok := ml.Clicked(localX, localY)
 			if ok {
 				ml.Select(selectedMsg)
-				acct := ml.aerc.SelectedAccount()
+				acct := SelectedAccount()
 				if acct == nil || acct.Messages().Empty() {
 					return
 				}
@@ -275,14 +273,14 @@ func (ml *MessageList) MouseEvent(localX int, localY int, event tcell.Event) {
 					return
 				}
 				lib.NewMessageStoreView(msg, acct.UiConfig().AutoMarkRead,
-					store, ml.aerc.Crypto, ml.aerc.DecryptKeys,
+					store, CryptoProvider(), DecryptKeys,
 					func(view lib.MessageView, err error) {
 						if err != nil {
-							ml.aerc.PushError(err.Error())
+							PushError(err.Error())
 							return
 						}
 						viewer := NewMessageViewer(acct, view)
-						ml.aerc.NewTab(viewer, msg.Envelope.Subject)
+						NewTab(viewer, msg.Envelope.Subject)
 					})
 			}
 		case tcell.WheelDown:
@@ -391,7 +389,7 @@ func (ml *MessageList) Select(index int) {
 }
 
 func (ml *MessageList) drawEmptyMessage(ctx *ui.Context) {
-	uiConfig := ml.aerc.SelectedAccountUiConfig()
+	uiConfig := SelectedAccountUiConfig()
 	msg := uiConfig.EmptyMessage
 	ctx.Printf((ctx.Width()/2)-(len(msg)/2), 0,
 		uiConfig.GetStyle(config.STYLE_MSGLIST_DEFAULT), "%s", msg)

app/status.go
@@ -18,7 +18,6 @@ import (
 type StatusLine struct {
 	sync.Mutex
 	stack []*StatusMessage
-	aerc  *Aerc
 	acct  *AccountView
 	err   string
 }
@@ -48,9 +47,9 @@ func (status *StatusLine) Draw(ctx *ui.Context) {
 		msg = runewidth.FillRight(msg, ctx.Width())
 		style := status.uiConfig().GetStyle(config.STYLE_STATUSLINE_ERROR)
 		ctx.Printf(0, 0, style, "%s", msg)
-	case status.aerc != nil && status.acct != nil:
+	case aerc != nil && status.acct != nil:
 		data := state.NewDataSetter()
-		data.SetPendingKeys(status.aerc.pendingKeys)
+		data.SetPendingKeys(aerc.pendingKeys)
 		data.SetState(&status.acct.state)
 		data.SetAccount(status.acct.acct)
 		data.SetFolder(status.acct.Directories().SelectedDirectory())
@@ -154,11 +153,7 @@ func (status *StatusLine) Expire() {
 }
 
 func (status *StatusLine) uiConfig() *config.UIConfig {
-	return status.aerc.SelectedAccountUiConfig()
-}
-
-func (status *StatusLine) SetAerc(aerc *Aerc) {
-	status.aerc = aerc
+	return SelectedAccountUiConfig()
 }
 
 func (msg *StatusMessage) Color(style tcell.Style) {

commit 34650131379a4542538da63751a89588d2f2cc85

app: fix nil pointer dereference on startup
Fix the following crash on startup:

panic: runtime error: invalid memory address or nil pointer dereference
[signal SIGSEGV: segmentation violation code=0x1 addr=0x80 pc=0x9e2314]

goroutine 1 [running]:
git.sr.ht/~rjarry/aerc/log.PanicHandler()
        git.sr.ht/~rjarry/aerc/log/panic-logger.go:51 +0x70f
panic({0xae95a0, 0x119f9b0})
        runtime/panic.go:890 +0x263
git.sr.ht/~rjarry/aerc/app.(*Aerc).SelectedAccount(0x8503cdd28?)
        git.sr.ht/~rjarry/aerc/app/aerc.go:384 +0x14
git.sr.ht/~rjarry/aerc/app.SelectedAccount(...)
        git.sr.ht/~rjarry/aerc/app/app.go:44
git.sr.ht/~rjarry/aerc/app.(*AccountView).isSelected(...)
        git.sr.ht/~rjarry/aerc/app/account.go:225
git.sr.ht/~rjarry/aerc/app.(*AccountView).UpdateStatus(0x850364380)
        git.sr.ht/~rjarry/aerc/app/account.go:127 +0x28
git.sr.ht/~rjarry/aerc/app.(*AccountView).SetStatus(0x850364380, {0x850243a50, 0x1,
0x0?})
        git.sr.ht/~rjarry/aerc/app/account.go:123 +0x94
git.sr.ht/~rjarry/aerc/app.NewAccountView(0x8503d38c0, 0x85041bf80)
        git.sr.ht/~rjarry/aerc/app/account.go:111 +0x573
git.sr.ht/~rjarry/aerc/app.NewAerc({0xcab0c0?, 0x11fa3c8}, 0x850433860, 0xbf3040, {0xca75e8?, 0x11ca800}, 0x0?)
        git.sr.ht/~rjarry/aerc/app/aerc.go:91 +0x6ce
git.sr.ht/~rjarry/aerc/app.Init(...)
        git.sr.ht/~rjarry/aerc/app/app.go:24
main.main()
        git.sr.ht/~rjarry/aerc/main.go:242 +0x52e

There was two things very wrong:

- Access of the global aerc pointer before it was initialized.
- The host field of AccountView was left there and still accessed but
  never initialized.

Replace the global aerc pointer with a real struct value. Update code
accordingly.

Remove the AccountView.host field which is now useless.

Reported-by: Jens Grassel <jens@wegtam.com>
Reported-by: Matěj Cepl <mcepl@cepl.eu>
Fixes: bc176bd61ba7 ("app: export global functions")
Signed-off-by: Robin Jarry <robin@jarry.cc>

app/account.go
@@ -31,7 +31,6 @@ type AccountView struct {
 	dirlist DirectoryLister
 	labels  []string
 	grid    *ui.Grid
-	host    TabHost
 	tab     *ui.Tab
 	msglist *MessageList
 	worker  *types.Worker
@@ -125,7 +124,7 @@ func (acct *AccountView) SetStatus(setters ...state.SetStateFunc) {
 
 func (acct *AccountView) UpdateStatus() {
 	if acct.isSelected() {
-		acct.host.UpdateStatus()
+		UpdateStatus()
 	}
 }
 
@@ -247,7 +246,7 @@ func (acct *AccountView) newStore(name string) *lib.MessageStore {
 			}
 		}, func() {
 			if uiConf.NewMessageBell {
-				acct.host.Beep()
+				aerc.Beep()
 			}
 		},
 		acct.updateSplitView,

app/aerc.go
@@ -51,12 +51,12 @@ type Choice struct {
 	Command []string
 }
 
-func NewAerc(
+func (aerc *Aerc) Init(
 	crypto crypto.Provider,
 	cmd func([]string, *config.AccountConfig, *models.MessageInfo) error,
 	complete func(cmd string) ([]string, string), cmdHistory lib.History,
 	deferLoop chan struct{},
-) *Aerc {
+) {
 	tabs := ui.NewTabs(config.Ui)
 
 	statusbar := ui.NewStack(config.Ui)
@@ -74,18 +74,16 @@ func NewAerc(
 	grid.AddChild(tabs.TabContent).At(1, 0)
 	grid.AddChild(statusbar).At(2, 0)
 
-	aerc := &Aerc{
-		accounts:   make(map[string]*AccountView),
-		cmd:        cmd,
-		cmdHistory: cmdHistory,
-		complete:   complete,
-		grid:       grid,
-		statusbar:  statusbar,
-		statusline: statusline,
-		prompts:    ui.NewStack(config.Ui),
-		tabs:       tabs,
-		Crypto:     crypto,
-	}
+	aerc.accounts = make(map[string]*AccountView)
+	aerc.cmd = cmd
+	aerc.cmdHistory = cmdHistory
+	aerc.complete = complete
+	aerc.grid = grid
+	aerc.statusbar = statusbar
+	aerc.statusline = statusline
+	aerc.prompts = ui.NewStack(config.Ui)
+	aerc.tabs = tabs
+	aerc.Crypto = crypto
 
 	for _, acct := range config.Accounts {
 		view, err := NewAccountView(acct, deferLoop)
@@ -121,8 +119,6 @@ func NewAerc(
 	}
 
 	aerc.showConfigWarnings()
-
-	return aerc
 }
 
 func (aerc *Aerc) showConfigWarnings() {

app/app.go
@@ -13,7 +13,7 @@ import (
 	"github.com/ProtonMail/go-crypto/openpgp"
 )
 
-var aerc *Aerc
+var aerc Aerc
 
 func Init(
 	crypto crypto.Provider,
@@ -21,11 +21,11 @@ func Init(
 	complete func(cmd string) ([]string, string), history lib.History,
 	deferLoop chan struct{},
 ) {
-	aerc = NewAerc(crypto, cmd, complete, history, deferLoop)
+	aerc.Init(crypto, cmd, complete, history, deferLoop)
 }
 
-func Drawable() ui.DrawableInteractive      { return aerc }
-func IPCHandler() ipc.Handler               { return aerc }
+func Drawable() ui.DrawableInteractive      { return &aerc }
+func IPCHandler() ipc.Handler               { return &aerc }
 func HandleMessage(msg types.WorkerMessage) { aerc.HandleMessage(msg) }
 
 func CloseBackends() error { return aerc.CloseBackends() }

app/msglist.go
@@ -257,9 +257,6 @@ func (ml *MessageList) MouseEvent(localX int, localY int, event tcell.Event) {
 	if event, ok := event.(*tcell.EventMouse); ok {
 		switch event.Buttons() {
 		case tcell.Button1:
-			if aerc == nil {
-				return
-			}
 			selectedMsg, ok := ml.Clicked(localX, localY)
 			if ok {
 				ml.Select(selectedMsg)

app/status.go
@@ -47,7 +47,7 @@ func (status *StatusLine) Draw(ctx *ui.Context) {
 		msg = runewidth.FillRight(msg, ctx.Width())
 		style := status.uiConfig().GetStyle(config.STYLE_STATUSLINE_ERROR)
 		ctx.Printf(0, 0, style, "%s", msg)
-	case aerc != nil && status.acct != nil:
+	case status.acct != nil:
 		data := state.NewDataSetter()
 		data.SetPendingKeys(aerc.pendingKeys)
 		data.SetState(&status.acct.state)

app/tabhost.go
@@ -1,15 +1,0 @@
-package app
-
-import (
-	"time"
-)
-
-type TabHost interface {
-	BeginExCommand(cmd string)
-	UpdateStatus()
-	SetError(err string)
-	PushStatus(text string, expiry time.Duration) *StatusMessage
-	PushError(text string) *StatusMessage
-	PushSuccess(text string) *StatusMessage
-	Beep()
-}