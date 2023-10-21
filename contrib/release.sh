rjarry/aerc

commit d179485eefe50da9d21abdd392cffd865e369509

release.sh: update version in GNUMakefile
The version is no longer present in Makefile which has been preserved
for explicit error reporting with non-GNU make versions.

Update the version in GNUMakefile instead.

Fixes: d725defa07b5 ("mk: deprecate BSD make in favor of GNU make")
Reported-by: Anton Mosich <anton@mosich.at>
Signed-off-by: Robin Jarry <robin@jarry.cc>
Acked-by: Moritz Poldrack <moritz@poldrack.dev>

contrib/release.sh
@@ -12,9 +12,9 @@ fi
 tag_url="https://git.sr.ht/~rjarry/aerc/refs/$next_tag"
 
 echo "======= Creating release commit..."
-sed -i Makefile -e "s/$prev_tag/$next_tag/g"
+sed -i GNUMakefile -e "s/$prev_tag/$next_tag/g"
 sed -i CHANGELOG.md -e "s|^## \[Unreleased\].*|&\n\n## [$next_tag]($tag_url) - $(date +%Y-%m-%d)|"
-git add Makefile CHANGELOG.md
+git add GNUMakefile CHANGELOG.md
 git commit -sm "Release version $next_tag"
 
 echo "======= Creating tag..."

.editorconfig
@@ -12,7 +12,7 @@ indent_style = tab
 max_line_length = 80
 tab_width = 8
 
-[Makefile]
+[*Makefile]
 indent_style = tab
 
 [**.scd]

GNUmakefile
@@ -1,6 +1,6 @@
 # variables that can be changed by users
 #
-VERSION ?= $(shell git describe --long --abbrev=12 --tags --dirty 2>/dev/null || echo 0.15.2)
+VERSION ?= $(shell git describe --long --abbrev=12 --tags --dirty 2>/dev/null || echo 0.16.0)
 PREFIX ?= /usr/local
 BINDIR ?= $(PREFIX)/bin
 SHAREDIR ?= $(PREFIX)/share/aerc

commit 6bbb477d441b17756f89e0034c26c4c3cdc941af

release.sh: fix typo
It is GNUmakefile, not GNUMakefile.

Fixes: d179485eefe5 ("release.sh: update version in GNUMakefile")
Signed-off-by: Robin Jarry <robin@jarry.cc>

contrib/release.sh
@@ -12,9 +12,9 @@ fi
 tag_url="https://git.sr.ht/~rjarry/aerc/refs/$next_tag"
 
 echo "======= Creating release commit..."
-sed -i GNUMakefile -e "s/$prev_tag/$next_tag/g"
+sed -i GNUmakefile -e "s/$prev_tag/$next_tag/g"
 sed -i CHANGELOG.md -e "s|^## \[Unreleased\].*|&\n\n## [$next_tag]($tag_url) - $(date +%Y-%m-%d)|"
-git add GNUMakefile CHANGELOG.md
+git add GNUmakefile CHANGELOG.md
 git commit -sm "Release version $next_tag"
 
 echo "======= Creating tag..."