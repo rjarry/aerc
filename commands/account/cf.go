rjarry/aerc

commit e54486ee40c9cedde9d4dae3e89d8b5f932188ee

commands: parse arguments with go-opt
Use the argument parsing framework introduced earlier to unify the
parsing of (almost) all command options. Remove custom parsing code and
to avoid extraneous types, add fields with `opt` tags on command structs
that have options and arguments. Commands that take no argument do not
need anything.

Since the command objects now carry data, create a new temporary
instance of them before passing them to opt.ArgsToStruct when executing
a command.

A few of the commands use specific semantics for parsing (:choose), or
are delegating argument parsing to another function (:sort, :search,
:filter). For these commands, simply add a dummy "-" passthrough
argument. Since all commands still have the argument list (after split)
nothing needs to be changed in this area.

There should be no functional change besides the Usage strings and
reported errors which are now generated automatically.

Signed-off-by: Robin Jarry <robin@jarry.cc>
Reviewed-by: Koni Marti <koni.marti@gmail.com>
Tested-by: Moritz Poldrack <moritz@poldrack.dev>
Tested-by: Inwit <inwit@sindominio.net>

commands/account/cf.go
@@ -2,7 +2,6 @@ package account
 
 import (
 	"errors"
-	"strings"
 
 	"git.sr.ht/~rjarry/aerc/app"
 	"git.sr.ht/~rjarry/aerc/commands"
@@ -11,7 +10,9 @@ import (
 
 var history map[string]string
 
-type ChangeFolder struct{}
+type ChangeFolder struct {
+	Folder string `opt:"..." metavar:"<folder>"`
+}
 
 func init() {
 	history = make(map[string]string)
@@ -26,24 +27,21 @@ func (ChangeFolder) Complete(args []string) []string {
 	return commands.GetFolders(args)
 }
 
-func (ChangeFolder) Execute(args []string) error {
-	if len(args) == 1 {
-		return errors.New("Usage: cf <folder>")
-	}
+func (c ChangeFolder) Execute(args []string) error {
 	acct := app.SelectedAccount()
 	if acct == nil {
 		return errors.New("No account selected")
 	}
 	previous := acct.Directories().Selected()
-	joinedArgs := strings.Join(args[1:], " ")
-	if joinedArgs == "-" {
+
+	if c.Folder == "-" {
 		if dir, ok := history[acct.Name()]; ok {
 			acct.Directories().Select(dir)
 		} else {
 			return errors.New("No previous folder to return to")
 		}
 	} else {
-		acct.Directories().Select(joinedArgs)
+		acct.Directories().Select(c.Folder)
 	}
 	history[acct.Name()] = previous
 

commit 6613d9b555beec211b2c30d7049bb75957faeee9

cf: fix unexpected argument on notmuch
Notmuch supports creating "dynamic" folders with the :cf command. When
the backend is notmuch, :cf must accept multiple arguments since it can
be a free-form notmuch query.

Detect if the backend is notmuch and build a quoted query based on the
provided arguments. Otherwise require a single argument.

Fixes: e54486ee40c9 ("commands: parse arguments with go-opt")
Reported-by: Inwit <inwit@sindominio.net>
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Inwit <inwit@sindominio.net>

commands/account/cf.go
@@ -2,16 +2,20 @@ package account
 
 import (
 	"errors"
+	"reflect"
 
 	"git.sr.ht/~rjarry/aerc/app"
 	"git.sr.ht/~rjarry/aerc/commands"
 	"git.sr.ht/~rjarry/aerc/lib/state"
+	"git.sr.ht/~rjarry/aerc/worker/handlers"
+	"git.sr.ht/~rjarry/aerc/worker/types"
+	"git.sr.ht/~rjarry/go-opt"
 )
 
 var history map[string]string
 
 type ChangeFolder struct {
-	Folder string `opt:"folder" complete:"CompleteFolder"`
+	Folder []string `opt:"..." complete:"CompleteFolder"`
 }
 
 func init() {
@@ -32,16 +36,30 @@ func (c ChangeFolder) Execute(args []string) error {
 	if acct == nil {
 		return errors.New("No account selected")
 	}
+
+	var target string
+
+	notmuch, _ := handlers.GetHandlerForScheme("notmuch", new(types.Worker))
+	switch {
+	case reflect.TypeOf(notmuch) == reflect.TypeOf(acct.Worker().Backend):
+		// notmuch query may have arguments that require quoting
+		target = opt.QuoteArgs(c.Folder...).String()
+	case len(c.Folder) == 1:
+		target = c.Folder[0]
+	default:
+		return errors.New("Unexpected argument(s). Usage: cf <folder>")
+	}
+
 	previous := acct.Directories().Selected()
 
-	if c.Folder == "-" {
+	if target == "-" {
 		if dir, ok := history[acct.Name()]; ok {
 			acct.Directories().Select(dir)
 		} else {
 			return errors.New("No previous folder to return to")
 		}
 	} else {
-		acct.Directories().Select(c.Folder)
+		acct.Directories().Select(target)
 	}
 	history[acct.Name()] = previous
 