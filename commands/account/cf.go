rjarry/aerc

commit 6613d9b555beec211b2c30d7049bb75957faeee9

cf: fix unexpected argument on notmuch
Notmuch supports creating "dynamic" folders with the :cf command. When
the backend is notmuch, :cf must accept multiple arguments since it can
be a free-form notmuch query.

Detect if the backend is notmuch and build a quoted query based on the
provided arguments. Otherwise require a single argument.

Fixes: e54486ee40c9 ("commands: parse arguments with go-opt")
Reported-by: Inwit <inwit@sindominio.net>
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Inwit <inwit@sindominio.net>

commands/account/cf.go
@@ -2,16 +2,20 @@ package account
 
 import (
 	"errors"
+	"reflect"
 
 	"git.sr.ht/~rjarry/aerc/app"
 	"git.sr.ht/~rjarry/aerc/commands"
 	"git.sr.ht/~rjarry/aerc/lib/state"
+	"git.sr.ht/~rjarry/aerc/worker/handlers"
+	"git.sr.ht/~rjarry/aerc/worker/types"
+	"git.sr.ht/~rjarry/go-opt"
 )
 
 var history map[string]string
 
 type ChangeFolder struct {
-	Folder string `opt:"folder" complete:"CompleteFolder"`
+	Folder []string `opt:"..." complete:"CompleteFolder"`
 }
 
 func init() {
@@ -32,16 +36,30 @@ func (c ChangeFolder) Execute(args []string) error {
 	if acct == nil {
 		return errors.New("No account selected")
 	}
+
+	var target string
+
+	notmuch, _ := handlers.GetHandlerForScheme("notmuch", new(types.Worker))
+	switch {
+	case reflect.TypeOf(notmuch) == reflect.TypeOf(acct.Worker().Backend):
+		// notmuch query may have arguments that require quoting
+		target = opt.QuoteArgs(c.Folder...).String()
+	case len(c.Folder) == 1:
+		target = c.Folder[0]
+	default:
+		return errors.New("Unexpected argument(s). Usage: cf <folder>")
+	}
+
 	previous := acct.Directories().Selected()
 
-	if c.Folder == "-" {
+	if target == "-" {
 		if dir, ok := history[acct.Name()]; ok {
 			acct.Directories().Select(dir)
 		} else {
 			return errors.New("No previous folder to return to")
 		}
 	} else {
-		acct.Directories().Select(c.Folder)
+		acct.Directories().Select(target)
 	}
 	history[acct.Name()] = previous
 

commit 213d65d00fb963c42cb3fa2b9b21c1613ec316a2

cf: fix over quoting of notmuch queries
Currently, :cf thread:\{id:{{.MessageId}}\} is broken because it is
quoted before being interpreted by notmuch. The dynamic folder is
created with this "query" (including the quotes):

	'thread:{id:23627381....}'

Notmuch queries use the xapian syntax and do not follow basic shell
quotes interpretation.

Change :cf only argument to preserve the command line as entered by the
user without any interpretation. When the backend is notmuch, forward
that as the dynamic folder name.

For other backends, interpret shell quoting on the user entry and fail
if it produces more than one argument.

Link: https://xapian.org/docs/queryparser.html
Fixes: 6613d9b555be ("cf: fix unexpected argument on notmuch")
Reported-by: Inwit <inwit@sindominio.net>
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Inwit <inwit@sindominio.net>

commands/account/cf.go
@@ -16,7 +16,7 @@ import (
 var history map[string]string
 
 type ChangeFolder struct {
-	Folder []string `opt:"..." complete:"CompleteFolder"`
+	Folder string `opt:"..." complete:"CompleteFolder"`
 }
 
 func init() {
@@ -54,14 +54,17 @@ func (c ChangeFolder) Execute(args []string) error {
 	var target string
 
 	notmuch, _ := handlers.GetHandlerForScheme("notmuch", new(types.Worker))
-	switch {
-	case reflect.TypeOf(notmuch) == reflect.TypeOf(acct.Worker().Backend):
-		// notmuch query may have arguments that require quoting
-		target = opt.QuoteArgs(c.Folder...).String()
-	case len(c.Folder) == 1:
-		target = c.Folder[0]
-	default:
-		return errors.New("Unexpected argument(s). Usage: cf <folder>")
+	if reflect.TypeOf(notmuch) == reflect.TypeOf(acct.Worker().Backend) {
+		// With notmuch, :cf can change to a "dynamic folder" that
+		// contains the result of a query. Preserve the entered
+		// arguments verbatim.
+		target = c.Folder
+	} else {
+		parts := opt.SplitArgs(c.Folder)
+		if len(parts) != 1 {
+			return errors.New("Unexpected argument(s). Usage: cf <folder>")
+		}
+		target = parts[0]
 	}
 
 	previous := acct.Directories().Selected()