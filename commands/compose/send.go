rjarry/aerc

commit faa879f9a84d44f9b251410fc923a827a44df1a7

hooks: add mail-added hook
The mail-added hook runs whenever a message is added to a folder.
Note that the hook does not run when a new message is received (the
mail-received hook already covers that) but instead runs whenever aerc
itself adds a message to a folder, e.g. when moving or copying a
message.

Changelog-added: `mail-added` hook that triggers when a message is added
 to a folder.
References: https://todo.sr.ht/~rjarry/aerc/136
Signed-off-by: Jason Cox <me@jasoncarloscox.com>
Acked-by: Robin Jarry <robin@jarry.cc>

commands/compose/send.go
@@ -228,8 +228,7 @@ func send(composer *app.Composer, ctx sendCtx,
 		}
 		if ctx.copyto != "" && ctx.scheme != "jmap" {
 			app.PushStatus("Copying to "+ctx.copyto, 10*time.Second)
-			errch := copyToSent(composer.Worker(), ctx.copyto,
-				copyBuf.Len(), &copyBuf)
+			errch := copyToSent(ctx.copyto, copyBuf.Len(), &copyBuf)
 			err = <-errch
 			if err != nil {
 				errmsg := fmt.Sprintf(
@@ -558,23 +557,32 @@ func newJmapSender(
 	return writer, err
 }
 
-func copyToSent(worker *types.Worker, dest string,
-	n int, msg io.Reader,
-) <-chan error {
-	errCh := make(chan error)
-	worker.PostAction(&types.AppendMessage{
-		Destination: dest,
-		Flags:       models.SeenFlag,
-		Date:        time.Now(),
-		Reader:      msg,
-		Length:      n,
-	}, func(msg types.WorkerMessage) {
-		switch msg := msg.(type) {
-		case *types.Done:
-			errCh <- nil
-		case *types.Error:
-			errCh <- msg.Error
-		}
-	})
+func copyToSent(dest string, n int, msg io.Reader) <-chan error {
+	errCh := make(chan error, 1)
+	acct := app.SelectedAccount()
+	if acct == nil {
+		errCh <- errors.New("No account selected")
+		return errCh
+	}
+	store := acct.Store()
+	if store == nil {
+		errCh <- errors.New("No message store selected")
+		return errCh
+	}
+	store.Append(
+		dest,
+		models.SeenFlag,
+		time.Now(),
+		msg,
+		n,
+		func(msg types.WorkerMessage) {
+			switch msg := msg.(type) {
+			case *types.Done:
+				errCh <- nil
+			case *types.Error:
+				errCh <- msg.Error
+			}
+		},
+	)
 	return errCh
 }

app/account.go
@@ -265,6 +265,15 @@ func (acct *AccountView) newStore(name string) *lib.MessageStore {
 				msg := fmt.Sprintf("mail-deleted hook: %s", err)
 				PushError(msg)
 			}
+		}, func(dest string) {
+			err := hooks.RunHook(&hooks.MailAdded{
+				Account: acct.Name(),
+				Folder:  dest,
+			})
+			if err != nil {
+				msg := fmt.Sprintf("mail-added hook: %s", err)
+				PushError(msg)
+			}
 		},
 		acct.updateSplitView,
 		acct.dirlist.UiConfig(name).ThreadContext,

commands/account/import-mbox.go
@@ -65,7 +65,6 @@ func (i ImportMbox) Execute(args []string) error {
 			app.PushError(err.Error())
 			return
 		}
-		worker := acct.Worker()
 
 		var appended uint32
 		for i, m := range messages {
@@ -79,27 +78,28 @@ func (i ImportMbox) Execute(args []string) error {
 					break
 				}
 				nbytes, _ := io.Copy(&buf, r)
-				worker.PostAction(&types.AppendMessage{
-					Destination: folder,
-					Flags:       models.SeenFlag,
-					Date:        time.Now(),
-					Reader:      &buf,
-					Length:      int(nbytes),
-				}, func(msg types.WorkerMessage) {
-					switch msg := msg.(type) {
-					case *types.Unsupported:
-						errMsg := fmt.Sprintf("%s: AppendMessage is unsupported", args[0])
-						log.Errorf(errMsg)
-						app.PushError(errMsg)
-						return
-					case *types.Error:
-						log.Errorf("AppendMessage failed: %v", msg.Error)
-						done <- false
-					case *types.Done:
-						atomic.AddUint32(&appended, 1)
-						done <- true
-					}
-				})
+				store.Append(
+					folder,
+					models.SeenFlag,
+					time.Now(),
+					&buf,
+					int(nbytes),
+					func(msg types.WorkerMessage) {
+						switch msg := msg.(type) {
+						case *types.Unsupported:
+							errMsg := fmt.Sprintf("%s: AppendMessage is unsupported", args[0])
+							log.Errorf(errMsg)
+							app.PushError(errMsg)
+							return
+						case *types.Error:
+							log.Errorf("AppendMessage failed: %v", msg.Error)
+							done <- false
+						case *types.Done:
+							atomic.AddUint32(&appended, 1)
+							done <- true
+						}
+					},
+				)
 
 				select {
 				case ok := <-done:

commands/compose/postpone.go
@@ -34,6 +34,10 @@ func (p Postpone) Execute(args []string) error {
 	if acct == nil {
 		return errors.New("No account selected")
 	}
+	store := acct.Store()
+	if store == nil {
+		return errors.New("No message store selected")
+	}
 	tab := app.SelectedTab()
 	if tab == nil {
 		return errors.New("No tab selected")
@@ -98,22 +102,23 @@ func (p Postpone) Execute(args []string) error {
 			handleErr(errors.Wrap(err, "WriteMessage"))
 			return
 		}
-		worker.PostAction(&types.AppendMessage{
-			Destination: targetFolder,
-			Flags:       models.SeenFlag,
-			Date:        time.Now(),
-			Reader:      buf,
-			Length:      buf.Len(),
-		}, func(msg types.WorkerMessage) {
-			switch msg := msg.(type) {
-			case *types.Done:
-				app.PushStatus("Message postponed.", 10*time.Second)
-				composer.SetPostponed()
-				composer.Close()
-			case *types.Error:
-				handleErr(msg.Error)
-			}
-		})
+		store.Append(
+			targetFolder,
+			models.SeenFlag,
+			time.Now(),
+			buf,
+			buf.Len(),
+			func(msg types.WorkerMessage) {
+				switch msg := msg.(type) {
+				case *types.Done:
+					app.PushStatus("Message postponed.", 10*time.Second)
+					composer.SetPostponed()
+					composer.Close()
+				case *types.Error:
+					handleErr(msg.Error)
+				}
+			},
+		)
 	}()
 
 	if !alreadyCreated {

config/aerc.conf
@@ -585,6 +585,10 @@ message/rfc822=colorize
 #mail-deleted=mbsync "$AERC_ACCOUNT:$AERC_FOLDER" &
 
 #
+# Executed when aerc adds mail to a folder
+#mail-added=mbsync "$AERC_ACCOUNT:$AERC_FOLDER" &
+
+#
 # Executed when aerc starts
 #aerc-startup=aerc :terminal calcurse && aerc :next-tab
 

config/hooks.go
@@ -12,6 +12,7 @@ type HooksConfig struct {
 	AercShutdown string `ini:"aerc-shutdown"`
 	MailReceived string `ini:"mail-received"`
 	MailDeleted  string `ini:"mail-deleted"`
+	MailAdded    string `ini:"mail-added"`
 }
 
 var Hooks HooksConfig

doc/aerc-config.5.scd
@@ -971,6 +971,21 @@ They are configured in the *[hooks]* section of aerc.conf.
 
 		*mail-deleted* = _mbsync "$AERC_ACCOUNT:$AERC_FOLDER"_
 
+*mail-added* = _<command>_
+	Executed when a message is added to a folder. Note that this hook is not
+	triggered when a new message is received (use *mail-received* for that) but
+	rather is only triggered when aerc itself adds a message to a folder, e.g.
+	when moving or copying a message.
+
+	Variables:
+
+	- *AERC_ACCOUNT*
+	- *AERC_FOLDER*
+
+	Example:
+
+		*mail-added* = _mbsync "$AERC_ACCOUNT:$AERC_FOLDER"_
+
 *aerc-shutdown* = _<command>_
 	Executed when aerc shuts down. Aerc will wait for the command to finish
 	before exiting.

lib/hooks/mail-added.go
@@ -1,0 +1,23 @@
+package hooks
+
+import (
+	"fmt"
+
+	"git.sr.ht/~rjarry/aerc/config"
+)
+
+type MailAdded struct {
+	Account string
+	Folder  string
+}
+
+func (m *MailAdded) Cmd() string {
+	return config.Hooks.MailAdded
+}
+
+func (m *MailAdded) Env() []string {
+	return []string{
+		fmt.Sprintf("AERC_ACCOUNT=%s", m.Account),
+		fmt.Sprintf("AERC_FOLDER=%s", m.Folder),
+	}
+}

commit 48b9589d8bbe4ec8982c08a9fe6e59b77734eb80

compose: copy to composing account's sent folder
The composing account may be different than the currently selected
account if the user has moved between different tabs since beginning the
message.

Fixes: faa879f9a84d ("hooks: add mail-added hook")
Reported-by: Vitaly Ovchinnikov <v@ovch.ru>
Signed-off-by: Jason Cox <me@jasoncarloscox.com>
Acked-by: Robin Jarry <robin@jarry.cc>
Tested-by: Vitaly Ovchinnikov <v@ovch.ru>

commands/compose/send.go
@@ -228,7 +228,8 @@ func send(composer *app.Composer, ctx sendCtx,
 		}
 		if ctx.copyto != "" && ctx.scheme != "jmap" {
 			app.PushStatus("Copying to "+ctx.copyto, 10*time.Second)
-			errch := copyToSent(ctx.copyto, copyBuf.Len(), &copyBuf)
+			errch := copyToSent(ctx.copyto, copyBuf.Len(), &copyBuf,
+				composer)
 			err = <-errch
 			if err != nil {
 				errmsg := fmt.Sprintf(
@@ -557,9 +558,9 @@ func newJmapSender(
 	return writer, err
 }
 
-func copyToSent(dest string, n int, msg io.Reader) <-chan error {
+func copyToSent(dest string, n int, msg io.Reader, composer *app.Composer) <-chan error {
 	errCh := make(chan error, 1)
-	acct := app.SelectedAccount()
+	acct := composer.Account()
 	if acct == nil {
 		errCh <- errors.New("No account selected")
 		return errCh