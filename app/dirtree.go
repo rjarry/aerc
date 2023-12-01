rjarry/aerc

commit 0be135a38186725e6414cb7c6945f93ae6e71ef9

dirtree: fix jumping folders
Fix jumping folders in the directory structure.

Fixes: a35d9bab ("rmdir: ensure proper sequence of operations")
Reported-by: inwit <inwit@sindominio.net>
Reported-by: Bence Ferdinandy <bence@ferdinandy.com>
Signed-off-by: Koni Marti <koni.marti@gmail.com>
Tested-by: Bence Ferdinandy <bence@ferdinandy.com>
Acked-by: Robin Jarry <robin@jarry.cc>

app/dirtree.go
@@ -76,13 +76,15 @@ func (dt *DirectoryTree) Update(msg types.WorkerMessage) {
 
 	case *types.Done:
 		switch resp := msg.InResponseTo().(type) {
-		case *types.RemoveDirectory, *types.ListDirectories, *types.CreateDirectory:
+		case *types.RemoveDirectory, *types.ListDirectories:
 			dt.DirectoryList.Update(msg)
 			dt.buildTree()
 			dt.Invalidate()
-		case *types.OpenDirectory:
-			dt.reindex(resp.Directory)
+		case *types.CreateDirectory:
 			dt.DirectoryList.Update(msg)
+			dt.buildTree()
+			dt.reindex(resp.Directory)
+			dt.Invalidate()
 		default:
 			dt.DirectoryList.Update(msg)
 		}

commit feafc433f08761290831e44fccf023badb5588aa

dirtree: prevent folder selection when archiving
Prevent an unwanted folder selection in the directory tree when
archiving.

A previous fix (commit 0be135a3) introduced a regression which caused
the directory tree to select the folder for which a CreateDirectory
message returned successfully.

Since the :archive command will issue a CreateDirectory message in every
operation, the directory tree will thus erroneously select the target
archive directory.

However, the functionality to select a newly created folder by the
:mkdir command is actually correct and should be kept since this
corresponds to the regular directory list behavior.

Another quirk that is addressed is caused by the rebuilding of the
directory tree when the underyling directories change. In that case, the
ui.DirListCollapse setting would overwrite the user changes. Therefore,
only apply the ui.DirListCollapse setting at the first time of building
the directory tree.

Fixes: 0be135a38186 ("dirtree: fix jumping folders")
Signed-off-by: Koni Marti <koni.marti@gmail.com>
Acked-by: Robin Jarry <robin@jarry.cc>
Tested-by: Bence Ferdinandy <bence@ferdinandy.com>

app/dirtree.go
@@ -72,18 +72,16 @@ func (dt *DirectoryTree) ClearList() {
 }
 
 func (dt *DirectoryTree) Update(msg types.WorkerMessage) {
+	selected := dt.Selected()
 	switch msg := msg.(type) {
-
 	case *types.Done:
-		switch resp := msg.InResponseTo().(type) {
-		case *types.RemoveDirectory, *types.ListDirectories:
+		switch msg.InResponseTo().(type) {
+		case *types.RemoveDirectory, *types.ListDirectories, *types.CreateDirectory:
 			dt.DirectoryList.Update(msg)
 			dt.buildTree()
-			dt.Invalidate()
-		case *types.CreateDirectory:
-			dt.DirectoryList.Update(msg)
-			dt.buildTree()
-			dt.reindex(resp.Directory)
+			if selected != "" {
+				dt.reindex(selected)
+			}
 			dt.Invalidate()
 		default:
 			dt.DirectoryList.Update(msg)
@@ -241,16 +239,31 @@ func (dt *DirectoryTree) Select(name string) {
 	if name == "" {
 		return
 	}
-	dt.reindex(name)
-	dt.DirectoryList.Select(name)
+	dt.Open(name, dt.UiConfig(name).DirListDelay, nil)
 }
 
 func (dt *DirectoryTree) Open(name string, delay time.Duration, cb func(types.WorkerMessage)) {
 	if name == "" {
 		return
 	}
-	dt.reindex(name)
-	dt.DirectoryList.Open(name, delay, cb)
+	again := false
+	if findString(dt.dirs, name) < 0 {
+		again = true
+	} else {
+		dt.reindex(name)
+	}
+	dt.DirectoryList.Open(name, delay, func(msg types.WorkerMessage) {
+		if cb != nil {
+			cb(msg)
+		}
+		if _, ok := msg.(*types.Done); ok && again {
+			if findString(dt.dirs, name) < 0 {
+				dt.dirs = append(dt.dirs, name)
+			}
+			dt.buildTree()
+			dt.reindex(name)
+		}
+	})
 }
 
 func (dt *DirectoryTree) NextPrev(delta int) {
@@ -483,7 +496,7 @@ func (dt *DirectoryTree) buildTreeNode(node *types.Thread, stree [][]string, def
 		}
 		nextNode := &types.Thread{Uid: uid}
 		node.AddChild(nextNode)
-		if dt.UiConfig(path).DirListCollapse != 0 {
+		if dt.UiConfig(path).DirListCollapse != 0 && dt.listIdx < 0 {
 			if depth > dt.UiConfig(path).DirListCollapse {
 				node.Hidden = 1
 			} else {