rjarry/aerc

commit 07c1cefcf62dbd094b6f97061c469264ee089898

patch: create sub-command structure
Create the sub-command structure for the :patch command. Make the :patch
command accessible from any context.

Signed-off-by: Koni Marti <koni.marti@gmail.com>
Acked-by: Robin Jarry <robin@jarry.cc>

main.go
@@ -22,6 +22,7 @@ import (
 	"git.sr.ht/~rjarry/aerc/commands/compose"
 	"git.sr.ht/~rjarry/aerc/commands/msg"
 	"git.sr.ht/~rjarry/aerc/commands/msgview"
+	"git.sr.ht/~rjarry/aerc/commands/patch"
 	"git.sr.ht/~rjarry/aerc/commands/terminal"
 	"git.sr.ht/~rjarry/aerc/config"
 	"git.sr.ht/~rjarry/aerc/lib/crypto"
@@ -41,25 +42,32 @@ func getCommands(selected ui.Drawable) []*commands.Commands {
 			account.AccountCommands,
 			msg.MessageCommands,
 			commands.GlobalCommands,
+			patch.PatchCommands,
 		}
 	case *app.Composer:
 		return []*commands.Commands{
 			compose.ComposeCommands,
 			commands.GlobalCommands,
+			patch.PatchCommands,
 		}
 	case *app.MessageViewer:
 		return []*commands.Commands{
 			msgview.MessageViewCommands,
 			msg.MessageCommands,
 			commands.GlobalCommands,
+			patch.PatchCommands,
 		}
 	case *app.Terminal:
 		return []*commands.Commands{
 			terminal.TerminalCommands,
 			commands.GlobalCommands,
+			patch.PatchCommands,
 		}
 	default:
-		return []*commands.Commands{commands.GlobalCommands}
+		return []*commands.Commands{
+			commands.GlobalCommands,
+			patch.PatchCommands,
+		}
 	}
 }
 

commands/commands.go
@@ -185,7 +185,12 @@ func GetCompletions(
 ) (options []string, prefix string) {
 	// copy zeroed struct
 	tmp := reflect.New(reflect.TypeOf(cmd)).Interface().(Command)
-	spec := opt.NewCmdSpec(args.Arg(0), tmp)
+	s, err := args.ArgSafe(0)
+	if err != nil {
+		log.Errorf("completions error: %v", err)
+		return options, prefix
+	}
+	spec := opt.NewCmdSpec(s, tmp)
 	return spec.GetCompletions(args)
 }
 

commands/patch/patch.go
@@ -1,0 +1,71 @@
+package patch
+
+import (
+	"errors"
+	"fmt"
+
+	"git.sr.ht/~rjarry/aerc/commands"
+	"git.sr.ht/~rjarry/go-opt"
+)
+
+var (
+	PatchCommands *commands.Commands
+	subCommands   *commands.Commands
+)
+
+func register(cmd commands.Command) {
+	if subCommands == nil {
+		subCommands = commands.NewCommands()
+	}
+	subCommands.Register(cmd)
+}
+
+func registerPatch(cmd commands.Command) {
+	if PatchCommands == nil {
+		PatchCommands = commands.NewCommands()
+	}
+	PatchCommands.Register(cmd)
+}
+
+func init() {
+	registerPatch(Patch{})
+}
+
+type Patch struct {
+	SubCmd commands.Command `opt:"command" action:"ParseSub" complete:"CompleteSubNames"`
+	Args   string           `opt:"..." required:"false" complete:"CompleteSubArgs"`
+}
+
+func (Patch) Aliases() []string {
+	return []string{"patch"}
+}
+
+func (p *Patch) ParseSub(arg string) error {
+	p.SubCmd = subCommands.ByName(arg)
+	if p.SubCmd == nil {
+		return fmt.Errorf("%s unknown sub-command", arg)
+	}
+	return nil
+}
+
+func (*Patch) CompleteSubNames(arg string) []string {
+	options := subCommands.Names()
+	return commands.FilterList(options, arg, nil)
+}
+
+func (p *Patch) CompleteSubArgs(arg string) []string {
+	if p.SubCmd == nil {
+		return nil
+	}
+	// prepend arbitrary string to arg to work with sub-commands
+	options, _ := commands.GetCompletions(p.SubCmd, opt.LexArgs("a "+arg))
+	return options
+}
+
+func (p Patch) Execute(args []string) error {
+	if p.SubCmd == nil {
+		return errors.New("no subcommand found")
+	}
+	a := opt.QuoteArgs(args[1:]...)
+	return commands.ExecuteCommand(p.SubCmd, a.String())
+}

commit 90c44dab95a315d05e32a8f5e700b4ad8efdd38f

commands: fix abbreviations regression
Make commands abbreviations work again by fixing the mistake introduced
in one of the recent commits.

Fixes: 07c1cefcf62d ("patch: create sub-command structure")
Signed-off-by: Vitaly Ovchinnikov <v@ovch.ru>
Reviewed-by: Koni Marti <koni.marti@gmail.com>
Acked-by: Robin Jarry <robin@jarry.cc>

main.go
@@ -77,11 +77,11 @@ func getCommands(selected ui.Drawable) []*commands.Commands {
 //	ar --> archive
 //	im --> import-mbox
 func expandAbbreviations(name string, sets []*commands.Commands) (string, commands.Command) {
-	var cmd commands.Command
-	candidate := name
+	var candidateCmd commands.Command
+	candidateName := name
 
 	for _, set := range sets {
-		cmd = set.ByName(name)
+		cmd := set.ByName(name)
 		if cmd != nil {
 			// Direct match, return it directly.
 			return name, cmd
@@ -91,7 +91,7 @@ func expandAbbreviations(name string, sets []*commands.Commands) (string, comman
 			if !strings.HasPrefix(n, name) {
 				continue
 			}
-			if cmd != nil {
+			if candidateCmd != nil {
 				// We have more than one command partially
 				// matching the input. We can't expand such an
 				// abbreviation, so return the command as is so
@@ -99,11 +99,11 @@ func expandAbbreviations(name string, sets []*commands.Commands) (string, comman
 				return name, nil
 			}
 			// We have a partial match.
-			candidate = n
-			cmd = set.ByName(n)
+			candidateName = n
+			candidateCmd = set.ByName(n)
 		}
 	}
-	return candidate, cmd
+	return candidateName, candidateCmd
 }
 
 func execCommand(