430dd0022c92ee6cfca47b090aa7c558f7f54cb9
Vitaly Ovchinnikov <v@ovch.ru>
2024-01-09 07:56:20 +0000

    rfc822: modify unit tests for message date parsing
    Modify the message date parser tests so they accept all the possible
    parsing results, depending on the user's locale settings.
    
    Fixes: 49a8cf10935f ("rfc822: improve dates parsing")
    Reported-by: Jason Cox <me@jasoncarloscox.com>
    Signed-off-by: Vitaly Ovchinnikov <v@ovch.ru>
    Tested-by: Jason Cox <me@jasoncarloscox.com>
    Acked-by: Robin Jarry <robin@jarry.cc>

--- lib/rfc822/message_test.go
+++ lib/rfc822/message_test.go
@@ -63,26 +63,34 @@ func TestMessageInfoHandledError(t *testing.T) {
 }
 
 func TestParseMessageDate(t *testing.T) {
-	// we use different times for "Date" and "Received" fields to make sure the right one is parsed
+	// we use different times for "Date" and "Received" fields so we can check which one is parsed
+	// however, we accept both if the date header can be parsed using the current locale
 	tests := []struct {
 		date     string
 		received string
-		utc      time.Time
+		utc      []time.Time
 	}{
 		{
 			date:     "Fri, 22 Dec 2023 11:19:01 +0000",
 			received: "from aaa.bbb.com for <user@host.com>; Fri, 22 Dec 2023 06:19:02 -0500 (EST)",
-			utc:      time.Date(2023, time.December, 22, 11, 19, 1, 0, time.UTC),
+			utc: []time.Time{
+				time.Date(2023, time.December, 22, 11, 19, 1, 0, time.UTC), // we expect the Date field to be parsed straight away
+			},
 		},
 		{
 			date:     "Fri, 29 Dec 2023 14:06:37 +0100",
 			received: "from somewhere.com for a@b.c; Fri, 30 Dec 2023 4:06:43 +1300",
-			utc:      time.Date(2023, time.December, 29, 13, 6, 37, 0, time.UTC),
+			utc: []time.Time{
+				time.Date(2023, time.December, 29, 13, 6, 37, 0, time.UTC), // we expect the Date field to be parsed here
+			},
 		},
 		{
 			date:     "Fri, 29 Dec 2023 00:51:00 EST",
 			received: "by hostname.com; Fri, 29 Dec 2023 00:51:33 -0500 (EST)",
-			utc:      time.Date(2023, time.December, 29, 5, 51, 33, 0, time.UTC),
+			utc: []time.Time{
+				time.Date(2023, time.December, 29, 5, 51, 33, 0, time.UTC),  // in most cases the Received field will be parsed
+				time.Date(2023, time.December, 29, 5, 51, 0o0, 0, time.UTC), // however, if the EST locale is loaded, the Date header can be parsed too
+			},
 		},
 	}
 
@@ -92,7 +100,14 @@ func TestParseMessageDate(t *testing.T) {
 		h.SetText("Received", test.received)
 		res, err := parseDate(&h)
 		require.Nil(t, err)
-		require.Equal(t, test.utc, res.UTC())
+		found := false
+		for _, ref := range test.utc {
+			if ref.Equal(res.UTC()) {
+				found = true
+				break
+			}
+		}
+		require.True(t, found, "Can't properly parse date and time from the Date/Received headers")
 	}
 }
 
