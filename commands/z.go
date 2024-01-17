rjarry/aerc

commit abe228b14d97d8d47e8ff4406de387fac45cfe68

commands: use completion from go-opt
Implement command completion with complete struct field tags from the
get-opt library introduced earlier.

Changelog-changed: Improved command completion.
Signed-off-by: Robin Jarry <robin@jarry.cc>
Reviewed-by: Koni Marti <koni.marti@gmail.com>
Tested-by: Moritz Poldrack <moritz@poldrack.dev>
Tested-by: Inwit <inwit@sindominio.net>

commands/z.go
@@ -8,7 +8,8 @@ import (
 )
 
 type Zoxide struct {
-	Target string `opt:"..." default:"~" metavar:"<folder> | <query>..."`
+	Target string   `opt:"folder" default:"~" complete:"CompleteFolder"`
+	Args   []string `opt:"..." required:"false" metavar:"<query>..."`
 }
 
 func ZoxideAdd(arg string) error {
@@ -36,8 +37,8 @@ func (Zoxide) Aliases() []string {
 	return []string{"z"}
 }
 
-func (Zoxide) Complete(args []string) []string {
-	return ChangeDirectory{}.Complete(args)
+func (*Zoxide) CompleteFolder(arg string) []string {
+	return GetFolders(arg)
 }
 
 // Execute calls zoxide add and query and delegates actually changing the

commit 680244d0533d2687039d947e00ff9c19abcf3ba8

z: complete with fs folders instead of account folders
GetFolders returns the current account email folders, not filesystem
folders. Use CompleteDirs instead.

Fixes: abe228b14d97 ("commands: use completion from go-opt")
Fixes: https://todo.sr.ht/~rjarry/aerc/205
Reported-by: Bence Ferdinandy <bence@ferdinandy.com>
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Bence Ferdinandy <bence@ferdinandy.com>

commands/z.go
@@ -39,7 +39,7 @@ func (Zoxide) Aliases() []string {
 }
 
 func (*Zoxide) CompleteFolder(arg string) []string {
-	return GetFolders(arg)
+	return CompletePath(arg, true)
 }
 
 // Execute calls zoxide add and query and delegates actually changing the