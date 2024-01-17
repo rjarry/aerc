rjarry/aerc

commit abe228b14d97d8d47e8ff4406de387fac45cfe68

commands: use completion from go-opt
Implement command completion with complete struct field tags from the
get-opt library introduced earlier.

Changelog-changed: Improved command completion.
Signed-off-by: Robin Jarry <robin@jarry.cc>
Reviewed-by: Koni Marti <koni.marti@gmail.com>
Tested-by: Moritz Poldrack <moritz@poldrack.dev>
Tested-by: Inwit <inwit@sindominio.net>

commands/account/export-mbox.go
@@ -17,7 +17,7 @@ import (
 )
 
 type ExportMbox struct {
-	Filename string `opt:"filename"`
+	Filename string `opt:"filename" complete:"CompleteFilename"`
 }
 
 func init() {
@@ -28,8 +28,8 @@ func (ExportMbox) Aliases() []string {
 	return []string{"export-mbox"}
 }
 
-func (ExportMbox) Complete(args []string) []string {
-	return commands.CompletePath(filepath.Join(args...))
+func (*ExportMbox) CompleteFilename(arg string) []string {
+	return commands.CompletePath(arg)
 }
 
 func (e ExportMbox) Execute(args []string) error {

commands/account/import-mbox.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"path/filepath"
 	"sync/atomic"
 	"time"
 
@@ -19,7 +18,7 @@ import (
 )
 
 type ImportMbox struct {
-	Filename string `opt:"filename"`
+	Filename string `opt:"filename" complete:"CompleteFilename"`
 }
 
 func init() {
@@ -30,8 +29,8 @@ func (ImportMbox) Aliases() []string {
 	return []string{"import-mbox"}
 }
 
-func (ImportMbox) Complete(args []string) []string {
-	return commands.CompletePath(filepath.Join(args...))
+func (*ImportMbox) CompleteFilename(arg string) []string {
+	return commands.CompletePath(arg)
 }
 
 func (i ImportMbox) Execute(args []string) error {

commands/cd.go
@@ -12,7 +12,7 @@ import (
 var previousDir string
 
 type ChangeDirectory struct {
-	Target string `opt:"directory" default:"~"`
+	Target string `opt:"directory" default:"~" complete:"CompleteTarget"`
 }
 
 func init() {
@@ -23,9 +23,8 @@ func (ChangeDirectory) Aliases() []string {
 	return []string{"cd"}
 }
 
-func (ChangeDirectory) Complete(args []string) []string {
-	path := strings.Join(args, " ")
-	completions := CompletePath(path)
+func (*ChangeDirectory) CompleteTarget(arg string) []string {
+	completions := CompletePath(arg)
 
 	var dirs []string
 	for _, c := range completions {

commands/compose/attach.go
@@ -23,7 +23,8 @@ import (
 type Attach struct {
 	Menu bool   `opt:"-m"`
 	Name string `opt:"-r"`
-	Path string `opt:"..." metavar:"<path>" required:"false"`
+	Path string `opt:"path" required:"false" complete:"CompletePath"`
+	Args string `opt:"..." required:"false"`
 }
 
 func init() {
@@ -34,9 +35,8 @@ func (Attach) Aliases() []string {
 	return []string{"attach"}
 }
 
-func (Attach) Complete(args []string) []string {
-	path := strings.Join(args, " ")
-	return commands.CompletePath(path)
+func (*Attach) CompletePath(arg string) []string {
+	return commands.CompletePath(arg)
 }
 
 func (a Attach) Execute(args []string) error {
@@ -52,6 +52,9 @@ func (a Attach) Execute(args []string) error {
 		}
 		return a.readCommand()
 	default:
+		if a.Args != "" {
+			return errors.New("only a single path is supported")
+		}
 		return a.addPath(a.Path)
 	}
 }
@@ -186,7 +189,7 @@ func (a Attach) openMenu() error {
 }
 
 func (a Attach) readCommand() error {
-	cmd := exec.Command("sh", "-c", a.Path)
+	cmd := exec.Command("sh", "-c", a.Path+" "+a.Args)
 
 	data, err := cmd.Output()
 	if err != nil {

commands/eml.go
@@ -5,14 +5,13 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"strings"
 
 	"git.sr.ht/~rjarry/aerc/app"
 	"git.sr.ht/~rjarry/aerc/lib"
 )
 
 type Eml struct {
-	Path string `opt:"path" required:"false"`
+	Path string `opt:"path" required:"false" complete:"CompletePath"`
 }
 
 func init() {
@@ -23,8 +22,8 @@ func (Eml) Aliases() []string {
 	return []string{"eml", "preview"}
 }
 
-func (Eml) Complete(args []string) []string {
-	return CompletePath(strings.Join(args, " "))
+func (*Eml) CompletePath(arg string) []string {
+	return CompletePath(arg)
 }
 
 func (e Eml) Execute(args []string) error {

commands/msgview/save.go
@@ -22,7 +22,7 @@ type Save struct {
 	CreateDirs     bool   `opt:"-p"`
 	Attachments    bool   `opt:"-a"`
 	AllAttachments bool   `opt:"-A"`
-	Path           string `opt:"..." required:"false" metavar:"<path>"`
+	Path           string `opt:"path" required:"false" complete:"CompletePath"`
 }
 
 func init() {
@@ -37,14 +37,12 @@ func (Save) Aliases() []string {
 	return []string{"save"}
 }
 
-func (s Save) Complete(args []string) []string {
-	trimmed := commands.Operands(args, s.Options())
-	path := strings.Join(trimmed, " ")
+func (*Save) CompletePath(arg string) []string {
 	defaultPath := config.General.DefaultSavePath
-	if defaultPath != "" && !isAbsPath(path) {
-		path = filepath.Join(defaultPath, path)
+	if defaultPath != "" && !isAbsPath(arg) {
+		arg = filepath.Join(defaultPath, arg)
 	}
-	return commands.CompletePath(xdg.ExpandHome(path))
+	return commands.CompletePath(xdg.ExpandHome(arg))
 }
 
 func (s Save) Execute(args []string) error {

commands/util.go
@@ -18,6 +18,7 @@ import (
 	"git.sr.ht/~rjarry/aerc/log"
 	"git.sr.ht/~rjarry/aerc/models"
 	"git.sr.ht/~rjarry/aerc/worker/types"
+	"git.sr.ht/~rjarry/go-opt"
 	"github.com/gdamore/tcell/v2"
 )
 
@@ -93,7 +94,7 @@ func CompletePath(path string) []string {
 		}
 
 		if !strings.HasPrefix(path, ".") && !strings.Contains(path, "/.") {
-			log.Debugf("removing hidden files from glob results")
+			log.Tracef("removing hidden files from glob results")
 			for i := len(matches) - 1; i >= 0; i-- {
 				if strings.HasPrefix(filepath.Base(matches[i]), ".") {
 					if i == len(matches)-1 {
@@ -107,11 +108,13 @@ func CompletePath(path string) []string {
 
 		for i, m := range matches {
 			if isDir(m) {
-				matches[i] = m + "/"
+				m += "/"
 			}
+			matches[i] = opt.QuoteArg((xdg.TildeHome(m)))
 		}
 
 		sort.Strings(matches)
+
 		return matches
 	}
 
@@ -122,11 +125,11 @@ func CompletePath(path string) []string {
 		if isDir(f) {
 			f += "/"
 		}
-
-		files[i] = f
+		files[i] = opt.QuoteArg((xdg.TildeHome(f)))
 	}
 
 	sort.Strings(files)
+
 	return files
 }
 
@@ -227,16 +230,16 @@ func MsgInfoFromUids(store *lib.MessageStore, uids []uint32, statusInfo func(str
 
 // FilterList takes a list of valid completions and filters it, either
 // by case smart prefix, or by fuzzy matching, prepending "prefix" to each completion
-func FilterList(valid []string, search, prefix string, isFuzzy bool) []string {
-	out := make([]string, 0)
+func FilterList(valid []string, search, prefix, suffix string, isFuzzy bool) []string {
+	out := make([]string, 0, len(valid))
 	if isFuzzy {
 		for _, v := range fuzzy.RankFindFold(search, valid) {
-			out = append(out, prefix+v.Target)
+			out = append(out, opt.QuoteArg(prefix+v.Target+suffix))
 		}
 	} else {
 		for _, v := range valid {
 			if hasCaseSmartPrefix(v, search) {
-				out = append(out, prefix+v)
+				out = append(out, opt.QuoteArg(prefix+v+suffix))
 			}
 		}
 	}

commit 3622f7a99e91bfe38eab9e91dc982aafa00fe211

cd: fix completion of folders with a space
Folders that contain spaces are surrounded by quotes. They can never end
with '/'. Hence they are never returned in the completion results.

Update CompletePath with an additional onlyDirs argument to take care of
this before quotes are inserted.

Fixes: abe228b14d97 ("commands: use completion from go-opt")
Fixes: https://todo.sr.ht/~rjarry/aerc/204
Reported-by: Bence Ferdinandy <bence@ferdinandy.com>
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Bence Ferdinandy <bence@ferdinandy.com>

commands/account/export-mbox.go
@@ -30,7 +30,7 @@ func (ExportMbox) Aliases() []string {
 }
 
 func (*ExportMbox) CompleteFilename(arg string) []string {
-	return commands.CompletePath(arg)
+	return commands.CompletePath(arg, false)
 }
 
 func (e ExportMbox) Execute(args []string) error {

commands/account/import-mbox.go
@@ -31,7 +31,7 @@ func (ImportMbox) Aliases() []string {
 }
 
 func (*ImportMbox) CompleteFilename(arg string) []string {
-	return commands.CompletePath(arg)
+	return commands.CompletePath(arg, false)
 }
 
 func (i ImportMbox) Execute(args []string) error {

commands/cd.go
@@ -3,7 +3,6 @@ package commands
 import (
 	"errors"
 	"os"
-	"strings"
 
 	"git.sr.ht/~rjarry/aerc/app"
 	"git.sr.ht/~rjarry/aerc/lib/xdg"
@@ -24,17 +23,7 @@ func (ChangeDirectory) Aliases() []string {
 }
 
 func (*ChangeDirectory) CompleteTarget(arg string) []string {
-	completions := CompletePath(arg)
-
-	var dirs []string
-	for _, c := range completions {
-		// filter out non-directories
-		if strings.HasSuffix(c, "/") {
-			dirs = append(dirs, c)
-		}
-	}
-
-	return dirs
+	return CompletePath(arg, true)
 }
 
 func (cd ChangeDirectory) Execute(args []string) error {

commands/compose/attach.go
@@ -36,7 +36,7 @@ func (Attach) Aliases() []string {
 }
 
 func (*Attach) CompletePath(arg string) []string {
-	return commands.CompletePath(arg)
+	return commands.CompletePath(arg, false)
 }
 
 func (a Attach) Execute(args []string) error {

commands/eml.go
@@ -24,7 +24,7 @@ func (Eml) Aliases() []string {
 }
 
 func (*Eml) CompletePath(arg string) []string {
-	return CompletePath(arg)
+	return CompletePath(arg, false)
 }
 
 func (e Eml) Execute(args []string) error {

commands/msgview/save.go
@@ -42,7 +42,7 @@ func (*Save) CompletePath(arg string) []string {
 	if defaultPath != "" && !isAbsPath(arg) {
 		arg = filepath.Join(defaultPath, arg)
 	}
-	return commands.CompletePath(xdg.ExpandHome(arg))
+	return commands.CompletePath(arg, false)
 }
 
 func (s Save) Execute(args []string) error {

commands/util.go
@@ -71,7 +71,7 @@ func QuickTerm(args []string, stdin io.Reader) (*app.Terminal, error) {
 }
 
 // CompletePath provides filesystem completions given a starting path.
-func CompletePath(path string) []string {
+func CompletePath(path string, onlyDirs bool) []string {
 	if path == ".." || strings.HasSuffix(path, "/..") {
 		return []string{path + "/"}
 	}
@@ -93,6 +93,8 @@ func CompletePath(path string) []string {
 	for _, m := range matches {
 		if isDir(m) {
 			m += "/"
+		} else if onlyDirs {
+			continue
 		}
 		if strings.HasPrefix(filepath.Base(m), ".") && !includeHidden {
 			continue

commands/util_test.go
@@ -14,11 +14,17 @@ func TestCompletePath(t *testing.T) {
 
 	vectors := []struct {
 		arg      string
+		onlyDirs bool
 		expected []string
 	}{
 		{
 			arg:      "",
 			expected: []string{"baz/", "foo.ini", "foo/"},
+		},
+		{
+			arg:      "",
+			onlyDirs: true,
+			expected: []string{"baz/", "foo/"},
 		},
 		{
 			arg:      ".",
@@ -44,10 +50,15 @@ func TestCompletePath(t *testing.T) {
 				"../testdata/foo/",
 			},
 		},
+		{
+			arg:      "../testdata/f",
+			onlyDirs: true,
+			expected: []string{"../testdata/foo/"},
+		},
 	}
 	for _, vec := range vectors {
 		t.Run(vec.arg, func(t *testing.T) {
-			res := commands.CompletePath(vec.arg)
+			res := commands.CompletePath(vec.arg, vec.onlyDirs)
 			assert.Equal(t, vec.expected, res)
 		})
 	}