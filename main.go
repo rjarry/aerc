rjarry/aerc

commit fd4dd42408856048dd71f83ae1f30e2ab84621da

ipc: wait for app to be ready before accepting commands
Pass a cancellable context to the IPC server and defer accepting
connections until the first worker message has been received by the main
thread.

This is not a real fix for the :split command not working at startup.
:split requires a mail store to be present on the currently selected
account tab+folder. I could have waited for a types.DirectoryContents
message but it seems silly and bug prone since such a message may never
arrive and we don't want to pool IPC messages forever.

Also, a types.DirectoryContents message may arrive for one account but
:split is only effective *per account* and the default selected account
tab is the first one.

In any case, this band aid prevents aerc from crashing or breaking down
the terminal when running IPC commands in the aerc-startup hook.

Fixes: https://lists.sr.ht/~rjarry/aerc-devel/%3CCYPN7AVYQ69S.WV0T67VM6WX3%40wegtam.com%3E
Fixes: https://todo.sr.ht/~rjarry/aerc/173
Reported-by: Jens Grassel <jens@wegtam.com>
Reported-by: Justine Smithies <justine@smithies.me.uk>
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Thomas Böhler <witcher@wiredspace.de>
Reviewed-by: Thomas Böhler <witcher@wiredspace.de>

main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/base64"
 	"errors"
 	"fmt"
@@ -10,6 +11,7 @@ import (
 	"runtime"
 	"sort"
 	"strings"
+	"sync"
 	"time"
 
 	"git.sr.ht/~rjarry/go-opt"
@@ -225,8 +227,9 @@ func main() {
 	if config.Ui.MouseEnabled {
 		ui.EnableMouse()
 	}
+	startup, startupDone := context.WithCancel(context.Background())
 
-	as, err := ipc.StartServer(app.IPCHandler())
+	as, err := ipc.StartServer(app.IPCHandler(), startup)
 	if err != nil {
 		log.Warnf("Failed to start Unix server: %v", err)
 	} else {
@@ -269,6 +272,7 @@ func main() {
 			log.Errorf("aerc-shutdown hook: %s", err)
 		}
 	}(time.Now())
+	var once sync.Once
 loop:
 	for {
 		select {
@@ -276,6 +280,12 @@ loop:
 			ui.HandleEvent(event)
 		case msg := <-types.WorkerMessages:
 			app.HandleMessage(msg)
+			// XXX: The app may not be 100% ready at this point.
+			// The issue is that there is no real way to tell when
+			// it will be ready. And in some cases, it may never be.
+			// At least, we can be confident that accepting IPC
+			// commands will not crash the whole process.
+			once.Do(startupDone)
 		case callback := <-ui.Callbacks:
 			callback()
 		case <-ui.Redraw:

lib/ipc/receive.go
@@ -2,6 +2,7 @@ package ipc
 
 import (
 	"bufio"
+	"context"
 	"errors"
 	"net"
 	"net/url"
@@ -20,9 +21,10 @@ import (
 type AercServer struct {
 	listener net.Listener
 	handler  Handler
+	startup  context.Context
 }
 
-func StartServer(handler Handler) (*AercServer, error) {
+func StartServer(handler Handler, startup context.Context) (*AercServer, error) {
 	sockpath := xdg.RuntimePath("aerc.sock")
 	// remove the socket if it is not connected to a session
 	if err := ConnectAndExec(nil); err != nil {
@@ -33,7 +35,7 @@ func StartServer(handler Handler) (*AercServer, error) {
 	if err != nil {
 		return nil, err
 	}
-	as := &AercServer{listener: l, handler: handler}
+	as := &AercServer{listener: l, handler: handler, startup: startup}
 	go as.Serve()
 
 	return as, nil
@@ -47,6 +49,8 @@ var lastId int64 = 0 // access via atomic
 
 func (as *AercServer) Serve() {
 	defer log.PanicHandler()
+
+	<-as.startup.Done()
 
 	for {
 		conn, err := as.listener.Accept()

commit ff77c7a52d78a487ea7775515aab4db399371075

ipc: fix deadlock when aerc is not running
Defer the execution of failed IPC commands *after* the IPC socket has
been unblocked.

Fixes: fd4dd4240885 ("ipc: wait for app to be ready before accepting commands")
Fixes: https://todo.sr.ht/~rjarry/aerc/229
Changelog-fixed: Deadlock when running `aerc mailto:foo@bar.com`
 without another instance of aerc already running.
Reported-by: Skejg <grolleman@zoho.com>
Signed-off-by: Robin Jarry <robin@jarry.cc>
Reviewed-by: Tim Culverhouse <tim@timculverhouse.com>

main.go
@@ -164,7 +164,6 @@ func main() {
 		if err == nil {
 			return // other aerc instance takes over
 		}
-		fmt.Fprintf(os.Stderr, "Failed to communicate to aerc: %v\n", err)
 		// continue with setting up a new aerc instance and retry after init
 		retryExec = true
 	}
@@ -211,7 +210,11 @@ func main() {
 	// set the aerc version so that we can use it in the template funcs
 	templates.SetVersion(Version)
 
-	if retryExec {
+	enableIpc := func() {
+		startupDone()
+		if !retryExec {
+			return
+		}
 		// retry execution
 		err := ipc.ConnectAndExec(opts.Command)
 		if err != nil {
@@ -220,7 +223,7 @@ func main() {
 			if err != nil {
 				log.Warnf("failed to close backends: %v", err)
 			}
-			return
+			os.Exit(1)
 		}
 	}
 
@@ -253,7 +256,7 @@ loop:
 			// it will be ready. And in some cases, it may never be.
 			// At least, we can be confident that accepting IPC
 			// commands will not crash the whole process.
-			once.Do(startupDone)
+			once.Do(enableIpc)
 		case callback := <-ui.Callbacks:
 			callback()
 		case <-ui.Redraw: