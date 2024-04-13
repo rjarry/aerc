rjarry/aerc

commit cbcabfafaab20eaffad642f20151e890161efddc

compose: allow writing multipart/alternative messages
Add a new :multipart command that can be executed on the composer review
screen. This command takes a MIME type as argument which needs to match
a setting in the new [multipart-converters] section of aerc.conf. A part
can be removed by using the -d flag.

The [multipart-converters] section has MIME types associated with
commands. These commands are executed with sh -c every time the main
email body is updated to generate each part content. The commands are
expected to output valid UTF-8 text.

If a command fails, an explicit error will be printed next to the part
MIME type to allow users to debug their issue but the email may still be
sent anyway with an empty alternative part.

This is mostly intended for people who *really* need to send html
messages for their boss or for corporate reasons. For now, it is
a manual and explicit action to convert a message in such a way.

Here is an example configuration:

  [multipart-converters]
  text/html = pandoc -f markdown -t html

And the associated binding to append an HTML alternative to a message:

  [compose::review]
  H = :multipart text/html<enter>
  hh = :multipart -d text/html<enter>

Link: https://lists.sr.ht/~rjarry/aerc-discuss/%3CCO5KH4W57XNB.2PZLR1CNFK22H%40mashenka%3E
Co-authored-by: Eric McConville <emcconville@emcconville.com>
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Bence Ferdinandy <bence@ferdinandy.com>
Acked-by: Moritz Poldrack <moritz@poldrack.dev>

commands/compose/multipart.go
@@ -1,0 +1,73 @@
+package compose
+
+import (
+	"bytes"
+	"fmt"
+
+	"git.sr.ht/~rjarry/aerc/commands"
+	"git.sr.ht/~rjarry/aerc/widgets"
+	"git.sr.ht/~sircmpwn/getopt"
+)
+
+type Multipart struct{}
+
+func init() {
+	register(Multipart{})
+}
+
+func (Multipart) Aliases() []string {
+	return []string{"multipart"}
+}
+
+func (Multipart) Complete(aerc *widgets.Aerc, args []string) []string {
+	var completions []string
+	completions = append(completions, "-d")
+	for mime := range aerc.Config().Converters {
+		completions = append(completions, mime)
+	}
+	return commands.CompletionFromList(aerc, completions, args)
+}
+
+func (a Multipart) Execute(aerc *widgets.Aerc, args []string) error {
+	composer, ok := aerc.SelectedTabContent().(*widgets.Composer)
+	if !ok {
+		return fmt.Errorf(":multipart is only available on the compose::review screen")
+	}
+
+	opts, optind, err := getopt.Getopts(args, "d")
+	if err != nil {
+		return fmt.Errorf("Usage: :multipart [-d] <mime/type>")
+	}
+	var remove bool = false
+	for _, opt := range opts {
+		if opt.Option == 'd' {
+			remove = true
+		}
+	}
+	args = args[optind:]
+	if len(args) != 1 {
+		return fmt.Errorf("Usage: :multipart [-d] <mime/type>")
+	}
+	mime := args[0]
+
+	if remove {
+		return composer.RemovePart(mime)
+	} else {
+		_, found := aerc.Config().Converters[mime]
+		if !found {
+			return fmt.Errorf("no command defined for MIME type: %s", mime)
+		}
+		err = composer.AppendPart(
+			mime,
+			map[string]string{"Charset": "UTF-8"},
+			// the actual content of the part will be rendered
+			// every time the body of the email is updated
+			bytes.NewReader([]byte{}),
+		)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}

widgets/compose.go
@@ -464,6 +464,11 @@ func (c *Composer) AppendPart(mimetype string, params map[string]string, body io
 	if !strings.HasPrefix(mimetype, "text") {
 		return fmt.Errorf("can only append text mimetypes")
 	}
+	for _, part := range c.textParts {
+		if part.MimeType == mimetype {
+			return fmt.Errorf("%s part already exists", mimetype)
+		}
+	}
 	newPart, err := lib.NewPart(mimetype, params, body)
 	if err != nil {
 		return err
@@ -471,6 +476,21 @@ func (c *Composer) AppendPart(mimetype string, params map[string]string, body io
 	c.textParts = append(c.textParts, newPart)
 	c.resetReview()
 	return nil
+}
+
+func (c *Composer) RemovePart(mimetype string) error {
+	if mimetype == "text/plain" {
+		return fmt.Errorf("cannot remove text/plain parts")
+	}
+	for i, part := range c.textParts {
+		if part.MimeType != mimetype {
+			continue
+		}
+		c.textParts = append(c.textParts[:i], c.textParts[i+1:]...)
+		c.resetReview()
+		return nil
+	}
+	return fmt.Errorf("%s part not found", mimetype)
 }
 
 func (c *Composer) AddTemplate(template string, data interface{}) error {
@@ -1361,7 +1381,15 @@ func newReviewMessage(composer *Composer, err error) *reviewMessage {
 			grid.AddChild(ui.NewText("text/plain", uiConfig.GetStyle(config.STYLE_DEFAULT))).At(i, 0)
 			i += 1
 			for _, p := range composer.textParts {
-				grid.AddChild(ui.NewText(p.MimeType, uiConfig.GetStyle(config.STYLE_DEFAULT))).At(i, 0)
+				err := composer.updateMultipart(p)
+				if err != nil {
+					msg := fmt.Sprintf("%s error: %s", p.MimeType, err)
+					grid.AddChild(ui.NewText(msg,
+						uiConfig.GetStyle(config.STYLE_ERROR))).At(i, 0)
+				} else {
+					grid.AddChild(ui.NewText(p.MimeType,
+						uiConfig.GetStyle(config.STYLE_DEFAULT))).At(i, 0)
+				}
 				i += 1
 			}
 
@@ -1372,6 +1400,42 @@ func newReviewMessage(composer *Composer, err error) *reviewMessage {
 		composer: composer,
 		grid:     grid,
 	}
+}
+
+func (c *Composer) updateMultipart(p *lib.Part) error {
+	command, found := c.aerc.Config().Converters[p.MimeType]
+	if !found {
+		// unreachable
+		return fmt.Errorf("no command defined for mime/type")
+	}
+	// reset part body to avoid it leaving outdated if the command fails
+	p.Data = nil
+	err := c.reloadEmail()
+	if err != nil {
+		return errors.Wrap(err, "reloadEmail")
+	}
+	body, err := io.ReadAll(c.email)
+	if err != nil {
+		return errors.Wrap(err, "io.ReadAll")
+	}
+	cmd := exec.Command("sh", "-c", command)
+	cmd.Stdin = bytes.NewReader(body)
+	out, err := cmd.Output()
+	if err != nil {
+		var stderr string
+		var ee *exec.ExitError
+		if errors.As(err, &ee) {
+			// append the first 30 chars of stderr if any
+			stderr = strings.Trim(string(ee.Stderr), " \t\n\r")
+			stderr = strings.ReplaceAll(stderr, "\n", "; ")
+			if stderr != "" {
+				stderr = fmt.Sprintf(": %.30s", stderr)
+			}
+		}
+		return fmt.Errorf("%s: %w%s", command, err, stderr)
+	}
+	p.Data = out
+	return nil
 }
 
 func (rm *reviewMessage) Invalidate() {

config/aerc.conf
@@ -359,6 +359,17 @@
 #
 #no-attachment-warning=
 
+[multipart-converters]
+#
+# Converters allow to generate multipart/alternative messages by converting the
+# main text/plain part into any other MIME type. Only exact MIME types are
+# accepted. The commands are invoked with sh -c and are expected to output
+# valid UTF-8 text.
+#
+# Example (obviously, this requires that you write your main text/plain body
+# using the markdown syntax):
+#text/html=pandoc -f markdown -t html --standalone
+
 [filters]
 #
 # Filters allow you to pipe an email body through a shell command to render

config/config.go
@@ -18,6 +18,7 @@ type AercConfig struct {
 	Bindings        BindingConfig
 	ContextualBinds []BindingConfigContext
 	Compose         ComposeConfig
+	Converters      map[string]string
 	Accounts        []AccountConfig  `ini:"-"`
 	Filters         []FilterConfig   `ini:"-"`
 	Viewer          ViewerConfig     `ini:"-"`
@@ -140,6 +141,7 @@ func LoadConfigFromFile(root *string, accts []string) (*AercConfig, error) {
 		Viewer:          defaultViewerConfig(),
 		Statusline:      defaultStatuslineConfig(),
 		Compose:         defaultComposeConfig(),
+		Converters:      make(map[string]string),
 		Templates:       defaultTemplatesConfig(),
 		Openers:         make(map[string][]string),
 	}
@@ -151,6 +153,9 @@ func LoadConfigFromFile(root *string, accts []string) (*AercConfig, error) {
 		return nil, err
 	}
 	if err := config.parseCompose(file); err != nil {
+		return nil, err
+	}
+	if err := config.parseConverters(file); err != nil {
 		return nil, err
 	}
 	if err := config.parseViewer(file); err != nil {

config/converters.go
@@ -1,0 +1,34 @@
+package config
+
+import (
+	"fmt"
+	"strings"
+
+	"git.sr.ht/~rjarry/aerc/log"
+	"github.com/go-ini/ini"
+)
+
+func (config *AercConfig) parseConverters(file *ini.File) error {
+	converters, err := file.GetSection("multipart-converters")
+	if err != nil {
+		goto out
+	}
+
+	for mimeType, command := range converters.KeysHash() {
+		mimeType = strings.ToLower(mimeType)
+		if mimeType == "text/plain" {
+			return fmt.Errorf(
+				"multipart-converters: text/plain is reserved")
+		}
+		if !strings.HasPrefix(mimeType, "text/") {
+			return fmt.Errorf(
+				"multipart-converters: %q: only text/* MIME types are supported",
+				mimeType)
+		}
+		config.Converters[mimeType] = command
+	}
+
+out:
+	log.Debugf("aerc.conf: [multipart-converters] %#v", config.Converters)
+	return nil
+}

doc/aerc-config.5.scd
@@ -569,6 +569,32 @@ These options are configured in the *[compose]* section of _aerc.conf_.
 	Example:
 		*no-attachment-warning* = _^[^>]\*attach(ed|ment)_
 
+# MULTIPART CONVERTERS
+
+Converters allow to generate _multipart/alternative_ messages by converting the
+main _text/plain_ body into any other text MIME type with the *:multipart*
+command. Only exact MIME types are accepted. The commands are invoked with
+_sh -c_ and are expected to output valid UTF-8 text.
+
+Only _text/<subtype>_ MIME parts can be generated. The _text/plain_ MIME type is
+reserved and cannot be generated. You still need to write your emails by hand in
+your favorite text editor.
+
+Converters are configured in the *[multipart-converters]* section of
+_aerc.conf_.
+
+Example:
+
+```
+[multipart-converters]
+text/html=pandoc -f markdown -t html --standalone
+```
+
+Obviously, this requires that you write your main _text/plain_ body using the
+markdown syntax. Also, mind that some mailing lists reject emails that contain
+_text/html_ alternative parts. Use this feature carefully and when possible,
+avoid using it at all.
+
 # FILTERS
 
 Filters are a flexible and powerful way of handling viewing parts of an opened

doc/aerc.1.scd
@@ -524,6 +524,17 @@ message list, the message in the message viewer, etc).
 *:edit*
 	(Re-)opens your text editor to edit the message in progress.
 
+*:multipart* [*-d*] _<mime/type>_
+	Makes the message to multipart/alternative and add the specified
+	_<mime/type>_ part. Only the MIME types that are configured in the
+	*[multipart-converters]* section of _aerc.conf_ are supported and their
+	related commands will be used to generate the alternate part.
+
+	*-d*:
+		Remove the specified alternative _<mime/type>_ instead of
+		adding it. If no alternative parts are left, make the message
+		text/plain (i.e. not multipart/alternative).
+
 *:next-field*++
 *:prev-field*
 	Cycles between input fields in the compose window.

commit 1b912fbbe98ae9b0a78da2b448f6121944d9b9b1

compose: explicitly identify converted text/* parts
When running :accept, an error is displayed on the review screen:

 text/calendar error: no command defined for mime/type

When running :multipart text/xxx, its contents are not specified. They
are regenerated every time the review screen is displayed. When running
:accept, a text/calendar part is added with actual contents.

Update the Part object to hold a boolean initialized when first being
created. If body is nil, identify the part as "Converted" and update its
contents every time the review screen is displayed. When body is not nil
but contains text (e.g. when running :accept), identify the part as
*not* converted and ignore the conversion step.

Fixes: cbcabfafaab2 ("compose: allow writing multipart/alternative messages")
Reported-by: Inwit <inwit@sindominio.net>
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Inwit <inwit@sindominio.net>

app/compose.go
@@ -1787,7 +1787,10 @@ func (c *Composer) updateMultipart(p *lib.Part) error {
 		p.ConversionError = e
 		return e
 	}
-
+	if !p.Converted {
+		// text/* multipart created without a command (e.g. by :accept)
+		return nil
+	}
 	command, found := config.Converters[p.MimeType]
 	if !found {
 		// unreachable

commands/compose/multipart.go
@@ -1,7 +1,6 @@
 package compose
 
 import (
-	"bytes"
 	"fmt"
 
 	"git.sr.ht/~rjarry/aerc/app"
@@ -52,7 +51,7 @@ func (m Multipart) Execute(args []string) error {
 			map[string]string{"Charset": "UTF-8"},
 			// the actual content of the part will be rendered
 			// every time the body of the email is updated
-			bytes.NewReader([]byte{}),
+			nil,
 		)
 		if err != nil {
 			return err

lib/attachment.go
@@ -19,19 +19,28 @@ type Part struct {
 	MimeType        string
 	Params          map[string]string
 	Data            []byte
+	Converted       bool
 	ConversionError error
 }
 
 func NewPart(mimetype string, params map[string]string, body io.Reader,
 ) (*Part, error) {
-	d, err := io.ReadAll(body)
-	if err != nil {
-		return nil, err
+	var d []byte
+	var err error
+	var converted bool
+	if body == nil {
+		converted = true
+	} else {
+		d, err = io.ReadAll(body)
+		if err != nil {
+			return nil, err
+		}
 	}
 	return &Part{
-		MimeType: mimetype,
-		Params:   params,
-		Data:     d,
+		MimeType:  mimetype,
+		Params:    params,
+		Data:      d,
+		Converted: converted,
 	}, nil
 }
 