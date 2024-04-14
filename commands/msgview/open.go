rjarry/aerc

commit d99c49de2fc12b64a7cd79044a8b042e7e59d02b

open: preserve the original filename
Change the name of the temporary file that is :open'ed using the system
handler from `aerc-<randint>.ext` to
`aerc-<randint>/<actual-filename.ext>`.  This preserves the original
filename, while retaining collision avoidance and the current base
location (os.TempDir()).

Changelog-changed: `:open` commands now preserve the original filename.
Signed-off-by: Maarten Aertsen <maarten@nlnetlabs.nl>
Tested-by: Bence Ferdinandy <bence@ferdinandy.com>
Acked-by: Robin Jarry <robin@jarry.cc>

commands/msgview/open.go
@@ -3,7 +3,6 @@ package msgview
 import (
 	"errors"
 	"io"
-	"mime"
 	"os"
 	"path/filepath"
 
@@ -42,23 +41,21 @@ func (o Open) Execute(args []string) error {
 	p := mv.SelectedMessagePart()
 
 	mv.MessageView().FetchBodyPart(p.Index, func(reader io.Reader) {
-		extension := ""
 		mimeType := ""
 
-		// try to determine the correct extension
-		if part, err := mv.MessageView().BodyStructure().PartAtIndex(p.Index); err == nil {
-			mimeType = part.FullMIMEType()
-			// see if we can get extension directly from the attachment name
-			extension = filepath.Ext(part.FileName())
-			// if there is no extension, try using the attachment mime type instead
-			if extension == "" {
-				if exts, _ := mime.ExtensionsByType(mimeType); len(exts) > 0 {
-					extension = exts[0]
-				}
-			}
+		part, err := mv.MessageView().BodyStructure().PartAtIndex(p.Index)
+		if err != nil {
+			app.PushError(err.Error())
+			return
 		}
+		mimeType = part.FullMIMEType()
 
-		tmpFile, err := os.CreateTemp(os.TempDir(), "aerc-*"+extension)
+		tmpDir, err := os.MkdirTemp(os.TempDir(), "aerc-*")
+		if err != nil {
+			app.PushError(err.Error())
+			return
+		}
+		tmpFile, err := os.Create(filepath.Join(tmpDir, part.FileName()))
 		if err != nil {
 			app.PushError(err.Error())
 			return
@@ -74,7 +71,7 @@ func (o Open) Execute(args []string) error {
 		go func() {
 			defer log.PanicHandler()
 			if o.Delete {
-				defer os.Remove(tmpFile.Name())
+				defer os.RemoveAll(tmpDir)
 			}
 			err = lib.XDGOpenMime(tmpFile.Name(), mimeType, o.Cmd)
 			if err != nil {

commit ae3f7419ce1b6d4a81c4f240ff1073ba45f45b52

open: create a file even if filename is empty
Without a filename, aerc tries to open a directory. In this case, create
a random filename.

Fixes: d99c49de2fc1 ("open: preserve the original filename")
Signed-off-by: Bence Ferdinandy <bence@ferdinandy.com>
Acked-by: Robin Jarry <robin@jarry.cc>

commands/msgview/open.go
@@ -3,6 +3,7 @@ package msgview
 import (
 	"errors"
 	"io"
+	"mime"
 	"os"
 	"path/filepath"
 
@@ -55,7 +56,17 @@ func (o Open) Execute(args []string) error {
 			app.PushError(err.Error())
 			return
 		}
-		tmpFile, err := os.Create(filepath.Join(tmpDir, part.FileName()))
+		filename := part.FileName()
+		var tmpFile *os.File
+		if filename == "" {
+			extension := ""
+			if exts, _ := mime.ExtensionsByType(mimeType); len(exts) > 0 {
+				extension = exts[0]
+			}
+			tmpFile, err = os.CreateTemp(tmpDir, "aerc-*"+extension)
+		} else {
+			tmpFile, err = os.Create(filepath.Join(tmpDir, filename))
+		}
 		if err != nil {
 			app.PushError(err.Error())
 			return