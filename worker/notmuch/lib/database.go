rjarry/aerc

commit 672b4edca7af2303cfcb1d69a676556d12476b5f

notmuch: draw incomplete threads
Create a "pseudo thread" with the first message as root. This is a
temporary solution, until we have a more generic implementation of dummy
messages that works for all backends.

Signed-off-by: Julio B <julio.bacel@gmail.com>
Tested-by: Inwit <inwit@sindominio.net>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/notmuch/lib/database.go
@@ -137,11 +137,17 @@ func (db *DB) ThreadsFromQuery(ctx context.Context, q string, entireThread bool)
 			thread := threads.Thread()
 			tlm := thread.TopLevelMessages()
 			root := db.makeThread(nil, &tlm, entireThread)
-			// if len(root) > 1 {
-			// TODO make a dummy root node and link all the
-			// first level children to it
-			// }
-			res = append(res, root...)
+			if len(root) > 1 {
+				root[0].FirstChild = root[0].NextSibling
+				root[0].NextSibling.PrevSibling = nil
+				root[0].NextSibling = nil
+				for i := 1; i < len(root); i++ {
+					root[i].Parent = root[0]
+				}
+				res = append(res, root[0])
+			} else {
+				res = append(res, root...)
+			}
 			tlm.Close()
 			thread.Close()
 		}

commit 0a1af14fd91640cc19f7e06d7fd70b9424b459cc

notmuch: fix disappearing messages
The current implementation assumes that 'root[0].FirstChild' is nil, but
this is not always the case. Instead of overwriting the FirstChild,
detect and link the messages to the LastChild of root[0].

Fixes: 672b4edca7af ("notmuch: draw incomplete threads")
Signed-off-by: Julio B <julio.bacel@gmail.com>
Tested-by: Inwit <inwit@sindominio.net>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/notmuch/lib/database.go
@@ -139,7 +139,10 @@ func (db *DB) ThreadsFromQuery(ctx context.Context, q string, entireThread bool)
 			root := db.makeThread(nil, &tlm, entireThread)
 			if len(root) > 1 {
 				root[0].Dummy = true
-				root[0].FirstChild = root[0].NextSibling
+				fc := &(root[0].FirstChild)
+				for ; *fc != nil; fc = &((*fc).NextSibling) {
+				}
+				*fc = root[0].NextSibling
 				root[0].NextSibling.PrevSibling = nil
 				root[0].NextSibling = nil
 				for i := 1; i < len(root); i++ {