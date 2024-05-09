rjarry/aerc

commit 24eab0f5ef6324b00717997f671cf1d13317bd5c

msglist: fetch headers even when not focused
Do not rely on MessageList.Draw only to fetch missing headers. In Draw,
report the current scroll offset and length to the message store and use
them to determine if a new message UID should be candidate for fetching
headers.

This allows the mail-received hook to work even when the message list is
not focused.

Fixes: https://todo.sr.ht/~rjarry/aerc/147
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Tristan Partin <tristan@partin.io>

lib/msgstore.go
@@ -29,6 +29,10 @@ type MessageStore struct {
 	uids    []uint32
 	threads []*types.Thread
 
+	// Visible UIDs
+	scrollOffset int
+	scrollLen    int
+
 	selectedUid   uint32
 	bodyCallbacks map[uint32][]func(*types.FullMessage)
 
@@ -93,6 +97,8 @@ func NewMessageStore(worker *types.Worker,
 		Messages: make(map[uint32]*models.MessageInfo),
 
 		selectedUid: MagicUid,
+		// default window height until account is drawn once
+		scrollLen: 25,
 
 		bodyCallbacks: make(map[uint32][]func(*types.FullMessage)),
 
@@ -124,6 +130,11 @@ func NewMessageStore(worker *types.Worker,
 
 func (store *MessageStore) SetContext(ctx context.Context) {
 	store.ctx = ctx
+}
+
+func (store *MessageStore) UpdateScroll(offset, length int) {
+	store.scrollOffset = offset
+	store.scrollLen = length
 }
 
 func (store *MessageStore) FetchHeaders(uids []uint32,
@@ -220,21 +231,26 @@ func merge(to *models.MessageInfo, from *models.MessageInfo) {
 }
 
 func (store *MessageStore) Update(msg types.WorkerMessage) {
+	var newUids []uint32
 	update := false
 	updateThreads := false
-	directoryChange := false
+	start := store.scrollOffset
+	end := store.scrollOffset + store.scrollLen
+
 	switch msg := msg.(type) {
 	case *types.OpenDirectory:
 		store.Sort(store.sortCriteria, nil)
 		update = true
 	case *types.DirectoryContents:
 		newMap := make(map[uint32]*models.MessageInfo, len(msg.Uids))
-		for _, uid := range msg.Uids {
+		for i, uid := range msg.Uids {
 			if msg, ok := store.Messages[uid]; ok {
 				newMap[uid] = msg
 			} else {
 				newMap[uid] = nil
-				directoryChange = true
+				if i >= start && i < end {
+					newUids = append(newUids, uid)
+				}
 			}
 		}
 		store.Messages = newMap
@@ -251,12 +267,14 @@ func (store *MessageStore) Update(msg types.WorkerMessage) {
 		store.threads = msg.Threads
 
 		newMap := make(map[uint32]*models.MessageInfo, len(store.uids))
-		for _, uid := range store.uids {
+		for i, uid := range store.uids {
 			if msg, ok := store.Messages[uid]; ok {
 				newMap[uid] = msg
 			} else {
 				newMap[uid] = nil
-				directoryChange = true
+				if i >= start && i < end {
+					newUids = append(newUids, uid)
+				}
 			}
 		}
 
@@ -348,8 +366,11 @@ func (store *MessageStore) Update(msg types.WorkerMessage) {
 		store.update(updateThreads)
 	}
 
-	if directoryChange && store.triggerDirectoryChange != nil {
-		store.triggerDirectoryChange()
+	if len(newUids) > 0 {
+		store.FetchHeaders(newUids, nil)
+		if store.triggerDirectoryChange != nil {
+			store.triggerDirectoryChange()
+		}
 	}
 }
 

widgets/msglist.go
@@ -81,6 +81,8 @@ func (ml *MessageList) Draw(ctx *ui.Context) {
 		}
 	}
 
+	store.UpdateScroll(ml.Scroll(), ml.height)
+
 	textWidth := ctx.Width()
 	if ml.NeedScrollbar() {
 		textWidth -= 1

commit dc306cf6dcd3d99c9f8ee4db486864b050dc7af7

msgstore: restore the directoryChange bool to fix new message bell
In 24eab0f5ef63 the condition under which the triggerDirectoryChange
callback is executed was changed so that it would now only run if there
were new messages in the currently scrolled into view part of the
message list whereas previously it ran for new messages anywhere in the
list.

The motivation for this was to make the mail-received hook work even
when the tab of the receiving account is not currently focused by
fetching the new message's/s' headers needed for the hook to execute
based on what changed in the scrolled into part of the list. This
limitation is needed because apparently we don't currently have a better
way to recognize new messages other than comparing the list of UIDs
provided by the worker and that kept by the store.

My current understanding is that we cannot remove the scroll view limit
because then we would be fetching headers for all messages in the
directory which can be a somewhat intensive operation. However we can
disregard the view limit for the bell as that does not require the
headers: it only cares about that something has changed.

It is noteworthy that the bell is rung on aerc startup: I assume that
this occurs with the initial load of the message store for every account
when the else branch where directoryChange is set to true inevitably
executes.

Overall, this patch is more of a workaround than a proper fix: the ideal
situation would be if we were able to fetch headers for new messages
independently of the scroll status. However as this is how this was
before, it should be suitable as a temporary solution.

There are also further problems here: currently we have triggerNewEmail
and triggerDirectoryChange callbacks which are both supposed to run when
new mail is received, the latter only implicitly. And yet they both use
a different mechanism and thus execute under different circumstances. It
would be ideal to move the bell into the new mail trigger and get rid of
the directory change one as it is otherwise unused. However this is not
done here because for some setups the new mail trigger does not run
until the tab is focused which is what the aforementioned commit was
meant to fix but apparently succeeded in doing so only partially. The
directoryChange trigger does not have this drawback and thus should be
kept until the issue is resolved for all setups.

Also note that for instance for O365 mail the bell still doesn't work
properly as there new mail only appears in the store after the given
folder is reopened (:prev-folder :next-folder) and the bell is thus rung
only then.

Fixes: 24eab0f5ef63 ("msglist: fetch headers even when not focused")

lib/msgstore.go
@@ -249,6 +249,7 @@ func (store *MessageStore) Update(msg types.WorkerMessage) {
 	var newUids []uint32
 	update := false
 	updateThreads := false
+	directoryChange := false
 	start := store.scrollOffset
 	end := store.scrollOffset + store.scrollLen
 
@@ -263,6 +264,7 @@ func (store *MessageStore) Update(msg types.WorkerMessage) {
 				newMap[uid] = msg
 			} else {
 				newMap[uid] = nil
+				directoryChange = true
 				if i >= start && i < end {
 					newUids = append(newUids, uid)
 				}
@@ -287,6 +289,7 @@ func (store *MessageStore) Update(msg types.WorkerMessage) {
 				newMap[uid] = msg
 			} else {
 				newMap[uid] = nil
+				directoryChange = true
 				if i >= start && i < end {
 					newUids = append(newUids, uid)
 				}
@@ -379,6 +382,10 @@ func (store *MessageStore) Update(msg types.WorkerMessage) {
 
 	if update {
 		store.update(updateThreads)
+	}
+
+	if directoryChange && store.triggerDirectoryChange != nil {
+		store.triggerDirectoryChange()
 	}
 
 	if len(newUids) > 0 {