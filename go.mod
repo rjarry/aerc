rjarry/aerc

commit 7c789624d7da0a45c33519ff10830e6c1b97dd1e

mod: update all dependencies
Patch produced with the following commands:

	go mod edit -dropreplace=golang.org/x/crypto
	go get -t -u
	go mod tidy

I dropped the replace of x/crypto. The only use for the fork from
ProtonMail is for openpgp which has been deprecated by the official
implementation. And aerc explicitly uses the correct import path where
needed.

go-smtp update was a bit harsh. The API was changed completely.

Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Bence Ferdinandy <bence@ferdinandy.com>

go.mod
@@ -3,61 +3,58 @@ module git.sr.ht/~rjarry/aerc
 go 1.18
 
 require (
-	git.sr.ht/~rjarry/go-opt v1.3.0
-	git.sr.ht/~rockorager/go-jmap v0.4.5
+	git.sr.ht/~rjarry/go-opt v1.4.0
+	git.sr.ht/~rockorager/go-jmap v0.4.6
 	git.sr.ht/~rockorager/vaxis v0.8.4
-	github.com/ProtonMail/go-crypto v0.0.0-20230417170513-8ee5748c52b5
-	github.com/arran4/golang-ical v0.0.0-20230318005454-19abf92700cc
+	github.com/ProtonMail/go-crypto v1.0.0
+	github.com/arran4/golang-ical v0.2.7
 	github.com/danwakefield/fnmatch v0.0.0-20160403171240-cbb64ac3d964
 	github.com/emersion/go-imap v1.2.1
 	github.com/emersion/go-imap-sortthread v1.2.0
 	github.com/emersion/go-maildir v0.4.1
 	github.com/emersion/go-mbox v1.0.3
-	github.com/emersion/go-message v0.17.0
-	github.com/emersion/go-msgauth v0.6.6
-	github.com/emersion/go-pgpmail v0.2.0
-	github.com/emersion/go-sasl v0.0.0-20220912192320-0145f2c60ead
-	github.com/emersion/go-smtp v0.16.0
+	github.com/emersion/go-message v0.18.0
+	github.com/emersion/go-msgauth v0.6.8
+	github.com/emersion/go-pgpmail v0.2.1
+	github.com/emersion/go-sasl v0.0.0-20231106173351-e73c9f7bad43
+	github.com/emersion/go-smtp v0.21.0
 	github.com/fsnotify/fsevents v0.1.1
-	github.com/fsnotify/fsnotify v1.6.0
+	github.com/fsnotify/fsnotify v1.7.0
 	github.com/gatherstars-com/jwz v1.4.0
 	github.com/go-ini/ini v1.67.0
-	github.com/lithammer/fuzzysearch v1.1.5
-	github.com/mattn/go-isatty v0.0.18
+	github.com/lithammer/fuzzysearch v1.1.8
+	github.com/mattn/go-isatty v0.0.20
 	github.com/mattn/go-runewidth v0.0.15
 	github.com/pkg/errors v0.9.1
 	github.com/riywo/loginshell v0.0.0-20200815045211-7d26008be1ab
 	github.com/stretchr/testify v1.8.4
 	github.com/syndtr/goleveldb v1.0.0
-	golang.org/x/image v0.13.0
-	golang.org/x/oauth2 v0.7.0
-	golang.org/x/sys v0.16.0
+	golang.org/x/image v0.15.0
+	golang.org/x/oauth2 v0.18.0
+	golang.org/x/sys v0.18.0
 	golang.org/x/tools v0.17.0
 )
 
 require (
 	github.com/cloudflare/circl v1.3.7 // indirect
-	github.com/containerd/console v1.0.3 // indirect
-	github.com/creack/pty v1.1.18 // indirect
+	github.com/containerd/console v1.0.4 // indirect
+	github.com/creack/pty v1.1.21 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emersion/go-textwrapper v0.0.0-20200911093747-65d896831594 // indirect
-	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/kr/pretty v0.3.0 // indirect
 	github.com/mattn/go-sixel v0.0.5 // indirect
 	github.com/onsi/gomega v1.20.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/rogpeppe/go-internal v1.8.1 // indirect
 	github.com/soniakeys/quant v1.0.0 // indirect
-	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
 	golang.org/x/mod v0.14.0 // indirect
-	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
-
-replace golang.org/x/crypto => github.com/ProtonMail/crypto v0.0.0-20200420072808-71bec3603bf3

lib/send/smtp.go
@@ -19,32 +19,23 @@ func connectSmtp(starttls bool, host string, domain string) (*smtp.Client, error
 	} else {
 		serverName = host[:strings.IndexRune(host, ':')]
 	}
-	conn, err := smtp.Dial(host)
+	var conn *smtp.Client
+	var err error
+	if starttls {
+		conn, err = smtp.DialStartTLS(host, &tls.Config{ServerName: serverName})
+	} else {
+		conn, err = smtp.Dial(host)
+	}
 	if err != nil {
 		return nil, errors.Wrap(err, "smtp.Dial")
 	}
 	if domain != "" {
 		err := conn.Hello(domain)
 		if err != nil {
+			conn.Close()
 			return nil, errors.Wrap(err, "Hello")
 		}
 	}
-	if starttls {
-		if sup, _ := conn.Extension("STARTTLS"); !sup {
-			err := errors.New("STARTTLS requested, but not supported " +
-				"by this SMTP server. Is someone tampering with your " +
-				"connection?")
-			conn.Close()
-			return nil, err
-		}
-		if err = conn.StartTLS(&tls.Config{
-			ServerName: serverName,
-		}); err != nil {
-			conn.Close()
-			return nil, errors.Wrap(err, "StartTLS")
-		}
-	}
-
 	return conn, nil
 }
 
@@ -122,7 +113,7 @@ func newSmtpSender(
 		return nil, errors.Wrap(err, "conn.Mail")
 	}
 	for _, rcpt := range rcpts {
-		if err := s.conn.Rcpt(rcpt.Address); err != nil {
+		if err := s.conn.Rcpt(rcpt.Address, nil); err != nil {
 			conn.Close()
 			return nil, errors.Wrap(err, "conn.Rcpt")
 		}

commit 017c5468de4fd515b4dc05d70e372808fd98fb88

smtp: fix error when setting smtp-domain with starttls
Some MTA servers require clients to specify their fully qualified
hostname in the HELO command. By default, go-smtp uses "localhost".

 SMTP error 504: <localhost>: Helo command rejected: need fully
 qualified hostname

In go-smtp v0.21.0, there is a regression preventing manually issuing
a HELO command after starttls authentication:

 Connection failed: Hello: smtp: Hello called after other methods

The regression was fixed in go-smtp v0.21.3. Update our dependency.

Fixes: 7c789624d7da ("mod: update all dependencies")
Changelog-fixed: Error when trying to configure `smtp-domain` with
 STARTTLS enabled.
Reported-by: Stefano <foxy@teapot.ovh>
Signed-off-by: Robin Jarry <robin@jarry.cc>
Reviewed-by: Tristan Partin <tristan@partin.io>

go.mod
@@ -17,7 +17,7 @@ require (
 	github.com/emersion/go-msgauth v0.6.8
 	github.com/emersion/go-pgpmail v0.2.1
 	github.com/emersion/go-sasl v0.0.0-20231106173351-e73c9f7bad43
-	github.com/emersion/go-smtp v0.21.0
+	github.com/emersion/go-smtp v0.21.3
 	github.com/fsnotify/fsevents v0.1.1
 	github.com/fsnotify/fsnotify v1.7.0
 	github.com/gatherstars-com/jwz v1.4.0

go.sum
@@ -47,8 +47,8 @@ github.com/emersion/go-sasl v0.0.0-20191210011802-430746ea8b9b/go.mod h1:G/dpzLu
 github.com/emersion/go-sasl v0.0.0-20200509203442-7bfe0ed36a21/go.mod h1:iL2twTeMvZnrg54ZoPDNfJaJaqy0xIQFuBdrLsmspwQ=
 github.com/emersion/go-sasl v0.0.0-20231106173351-e73c9f7bad43 h1:hH4PQfOndHDlpzYfLAAfl63E8Le6F2+EL/cdhlkyRJY=
 github.com/emersion/go-sasl v0.0.0-20231106173351-e73c9f7bad43/go.mod h1:iL2twTeMvZnrg54ZoPDNfJaJaqy0xIQFuBdrLsmspwQ=
-github.com/emersion/go-smtp v0.21.0 h1:ZDZmX9aFUuPlD1lpoT0nC/nozZuIkSCyQIyxdijjCy0=
-github.com/emersion/go-smtp v0.21.0/go.mod h1:qm27SGYgoIPRot6ubfQ/GpiPy/g3PaZAVRxiO/sDUgQ=
+github.com/emersion/go-smtp v0.21.3 h1:7uVwagE8iPYE48WhNsng3RRpCUpFvNl39JGNSIyGVMY=
+github.com/emersion/go-smtp v0.21.3/go.mod h1:qm27SGYgoIPRot6ubfQ/GpiPy/g3PaZAVRxiO/sDUgQ=
 github.com/emersion/go-textwrapper v0.0.0-20160606182133-d0e65e56babe/go.mod h1:aqO8z8wPrjkscevZJFVE1wXJrLpC5LtJG7fqLOsPb2U=
 github.com/emersion/go-textwrapper v0.0.0-20200911093747-65d896831594 h1:IbFBtwoTQyw0fIM5xv1HF+Y+3ZijDR839WMulgxCcUY=
 github.com/emersion/go-textwrapper v0.0.0-20200911093747-65d896831594/go.mod h1:aqO8z8wPrjkscevZJFVE1wXJrLpC5LtJG7fqLOsPb2U=