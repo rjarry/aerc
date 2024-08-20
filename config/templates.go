rjarry/aerc

commit 1d4eafb011f46966848d55f381160e846d0d50c8

templates: add {{.AccountFrom}} template
Add template variable to access the current account's From address.

Implements: https://todo.sr.ht/~rjarry/aerc/150
Changelog-added: New `{{.AccountFrom}}` template variable.
Signed-off-by: Bence Ferdinandy <bence@ferdinandy.com>
Tested-by: Koni Marti <koni.marti@gmail.com>
Acked-by: Robin Jarry <robin@jarry.cc>

config/templates.go
@@ -66,6 +66,7 @@ var (
 
 func (d *dummyData) Account() string                 { return "work" }
 func (d *dummyData) AccountBackend() string          { return "maildir" }
+func (d *dummyData) AccountFrom() *mail.Address      { return nil }
 func (d *dummyData) Signature() string               { return "" }
 func (d *dummyData) Folder() string                  { return "INBOX" }
 func (d *dummyData) To() []*mail.Address             { return []*mail.Address{&addr1} }

lib/state/templates.go
@@ -161,6 +161,13 @@ func (d *templateData) AccountBackend() string {
 	return ""
 }
 
+func (d *templateData) AccountFrom() *mail.Address {
+	if d.account != nil {
+		return d.account.From
+	}
+	return nil
+}
+
 func (d *templateData) Folder() string {
 	if d.folder != nil {
 		return d.folder.Name

models/templates.go
@@ -10,6 +10,7 @@ import (
 type TemplateData interface {
 	Account() string
 	AccountBackend() string
+	AccountFrom() *mail.Address
 	Folder() string
 	To() []*mail.Address
 	Cc() []*mail.Address

doc/aerc-templates.7.scd
@@ -206,6 +206,13 @@ available always.
 	{{.AccountBackend}}
 	```
 
+	The current account's from address:
+
+	```
+	{{.AccountFrom}}
+	{{.AccountFrom.Address}}
+	```
+
 	Currently selected mailbox folder:
 
 	```

commit f2ac5a7c3c6516289c5c07e85dad41d7379df771

templates: fix AccountFrom default breaking startup
The default of {{.AccountFrom}} is currently nil. During startup, if
index-columns has something like {{.AccountFrom.Address}} then startup
will fail due to nil pointer evaluation. Add an example address to
return.

Fixes: 1d4eafb011f4 ("templates: add {{.AccountFrom}} template")
Signed-off-by: Bence Ferdinandy <bence@ferdinandy.com>
Tested-by: skejg <grolleman@zoho.com>
Acked-by: Robin Jarry <robin@jarry.cc>

config/templates.go
@@ -66,7 +66,7 @@ var (
 
 func (d *dummyData) Account() string                 { return "work" }
 func (d *dummyData) AccountBackend() string          { return "maildir" }
-func (d *dummyData) AccountFrom() *mail.Address      { return nil }
+func (d *dummyData) AccountFrom() *mail.Address      { return &addr1 }
 func (d *dummyData) Signature() string               { return "" }
 func (d *dummyData) Folder() string                  { return "INBOX" }
 func (d *dummyData) To() []*mail.Address             { return []*mail.Address{&addr1} }