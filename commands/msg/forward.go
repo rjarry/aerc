rjarry/aerc

commit b94b4c00c9c96355c411881b5e5cc42c5f83d159

forward: allow forwarded flag to be set
Usually, a MUA sets a flag for messages that have been forwarded, but
this is currently not the case for aerc. Consider the forwarded flag
and prepare aerc to set it everytime the :forward command is called and
ends successfully.

Changelog-added: The :forward command now sets the forwarded flag.
Signed-off-by: inwit <inwit@sindominio.net>
Acked-by: Robin Jarry <robin@jarry.cc>

commands/msg/forward.go
@@ -162,7 +162,10 @@ func (f forward) Execute(args []string) error {
 				return
 			}
 			composer.AddAttachment(tmpFileName)
-			composer.OnClose(func(_ *app.Composer) {
+			composer.OnClose(func(c *app.Composer) {
+				if c.Sent() {
+					store.Forwarded([]uint32{msg.Uid}, true, nil)
+				}
 				os.RemoveAll(tmpDir)
 			})
 		})
@@ -210,6 +213,12 @@ func (f forward) Execute(args []string) error {
 			if err != nil {
 				return
 			}
+
+			composer.OnClose(func(c *app.Composer) {
+				if c.Sent() {
+					store.Forwarded([]uint32{msg.Uid}, true, nil)
+				}
+			})
 
 			// add attachments
 			if f.AttachAll {

commands/completion_helpers.go
@@ -45,7 +45,7 @@ func GetAddress(search string) []string {
 
 // GetFlagList returns a list of available flags for completion
 func GetFlagList() []string {
-	return []string{"Seen", "Answered", "Flagged", "Draft"}
+	return []string{"Seen", "Answered", "Forwarded", "Flagged", "Draft"}
 }
 
 // GetDateList returns a list of date terms for completion

config/ui.go
@@ -56,6 +56,7 @@ type UIConfig struct {
 	IconInvalid                   string        `ini:"icon-invalid" default:"[s!]"`
 	IconAttachment                string        `ini:"icon-attachment" default:"a"`
 	IconReplied                   string        `ini:"icon-replied" default:"r"`
+	IconForwarded                 string        `ini:"icon-forwarded" default:"f"`
 	IconNew                       string        `ini:"icon-new" default:"N"`
 	IconOld                       string        `ini:"icon-old" default:"O"`
 	IconDraft                     string        `ini:"icon-draft" default:"d"`

doc/aerc-config.5.scd
@@ -428,6 +428,11 @@ These options are configured in the *[ui]* section of _aerc.conf_.
 
 	Default: _r_
 
+*icon-forwarded* = _<string>_
+	The icon to display in *column-flags* when the message has been forwarded.
+
+	Default: _f_
+
 *icon-flagged* = _<string>_
 	The icon to display in *column-flags* when the message is flagged.
 

doc/aerc.1.scd
@@ -475,6 +475,8 @@ message list, the message in the message viewer, etc).
 			Message has been read
 		_Answered_
 			Message has been answered
+		_Forwarded_
+			Message has been forwarded
 		_Flagged_
 			Message is flagged for urgent/special attention
 		_Draft_

lib/msgstore.go
@@ -711,6 +711,8 @@ func (store *MessageStore) Flag(uids []uint32, flags models.Flags,
 			flagName = "seen"
 		case models.AnsweredFlag:
 			flagName = "answered"
+		case models.ForwardedFlag:
+			flagName = "forwarded"
 		case models.FlaggedFlag:
 			flagName = "flagged"
 		case models.DraftFlag:
@@ -731,6 +733,15 @@ func (store *MessageStore) Answered(uids []uint32, answered bool,
 	store.worker.PostAction(&types.AnsweredMessages{
 		Answered: answered,
 		Uids:     uids,
+	}, cb)
+}
+
+func (store *MessageStore) Forwarded(uids []uint32, forwarded bool,
+	cb func(msg types.WorkerMessage),
+) {
+	store.worker.PostAction(&types.ForwardedMessages{
+		Forwarded: forwarded,
+		Uids:      uids,
 	}, cb)
 }
 

models/models.go
@@ -25,6 +25,9 @@ const (
 	// AnsweredFlag marks a message as having been replied to
 	AnsweredFlag
 
+	// ForwardedFlag marks a message as having been forwarded
+	ForwardedFlag
+
 	// DeletedFlag marks a message as having been deleted
 	DeletedFlag
 

worker/types/messages.go
@@ -186,6 +186,12 @@ type AnsweredMessages struct {
 	Uids     []uint32
 }
 
+type ForwardedMessages struct {
+	Message
+	Forwarded bool
+	Uids      []uint32
+}
+
 type CopyMessages struct {
 	Message
 	Destination       string

commit 7f28ba10d5e900e2ba41ff411f8e60580e663016

forward: fix panic when using :eml
Fix a panic when forwarding a message that was opened with :eml.
:forward will try to the set the forwarded flag but there's no
associated message store available for that message (it's either loaded
from disk or an attachment).

Fixes: b94b4c00c9c9 ("forward: allow forwarded flag to be set")
Reported-by: inwit <inwit@sindominio.net>
Signed-off-by: Koni Marti <koni.marti@gmail.com>
Tested-by: Inwit <inwit@sindominio.net>
Acked-by: Robin Jarry <robin@jarry.cc>

commands/msg/forward.go
@@ -167,7 +167,7 @@ func (f forward) Execute(args []string) error {
 			}
 			composer.AddAttachment(tmpFileName)
 			composer.OnClose(func(c *app.Composer) {
-				if c.Sent() {
+				if c.Sent() && store != nil {
 					store.Forwarded([]models.UID{msg.Uid}, true, nil)
 				}
 				os.RemoveAll(tmpDir)
@@ -219,7 +219,7 @@ func (f forward) Execute(args []string) error {
 			}
 
 			composer.OnClose(func(c *app.Composer) {
-				if c.Sent() {
+				if c.Sent() && store != nil {
 					store.Forwarded([]models.UID{msg.Uid}, true, nil)
 				}
 			})