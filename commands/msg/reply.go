rjarry/aerc

commit 88d5de97d6c2caf37c693a51daf98f81951a37ac

reply: handle the Sender header
Sometimes emails will have a Sender: header, that is different from the
From: header. The only use of this in the wild I have seen to date is
when meeting invitation is forwarded by somebody. The From: header will
be the person organising the meeting, and the Sender: will be the person
forwarding. Naturally, when one replies (e.g. with on accept), it should
go to the meeting oragniser, but sometimes one would want to include the
Sender in such a reply.

When executing :reply determine the To: address in order of Reply-To:,
From:, Sender:. When executing :reply -a, include the Sender: in Cc:.

Implements: https://todo.sr.ht/~rjarry/aerc/259
Link: https://www.rfc-editor.org/rfc/rfc4021#section-2.1.3
Changelog-added: Replying to all will include the Sender in Cc.
Signed-off-by: Bence Ferdinandy <bence@ferdinandy.com>
Reviewed-by: Tim Culverhouse <tim@timculverhouse.com>
Reviewed-by: Tristan Partin <tristan@partin.io>
Acked-by: Robin Jarry <robin@jarry.cc>

commands/msg/reply.go
@@ -86,11 +86,13 @@ func (r reply) Execute(args []string) error {
 	)
 
 	recSet := newAddrSet() // used for de-duping
-
-	if len(msg.Envelope.ReplyTo) != 0 {
+	switch {
+	case len(msg.Envelope.ReplyTo) != 0:
 		to = msg.Envelope.ReplyTo
-	} else {
+	case len(msg.Envelope.From) != 0:
 		to = msg.Envelope.From
+	default:
+		to = msg.Envelope.Sender
 	}
 
 	if !config.Compose.ReplyToSelf {
@@ -125,6 +127,13 @@ func (r reply) Execute(args []string) error {
 		to = append(to, envTos...)
 
 		for _, addr := range msg.Envelope.Cc {
+			// dedupe stuff from the to/from headers
+			if recSet.Contains(addr) {
+				continue
+			}
+			cc = append(cc, addr)
+		}
+		for _, addr := range msg.Envelope.Sender {
 			// dedupe stuff from the to/from headers
 			if recSet.Contains(addr) {
 				continue

lib/rfc822/message.go
@@ -163,6 +163,7 @@ func parseEnvelope(h *mail.Header) *models.Envelope {
 		MessageId: msgID,
 		From:      parseAddressList(h, "from"),
 		ReplyTo:   parseAddressList(h, "reply-to"),
+		Sender:    parseAddressList(h, "sender"),
 		To:        parseAddressList(h, "to"),
 		Cc:        parseAddressList(h, "cc"),
 		Bcc:       parseAddressList(h, "bcc"),

models/models.go
@@ -229,6 +229,7 @@ type Envelope struct {
 	Subject   string
 	From      []*mail.Address
 	ReplyTo   []*mail.Address
+	Sender    []*mail.Address
 	To        []*mail.Address
 	Cc        []*mail.Address
 	Bcc       []*mail.Address

commit bcbcaf617fe2f9c03da78fe544f7ba7d5c44219a

reply: de-duplicate sender address
Sender could already be in the Cc list; make sure it is not duplicated.

Adding the whole cc list to recSet twice is fine.

Fixes: 88d5de97d6c2 ("reply: handle the Sender header")
Signed-off-by: Radim Krčmář <radim@krcmar.dev>
Acked-by: Robin Jarry <robin@jarry.cc>

commands/msg/reply.go
@@ -137,6 +137,7 @@ func (r reply) Execute(args []string) error {
 			}
 			cc = append(cc, addr)
 		}
+		recSet.AddList(cc)
 		for _, addr := range msg.Envelope.Sender {
 			// dedupe stuff from the to/from headers
 			if recSet.Contains(addr) {