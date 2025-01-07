rjarry/aerc

commit 57699b1fa6367a42d5877afcfdb1504e52835ed9

feat: add gpg integration
This commit adds gpg system integration. This is done through two new
packages: gpgbin, which handles the system calls and parsing; and gpg
which is mostly a copy of emersion/go-pgpmail with modifications to
interface with package gpgbin. gpg includes tests for many cases, and
by it's nature also tests package gpgbin. I separated these in case an
external dependency is ever used for the gpg sys-calls/parsing (IE we
mirror how go-pgpmail+openpgp currently are dependencies)

Two new config options are introduced:
* pgp-provider. If it is not explicitly set to "gpg", aerc will default to
it's internal pgp provider
* pgp-key-id: (Optionally) specify a key by short or long keyId

Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>
Acked-by: Koni Marti <koni.marti@gmail.com>
Acked-by: Robin Jarry <robin@jarry.cc>

lib/crypto/gpg/writer.go
@@ -1,0 +1,179 @@
+// writer.go largerly mimics github.com/emersion/go-pgpmail, with changes made
+// to interface with the gpg package in aerc
+
+package gpg
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"mime"
+
+	"git.sr.ht/~rjarry/aerc/lib/crypto/gpg/gpgbin"
+	"github.com/emersion/go-message/textproto"
+)
+
+type EncrypterSigner struct {
+	msgBuf          bytes.Buffer
+	encryptedWriter io.Writer
+	to              []string
+	from            string
+}
+
+func (es *EncrypterSigner) Write(p []byte) (int, error) {
+	return es.msgBuf.Write(p)
+}
+
+func (es *EncrypterSigner) Close() (err error) {
+	r := bytes.NewReader(es.msgBuf.Bytes())
+	enc, err := gpgbin.Encrypt(r, es.to, es.from)
+	if err != nil {
+		return err
+	}
+	es.encryptedWriter.Write(enc)
+	return nil
+}
+
+type Signer struct {
+	mw        *textproto.MultipartWriter
+	signedMsg bytes.Buffer
+	w         io.Writer
+	from      string
+	header    textproto.Header
+}
+
+func (s *Signer) Write(p []byte) (int, error) {
+	return s.signedMsg.Write(p)
+}
+
+func (s *Signer) Close() (err error) {
+	// TODO should write the whole message up here so we can get the proper micalg from the signature packet
+
+	sig, micalg, err := gpgbin.Sign(bytes.NewReader(s.signedMsg.Bytes()), s.from)
+	if err != nil {
+		return err
+	}
+	params := map[string]string{
+		"boundary": s.mw.Boundary(),
+		"protocol": "application/pgp-signature",
+		"micalg":   micalg,
+	}
+	s.header.Set("Content-Type", mime.FormatMediaType("multipart/signed", params))
+
+	if err = textproto.WriteHeader(s.w, s.header); err != nil {
+		return err
+	}
+	boundary := s.mw.Boundary()
+	fmt.Fprintf(s.w, "--%s\r\n", boundary)
+	s.w.Write(s.signedMsg.Bytes())
+	s.w.Write([]byte("\r\n"))
+
+	var signedHeader textproto.Header
+	signedHeader.Set("Content-Type", "application/pgp-signature")
+	signatureWriter, err := s.mw.CreatePart(signedHeader)
+	if err != nil {
+		return err
+	}
+
+	_, err = signatureWriter.Write(sig)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// for tests
+var forceBoundary = ""
+
+type multiCloser []io.Closer
+
+func (mc multiCloser) Close() error {
+	for _, c := range mc {
+		if err := c.Close(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func Encrypt(w io.Writer, h textproto.Header, rcpts []string, from string) (io.WriteCloser, error) {
+	mw := textproto.NewMultipartWriter(w)
+
+	if forceBoundary != "" {
+		mw.SetBoundary(forceBoundary)
+	}
+
+	params := map[string]string{
+		"boundary": mw.Boundary(),
+		"protocol": "application/pgp-encrypted",
+	}
+	h.Set("Content-Type", mime.FormatMediaType("multipart/encrypted", params))
+
+	if err := textproto.WriteHeader(w, h); err != nil {
+		return nil, err
+	}
+
+	var controlHeader textproto.Header
+	controlHeader.Set("Content-Type", "application/pgp-encrypted")
+	controlWriter, err := mw.CreatePart(controlHeader)
+	if err != nil {
+		return nil, err
+	}
+	if _, err = controlWriter.Write([]byte("Version: 1\r\n")); err != nil {
+		return nil, err
+	}
+
+	var encryptedHeader textproto.Header
+	encryptedHeader.Set("Content-Type", "application/octet-stream")
+	encryptedWriter, err := mw.CreatePart(encryptedHeader)
+	if err != nil {
+		return nil, err
+	}
+
+	var buf bytes.Buffer
+	plaintext := &EncrypterSigner{
+		msgBuf:          buf,
+		encryptedWriter: encryptedWriter,
+		to:              rcpts,
+		from:            from,
+	}
+
+	return struct {
+		io.Writer
+		io.Closer
+	}{
+		plaintext,
+		multiCloser{
+			plaintext,
+			mw,
+		},
+	}, nil
+}
+
+func Sign(w io.Writer, h textproto.Header, from string) (io.WriteCloser, error) {
+	mw := textproto.NewMultipartWriter(w)
+
+	if forceBoundary != "" {
+		mw.SetBoundary(forceBoundary)
+	}
+
+	var msg bytes.Buffer
+	plaintext := &Signer{
+		mw:        mw,
+		signedMsg: msg,
+		w:         w,
+		from:      from,
+		header:    h,
+	}
+
+	return struct {
+		io.Writer
+		io.Closer
+	}{
+		plaintext,
+		multiCloser{
+			plaintext,
+			mw,
+		},
+	}, nil
+}

config/aerc.conf
@@ -3,6 +3,12 @@
 
 [general]
 #
+# If set to "gpg", aerc will use system gpg binary and keystore for all crypto
+# operations. Otherwise, the internal openpgp implemenation will be used.
+#
+# Default: internal
+pgp-provider=internal
+
 # By default, the file permissions of accounts.conf must be restrictive and
 # only allow reading by the file owner (0600). Set this option to true to
 # ignore this permission check. Use this with care as it may expose your

config/config.go
@@ -104,6 +104,7 @@ type AccountConfig struct {
 	SignatureCmd      string
 	EnableFoldersSort bool     `ini:"enable-folders-sort"`
 	FoldersSort       []string `ini:"folders-sort" delim:","`
+	PgpKeyId          string   `ini:"pgp-key-id"`
 }
 
 type BindingConfig struct {
@@ -248,6 +249,8 @@ func loadAccountConfig(path string) ([]AccountConfig, error) {
 				account.Archive = val
 			} else if key == "enable-folders-sort" {
 				account.EnableFoldersSort, _ = strconv.ParseBool(val)
+			} else if key == "pgp-key-id" {
+				account.PgpKeyId = val
 			} else if key != "name" {
 				account.Params[key] = val
 			}
@@ -582,13 +585,14 @@ func validateBorderChars(section *ini.Section, config *UIConfig) error {
 
 func validatePgpProvider(section *ini.Section) error {
 	m := map[string]bool{
+		"gpg":      true,
 		"internal": true,
 	}
 	for key, val := range section.KeysHash() {
 		switch key {
 		case "pgp-provider":
 			if !m[strings.ToLower(val)] {
-				return fmt.Errorf("%v must be 'internal'", key)
+				return fmt.Errorf("%v must be either 'gpg' or 'internal'", key)
 			}
 		}
 	}

doc/aerc-config.5.scd
@@ -30,6 +30,13 @@ These options are configured in the *[general]* section of aerc.conf.
 *default-save-path*
 	Used as a default path for save operations if no other path is specified.
 
+*pgp-provider*
+	If set to "gpg", aerc will use system gpg binary and keystore for all
+	crypto operations. Otherwise, the internal openpgp implemenation will be
+	used.
+
+	Default: internal
+
 *unsafe-accounts-conf*
 	By default, the file permissions of accounts.conf must be restrictive
 	and only allow reading by the file owner (_0600_). Set this option to
@@ -578,6 +585,10 @@ Note that many of these configuration options are written for you, such as
 	password. See each protocol's man page for more details.
 
 	Default: none
+
+*pgp-key-id*
+	Specify the key id to use when signing a message. Can be either short or
+	long key id. If unset, aerc will look up the key by email
 
 *postpone*
 	Specifies the folder to save postponed messages to.

lib/crypto/crypto.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 
+	"git.sr.ht/~rjarry/aerc/lib/crypto/gpg"
 	"git.sr.ht/~rjarry/aerc/lib/crypto/pgp"
 	"git.sr.ht/~rjarry/aerc/models"
 	"github.com/ProtonMail/go-crypto/openpgp"
@@ -22,6 +23,8 @@ type Provider interface {
 
 func New(s string) Provider {
 	switch s {
+	case "gpg":
+		return &gpg.Mail{}
 	default:
 		return &pgp.Mail{}
 	}

lib/crypto/gpg/gpg.go
@@ -1,0 +1,62 @@
+package gpg
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"os/exec"
+
+	"git.sr.ht/~rjarry/aerc/lib/crypto/gpg/gpgbin"
+	"git.sr.ht/~rjarry/aerc/models"
+	"github.com/ProtonMail/go-crypto/openpgp"
+	"github.com/emersion/go-message/mail"
+)
+
+// Mail satisfies the PGPProvider interface in aerc
+type Mail struct {
+	logger *log.Logger
+}
+
+func (m *Mail) Init(l *log.Logger) error {
+	m.logger = l
+	_, err := exec.LookPath("gpg")
+	return err
+}
+
+func (m *Mail) Decrypt(r io.Reader, decryptKeys openpgp.PromptFunction) (*models.MessageDetails, error) {
+	gpgReader, err := Read(r)
+	if err != nil {
+		return nil, err
+	}
+	md := gpgReader.MessageDetails
+	md.SignatureValidity = models.Valid
+	if md.SignatureError != "" {
+		md.SignatureValidity = handleSignatureError(md.SignatureError)
+	}
+	return md, nil
+}
+
+func (m *Mail) ImportKeys(r io.Reader) error {
+	return gpgbin.Import(r)
+}
+
+func (m *Mail) Encrypt(buf *bytes.Buffer, rcpts []string, signer string, decryptKeys openpgp.PromptFunction, header *mail.Header) (io.WriteCloser, error) {
+
+	return Encrypt(buf, header.Header.Header, rcpts, signer)
+}
+
+func (m *Mail) Sign(buf *bytes.Buffer, signer string, decryptKeys openpgp.PromptFunction, header *mail.Header) (io.WriteCloser, error) {
+	return Sign(buf, header.Header.Header, signer)
+}
+
+func (m *Mail) Close() {}
+
+func handleSignatureError(e string) models.SignatureValidity {
+	if e == "gpg: missing public key" {
+		return models.UnknownEntity
+	}
+	if e == "gpg: header hash does not match actual sig hash" {
+		return models.MicalgMismatch
+	}
+	return models.UnknownValidity
+}

lib/crypto/gpg/gpg_test.go
@@ -1,0 +1,167 @@
+package gpg
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os/exec"
+	"strings"
+	"testing"
+
+	"git.sr.ht/~rjarry/aerc/models"
+)
+
+func TestHasGpg(t *testing.T) {
+	gpgmail := new(Mail)
+	hasGpg := gpgmail.Init(new(log.Logger))
+
+	if hasGpg != nil {
+		t.Errorf("System does not have GPG")
+	}
+}
+
+func CleanUp() {
+	cmd := exec.Command("gpg", "--batch", "--yes", "--delete-secret-and-public-keys", testKeyId)
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Test cleanup failed: you may need to delete the test keys from your GPG keyring")
+		return
+	}
+}
+
+func toCRLF(s string) string {
+	return strings.ReplaceAll(s, "\n", "\r\n")
+}
+
+func deepEqual(t *testing.T, r *models.MessageDetails, expect *models.MessageDetails) {
+	var resBuf bytes.Buffer
+	if _, err := io.Copy(&resBuf, r.Body); err != nil {
+		t.Fatalf("io.Copy() = %v", err)
+	}
+
+	var expBuf bytes.Buffer
+	if _, err := io.Copy(&expBuf, expect.Body); err != nil {
+		t.Fatalf("io.Copy() = %v", err)
+	}
+
+	if resBuf.String() != expBuf.String() {
+		t.Errorf("MessagesDetails.Body = \n%v\n but want \n%v", resBuf.String(), expBuf.String())
+	}
+
+	if r.IsEncrypted != expect.IsEncrypted {
+		t.Errorf("IsEncrypted = \n%v\n but want \n%v", r.IsEncrypted, expect.IsEncrypted)
+	}
+	if r.IsSigned != expect.IsSigned {
+		t.Errorf("IsSigned = \n%v\n but want \n%v", r.IsSigned, expect.IsSigned)
+	}
+	if r.SignedBy != expect.SignedBy {
+		t.Errorf("SignedBy = \n%v\n but want \n%v", r.SignedBy, expect.SignedBy)
+	}
+	if r.SignedByKeyId != expect.SignedByKeyId {
+		t.Errorf("SignedByKeyId = \n%v\n but want \n%v", r.SignedByKeyId, expect.SignedByKeyId)
+	}
+	if r.SignatureError != expect.SignatureError {
+		t.Errorf("SignatureError = \n%v\n but want \n%v", r.SignatureError, expect.SignatureError)
+	}
+	if r.DecryptedWith != expect.DecryptedWith {
+		t.Errorf("DecryptedWith = \n%v\n but want \n%v", r.DecryptedWith, expect.DecryptedWith)
+	}
+	if r.DecryptedWithKeyId != expect.DecryptedWithKeyId {
+		t.Errorf("DecryptedWithKeyId = \n%v\n but want \n%v", r.DecryptedWithKeyId, expect.DecryptedWithKeyId)
+	}
+}
+
+const testKeyId = `B1A8669354153B799F2217BF307215C13DF7A964`
+
+const testPrivateKeyArmored = `-----BEGIN PGP PRIVATE KEY BLOCK-----
+
+lQOYBF5FJf8BCACvlKhSSsv4P8C3Wbv391SrNUBtFquoMuWKtuCr/Ks6KHuofGLn
+bM55uBSQp908aITBDPkaOPsQ3OvwgF7SM8bNIDVpO7FHzCEg2Ysp99iPET/+LsbY
+ugc8oYSuvA5aFFIOMYbAbI+HmbIBuCs+xp0AcU1cemAPzPBDCZs4xl5Y+/ce2yQz
+ZGK9O/tQQIKoBUOWLo/0byAWyD6Gwn/Le3fVxxK6RPeeizDV6VfzHLxhxBNkMgmd
+QUkBkvqF154wYxhzsHn72ushbJpspKz1LQN7d5u6QOq3h2sLwcLbT457qbMfZsZs
+HLhoOibOd+yJ7C6TRbbyC4sQRr+K1CNGcvhJABEBAAEAB/sGyvoOIP2uL409qreW
+eteoPgmtjsR6X+m4iaW8kaxwNhO+q31KFdARLnmBNTVeem60Z1OV26F/AAUSy2yf
+tkgZNIdMeHY94FxhwHjdWUzkEBdJNrcTuHLCOj9/YSAvBP09tlXPyQNujBgyb9Ug
+ex+k3j1PeB6STev3s/3w3t/Ukm6GvPpRSUac1i0yazGOJhGeVjBn34vqJA+D+JxP
+odlCZnBGaFlj86sQs+2qlrITGCZLeLlFGXo6GEEDipCBJ94ETcpHEEZLZxoZAcdp
+9iQhCK/BNpUO7H7GRs9DxiiWgV2GAeFwgt35kIwuf9X0/3Zt/23KaW/h7xe8G+0e
+C0rfBADGZt5tT+5g7vsdgMCGKqi0jCbHpeLDkPbLjlYKOiWQZntLi+i6My4hjZbh
+sFpWHUfc5SqBe+unClwXKO084UIzFQU5U7v9JKP+s1lCAXf1oNziDeE8p/71O0Np
+J1DQ0WdjPFPH54IzLIbpUwoqha+f/4HERo2/pyIC8RMLNVcVYwQA4o27fAyLePwp
+8ZcfD7BwHoWVAoHx54jMlkFCE02SMR1xXswodvCVJQ3DJ02te6SiCTNac4Ad6rRg
+bL+NO+3pMhY+wY4Q9cte/13U5DAuNFrZpgum4lxQAAKDi8YgU3uEMIzB+WEvF/6d
+ALIZqEl1ASCgrnu2GqG800wyJ0PncWMEAJ8746o5PHS8NZBj7cLr5HlInGFSNaXr
+aclq5/eCbwjKcAYFoHCsc0MgYFtPTtSv7QwfpGcHMujjsuSpSPkwwXHXvfKBdQoF
+vBaQK4WvZ/gGM2GHH3NHf3xVlEffe0K2lvPbD7YNPnlNet2hKeF08nCVD+8Rwmzb
+wCZKimA98u5kM9S0NEpvaG4gRG9lIChUaGlzIGlzIGEgdGVzdCBrZXkpIDxqb2hu
+LmRvZUBleGFtcGxlLm9yZz6JAU4EEwEIADgWIQSxqGaTVBU7eZ8iF78wchXBPfep
+ZAUCXkUl/wIbAwULCQgHAgYVCgkICwIEFgIDAQIeAQIXgAAKCRAwchXBPfepZF4i
+B/49B7q4AfO3xHEa8LK2H+f7Mnm4dRfS2YPov2p6TRe1h2DxwpTevNQUhXw2U0nf
+RIEKBAZqgb7NVktkoh0DWtKatms2yHMAS+ahlQoHb2gRgXa9M9Tq0x5u9sl0NYnx
+7Wu5uu6Ybw9luPKoAfO91T0vei0p3eMn3fIV0O012ITvmgKJPppQDKFJHGZJMbVD
+O4TNxP89HgyhB41RO7AZadvu73S00x2K6x+OR4s/++4Y98vScCPm3DUOXeoHXKGq
+FcNYTxJL9bsE2I0uYgvJSxNoK1dVnmvxp3zzhcxAdzizgMz0ufY6YLMCjy5MDOzP
+ARkmYPXdkJ6jceOIqGLUw1kqnQOYBF5FJf8BCACpsh5cyHB7eEwQvLzJVsXpTW0R
+h/Fe36AwC2Vz13WeE6GFrOvw1qATvtYB1919M4B44YH9J7I5SrFZad86Aw4n5Gi0
+BwLlGNa/oCMvYzlNHaTXURA271ghJqdZizqVUETj3WNoaYm4mYMfb0dcayDJvVPW
+P7InzOsdIRU9WXBUSyVMxNMXccr2UvIuhdPglmVT8NtsWR+q8xBoL2Dp0ojYLVD3
+MlwKe1pE5mEwasYCkWePLWyGdfDW1MhUDsPH3K1IjpPLWU9FBk8KM4z8WooY9/ky
+MIyRw39MvOHGfgcFBpiZwlELNZGSFhbRun03PMk2Qd3k+0FGV1IhFAYsr7QRABEB
+AAEAB/9CfgQup+2HO85WWpYAsGsRLSD5FxLpcWeTm8uPdhPksl1+gxDaSEbmJcc2
+Zq6ngdgrxXUJTJYlo9JVLkplMVBJKlMqg3rLaQ2wfV98EH2h7WUrZ1yaofMe3kYB
+rK/yVMcBoDx067GmryQ1W4WTPXjWA8UHdOLqfH195vorFVIR/NKCK4xTgvXpGp/L
+CPdNRgUvE8Q1zLWUbHGYc7OyiIdcKZugAhZ2CTYybyIfudy4vZ6tMgW6Pm+DuXGq
+p1Lc1dKnZvQCu0pyw7/0EcXamQ1ZwTJel3dZa8Yg3MRHdO37i/fPoYwilT9r51b4
+IBn0nZlekq1pWbNYClrdFWWAgpbnBADKY1cyGZRcwTYWkNG03O46E3doJYmLAAD3
+f/HrQplRpqBohJj5HSMAev81mXLBB5QGpv2vGzkn8H+YlxwDm+2xPgfUR28mNVSQ
+DjQr1GJ7BATL/NB8HJHeNIph/MWmJkFECJCM0+24NRmTzhEUboFVlCeNkOU390fy
+LOGwal1RWwQA1qXMNc8VFqOGRYP8YiS3TWjoyqog1GIw/yxTXrtnUEJA/apkzhaO
+L6xKqmwY26XTaOJRVhtooYpVeMAX9Hj8xZaFQjPdggT9lpyOhAoCCdcNOXZqN+V9
+KMMIZL1fGeu3U0PlV1UwXzdOR3RhiWVKXjaICIBRTiwtKIWK60aTQAMD/0JDGCAa
+D2nHQz0jCXaJwe7Lc3+QpfrC0LboiYgOhKjJ1XyNJqmxQNihPfnd9zRFRvuSDyTE
+qClGZmS2k1FjJalFREW/KLLJL/pgf0Fsk8i50gqcFrA1x6isAgWSJgnWjTPVKLiG
+OOChBL6KzqPMC2joPIDOlyzpB4CgmOwhDIUXMXmJATYEGAEIACAWIQSxqGaTVBU7
+eZ8iF78wchXBPfepZAUCXkUl/wIbDAAKCRAwchXBPfepZOtqB/9xsGEgQgm70KYI
+D39H91k4ef/RlpRDY1ndC0MoPfqE03IEXTC/MjtU+ksPKEoZeQsxVaUJ2WBueI5W
+GJ3Y73pOHAd7N0SyGHT5s6gK1FSx29be1qiPwUu5KR2jpm3RjgpbymnOWe4C6iiY
+CFQ85IX+LzpE+p9bB02PUrmzOb4MBV6E5mg30UjXIX01+bwZq5XSB4/FaUrQOAxL
+uRvVRjK0CEcFbPGIlkPSW6s4M9xCC2sQi7caFKVK6Zqf78KbOwAHqfS0x9u2jtTI
+hsgCjGTIAOQ5lNwpLEMjwLias6e5sM6hcK9Wo+A9Sw23f8lMau5clOZTJeyAUAff
++5anTnUn
+=gemU
+-----END PGP PRIVATE KEY BLOCK-----
+`
+
+const testPublicKeyArmored = `-----BEGIN PGP PUBLIC KEY BLOCK-----
+
+mQENBF5FJf8BCACvlKhSSsv4P8C3Wbv391SrNUBtFquoMuWKtuCr/Ks6KHuofGLn
+bM55uBSQp908aITBDPkaOPsQ3OvwgF7SM8bNIDVpO7FHzCEg2Ysp99iPET/+LsbY
+ugc8oYSuvA5aFFIOMYbAbI+HmbIBuCs+xp0AcU1cemAPzPBDCZs4xl5Y+/ce2yQz
+ZGK9O/tQQIKoBUOWLo/0byAWyD6Gwn/Le3fVxxK6RPeeizDV6VfzHLxhxBNkMgmd
+QUkBkvqF154wYxhzsHn72ushbJpspKz1LQN7d5u6QOq3h2sLwcLbT457qbMfZsZs
+HLhoOibOd+yJ7C6TRbbyC4sQRr+K1CNGcvhJABEBAAG0NEpvaG4gRG9lIChUaGlz
+IGlzIGEgdGVzdCBrZXkpIDxqb2huLmRvZUBleGFtcGxlLm9yZz6JAU4EEwEIADgW
+IQSxqGaTVBU7eZ8iF78wchXBPfepZAUCXkUl/wIbAwULCQgHAgYVCgkICwIEFgID
+AQIeAQIXgAAKCRAwchXBPfepZF4iB/49B7q4AfO3xHEa8LK2H+f7Mnm4dRfS2YPo
+v2p6TRe1h2DxwpTevNQUhXw2U0nfRIEKBAZqgb7NVktkoh0DWtKatms2yHMAS+ah
+lQoHb2gRgXa9M9Tq0x5u9sl0NYnx7Wu5uu6Ybw9luPKoAfO91T0vei0p3eMn3fIV
+0O012ITvmgKJPppQDKFJHGZJMbVDO4TNxP89HgyhB41RO7AZadvu73S00x2K6x+O
+R4s/++4Y98vScCPm3DUOXeoHXKGqFcNYTxJL9bsE2I0uYgvJSxNoK1dVnmvxp3zz
+hcxAdzizgMz0ufY6YLMCjy5MDOzPARkmYPXdkJ6jceOIqGLUw1kquQENBF5FJf8B
+CACpsh5cyHB7eEwQvLzJVsXpTW0Rh/Fe36AwC2Vz13WeE6GFrOvw1qATvtYB1919
+M4B44YH9J7I5SrFZad86Aw4n5Gi0BwLlGNa/oCMvYzlNHaTXURA271ghJqdZizqV
+UETj3WNoaYm4mYMfb0dcayDJvVPWP7InzOsdIRU9WXBUSyVMxNMXccr2UvIuhdPg
+lmVT8NtsWR+q8xBoL2Dp0ojYLVD3MlwKe1pE5mEwasYCkWePLWyGdfDW1MhUDsPH
+3K1IjpPLWU9FBk8KM4z8WooY9/kyMIyRw39MvOHGfgcFBpiZwlELNZGSFhbRun03
+PMk2Qd3k+0FGV1IhFAYsr7QRABEBAAGJATYEGAEIACAWIQSxqGaTVBU7eZ8iF78w
+chXBPfepZAUCXkUl/wIbDAAKCRAwchXBPfepZOtqB/9xsGEgQgm70KYID39H91k4
+ef/RlpRDY1ndC0MoPfqE03IEXTC/MjtU+ksPKEoZeQsxVaUJ2WBueI5WGJ3Y73pO
+HAd7N0SyGHT5s6gK1FSx29be1qiPwUu5KR2jpm3RjgpbymnOWe4C6iiYCFQ85IX+
+LzpE+p9bB02PUrmzOb4MBV6E5mg30UjXIX01+bwZq5XSB4/FaUrQOAxLuRvVRjK0
+CEcFbPGIlkPSW6s4M9xCC2sQi7caFKVK6Zqf78KbOwAHqfS0x9u2jtTIhsgCjGTI
+AOQ5lNwpLEMjwLias6e5sM6hcK9Wo+A9Sw23f8lMau5clOZTJeyAUAff+5anTnUn
+=ZjQT
+-----END PGP PUBLIC KEY BLOCK-----
+`

lib/crypto/gpg/gpgbin/decrypt.go
@@ -1,0 +1,34 @@
+package gpgbin
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+
+	"git.sr.ht/~rjarry/aerc/models"
+)
+
+// Decrypt runs gpg --decrypt on the contents of r. If the packet is signed,
+// the signature is also verified
+func Decrypt(r io.Reader) (*models.MessageDetails, error) {
+	md := new(models.MessageDetails)
+	orig, err := ioutil.ReadAll(r)
+	if err != nil {
+		return md, err
+	}
+	args := []string{"--decrypt"}
+	g := newGpg(bytes.NewReader(orig), args)
+	err = g.cmd.Run()
+	if err != nil {
+		err = parseError(g.stderr.String())
+		switch GPGErrors[err.Error()] {
+		case ERROR_NO_PGP_DATA_FOUND:
+			md.Body = bytes.NewReader(orig)
+			return md, nil
+		default:
+		}
+	}
+	outRdr := bytes.NewReader(g.stdout.Bytes())
+	parse(outRdr, md)
+	return md, nil
+}

commit 7356be2be5b3297ddd351a693669fefd00e97268

gpg: force crlf on base64 blobs
When sending signed messages with outgoing = jmap://, the following
error is returned by the server (provider=Fastmail):

	Message contains bare newlines

RFC 5322 section 2.3 states that:

> CR and LF MUST only occur together as CRLF; they MUST NOT appear
> independently in the body.

Most MTA handle invalid messages without warnings, but some others
reject them.

Ensure GPG generated parts use CRLF to comply with the RFC.

Fixes: 57699b1fa636 ("feat: add gpg integration")
Link: https://www.rfc-editor.org/rfc/rfc5322.html#section-2.3
Changelog-fixed: GPG signatures and encrypted parts now use CRLF line
 endings as required by RFC 5322.
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Matěj Cepl <mcepl@cepl.eu>

lib/crypto/gpg/writer.go
@@ -12,6 +12,7 @@ import (
 
 	"git.sr.ht/~rjarry/aerc/lib/crypto/gpg/gpgbin"
 	"git.sr.ht/~rjarry/aerc/lib/pinentry"
+	"git.sr.ht/~rjarry/aerc/lib/rfc822"
 	"github.com/emersion/go-message"
 	"github.com/emersion/go-message/textproto"
 )
@@ -36,7 +37,7 @@ func (es *EncrypterSigner) Close() (err error) {
 	if err != nil {
 		return err
 	}
-	_, err = es.encryptedWriter.Write(enc)
+	_, err = io.Copy(es.encryptedWriter, rfc822.NewCRLFReader(bytes.NewReader(enc)))
 	if err != nil {
 		return fmt.Errorf("gpg: failed to write encrypted writer: %w", err)
 	}
@@ -107,8 +108,7 @@ func (s *Signer) Close() (err error) {
 	if err != nil {
 		return err
 	}
-
-	_, err = signatureWriter.Write(sig)
+	_, err = io.Copy(signatureWriter, rfc822.NewCRLFReader(bytes.NewReader(sig)))
 	if err != nil {
 		return err
 	}