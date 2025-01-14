rjarry/aerc

commit 7346d20343b9fa8b192c9a5294b26efac9186c4f

gpg: fix signed message encoding
Fix the content encoding for GPG-signed messages.

To remove the Mime-Version header field for the signed message part, the
raw message is parsed with go-message. go-message.Read(), however,
decodes the message body as well (i.e. from quoted-printable to UTF8
depending on the Content-Transfer-Encoding header). This means that the
msg.Body field now contains the decoded message (it is no longer encoded
as quoted-printable). We never encode the message back to the proper
Content-Transfer-Encoding.

To fix this, use net/mail.ReadMessage() to parse the headers and to not
decode the message body.

To verify the issue, send a signed message with the following text:
"19+1=20!"

The message will be properly signed, but the text is wrong; it shows
"19+1 !"; instead it should read "19+1=3D20!".

Fixes: 5e443bce ("gpg: fix mime-version header position")
References: https://todo.sr.ht/~rjarry/aerc/79
Signed-off-by: Koni Marti <koni.marti@gmail.com>
Tested-by: Jens Grassel <jens@wegtam.com>
Acked-by: Robin Jarry <robin@jarry.cc>

lib/crypto/gpg/writer.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"io"
 	"mime"
+	"net/mail"
 
 	"git.sr.ht/~rjarry/aerc/lib/crypto/gpg/gpgbin"
 	"github.com/emersion/go-message"
@@ -51,10 +52,11 @@ func (s *Signer) Write(p []byte) (int, error) {
 }
 
 func (s *Signer) Close() (err error) {
-	msg, err := message.Read(&s.signedMsg)
+	msg, err := mail.ReadMessage(&s.signedMsg)
 	if err != nil {
 		return err
 	}
+	header := message.HeaderFromMap(msg.Header)
 	// Make sure that MIME-Version is *not* set on the signed part header.
 	// It must be set *only* on the top level header.
 	//
@@ -64,10 +66,10 @@ func (s *Signer) Close() (err error) {
 	//
 	// Since the signature is computed on the whole part, including its
 	// header, changing the case can cause the signature to become invalid.
-	msg.Header.Del("Mime-Version")
+	header.Del("Mime-Version")
 
 	var buf bytes.Buffer
-	_ = textproto.WriteHeader(&buf, msg.Header.Header)
+	_ = textproto.WriteHeader(&buf, header.Header)
 	_, _ = io.Copy(&buf, msg.Body)
 
 	sig, micalg, err := gpgbin.Sign(bytes.NewReader(buf.Bytes()), s.from)

commit 3ff25aeaa186514028af1587ce00119e88050a70

pgp: ensure mime-version header is in the right place
Update go-pgpmail which has a similar fix to the one we had in place.
Update our fix to use the same implementation: do not parse the whole
signed part. Only parse the part header, remove the mime-version header
from it if any and re-write it, followed the part body verbatim without
any decoding/re-encoding.

Fixes: 7346d20343b9 ("gpg: fix signed message encoding")
Fixes: 7b8fbe2a3d7c ("gpg: fix mime-version header position")
Signed-off-by: Robin Jarry <robin@jarry.cc>
Reviewed-by: Simon Ser <contact@emersion.fr>

go.mod
@@ -15,7 +15,7 @@ require (
 	github.com/emersion/go-mbox v1.0.3
 	github.com/emersion/go-message v0.18.1
 	github.com/emersion/go-msgauth v0.6.8
-	github.com/emersion/go-pgpmail v0.2.1
+	github.com/emersion/go-pgpmail v0.2.2
 	github.com/emersion/go-sasl v0.0.0-20241020182733-b788ff22d5a6
 	github.com/emersion/go-smtp v0.21.3
 	github.com/fsnotify/fsevents v0.2.0

go.sum
@@ -42,8 +42,8 @@ github.com/emersion/go-message v0.18.1 h1:tfTxIoXFSFRwWaZsgnqS1DSZuGpYGzSmCZD8SK
 github.com/emersion/go-message v0.18.1/go.mod h1:XpJyL70LwRvq2a8rVbHXikPgKj8+aI0kGdHlg16ibYA=
 github.com/emersion/go-msgauth v0.6.8 h1:kW/0E9E8Zx5CdKsERC/WnAvnXvX7q9wTHia1OA4944A=
 github.com/emersion/go-msgauth v0.6.8/go.mod h1:YDwuyTCUHu9xxmAeVj0eW4INnwB6NNZoPdLerpSxRrc=
-github.com/emersion/go-pgpmail v0.2.1 h1:ZgdWoA5A6MnfmMSsQskKFQZ89rltbvZd2REM5nye/ms=
-github.com/emersion/go-pgpmail v0.2.1/go.mod h1:CwGcFRLkhYt0zhxKRu4cKeD0CIB3Vns9nfBb5Na18RA=
+github.com/emersion/go-pgpmail v0.2.2 h1:cO2jwsE0gb8aDdCcVH5Dfe1XV3Rhhw2GVWsmQd3CbaI=
+github.com/emersion/go-pgpmail v0.2.2/go.mod h1:mRB5P7QKiAuOvcT36tdRZvm7nSt7V+f6jbzzup3HuvU=
 github.com/emersion/go-sasl v0.0.0-20191210011802-430746ea8b9b/go.mod h1:G/dpzLu16WtQpBfQ/z3LYiYJn3ZhKSGWn83fyoyQe/k=
 github.com/emersion/go-sasl v0.0.0-20200509203442-7bfe0ed36a21/go.mod h1:iL2twTeMvZnrg54ZoPDNfJaJaqy0xIQFuBdrLsmspwQ=
 github.com/emersion/go-sasl v0.0.0-20241020182733-b788ff22d5a6 h1:oP4q0fw+fOSWn3DfFi4EXdT+B+gTtzx8GC9xsc26Znk=

lib/crypto/gpg/writer.go
@@ -4,16 +4,15 @@
 package gpg
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"io"
 	"mime"
-	"net/mail"
 
 	"git.sr.ht/~rjarry/aerc/lib/crypto/gpg/gpgbin"
 	"git.sr.ht/~rjarry/aerc/lib/pinentry"
 	"git.sr.ht/~rjarry/aerc/lib/rfc822"
-	"github.com/emersion/go-message"
 	"github.com/emersion/go-message/textproto"
 )
 
@@ -57,11 +56,12 @@ func (s *Signer) Write(p []byte) (int, error) {
 }
 
 func (s *Signer) Close() (err error) {
-	msg, err := mail.ReadMessage(&s.signedMsg)
+	reader := bufio.NewReader(&s.signedMsg)
+	header, err := textproto.ReadHeader(reader)
 	if err != nil {
 		return err
 	}
-	header := message.HeaderFromMap(msg.Header)
+
 	// Make sure that MIME-Version is *not* set on the signed part header.
 	// It must be set *only* on the top level header.
 	//
@@ -74,8 +74,8 @@ func (s *Signer) Close() (err error) {
 	header.Del("Mime-Version")
 
 	var buf bytes.Buffer
-	_ = textproto.WriteHeader(&buf, header.Header)
-	_, _ = io.Copy(&buf, msg.Body)
+	_ = textproto.WriteHeader(&buf, header)
+	_, _ = io.Copy(&buf, reader)
 
 	pinentry.Enable()
 	defer pinentry.Disable()