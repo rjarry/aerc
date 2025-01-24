rjarry/aerc

commit 928e5ae026525605d313f1832b0ca8ba8e99f45d

compose: only show default annotations if absent from config
Currently, it is impossible to "hide" bindings presented on the review
screen even if removing the annotations from binds.conf.

If the configuration contains any annotated bindings, completely ignore
all of the default annotations.

Only display bindings from the config that are both present and
annotated.

Fixes: 64502d9e8b04 ("compose: show annotations on the review screen")
Changelog-changed: Key bindings available on the compose review screen
 are now only displayed if they are annotated with a comment. If
 no binding is annotated in `[compose::review]`, then the default
 annotations will be used as a fallback to avoid displaying a blank
 screen.
Reported-by: Skejg <grolleman@zoho.com>
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Inwit <inwit@sindominio.net>

app/compose.go
@@ -1674,6 +1674,17 @@ type reviewMessage struct {
 	grid     *ui.Grid
 }
 
+var fallbackAnnotations = map[string]string{
+	":send<enter>":     "Send",
+	":edit<enter>":     "Edit (body and headers)",
+	":attach<space>":   "Add attachment",
+	":detach<space>":   "Remove attachment",
+	":postpone<enter>": "Postpone",
+	":preview<enter>":  "Preview message",
+	":abort<enter>":    "Abort (discard message, no confirmation)",
+	":choose -o d discard abort -o p postpone postpone<enter>": "Abort or postpone",
+}
+
 func newReviewMessage(composer *Composer, err error) *reviewMessage {
 	bindings := config.Binds.ComposeReview.ForAccount(
 		composer.acctConfig.Name,
@@ -1681,50 +1692,39 @@ func newReviewMessage(composer *Composer, err error) *reviewMessage {
 	bindings = bindings.ForFolder(composer.SelectedDirectory())
 
 	const maxInputWidth = 6
-	type reviewCmd struct {
-		output     string
-		annotation string
-		input      string
+
+	annotated := false
+	for _, binding := range bindings.Bindings {
+		if binding.Annotation != "" {
+			annotated = true
+			break
+		}
 	}
 
-	reviewCmds := []reviewCmd{
-		{":send<enter>", "Send", ""},
-		{":edit<enter>", "Edit (body and headers)", ""},
-		{":attach<space>", "Add attachment", ""},
-		{":detach<space>", "Remove attachment", ""},
-		{":postpone<enter>", "Postpone", ""},
-		{":preview<enter>", "Preview message", ""},
-		{":abort<enter>", "Abort (discard message, no confirmation)", ""},
-		{":choose -o d discard abort -o p postpone postpone<enter>", "Abort or postpone", ""},
+	type reviewCmd struct {
+		input      string
+		output     string
+		annotation string
 	}
-	var actions []string
+
+	var reviewCmds []reviewCmd
+
 	for _, binding := range bindings.Bindings {
 		inputs := config.FormatKeyStrokes(binding.Input)
 		outputs := config.FormatKeyStrokes(binding.Output)
-		found := false
-		for i, rcmd := range reviewCmds {
-			if outputs == rcmd.output {
-				found = true
-				if reviewCmds[i].input == "" {
-					reviewCmds[i].input = inputs
-				} else {
-					reviewCmds[i].input += ", " + inputs
-				}
-				if binding.Annotation != "" {
-					// overwrite default description with
-					// user annotations if present
-					reviewCmds[i].annotation = binding.Annotation
-				}
-				break
-			}
-		}
-		if !found {
-			rcmd := reviewCmd{
+
+		if annotated && binding.Annotation != "" {
+			reviewCmds = append(reviewCmds, reviewCmd{
+				input:      inputs,
 				output:     outputs,
 				annotation: binding.Annotation,
+			})
+		} else if annotation, ok := fallbackAnnotations[outputs]; ok {
+			reviewCmds = append(reviewCmds, reviewCmd{
 				input:      inputs,
-			}
-			reviewCmds = append(reviewCmds, rcmd)
+				output:     outputs,
+				annotation: annotation,
+			})
 		}
 	}
 
@@ -1741,6 +1741,7 @@ func newReviewMessage(composer *Composer, err error) *reviewMessage {
 	}
 	widthstr := strconv.Itoa(width)
 
+	var actions []string
 	for _, rcmd := range reviewCmds {
 		if rcmd.input != "" {
 			actions = append(actions, fmt.Sprintf("  %-"+widthstr+"s  %-40s  %s",

doc/aerc-binds.5.scd
@@ -60,7 +60,8 @@ The available contexts are:
 
 		p = :postpone<Enter> # I'll work on it later
 
-	The order in which the bindings are defined is preserved.
+	Only bindings that are annotated with a comment will be displayed on the
+	review screen. The order in which they are defined is preserved.
 
 *[terminal]*
 	keybindings for terminal tabs

commit 1203f924408a39dd5dd9ec91695cad150af85ab9

compose: only hide bindings explicitly identified
My boolean fu was too weak. Even if default bindings annotations are
removed, they will be forcibly restored.

I was made aware that this behaviour was preferable compared to simply
make them disappear without warning.

Add a special "-" annotation to explicitly exclude bindings from the
review screen.

If a binding has no annotation at all, still display it without
annotation, just with its command.

Fixes: 928e5ae02652 ("compose: only show default annotations if absent from config")
Changelog-changed: It is now possible to explicitly hide key bindings
 from the compose review screen by using a special ` # -` annotation.
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Skejg <grolleman@zoho.com>

app/compose.go
@@ -1674,7 +1674,7 @@ type reviewMessage struct {
 	grid     *ui.Grid
 }
 
-var fallbackAnnotations = map[string]string{
+var defaultAnnotations = map[string]string{
 	":send<enter>":     "Send",
 	":edit<enter>":     "Edit (body and headers)",
 	":attach<space>":   "Add attachment",
@@ -1691,16 +1691,6 @@ func newReviewMessage(composer *Composer, err error) *reviewMessage {
 	)
 	bindings = bindings.ForFolder(composer.SelectedDirectory())
 
-	const maxInputWidth = 6
-
-	annotated := false
-	for _, binding := range bindings.Bindings {
-		if binding.Annotation != "" {
-			annotated = true
-			break
-		}
-	}
-
 	type reviewCmd struct {
 		input      string
 		output     string
@@ -1710,22 +1700,22 @@ func newReviewMessage(composer *Composer, err error) *reviewMessage {
 	var reviewCmds []reviewCmd
 
 	for _, binding := range bindings.Bindings {
+		if binding.Annotation == "-" {
+			// explicitly hidden by user
+			continue
+		}
+
 		inputs := config.FormatKeyStrokes(binding.Input)
 		outputs := config.FormatKeyStrokes(binding.Output)
-
-		if annotated && binding.Annotation != "" {
-			reviewCmds = append(reviewCmds, reviewCmd{
-				input:      inputs,
-				output:     outputs,
-				annotation: binding.Annotation,
-			})
-		} else if annotation, ok := fallbackAnnotations[outputs]; ok {
-			reviewCmds = append(reviewCmds, reviewCmd{
-				input:      inputs,
-				output:     outputs,
-				annotation: annotation,
-			})
+		annotation := binding.Annotation
+		if annotation == "" {
+			annotation = defaultAnnotations[outputs]
 		}
+		reviewCmds = append(reviewCmds, reviewCmd{
+			input:      inputs,
+			output:     outputs,
+			annotation: annotation,
+		})
 	}
 
 	longest := 0
@@ -1735,6 +1725,7 @@ func newReviewMessage(composer *Composer, err error) *reviewMessage {
 		}
 	}
 
+	const maxInputWidth = 6
 	width := longest
 	if longest < maxInputWidth {
 		width = maxInputWidth
@@ -1743,10 +1734,8 @@ func newReviewMessage(composer *Composer, err error) *reviewMessage {
 
 	var actions []string
 	for _, rcmd := range reviewCmds {
-		if rcmd.input != "" {
-			actions = append(actions, fmt.Sprintf("  %-"+widthstr+"s  %-40s  %s",
-				rcmd.input, rcmd.annotation, rcmd.output))
-		}
+		actions = append(actions, fmt.Sprintf("  %-"+widthstr+"s  %-40s  %s",
+			rcmd.input, rcmd.annotation, rcmd.output))
 	}
 
 	spec := []ui.GridSpec{

doc/aerc-binds.5.scd
@@ -54,14 +54,18 @@ The available contexts are:
 	keybindings for the composer, when reviewing the email before it's sent
 
 	To customize the description shown on the review screen, add a comment
-	(" # ") at the end of the keybinding.
-
-	Example:
+	(_" # "_) at the end of the keybinding. Example:
 
 		p = :postpone<Enter> # I'll work on it later
 
-	Only bindings that are annotated with a comment will be displayed on the
-	review screen. The order in which they are defined is preserved.
+	The order in which bindings are defined is preserved on the review
+	screen. If a non-default binding is not annotated it will be displayed
+	without any description.
+
+	To hide a binding from the review screen, explicitly annotate it with
+	a _" # -"_ comment. Example:
+
+		<C-e> = :encrypt<Enter> # -
 
 *[terminal]*
 	keybindings for terminal tabs