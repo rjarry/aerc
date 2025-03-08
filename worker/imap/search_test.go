rjarry/aerc

commit 8464b373851142b0becaaa10db34df3559b2b62e

search: use a common api for all workers
Define a SearchCriteria structure. Update the FetchDirectoryContents,
FetchDirectoryThreaded and SearchDirectory worker messages to include
this SearchCriteria structure instead of a []string slice.

Parse the search arguments in a single place into a SearchCriteria
structure and use it to search/filter via the message store.

Update all workers to use that new API. Clarify the man page indicating
that notmuch supports searching with aerc's syntax and also with notmuch
specific syntax.

getopt is no longer needed, remove it from go.mod.

NB: to support more complex search filters in JMAP, we need to use an
email.Filter interface. Since GOB does not support encoding/decoding
interfaces, store the raw SearchCriteria and []SortCriterion values in
the cached FolderContents. Translate them to JMAP API objects when
sending an email.Query request to the server.

Signed-off-by: Robin Jarry <robin@jarry.cc>
Reviewed-by: Koni Marti <koni.marti@gmail.com>
Tested-by: Moritz Poldrack <moritz@poldrack.dev>
Tested-by: Inwit <inwit@sindominio.net>

worker/imap/search.go
@@ -1,95 +1,50 @@
 package imap
 
 import (
-	"errors"
-	"strings"
-
 	"github.com/emersion/go-imap"
 
-	"git.sr.ht/~rjarry/aerc/lib/parse"
-	"git.sr.ht/~rjarry/aerc/log"
-	"git.sr.ht/~sircmpwn/getopt"
+	"git.sr.ht/~rjarry/aerc/worker/types"
+	"git.sr.ht/~rjarry/go-opt"
 )
 
-func parseSearch(args []string) (*imap.SearchCriteria, error) {
+func translateSearch(c *types.SearchCriteria) *imap.SearchCriteria {
 	criteria := imap.NewSearchCriteria()
-	if len(args) == 0 {
-		return criteria, nil
+	if c == nil {
+		return criteria
 	}
+	criteria.WithFlags = translateFlags(c.WithFlags)
+	criteria.WithoutFlags = translateFlags(c.WithoutFlags)
 
-	opts, optind, err := getopt.Getopts(args, "rubax:X:t:H:f:c:d:")
-	if err != nil {
-		return nil, err
+	if !c.StartDate.IsZero() {
+		criteria.SentSince = c.StartDate
 	}
-	body := false
-	text := false
-	for _, opt := range opts {
-		switch opt.Option {
-		case 'r':
-			criteria.WithFlags = append(criteria.WithFlags, imap.SeenFlag)
-		case 'u':
-			criteria.WithoutFlags = append(criteria.WithoutFlags, imap.SeenFlag)
-		case 'x':
-			if f, err := getParsedFlag(opt.Value); err == nil {
-				criteria.WithFlags = append(criteria.WithFlags, f)
-			}
-		case 'X':
-			if f, err := getParsedFlag(opt.Value); err == nil {
-				criteria.WithoutFlags = append(criteria.WithoutFlags, f)
-			}
-		case 'H':
-			if strings.Contains(opt.Value, ": ") {
-				HeaderValue := strings.SplitN(opt.Value, ": ", 2)
-				criteria.Header.Add(HeaderValue[0], HeaderValue[1])
-			} else {
-				log.Errorf("Header is not given properly, must be given in format `Header: Value`")
-				continue
-			}
-		case 'f':
-			criteria.Header.Add("From", opt.Value)
-		case 't':
-			criteria.Header.Add("To", opt.Value)
-		case 'c':
-			criteria.Header.Add("Cc", opt.Value)
-		case 'b':
-			body = true
-		case 'a':
-			text = true
-		case 'd':
-			start, end, err := parse.DateRange(opt.Value)
-			if err != nil {
-				log.Errorf("failed to parse start date: %v", err)
-				continue
-			}
-			if !start.IsZero() {
-				criteria.SentSince = start
-			}
-			if !end.IsZero() {
-				criteria.SentBefore = end
+	if !c.StartDate.IsZero() {
+		criteria.SentBefore = c.EndDate
+	}
+	for k, v := range c.Headers {
+		criteria.Header[k] = v
+	}
+	for _, f := range c.From {
+		criteria.Header.Add("From", f)
+	}
+	for _, t := range c.To {
+		criteria.Header.Add("To", t)
+	}
+	for _, c := range c.Cc {
+		criteria.Header.Add("Cc", c)
+	}
+	terms := opt.LexArgs(c.Terms)
+	if terms.Count() > 0 {
+		switch {
+		case c.SearchAll:
+			criteria.Text = terms.Args()
+		case c.SearchBody:
+			criteria.Body = terms.Args()
+		default:
+			for _, term := range terms.Args() {
+				criteria.Header.Add("Subject", term)
 			}
 		}
 	}
-	switch {
-	case text:
-		criteria.Text = args[optind:]
-	case body:
-		criteria.Body = args[optind:]
-	default:
-		for _, arg := range args[optind:] {
-			criteria.Header.Add("Subject", arg)
-		}
-	}
-	return criteria, nil
-}
-
-func getParsedFlag(name string) (string, error) {
-	switch strings.ToLower(name) {
-	case "seen":
-		return imap.SeenFlag, nil
-	case "flagged":
-		return imap.FlaggedFlag, nil
-	case "answered":
-		return imap.AnsweredFlag, nil
-	}
-	return imap.FlaggedFlag, errors.New("Flag not suppored")
+	return criteria
 }

commands/account/search.go
@@ -2,18 +2,35 @@ package account
 
 import (
 	"errors"
+	"fmt"
+	"net/textproto"
 	"strings"
+	"time"
 
 	"git.sr.ht/~rjarry/aerc/app"
 	"git.sr.ht/~rjarry/aerc/commands"
+	"git.sr.ht/~rjarry/aerc/lib/parse"
 	"git.sr.ht/~rjarry/aerc/lib/state"
 	"git.sr.ht/~rjarry/aerc/lib/ui"
 	"git.sr.ht/~rjarry/aerc/log"
+	"git.sr.ht/~rjarry/aerc/models"
 	"git.sr.ht/~rjarry/aerc/worker/types"
 )
 
 type SearchFilter struct {
-	Unused struct{} `opt:"-"`
+	Read         bool                 `opt:"-r" action:"ParseRead"`
+	Unread       bool                 `opt:"-u" action:"ParseUnread"`
+	Body         bool                 `opt:"-b"`
+	All          bool                 `opt:"-a"`
+	Headers      textproto.MIMEHeader `opt:"-H" action:"ParseHeader" metavar:"<header>:<value>"`
+	WithFlags    models.Flags         `opt:"-x" action:"ParseFlag"`
+	WithoutFlags models.Flags         `opt:"-X" action:"ParseNotFlag"`
+	To           []string             `opt:"-t" action:"ParseTo"`
+	From         []string             `opt:"-f" action:"ParseFrom"`
+	Cc           []string             `opt:"-c" action:"ParseCc"`
+	StartDate    time.Time            `opt:"-d" action:"ParseDate"`
+	EndDate      time.Time
+	Terms        string `opt:"..." required:"false"`
 }
 
 func init() {
@@ -49,7 +66,82 @@ func (SearchFilter) Complete(args []string) []string {
 	return nil
 }
 
-func (SearchFilter) Execute(args []string) error {
+func (s *SearchFilter) ParseRead(arg string) error {
+	s.WithFlags |= models.SeenFlag
+	s.WithoutFlags &^= models.SeenFlag
+	return nil
+}
+
+func (s *SearchFilter) ParseUnread(arg string) error {
+	s.WithFlags &^= models.SeenFlag
+	s.WithoutFlags |= models.SeenFlag
+	return nil
+}
+
+var flagValues = map[string]models.Flags{
+	"seen":     models.SeenFlag,
+	"answered": models.AnsweredFlag,
+	"flagged":  models.FlaggedFlag,
+}
+
+func (s *SearchFilter) ParseFlag(arg string) error {
+	f, ok := flagValues[strings.ToLower(arg)]
+	if !ok {
+		return fmt.Errorf("%q unknown flag", arg)
+	}
+	s.WithFlags |= f
+	s.WithoutFlags &^= f
+	return nil
+}
+
+func (s *SearchFilter) ParseNotFlag(arg string) error {
+	f, ok := flagValues[strings.ToLower(arg)]
+	if !ok {
+		return fmt.Errorf("%q unknown flag", arg)
+	}
+	s.WithFlags &^= f
+	s.WithoutFlags |= f
+	return nil
+}
+
+func (s *SearchFilter) ParseHeader(arg string) error {
+	name, value, hasColon := strings.Cut(arg, ":")
+	if !hasColon {
+		return fmt.Errorf("%q invalid syntax", arg)
+	}
+	if s.Headers == nil {
+		s.Headers = make(textproto.MIMEHeader)
+	}
+	s.Headers.Add(name, strings.TrimSpace(value))
+	return nil
+}
+
+func (s *SearchFilter) ParseTo(arg string) error {
+	s.To = append(s.To, arg)
+	return nil
+}
+
+func (s *SearchFilter) ParseFrom(arg string) error {
+	s.From = append(s.From, arg)
+	return nil
+}
+
+func (s *SearchFilter) ParseCc(arg string) error {
+	s.Cc = append(s.Cc, arg)
+	return nil
+}
+
+func (s *SearchFilter) ParseDate(arg string) error {
+	start, end, err := parse.DateRange(arg)
+	if err != nil {
+		return err
+	}
+	s.StartDate = start
+	s.EndDate = end
+	return nil
+}
+
+func (s SearchFilter) Execute(args []string) error {
 	acct := app.SelectedAccount()
 	if acct == nil {
 		return errors.New("No account selected")
@@ -59,12 +151,26 @@ func (SearchFilter) Execute(args []string) error {
 		return errors.New("Cannot perform action. Messages still loading")
 	}
 
+	criteria := types.SearchCriteria{
+		WithFlags:    s.WithFlags,
+		WithoutFlags: s.WithoutFlags,
+		From:         s.From,
+		To:           s.To,
+		Cc:           s.Cc,
+		Headers:      s.Headers,
+		StartDate:    s.StartDate,
+		EndDate:      s.EndDate,
+		SearchBody:   s.Body,
+		SearchAll:    s.All,
+		Terms:        s.Terms,
+	}
+
 	if args[0] == "filter" {
 		if len(args[1:]) == 0 {
 			return Clear{}.Execute([]string{"clear"})
 		}
 		acct.SetStatus(state.FilterActivity("Filtering..."), state.Search(""))
-		store.SetFilter(args[1:])
+		store.SetFilter(&criteria)
 		cb := func(msg types.WorkerMessage) {
 			if _, ok := msg.(*types.Done); ok {
 				acct.SetStatus(state.FilterResult(strings.Join(args, " ")))
@@ -81,7 +187,7 @@ func (SearchFilter) Execute(args []string) error {
 			// TODO: Remove when stores have multiple OnUpdate handlers
 			ui.Invalidate()
 		}
-		store.Search(args, cb)
+		store.Search(&criteria, cb)
 	}
 	return nil
 }

worker/jmap/search.go
@@ -1,63 +1,98 @@
 package jmap
 
 import (
-	"strings"
-
-	"git.sr.ht/~rjarry/aerc/lib/parse"
-	"git.sr.ht/~rjarry/aerc/log"
+	"git.sr.ht/~rjarry/aerc/worker/types"
+	"git.sr.ht/~rockorager/go-jmap"
 	"git.sr.ht/~rockorager/go-jmap/mail/email"
-	"git.sr.ht/~sircmpwn/getopt"
+	"git.sr.ht/~rockorager/go-jmap/mail/mailbox"
 )
 
-func parseSearch(args []string) (*email.FilterCondition, error) {
-	f := new(email.FilterCondition)
-	if len(args) == 0 {
-		return f, nil
+func (w *JMAPWorker) translateSearch(
+	mbox jmap.ID, criteria *types.SearchCriteria,
+) email.Filter {
+	cond := new(email.FilterCondition)
+
+	if mbox == "" {
+		// all mail virtual folder: display all but trash and spam
+		var mboxes []jmap.ID
+		if id, ok := w.roles[mailbox.RoleJunk]; ok {
+			mboxes = append(mboxes, id)
+		}
+		if id, ok := w.roles[mailbox.RoleTrash]; ok {
+			mboxes = append(mboxes, id)
+		}
+		cond.InMailboxOtherThan = mboxes
+	} else {
+		cond.InMailbox = mbox
+	}
+	if criteria == nil {
+		return cond
 	}
 
-	opts, optind, err := getopt.Getopts(args, "rubax:X:t:H:f:c:d:")
-	if err != nil {
-		return nil, err
+	// dates
+	if !criteria.StartDate.IsZero() {
+		cond.After = &criteria.StartDate
 	}
-	body := false
-	text := false
-	for _, opt := range opts {
-		switch opt.Option {
-		case 'r':
-			f.HasKeyword = "$seen"
-		case 'u':
-			f.NotKeyword = "$seen"
-		case 'f':
-			f.From = opt.Value
-		case 't':
-			f.To = opt.Value
-		case 'c':
-			f.Cc = opt.Value
-		case 'b':
-			body = true
-		case 'a':
-			text = true
-		case 'd':
-			start, end, err := parse.DateRange(opt.Value)
-			if err != nil {
-				log.Errorf("failed to parse start date: %v", err)
-				continue
-			}
-			if !start.IsZero() {
-				f.After = &start
-			}
-			if !end.IsZero() {
-				f.Before = &end
-			}
+	if !criteria.EndDate.IsZero() {
+		cond.Before = &criteria.EndDate
+	}
+
+	// general search terms
+	switch {
+	case criteria.SearchAll:
+		cond.Text = criteria.Terms
+	case criteria.SearchBody:
+		cond.Body = criteria.Terms
+	default:
+		cond.Subject = criteria.Terms
+	}
+
+	filter := &email.FilterOperator{Operator: jmap.OperatorAND}
+	filter.Conditions = append(filter.Conditions, cond)
+
+	// keywords/flags
+	for kw := range flagsToKeywords(criteria.WithFlags) {
+		filter.Conditions = append(filter.Conditions,
+			&email.FilterCondition{HasKeyword: kw})
+	}
+	for kw := range flagsToKeywords(criteria.WithoutFlags) {
+		filter.Conditions = append(filter.Conditions,
+			&email.FilterCondition{NotKeyword: kw})
+	}
+
+	// recipients
+	addrs := &email.FilterOperator{
+		Operator: jmap.OperatorOR,
+	}
+	for _, from := range criteria.From {
+		addrs.Conditions = append(addrs.Conditions,
+			&email.FilterCondition{From: from})
+	}
+	for _, to := range criteria.To {
+		addrs.Conditions = append(addrs.Conditions,
+			&email.FilterCondition{To: to})
+	}
+	for _, cc := range criteria.Cc {
+		addrs.Conditions = append(addrs.Conditions,
+			&email.FilterCondition{Cc: cc})
+	}
+	if len(addrs.Conditions) > 0 {
+		filter.Conditions = append(filter.Conditions, addrs)
+	}
+
+	// specific headers
+	headers := &email.FilterOperator{
+		Operator: jmap.OperatorAND,
+	}
+	for h, values := range criteria.Headers {
+		for _, v := range values {
+			headers.Conditions = append(headers.Conditions,
+				&email.FilterCondition{Header: []string{h, v}})
 		}
 	}
-	switch {
-	case text:
-		f.Text = strings.Join(args[optind:], " ")
-	case body:
-		f.Body = strings.Join(args[optind:], " ")
-	default:
-		f.Subject = strings.Join(args[optind:], " ")
+	if len(headers.Conditions) > 0 {
+		filter.Conditions = append(filter.Conditions, headers)
 	}
-	return f, nil
+
+	return filter
 }

worker/lib/search.go
@@ -2,111 +2,23 @@ package lib
 
 import (
 	"io"
-	"net/textproto"
 	"strings"
-	"time"
 	"unicode"
 
-	"git.sr.ht/~sircmpwn/getopt"
-
-	"git.sr.ht/~rjarry/aerc/lib/parse"
+	"git.sr.ht/~rjarry/aerc/lib"
 	"git.sr.ht/~rjarry/aerc/lib/rfc822"
 	"git.sr.ht/~rjarry/aerc/log"
 	"git.sr.ht/~rjarry/aerc/models"
+	"git.sr.ht/~rjarry/aerc/worker/types"
+	"git.sr.ht/~rjarry/go-opt"
 )
 
-type searchCriteria struct {
-	Header textproto.MIMEHeader
-	Body   []string
-	Text   []string
-
-	WithFlags    models.Flags
-	WithoutFlags models.Flags
-
-	startDate, endDate time.Time
-}
-
-func GetSearchCriteria(args []string) (*searchCriteria, error) {
-	criteria := &searchCriteria{Header: make(textproto.MIMEHeader)}
-
-	opts, optind, err := getopt.Getopts(args, "rux:X:bat:H:f:c:d:")
-	if err != nil {
-		return nil, err
-	}
-	body := false
-	text := false
-	for _, opt := range opts {
-		switch opt.Option {
-		case 'r':
-			criteria.WithFlags |= models.SeenFlag
-		case 'u':
-			criteria.WithoutFlags |= models.SeenFlag
-		case 'x':
-			criteria.WithFlags |= getParsedFlag(opt.Value)
-		case 'X':
-			criteria.WithoutFlags |= getParsedFlag(opt.Value)
-		case 'H':
-			if strings.Contains(opt.Value, ": ") {
-				HeaderValue := strings.SplitN(opt.Value, ": ", 2)
-				criteria.Header.Add(HeaderValue[0], HeaderValue[1])
-			} else {
-				log.Errorf("Header is not given properly, must be given in format `Header: Value`")
-				continue
-			}
-		case 'f':
-			criteria.Header.Add("From", opt.Value)
-		case 't':
-			criteria.Header.Add("To", opt.Value)
-		case 'c':
-			criteria.Header.Add("Cc", opt.Value)
-		case 'b':
-			body = true
-		case 'd':
-			start, end, err := parse.DateRange(opt.Value)
-			if err != nil {
-				log.Errorf("failed to parse start date: %v", err)
-				continue
-			}
-			if !start.IsZero() {
-				criteria.startDate = start
-			}
-			if !end.IsZero() {
-				criteria.endDate = end
-			}
-		}
-	}
-	switch {
-	case text:
-		criteria.Text = args[optind:]
-	case body:
-		criteria.Body = args[optind:]
-	default:
-		for _, arg := range args[optind:] {
-			criteria.Header.Add("Subject", arg)
-		}
-	}
-	return criteria, nil
-}
-
-func getParsedFlag(name string) models.Flags {
-	var f models.Flags
-	switch strings.ToLower(name) {
-	case "seen":
-		f = models.SeenFlag
-	case "answered":
-		f = models.AnsweredFlag
-	case "flagged":
-		f = models.FlaggedFlag
-	}
-	return f
-}
-
-func Search(messages []rfc822.RawMessage, criteria *searchCriteria) ([]uint32, error) {
-	requiredParts := getRequiredParts(criteria)
+func Search(messages []rfc822.RawMessage, criteria *types.SearchCriteria) ([]uint32, error) {
+	requiredParts := GetRequiredParts(criteria)
 
 	matchedUids := []uint32{}
 	for _, m := range messages {
-		success, err := searchMessage(m, criteria, requiredParts)
+		success, err := SearchMessage(m, criteria, requiredParts)
 		if err != nil {
 			return nil, err
 		} else if success {
@@ -119,17 +31,19 @@ func Search(messages []rfc822.RawMessage, criteria *searchCriteria) ([]uint32, e
 
 // searchMessage executes the search criteria for the given RawMessage,
 // returns true if search succeeded
-func searchMessage(message rfc822.RawMessage, criteria *searchCriteria,
+func SearchMessage(message rfc822.RawMessage, criteria *types.SearchCriteria,
 	parts MsgParts,
 ) (bool, error) {
+	if criteria == nil {
+		return true, nil
+	}
 	// setup parts of the message to use in the search
 	// this is so that we try to minimise reading unnecessary parts
 	var (
-		flags  models.Flags
-		header *models.MessageInfo
-		body   string
-		all    string
-		err    error
+		flags models.Flags
+		info  *models.MessageInfo
+		text  string
+		err   error
 	)
 
 	if parts&FLAGS > 0 {
@@ -138,14 +52,34 @@ func searchMessage(message rfc822.RawMessage, criteria *searchCriteria,
 			return false, err
 		}
 	}
-	if parts&HEADER > 0 || parts&DATE > 0 {
-		header, err = rfc822.MessageInfo(message)
+	if parts&HEADER > 0 || parts&DATE > 0 || (parts&(BODY|ALL)) == 0 {
+		info, err = rfc822.MessageInfo(message)
 		if err != nil {
 			return false, err
 		}
 	}
-	if parts&BODY > 0 {
-		// TODO: select body properly; this is just an 'all' clone
+	switch {
+	case parts&BODY > 0:
+		path := lib.FindFirstNonMultipart(info.BodyStructure, nil)
+		reader, err := message.NewReader()
+		if err != nil {
+			return false, err
+		}
+		defer reader.Close()
+		msg, err := rfc822.ReadMessage(reader)
+		if err != nil {
+			return false, err
+		}
+		part, err := rfc822.FetchEntityPartReader(msg, path)
+		if err != nil {
+			return false, err
+		}
+		bytes, err := io.ReadAll(part)
+		if err != nil {
+			return false, err
+		}
+		text = string(bytes)
+	case parts&ALL > 0:
 		reader, err := message.NewReader()
 		if err != nil {
 			return false, err
@@ -155,26 +89,16 @@ func searchMessage(message rfc822.RawMessage, criteria *searchCriteria,
 		if err != nil {
 			return false, err
 		}
-		body = string(bytes)
-	}
-	if parts&ALL > 0 {
-		reader, err := message.NewReader()
-		if err != nil {
-			return false, err
-		}
-		defer reader.Close()
-		bytes, err := io.ReadAll(reader)
-		if err != nil {
-			return false, err
-		}
-		all = string(bytes)
+		text = string(bytes)
+	default:
+		text = info.Envelope.Subject
 	}
 
 	// now search through the criteria
 	// implicit AND at the moment so fail fast
-	if criteria.Header != nil {
-		for k, v := range criteria.Header {
-			headerValue := header.RFC822Headers.Get(k)
+	if criteria.Headers != nil {
+		for k, v := range criteria.Headers {
+			headerValue := info.RFC822Headers.Get(k)
 			for _, text := range v {
 				if !containsSmartCase(headerValue, text) {
 					return false, nil
@@ -182,18 +106,11 @@ func searchMessage(message rfc822.RawMessage, criteria *searchCriteria,
 			}
 		}
 	}
-	if criteria.Body != nil {
-		for _, searchTerm := range criteria.Body {
-			if !containsSmartCase(body, searchTerm) {
-				return false, nil
-			}
-		}
-	}
-	if criteria.Text != nil {
-		for _, searchTerm := range criteria.Text {
-			if !containsSmartCase(all, searchTerm) {
-				return false, nil
-			}
+
+	args := opt.LexArgs(criteria.Terms)
+	for _, searchTerm := range args.Args() {
+		if !containsSmartCase(text, searchTerm) {
+			return false, nil
 		}
 	}
 	if criteria.WithFlags != 0 {
@@ -207,16 +124,16 @@ func searchMessage(message rfc822.RawMessage, criteria *searchCriteria,
 		}
 	}
 	if parts&DATE > 0 {
-		if date, err := header.RFC822Headers.Date(); err != nil {
+		if date, err := info.RFC822Headers.Date(); err != nil {
 			log.Errorf("Failed to get date from header: %v", err)
 		} else {
-			if !criteria.startDate.IsZero() {
-				if date.Before(criteria.startDate) {
+			if !criteria.StartDate.IsZero() {
+				if date.Before(criteria.StartDate) {
 					return false, nil
 				}
 			}
-			if !criteria.endDate.IsZero() {
-				if date.After(criteria.endDate) {
+			if !criteria.EndDate.IsZero() {
+				if date.After(criteria.EndDate) {
 					return false, nil
 				}
 			}
@@ -257,18 +174,21 @@ const (
 
 // Returns a bitmask of the parts of the message required to be loaded for the
 // given criteria
-func getRequiredParts(criteria *searchCriteria) MsgParts {
+func GetRequiredParts(criteria *types.SearchCriteria) MsgParts {
 	required := NONE
-	if len(criteria.Header) > 0 {
+	if criteria == nil {
+		return required
+	}
+	if len(criteria.Headers) > 0 {
 		required |= HEADER
 	}
-	if !criteria.startDate.IsZero() || !criteria.endDate.IsZero() {
+	if !criteria.StartDate.IsZero() || !criteria.EndDate.IsZero() {
 		required |= DATE
 	}
-	if criteria.Body != nil && len(criteria.Body) > 0 {
+	if criteria.SearchBody {
 		required |= BODY
 	}
-	if criteria.Text != nil && len(criteria.Text) > 0 {
+	if criteria.SearchAll {
 		required |= ALL
 	}
 	if criteria.WithFlags != 0 {

worker/maildir/search.go
@@ -2,114 +2,17 @@ package maildir
 
 import (
 	"context"
-	"io"
-	"net/textproto"
 	"runtime"
-	"strings"
 	"sync"
-	"time"
-	"unicode"
 
-	"github.com/emersion/go-maildir"
-
-	"git.sr.ht/~sircmpwn/getopt"
-
-	"git.sr.ht/~rjarry/aerc/lib"
-	"git.sr.ht/~rjarry/aerc/lib/parse"
 	"git.sr.ht/~rjarry/aerc/log"
-	"git.sr.ht/~rjarry/aerc/models"
+	"git.sr.ht/~rjarry/aerc/worker/lib"
+	"git.sr.ht/~rjarry/aerc/worker/types"
 )
 
-type searchCriteria struct {
-	Header textproto.MIMEHeader
-	Body   []string
-	Text   []string
+func (w *Worker) search(ctx context.Context, criteria *types.SearchCriteria) ([]uint32, error) {
+	requiredParts := lib.GetRequiredParts(criteria)
 
-	WithFlags    []maildir.Flag
-	WithoutFlags []maildir.Flag
-
-	startDate, endDate time.Time
-}
-
-func parseSearch(args []string) (*searchCriteria, error) {
-	criteria := &searchCriteria{Header: make(textproto.MIMEHeader)}
-
-	opts, optind, err := getopt.Getopts(args, "rux:X:bat:H:f:c:d:")
-	if err != nil {
-		return nil, err
-	}
-	body := false
-	text := false
-	for _, opt := range opts {
-		switch opt.Option {
-		case 'r':
-			criteria.WithFlags = append(criteria.WithFlags, maildir.FlagSeen)
-		case 'u':
-			criteria.WithoutFlags = append(criteria.WithoutFlags, maildir.FlagSeen)
-		case 'x':
-			criteria.WithFlags = append(criteria.WithFlags, getParsedFlag(opt.Value))
-		case 'X':
-			criteria.WithoutFlags = append(criteria.WithoutFlags, getParsedFlag(opt.Value))
-		case 'H':
-			if strings.Contains(opt.Value, ": ") {
-				HeaderValue := strings.SplitN(opt.Value, ": ", 2)
-				criteria.Header.Add(HeaderValue[0], HeaderValue[1])
-			} else {
-				log.Errorf("Header is not given properly, must be given in format `Header: Value`")
-				continue
-			}
-		case 'f':
-			criteria.Header.Add("From", opt.Value)
-		case 't':
-			criteria.Header.Add("To", opt.Value)
-		case 'c':
-			criteria.Header.Add("Cc", opt.Value)
-		case 'b':
-			body = true
-		case 'a':
-			text = true
-		case 'd':
-			start, end, err := parse.DateRange(opt.Value)
-			if err != nil {
-				log.Errorf("failed to parse start date: %v", err)
-				continue
-			}
-			if !start.IsZero() {
-				criteria.startDate = start
-			}
-			if !end.IsZero() {
-				criteria.endDate = end
-			}
-		}
-	}
-	switch {
-	case text:
-		criteria.Text = args[optind:]
-	case body:
-		criteria.Body = args[optind:]
-	default:
-		for _, arg := range args[optind:] {
-			criteria.Header.Add("Subject", arg)
-		}
-	}
-	return criteria, nil
-}
-
-func getParsedFlag(name string) maildir.Flag {
-	var f maildir.Flag
-	switch strings.ToLower(name) {
-	case "seen":
-		f = maildir.FlagSeen
-	case "answered":
-		f = maildir.FlagReplied
-	case "flagged":
-		f = maildir.FlagFlagged
-	}
-	return f
-}
-
-func (w *Worker) search(ctx context.Context, criteria *searchCriteria) ([]uint32, error) {
-	requiredParts := getRequiredParts(criteria)
 	w.worker.Debugf("Required parts bitmask for search: %b", requiredParts)
 
 	keys, err := w.c.UIDs(*w.selected)
@@ -152,187 +55,12 @@ func (w *Worker) search(ctx context.Context, criteria *searchCriteria) ([]uint32
 }
 
 // Execute the search criteria for the given key, returns true if search succeeded
-func (w *Worker) searchKey(key uint32, criteria *searchCriteria,
-	parts MsgParts,
+func (w *Worker) searchKey(key uint32, criteria *types.SearchCriteria,
+	parts lib.MsgParts,
 ) (bool, error) {
 	message, err := w.c.Message(*w.selected, key)
 	if err != nil {
 		return false, err
 	}
-
-	// setup parts of the message to use in the search
-	// this is so that we try to minimise reading unnecessary parts
-	var (
-		flags  []maildir.Flag
-		header *models.MessageInfo
-		body   string
-		all    string
-	)
-
-	if parts&FLAGS > 0 {
-		flags, err = message.Flags()
-		if err != nil {
-			return false, err
-		}
-	}
-	if parts&HEADER > 0 || parts&DATE > 0 {
-		header, err = message.MessageInfo()
-		if err != nil {
-			return false, err
-		}
-	}
-	if parts&BODY > 0 {
-		// TODO: select which part to search, maybe look for text/plain
-		mi, err := message.MessageInfo()
-		if err != nil {
-			return false, err
-		}
-		path := lib.FindFirstNonMultipart(mi.BodyStructure, nil)
-		reader, err := message.NewBodyPartReader(path)
-		if err != nil {
-			return false, err
-		}
-		bytes, err := io.ReadAll(reader)
-		if err != nil {
-			return false, err
-		}
-		body = string(bytes)
-	}
-	if parts&ALL > 0 {
-		reader, err := message.NewReader()
-		if err != nil {
-			return false, err
-		}
-		defer reader.Close()
-		bytes, err := io.ReadAll(reader)
-		if err != nil {
-			return false, err
-		}
-		all = string(bytes)
-	}
-
-	// now search through the criteria
-	// implicit AND at the moment so fail fast
-	if criteria.Header != nil {
-		for k, v := range criteria.Header {
-			headerValue := header.RFC822Headers.Get(k)
-			for _, text := range v {
-				if !containsSmartCase(headerValue, text) {
-					return false, nil
-				}
-			}
-		}
-	}
-	if criteria.Body != nil {
-		for _, searchTerm := range criteria.Body {
-			if !containsSmartCase(body, searchTerm) {
-				return false, nil
-			}
-		}
-	}
-	if criteria.Text != nil {
-		for _, searchTerm := range criteria.Text {
-			if !containsSmartCase(all, searchTerm) {
-				return false, nil
-			}
-		}
-	}
-	if criteria.WithFlags != nil {
-		for _, searchFlag := range criteria.WithFlags {
-			if !containsFlag(flags, searchFlag) {
-				return false, nil
-			}
-		}
-	}
-	if criteria.WithoutFlags != nil {
-		for _, searchFlag := range criteria.WithoutFlags {
-			if containsFlag(flags, searchFlag) {
-				return false, nil
-			}
-		}
-	}
-	if parts&DATE > 0 {
-		if date, err := header.RFC822Headers.Date(); err != nil {
-			w.worker.Errorf("Failed to get date from header: %v", err)
-		} else {
-			if !criteria.startDate.IsZero() {
-				if date.Before(criteria.startDate) {
-					return false, nil
-				}
-			}
-			if !criteria.endDate.IsZero() {
-				if date.After(criteria.endDate) {
-					return false, nil
-				}
-			}
-		}
-	}
-	return true, nil
-}
-
-// Returns true if searchFlag appears in flags
-func containsFlag(flags []maildir.Flag, searchFlag maildir.Flag) bool {
-	match := false
-	for _, flag := range flags {
-		if searchFlag == flag {
-			match = true
-		}
-	}
-	return match
-}
-
-// Smarter version of strings.Contains for searching.
-// Is case-insensitive unless substr contains an upper case character
-func containsSmartCase(s string, substr string) bool {
-	if hasUpper(substr) {
-		return strings.Contains(s, substr)
-	}
-	return strings.Contains(strings.ToLower(s), strings.ToLower(substr))
-}
-
-func hasUpper(s string) bool {
-	for _, r := range s {
-		if unicode.IsUpper(r) {
-			return true
-		}
-	}
-	return false
-}
-
-// The parts of a message, kind of
-type MsgParts int
-
-const NONE MsgParts = 0
-const (
-	FLAGS MsgParts = 1 << iota
-	HEADER
-	DATE
-	BODY
-	ALL
-)
-
-// Returns a bitmask of the parts of the message required to be loaded for the
-// given criteria
-func getRequiredParts(criteria *searchCriteria) MsgParts {
-	required := NONE
-	if len(criteria.Header) > 0 {
-		required |= HEADER
-	}
-	if !criteria.startDate.IsZero() || !criteria.endDate.IsZero() {
-		required |= DATE
-	}
-	if criteria.Body != nil && len(criteria.Body) > 0 {
-		required |= BODY
-	}
-	if criteria.Text != nil && len(criteria.Text) > 0 {
-		required |= ALL
-	}
-	if criteria.WithFlags != nil && len(criteria.WithFlags) > 0 {
-		required |= FLAGS
-	}
-	if criteria.WithoutFlags != nil && len(criteria.WithoutFlags) > 0 {
-		required |= FLAGS
-	}
-
-	return required
+	return lib.SearchMessage(message, criteria, parts)
 }

commit 03061fe21f1c3d285f83c6d0494f0e633070ef13

imap: fix processing of search criteria end date
This patch fixes a copy/paste error in translateSearch that was leading
to the types.SearchCriteria's StartDate to be mapped to both SentSince
and SentBefore in the imap.SearchCriteria, and the actual EndDate to be
ignored.

Fixes: 8464b3738511 ("search: use a common api for all workers")
Signed-off-by: Simon Martin <simon@nasilyan.com>
Reviewed-by: Moritz Poldrack <moritz@poldrack.dev>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/imap/search.go
@@ -20,7 +20,7 @@ func translateSearch(c *types.SearchCriteria) *imap.SearchCriteria {
 	if !c.StartDate.IsZero() {
 		criteria.SentSince = c.StartDate
 	}
-	if !c.StartDate.IsZero() {
+	if !c.EndDate.IsZero() {
 		criteria.SentBefore = c.EndDate
 	}
 	for k, v := range c.Headers {

worker/imap/search_test.go
@@ -1,0 +1,45 @@
+package imap
+
+import (
+	"testing"
+	"time"
+
+	"git.sr.ht/~rjarry/aerc/worker/types"
+)
+
+func Test_translateSearch_ByDate(t *testing.T) {
+	tests := []struct {
+		name      string
+		StartDate time.Time
+		EndDate   time.Time
+	}{
+		{
+			name:      "Only StartDate",
+			StartDate: time.Now(),
+		},
+		{
+			name:    "Only EndDate",
+			EndDate: time.Now(),
+		},
+		{
+			name:      "Both dates",
+			StartDate: time.Now(),
+			EndDate:   time.Now(),
+		},
+	}
+	for _, test := range tests {
+		crit := &types.SearchCriteria{
+			StartDate: test.StartDate,
+			EndDate:   test.EndDate,
+		}
+		sc := translateSearch(crit)
+		if sc.SentSince != test.StartDate {
+			t.Errorf("test '%s' failed: got: '%s', but wanted: '%s'",
+				test.name, sc.SentSince, test.StartDate)
+		}
+		if sc.SentBefore != test.EndDate {
+			t.Errorf("test '%s' failed: got: '%s', but wanted: '%s'",
+				test.name, sc.SentBefore, test.EndDate)
+		}
+	}
+}