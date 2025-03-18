rjarry/aerc

commit da5ca1a53043b6b3d8c5bfc7f6eff340985e3edb

template: pass account and folder names to signature command
This can help to dynamically determine what signature you want.

Changelog-added: `AERC_ACCOUNT` and `AERC_FOLDER` are now available
 in the signature command environment.
Signed-off-by: Tristan Partin <tristan@partin.io>
Acked-by: Robin Jarry <robin@jarry.cc>

lib/state/templates.go
@@ -713,6 +713,10 @@ func (d *templateData) Signature() string {
 func (d *templateData) readSignatureFromCmd() ([]byte, error) {
 	sigCmd := d.account.SignatureCmd
 	cmd := exec.Command("sh", "-c", sigCmd)
+	env := os.Environ()
+	env = append(env, fmt.Sprintf("AERC_ACCOUNT=%s", d.account.Name))
+	env = append(env, fmt.Sprintf("AERC_FOLDER=%s", d.folder.Name))
+	cmd.Env = env
 	signature, err := cmd.Output()
 	if err != nil {
 		return nil, err

commit c460e0e6f138bdc17985e2d4b51651ac45a6a916

template: fix mailto on startup failing to open
During startup, d.account and d.folder are not set and thus not
lead to a segmentation fault when running a signature command.

Only set these values if they are available.

Fixes: da5ca1a5 ("template: pass account and folder names to signature command")
Changelog-fixed: Fix an error when starting aerc through a `mailto:`
 link when the `new_message` template runs a signature command.
Signed-off-by: Moritz Poldrack <git@moritz.sh>
Acked-by: Robin Jarry <robin@jarry.cc>

lib/state/templates.go
@@ -729,8 +729,12 @@ func (d *templateData) readSignatureFromCmd() ([]byte, error) {
 	sigCmd := d.account.SignatureCmd
 	cmd := exec.Command("sh", "-c", sigCmd)
 	env := os.Environ()
-	env = append(env, fmt.Sprintf("AERC_ACCOUNT=%s", d.account.Name))
-	env = append(env, fmt.Sprintf("AERC_FOLDER=%s", d.folder.Name))
+	if d.account != nil {
+		env = append(env, fmt.Sprintf("AERC_ACCOUNT=%s", d.account.Name))
+	}
+	if d.folder != nil {
+		env = append(env, fmt.Sprintf("AERC_FOLDER=%s", d.folder.Name))
+	}
 	cmd.Env = env
 	signature, err := cmd.Output()
 	if err != nil {