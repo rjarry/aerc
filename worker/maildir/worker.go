rjarry/aerc

commit 86c612e48025104b32b2048fead1248a3d3b1c8f

maildir: enable backend threads
Add thread capability to maildir backend. Enables the maildir worker to
thread the entire folder, as opposed to only the displayed messages via
client side threads.

Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>
Tested-by: Inwit <inwit@sindominio.net>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/maildir/worker.go
@@ -18,6 +18,8 @@ import (
 	"github.com/emersion/go-maildir"
 	"github.com/fsnotify/fsnotify"
 
+	aercLib "git.sr.ht/~rjarry/aerc/lib"
+	"git.sr.ht/~rjarry/aerc/lib/iterator"
 	"git.sr.ht/~rjarry/aerc/logging"
 	"git.sr.ht/~rjarry/aerc/models"
 	"git.sr.ht/~rjarry/aerc/worker/handlers"
@@ -180,7 +182,7 @@ func (w *Worker) getDirectoryInfo(name string) *models.DirectoryInfo {
 
 		Caps: &models.Capabilities{
 			Sort:   true,
-			Thread: false,
+			Thread: true,
 		},
 	}
 
@@ -265,6 +267,8 @@ func (w *Worker) handleMessage(msg types.WorkerMessage) error {
 		return w.handleOpenDirectory(msg)
 	case *types.FetchDirectoryContents:
 		return w.handleFetchDirectoryContents(msg)
+	case *types.FetchDirectoryThreaded:
+		return w.handleFetchDirectoryThreaded(msg)
 	case *types.CreateDirectory:
 		return w.handleCreateDirectory(msg)
 	case *types.RemoveDirectory:
@@ -459,6 +463,7 @@ func (w *Worker) sort(uids []uint32, criteria []*types.SortCriterion) ([]uint32,
 			<-limit
 		}(uid)
 	}
+
 	wg.Wait()
 	sortedUids, err := lib.Sort(msgInfos, criteria)
 	if err != nil {
@@ -466,6 +471,85 @@ func (w *Worker) sort(uids []uint32, criteria []*types.SortCriterion) ([]uint32,
 		return nil, err
 	}
 	return sortedUids, nil
+}
+
+func (w *Worker) handleFetchDirectoryThreaded(
+	msg *types.FetchDirectoryThreaded,
+) error {
+	var (
+		uids []uint32
+		err  error
+	)
+	if len(msg.FilterCriteria) > 1 {
+		filter, err := parseSearch(msg.FilterCriteria)
+		if err != nil {
+			return err
+		}
+		uids, err = w.search(filter)
+		if err != nil {
+			return err
+		}
+	} else {
+		uids, err = w.c.UIDs(*w.selected)
+		if err != nil {
+			logging.Errorf("failed scanning uids: %v", err)
+			return err
+		}
+	}
+	threads, err := w.threads(uids, msg.SortCriteria)
+	if err != nil {
+		logging.Errorf("failed sorting directory: %v", err)
+		return err
+	}
+	w.currentSortCriteria = msg.SortCriteria
+	w.worker.PostMessage(&types.DirectoryThreaded{
+		Message: types.RespondTo(msg),
+		Threads: threads,
+	}, nil)
+	return nil
+}
+
+func (w *Worker) threads(uids []uint32, criteria []*types.SortCriterion) ([]*types.Thread, error) {
+	builder := aercLib.NewThreadBuilder(iterator.NewFactory(false))
+	msgInfos := make([]*models.MessageInfo, 0, len(uids))
+	mu := sync.Mutex{}
+	wg := sync.WaitGroup{}
+	max := runtime.NumCPU() * 2
+	limit := make(chan struct{}, max)
+	for _, uid := range uids {
+		limit <- struct{}{}
+		wg.Add(1)
+		go func(uid uint32) {
+			defer wg.Done()
+			info, err := w.msgHeadersFromUid(uid)
+			if err != nil {
+				logging.Errorf("could not get message info: %v", err)
+				<-limit
+				return
+			}
+			mu.Lock()
+			builder.Update(info)
+			msgInfos = append(msgInfos, info)
+			mu.Unlock()
+			<-limit
+		}(uid)
+	}
+	wg.Wait()
+	var err error
+	switch {
+	case len(criteria) == 0:
+		sort.Slice(uids, func(i int, j int) bool {
+			return uids[i] < uids[j]
+		})
+	default:
+		uids, err = lib.Sort(msgInfos, criteria)
+		if err != nil {
+			logging.Errorf("could not sort the messages: %v", err)
+			return nil, err
+		}
+	}
+	threads := builder.Threads(uids, false, false)
+	return threads, nil
 }
 
 func (w *Worker) handleCreateDirectory(msg *types.CreateDirectory) error {

commit a135097c093a59ff919a605c1e3685f5032a48d1

maildir: fix sorting of thread siblings
Expose the account name in the `WorkerInteractor` interface and make use
of `ThreadingBySubject`, `ReverseThreadOrder` and `SortThreadSiblings`
in the maildir worker instead of constants.

Fixes: 86c612e48025 ("maildir: enable backend threads")
Changelog-fixed: Maildir backend now builds the therads according
 to the various config options such as `sort-thread-siblings`,
 `reverse-msglist-order`, `threading-by-subject` and
 `reverse-thread-order`.
Signed-off-by: Benno Lossin <benno.lossin@proton.me>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/maildir/worker.go
@@ -19,6 +19,7 @@ import (
 
 	"github.com/emersion/go-maildir"
 
+	"git.sr.ht/~rjarry/aerc/config"
 	aercLib "git.sr.ht/~rjarry/aerc/lib"
 	"git.sr.ht/~rjarry/aerc/lib/iterator"
 	"git.sr.ht/~rjarry/aerc/lib/log"
@@ -577,7 +578,8 @@ func (w *Worker) handleFetchDirectoryThreaded(
 func (w *Worker) threads(ctx context.Context, uids []models.UID,
 	criteria []*types.SortCriterion,
 ) ([]*types.Thread, error) {
-	builder := aercLib.NewThreadBuilder(iterator.NewFactory(false), false)
+	ui := config.Ui.ForAccount(w.worker.Name())
+	builder := aercLib.NewThreadBuilder(iterator.NewFactory(ui.ReverseOrder), ui.ThreadingBySubject)
 	msgInfos := make([]*models.MessageInfo, 0, len(uids))
 	mu := sync.Mutex{}
 	wg := sync.WaitGroup{}
@@ -621,7 +623,7 @@ func (w *Worker) threads(ctx context.Context, uids []models.UID,
 			return nil, err
 		}
 	}
-	threads := builder.Threads(uids, false, false)
+	threads := builder.Threads(uids, ui.ReverseThreadOrder, ui.SortThreadSiblings)
 	return threads, nil
 }
 

worker/types/worker.go
@@ -16,6 +16,7 @@ type WorkerInteractor interface {
 	PostAction(WorkerMessage, func(msg WorkerMessage))
 	PostMessage(WorkerMessage, func(msg WorkerMessage))
 	Unwrap() WorkerInteractor
+	Name() string
 }
 
 var lastId int64 = 1 // access via atomic
@@ -57,6 +58,10 @@ func (worker *Worker) Unwrap() WorkerInteractor {
 
 func (worker *Worker) Actions() chan WorkerMessage {
 	return worker.actions
+}
+
+func (worker *Worker) Name() string {
+	return worker.name
 }
 
 func (worker *Worker) setId(msg WorkerMessage) {