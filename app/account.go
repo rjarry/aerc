rjarry/aerc

commit 96ccb183fb5c9f3b1ea7352a1908789b9b2efd3f

imap: remove spurious checkmail when changing directories
I investigated why aerc felt so slow when changing directories with my
mailbox, and noticed that a spurious CheckMail is triggered whenever
OpenDirectory is called.

This happens because OpenDirectory does a Select(TheTargetMailbox),
which gets the server to reply a MailboxUpdate message for the target
directory, in turn triggering a (slow) CheckMail.

That CheckMail is completely useless, since the normal flow that follows
OpenDirectory involves a FetchDirectoryContents, which is what we need
to draw the message list (and not more).

This patch leverages a drainCloser object to ignore spurious messages
from the server, exactly like handle{Move,Delete}Messages do.

Some measurements to quantify the impact can be found in
  https://paste.sr.ht/~simartin/05011fb7615c5b60bf12bb36fc9d08221550e403
TL;DR: The latency of Shift+K => Shift+J to go to a large (8.6k message)
folder and back is divided by ~7.

Signed-off-by: Simon Martin <simon@nasilyan.com>
Reviewed-by: Moritz Poldrack <moritz@poldrack.dev>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/imap/open.go
@@ -10,6 +10,11 @@ import (
 )
 
 func (imapw *IMAPWorker) handleOpenDirectory(msg *types.OpenDirectory) {
+	// The SELECT we'll issue might trigger MailboxStatus replies from the
+	// server, causing unnecessary CheckMail commands. Let's ignore those.
+	drain := imapw.drainUpdates()
+	defer drain.Close()
+
 	imapw.worker.Debugf("Opening %s", msg.Directory)
 
 	sel, err := imapw.client.Select(msg.Directory, false)

commit 87a3b42daac670cd5ff6429f49102ad8290a3fe5

imap: partially revert two recent patches
Karel reported weird behaviour with RUE counters that are linked to two
of my recent patches:

- 96ccb183fb5c9f3b1ea7352a1908789b9b2efd3f is simply wrong
- c9a57f76bf529dada622bb166c792091118dc924 is a bit too eager at
  trusting the message store to recompute counters

This patch addresses those issues, and seems to address all the reported
issues at least on my mailboxes.

Fixes: 96ccb183fb5c ("imap: remove spurious checkmail when changing directories")
Fixes: c9a57f76bf52 ("msgstore: properly rebuild message store upon reconnection")
Signed-off-by: Simon Martin <simon@nasilyan.com>
Tested-by: Karel Balej <balejk@matfyz.cz>
Acked-by: Robin Jarry <robin@jarry.cc>

app/account.go
@@ -574,7 +574,9 @@ func (acct *AccountView) refreshDirCounts(destination string) {
 		for _, msg := range acct.Store().Messages {
 			count++
 			if msg == nil {
-				continue
+				// Don't trust the store for status if it's not fully loaded
+				// yet.
+				return
 			}
 			if msg.Flags.Has(models.RecentFlag) {
 				recent++

worker/imap/open.go
@@ -10,11 +10,6 @@ import (
 )
 
 func (imapw *IMAPWorker) handleOpenDirectory(msg *types.OpenDirectory) {
-	// The SELECT we'll issue might trigger MailboxStatus replies from the
-	// server, causing unnecessary CheckMail commands. Let's ignore those.
-	drain := imapw.drainUpdates()
-	defer drain.Close()
-
 	imapw.worker.Debugf("Opening %s", msg.Directory)
 
 	sel, err := imapw.client.Select(msg.Directory, false)