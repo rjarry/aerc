rjarry/aerc

commit 063c19b619c2fb45f782f27c4c0a59a9774aa694

imap: honor reply-to-self when replying to all
The reply-to-self configuration is not always properly honored when
replying to all, due to a bug in reply.Execute's dedupe logic.

It uses a map to keep track of the addresses it has already seen, and
exclude them from the lists subsequently passed to "dedupe".

Since we use "dedupe" to build the reply's From, our address is excluded
when building the To/Cc, even if reply-to-self is true. This patch fixes
this by making sure that our address is *not* part of the exclude list
before building the reply's To/Cc.

Basic logs showing the behaviour with and without this patch can be
found here:
  https://paste.sr.ht/~simartin/e502022d06f8a3d8f827179f28d89165545af67f

Signed-off-by: Simon Martin <simon@nasilyan.com>
Reviewed-by: Moritz Poldrack <moritz@poldrack.dev>
Acked-by: Robin Jarry <robin@jarry.cc>

commands/msg/reply.go
@@ -130,8 +130,12 @@ func (r reply) Execute(args []string) error {
 		// order matters, due to the deduping
 		// in order of importance, first parse the To, then the Cc header
 
-		// we add our from address, so that we don't self address ourselves
-		recSet.Add(from)
+		// If we accept to reply to ourselves, remove our address from recSet.
+		// This will ensure that we are at least once in the reply's recipients
+		// if we were a recipient of the message we're replying to.
+		if config.Compose.ReplyToSelf {
+			delete(recSet, from.Address)
+		}
 
 		to = append(to, dedupe(msg.Envelope.To)...)
 

commit cb95b819b73d48ccc73eb17f7526e4286ee88867

msg: fix issue with reply-to-self introduced by 063c19b6
Commit 063c19b6 fixes an issue with reply-all when reply-to-self is set
to try, but unfortunately introduces a new one, reported by Drew on IRC:
it will systematically Cc the replier if they reply to an email they
sent to themselves.

The problem with that fix is that it removes the from from the deduper
loo late, and should have instead not put it there in the first place if
reply-to-self is true; this is exactly what this patch does.

Fixes: 063c19b6 ("honor reply-to-self when replying to all")
Fixes: https://todo.sr.ht/~rjarry/aerc/311
Reported-by: Drew Devault <sir@cmpwn.com>
Signed-off-by: Simon Martin <simon@nasilyan.com>
Acked-by: Robin Jarry <robin@jarry.cc>

commands/msg/reply.go
@@ -104,7 +104,10 @@ func (r reply) Execute(args []string) error {
 		return deduped
 	}
 
-	if !config.Compose.ReplyToSelf {
+	if config.Compose.ReplyToSelf {
+		// We accept to reply to ourselves, so don't exclude our own address
+		// from the reply's recipients.
+	} else {
 		recSet.Add(from)
 	}
 
@@ -129,13 +132,6 @@ func (r reply) Execute(args []string) error {
 	if r.All {
 		// order matters, due to the deduping
 		// in order of importance, first parse the To, then the Cc header
-
-		// If we accept to reply to ourselves, remove our address from recSet.
-		// This will ensure that we are at least once in the reply's recipients
-		// if we were a recipient of the message we're replying to.
-		if config.Compose.ReplyToSelf {
-			delete(recSet, from.Address)
-		}
 
 		to = append(to, dedupe(msg.Envelope.To)...)
 