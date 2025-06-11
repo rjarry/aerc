rjarry/aerc

commit 7cb8e0e7ce2415ac4e9c05834c919c0edd9655b4

imap: properly handle out-of-band Unseen flag updates
I noticed that the fix I made via b576ab28 (and the general idea to get
rid of materialized RUE counters) does not work, because we're extremely
unlikely to have all the messages' flags in memory. So the patch worked
"by chance" when updating messages that are in the active message list
(e.g. their headers are already fetched) but not for "out of view"
messages.

This alternative patch "just" captures unsolicited MessageUpdate
messages from the server, and updates the Unseen counter according to
the status reported by the server for that message. This allows to
synchronize our view with the server's.

Fixes: b576ab28 ("properly update UI upon message (un)read in [...]")
Fixes: https://todo.sr.ht/~rjarry/aerc/307
Signed-off-by: Simon Martin <simon@nasilyan.com>
Tested-by: Karel Balej <balejk@matfyz.cz>
Acked-by: Robin Jarry <robin@jarry.cc>

app/account.go
@@ -455,7 +455,6 @@ func (acct *AccountView) onMessage(msg types.WorkerMessage) {
 				acct.msglist.SetStore(store)
 			}
 			store.Update(msg)
-			acct.refreshDirCounts(store.Name)
 			acct.SetStatus(state.Threading(store.ThreadedView()))
 		}
 		if acct.newConn && len(msg.Uids) == 0 {
@@ -478,14 +477,23 @@ func (acct *AccountView) onMessage(msg types.WorkerMessage) {
 		}
 	case *types.MessageInfo:
 		if store, ok := acct.dirlist.SelectedMsgStore(); ok {
-			store.Update(msg)
-			// It this is an update of a message we already know, we'll
-			// have merged the update into the existing message already,
-			// but not the properties materialized outside of the Messages
-			// array, e.g. the Seen and Recent counters; do it now.
-			if dir := acct.dirlist.SelectedDirectory(); dir != nil {
-				acct.refreshDirCounts(dir.Name)
+			if msg.Unsolicited {
+				// This is a server generated message update, e.g. a
+				// notification that a message has changed; this will happen
+				// mostly for flags according to section 2.3.1.1 alinea 4 in
+				// the IMAP4rev1 RFC.
+				if dir := acct.dirlist.SelectedDirectory(); dir != nil {
+					// Our view of Unseen is out-of-sync with the server's;
+					// update it now.
+					if msg.Info.Flags.Has(models.SeenFlag) {
+						dir.Unseen -= 1
+					} else {
+						dir.Unseen += 1
+					}
+					dir.Unseen = acct.ensurePositive(dir.Unseen, "Unseen")
+				}
 			}
+			store.Update(msg)
 		}
 	case *types.MessagesDeleted:
 		if dir := acct.dirlist.SelectedDirectory(); dir != nil {
@@ -565,39 +573,6 @@ func (acct *AccountView) updateDirCounts(destination string, uids []models.UID, 
 		destDir.Unseen = acct.ensurePositive(destDir.Unseen, "Unseen")
 		destDir.Recent = acct.ensurePositive(destDir.Recent, "Recent")
 		destDir.Exists = acct.ensurePositive(destDir.Exists, "Exists")
-	} else {
-		acct.worker.Errorf("Skipping unknown directory %s", destination)
-	}
-}
-
-func (acct *AccountView) refreshDirCounts(destination string) {
-	// Only update the destination destDir if it is initialized
-	if destDir := acct.dirlist.Directory(destination); destDir != nil {
-		store := acct.Store()
-		if store == nil {
-			// This may look a bit of unnecessary paranoid programming, but it
-			// happened once during my manual monkey testing :-)
-			acct.worker.Errorf("No message store for directory %s", destination)
-			return
-		}
-		var count, recent, unseen int
-		for _, msg := range acct.Store().Messages {
-			count++
-			if msg == nil {
-				// Don't trust the store for status if it's not fully loaded
-				// yet.
-				return
-			}
-			if msg.Flags.Has(models.RecentFlag) {
-				recent++
-			}
-			if !msg.Flags.Has(models.SeenFlag) {
-				unseen++
-			}
-		}
-		destDir.Unseen = acct.ensurePositive(unseen, "Unseen")
-		destDir.Recent = acct.ensurePositive(recent, "Recent")
-		destDir.Exists = acct.ensurePositive(count, "Exists")
 	} else {
 		acct.worker.Errorf("Skipping unknown directory %s", destination)
 	}

worker/types/messages.go
@@ -254,8 +254,9 @@ type SearchResults struct {
 
 type MessageInfo struct {
 	Message
-	Info       *models.MessageInfo
-	NeedsFlags bool
+	Info        *models.MessageInfo
+	NeedsFlags  bool
+	Unsolicited bool
 }
 
 type FullMessage struct {

lib/msgstore.go
@@ -319,6 +319,12 @@ func (store *MessageStore) Update(msg types.WorkerMessage) {
 			if store.selectedUid == msg.Info.Uid {
 				store.onSelect(msg.Info)
 			}
+		} else if msg.Unsolicited {
+			// We received an unsolicited update for a message we don't have
+			// in store; store that update as is to ensure we have the same
+			// "Unseen status" as the server. It will be properly replaced
+			// if/when we need to fetch the message.
+			store.Messages[msg.Info.Uid] = msg.Info
 		}
 		if msg.NeedsFlags {
 			store.Lock()

worker/imap/worker.go
@@ -291,6 +291,7 @@ func (w *IMAPWorker) handleImapUpdate(update client.Update) {
 				InternalDate:  msg.InternalDate,
 				Uid:           models.Uint32ToUid(msg.Uid),
 			},
+			Unsolicited: true,
 		}, nil)
 	case *client.ExpungeUpdate:
 		if uid, found := w.seqMap.Pop(update.SeqNum); !found {

commit 4cfc71b1be048aa93a5c95362d263bd05e850989

imap: improve seen count management
While investigating a reported slowness when marking messages (un)read,
I noticed an issue with commit 7cb8e0e7ce24: it systematically updates
the Seen count using the unsolicited message's status. This is wrong
because we might receive that message long after we updated things on
our end, or at the next action on the message (e.g. opening it).

We need to compare the status reported by the server with ours, and only
if they differ update the counter with what the server states.

Doing so also allows to fix the reported slowdown by reflecting Seen
flag updates right after an explicit :[un]read command, instead of
deferring to the confirmation by the IMAP server, and paying the
(unnecessary) latency of a round-trip. It is safe because we will only
do it after a successful UidStore call, hence confirmation from the IMAP
server of the flag change.

Fixes: 7cb8e0e7ce24 ("imap: properly handle out-of-band Unseen flag updates")
Link: https://lists.sr.ht/~rjarry/aerc-discuss/%3CDACPBQ04UHUF.30CJJ4QKJDYLY@zoho.com%3E
Reported-by: skejg <grolleman@zoho.com>
Signed-off-by: Simon Martin <simon@nasilyan.com>
Acked-by: Robin Jarry <robin@jarry.cc>

app/account.go
@@ -477,20 +477,29 @@ func (acct *AccountView) onMessage(msg types.WorkerMessage) {
 		}
 	case *types.MessageInfo:
 		if store, ok := acct.dirlist.SelectedMsgStore(); ok {
-			if msg.Unsolicited {
+			if msg.Unsolicited || msg.ReplaceFlags {
 				// This is a server generated message update, e.g. a
-				// notification that a message has changed; this will happen
+				// notification that a message has changed (this will happen
 				// mostly for flags according to section 2.3.1.1 alinea 4 in
-				// the IMAP4rev1 RFC.
-				if dir := acct.dirlist.SelectedDirectory(); dir != nil {
-					// Our view of Unseen is out-of-sync with the server's;
-					// update it now.
-					if msg.Info.Flags.Has(models.SeenFlag) {
-						dir.Unseen -= 1
-					} else {
-						dir.Unseen += 1
+				// the IMAP4rev1 RFC), or a forced flag update.
+				// In case the store and the notification disagree on the Seen
+				// flag, trust the notification.
+				seen_in_sync := true
+				if msg_in_store, ok := store.Messages[msg.Info.Uid]; ok && msg_in_store != nil {
+					seen_in_sync = msg_in_store.Flags.Has(models.SeenFlag) ==
+						msg.Info.Flags.Has(models.SeenFlag)
+				}
+				if !seen_in_sync {
+					if dir := acct.dirlist.SelectedDirectory(); dir != nil {
+						// Our view of Unseen is out-of-sync with the server's;
+						// update it now.
+						if msg.Info.Flags.Has(models.SeenFlag) {
+							dir.Unseen -= 1
+						} else {
+							dir.Unseen += 1
+						}
+						dir.Unseen = acct.ensurePositive(dir.Unseen, "Unseen")
 					}
-					dir.Unseen = acct.ensurePositive(dir.Unseen, "Unseen")
 				}
 			}
 			store.Update(msg)

worker/imap/flags.go
@@ -105,6 +105,7 @@ func (imapw *IMAPWorker) handleFlagMessages(msg *types.FlagMessages) {
 					Flags: translateImapFlags(_msg.Flags),
 					Uid:   models.Uint32ToUid(_msg.Uid),
 				},
+				ReplaceFlags: true,
 			}, nil)
 			return nil
 		})

worker/types/messages.go
@@ -254,9 +254,10 @@ type SearchResults struct {
 
 type MessageInfo struct {
 	Message
-	Info        *models.MessageInfo
-	NeedsFlags  bool
-	Unsolicited bool
+	Info         *models.MessageInfo
+	NeedsFlags   bool
+	ReplaceFlags bool
+	Unsolicited  bool
 }
 
 type FullMessage struct {