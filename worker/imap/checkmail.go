rjarry/aerc

commit 96603ea04e335f8e4bf7f82acfddcb8a5d1a6cf0

imap: only restrict list-status to a single directory when we can
rjarry mentioned on IRC that my e4b38efb29b45c88b4e801c4a15257f1727da6db
commit was a bit too brutal, and regressed check-mail (checking only the
current directory instead of all).

This patch improves it by continuing to use "" as ref for LIST-STATUS
unless CheckMail.Directories contains a single directory, in which case
it uses it as ref.

This keeps most of the performance benefits (since we usually have at
most one directory to check when navigating between folders) without
regressing in terms of functionality (in particular I've checked that
check-mail does look at and updates *all* folders, not only the current
one).

Signed-off-by: Simon Martin <simon@nasilyan.com>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/imap/checkmail.go
@@ -21,7 +21,12 @@ func (w *IMAPWorker) handleCheckMailMessage(msg *types.CheckMail) {
 	switch {
 	case w.liststatus:
 		w.worker.Tracef("Checking mail with LIST-STATUS")
-		statuses, err = w.client.liststatus.ListStatus(w.client.Mailbox().Name, "*", items, nil)
+		ref := ""
+		if len(msg.Directories) == 1 {
+			// If checking a single directory, restrict the ListStatus to it.
+			ref = msg.Directories[0]
+		}
+		statuses, err = w.client.liststatus.ListStatus(ref, "*", items, nil)
 		if err != nil {
 			w.worker.PostMessage(&types.Error{
 				Message: types.RespondTo(msg),

commit 3625e0f04a438e9212b85c6fbbd50abd24e3a4a9

imap: workaround list-status issue with zoho
I've been working with skejg on a regression from 0.20 that he's seeing
with his Zoho account: when receiving a new email, the unread counter is
incremented but the message list is not updated.

The change in scope is 96603ea04e33, that uses the directory name
instead of "" when LIST STATUS'ing a single directory, which improves
overall performance, especially for accounts with many folders.

The problem is that Zoho does not give an error in such a case, but also
does not return *any* status, making the CheckMail command virtually a
no-op.

This patch works this around (while not removing the performance benefit
to "normal providers") by always restricting the LIST STATUS' ref if a
single directory is required, and in case it returns neither a status
nor an error, do another LIST STATUS with "" as ref.

Fixes: 96603ea04e33 ("imap: only restrict list-status to a single directory when we can")
Reported-by: skejg <grolleman@zoho.com>
Signed-off-by: Simon Martin <simon@nasilyan.com>
Tested-by: skejg <grolleman@zoho.com>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/imap/checkmail.go
@@ -27,6 +27,12 @@ func (w *IMAPWorker) handleCheckMailMessage(msg *types.CheckMail) {
 			ref = msg.Directories[0]
 		}
 		statuses, err = w.client.liststatus.ListStatus(ref, "*", items, nil)
+		if err == nil && len(statuses) == 0 && len(msg.Directories) == 1 {
+			// For providers such as Zoho, we might get an empty list and
+			// no error when ref contains the name of a single directory.
+			// Workaround this bug by retrying with "".
+			statuses, err = w.client.liststatus.ListStatus("", "*", items, nil)
+		}
 		if err != nil {
 			w.worker.PostMessage(&types.Error{
 				Message: types.RespondTo(msg),