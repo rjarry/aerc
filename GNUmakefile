rjarry/aerc

commit 29413dc7833a633852e478a6725893f4fa45a8db

contrib: add application icon and logos
This is basically an 'æ' character enclosed in a '@' character followed
by a '_' character. An icon version is enclosed in a mini terminal.

                    .@@@@@@@@@@@@@@@@@@@.
                @@@@@@@@.              @@@@@@@
             @@@@@@                        @@@@@%
           @@@@&    .-@@@@%.       @@@@@.     @@@@@
         @@@@    @@@@@@@@@@@@*  @@@@@@@@@@     @@@@.
        @@@@     `@        ,@@@@@@      @@@.     @@@&
       @@@*                 .@@@@        @@@      @@@.
      @@@@                   @@@          @@@     @@@@
     /@@@                    @@@          @@@     (@@@
     @@@@         @@@@@@@@@@@@@@@@@@@@@@@@@@@     &@@@
     @@@@       @@@@@        @@@                  @@@@
     *@@@      @@@@          @@@                 @@@@
      @@@@     @@@@         @@@@,               @@@@
       @@@#    @@@@        %@@@@@/           .@@@@.
        @@@@    @@@@@   .@@@,  @@@@@/     &@@@@@
         @@@@/    @@@@@@@@       @@@@@@@@@@@
           @@@@@
             @@@@@@                        @@@@
                &@@@@@@@&            &@@@@@@@
                     @@@@@@@@@@@@@@@@@@@          @@@@@@@@@

Install the icon and reference it in the XDG .desktop file. Reference
the logo in README.md for maximum enjoyment.

Signed-off-by: Robin Jarry <robin@jarry.cc>
Acked-by: Moritz Poldrack <moritz@poldrack.dev>

GNUmakefile
@@ -212,6 +212,7 @@ $(foreach s,$(wildcard stylesets/*),\
 $(foreach t,$(wildcard templates/*),\
 	$(eval $(call install_file,$t,644,$(DESTDIR)$(SHAREDIR)/templates)))
 $(eval $(call install_file,contrib/aerc.desktop,644,$(DESTDIR)$(PREFIX)/share/applications))
+$(eval $(call install_file,contrib/icon.svg,644,$(DESTDIR)$(PREFIX)/share/icons/hicolor/scalable/apps/aerc.svg))
 $(eval $(call install_file,aerc,755,$(DESTDIR)$(BINDIR)))
 $(eval $(call install_file,contrib/carddav-query,755,$(DESTDIR)$(BINDIR)))
 

contrib/aerc.desktop
@@ -10,7 +10,7 @@ Keywords=Email,Mail,IMAP,SMTP
 Categories=Office;Network;Email;ConsoleOnly
 
 Type=Application
-Icon=utilities-terminal
+Icon=aerc
 Terminal=true
 Exec=aerc %u
 MimeType=x-scheme-handler/mailto

contrib/icon.svg
@@ -1,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!--
+ The aerc icon by Robin Jarry <robin@jarry.cc> distributed under a
+ Creative Commons Attribution 4.0 International License:
+ http://creativecommons.org/licenses/by/4.0/
+-->
+<svg width="256" height="256" version="1.1" xmlns="http://www.w3.org/2000/svg">
+ <g transform="translate(-1.29 -1040)">
+  <rect x="3.8" y="1068" width="251" height="213" stroke="#4c7899" stroke-linecap="round" stroke-width="3.02" style="paint-order:markers fill stroke"/>
+  <rect x="3.78" y="1056" width="251" height="11.8" fill="#285577" stroke="#4c7899" stroke-linecap="round" stroke-width="2.98" style="paint-order:markers fill stroke"/>
+  <g fill="#fff">
+   <rect x="7.72" y="1060" width="16.3" height="4.92" style="paint-order:markers fill stroke"/>
+   <rect x="63.6" y="1060" width="22.1" height="4.92" style="paint-order:markers fill stroke"/>
+   <rect x="25.6" y="1060" width="36.8" height="4.92" style="paint-order:markers fill stroke"/>
+  </g>
+ </g>
+ <g transform="matrix(.788 0 0 .788 -211 -19.8)" fill="#fff">
+  <path d="m424 81.2c-15.7 0-30.5 2.82-44.5 8.47-14 5.56-26.4 13.6-37.1 24.1-10.5 10.4-18.5 22.4-24.2 36.1-5.64 13.7-8.44 28.1-8.44 43.2 0 15.7 2.75 30.4 8.31 44.1 5.56 13.6 13.7 25.6 24.3 36s22.7 18.3 36.4 23.7c13.6 5.48 28.1 8.22 43.5 8.22 12.6 0 25-2.18 37.2-6.49 9.35-3.22 18.2-7.55 26.4-13 4.31-2.84 4.96-8.68 1.71-12.7l-0.504-0.622c-2.66-3.28-7.49-3.8-11-1.48-6.92 4.58-14.3 8.22-22.3 11-10.3 3.65-20.8 5.49-31.5 5.49-13 0-25.3-2.29-36.7-6.86-11.5-4.48-21.5-11-30.3-19.7-9.05-8.8-16-19-20.8-30.5-4.73-11.5-7.1-23.9-7.1-37.1 0-16 3.65-30.9 11-44.6 7.39-13.8 17.6-24.9 30.6-33.4 7.97-5.31 16.6-9.3 26-12 9.38-2.74 19.2-4.13 29.4-4.13 14.6 0 28.1 2.93 40.5 8.74 12.4 5.73 23 14 31.6 24.9 5.31 6.64 9.26 13.9 11.8 21.7 2.66 7.8 3.98 16.1 3.98 25 0 14.7-3.04 29.1-9.86 38.5-6.74 9.22-13.1 15.1-18.1 18.2-5.27 3.32-15.8 6.61-22.8 6.46-7.4-0.155-12.9-1.67-17.3-4.71-4.36-2.96-7.76-7.91-10.3-14.7-2.13-5.97-3.42-13.6-3.86-22.9-0.128-2.71 2.05-4.92 4.77-4.92h26.7c16.4 0 30.6-13.3 28.7-29.6-0.0344-0.306-0.0701-0.612-0.107-0.918-0.745-5.81-1.91-11-3.5-15.7-2.83-9-7-15.8-12.5-20.5-5.5-4.67-12.1-6.98-19.8-6.98-7.17 0-13.3 1.75-18.5 5.25-4.33 2.79-7.85 6.62-10.5 11.5-0.88 1.6-1.99 1.57-2.8-0.068-2.6-5.3-6.15-9.28-10.7-12-5.17-3.17-11.8-4.73-19.8-4.73-5.17 0-10.8 1.15-17 3.49-3.26 1.18-6.25 2.53-8.97 4.05-3.65 2.04-4.82 7.26-2.99 11l0.234 0.484c2.32 4.8 7.48 5.76 12.2 3.39 1.05-0.522 2.12-1 3.22-1.44 4.33-1.83 8.43-2.76 12.3-2.76 6.33 0 11.2 2.16 14.5 6.49 3.33 4.33 5.01 11.9 5.01 22.8v6.12c0 1.33-1.06 2.41-2.38 2.44l-17.1 0.45c-8.33 0.171-15.5 1.99-21.5 5.49-6 3.33-10.6 8.17-13.7 14.5-3.17 6.17-4.73 13.7-4.73 22.5 0 13.7 3.09 23.8 9.26 30.3 6.17 6.5 13.5 9.74 22 9.74 9 0 16.5-2.09 22.5-6.25 5.1-3.44 9.52-8.75 13.3-16 1.34-2.59 3.02-2.53 4.23 0.125 3.22 7.11 7.11 11.5 13.8 16.3 8.08 5.86 18.9 5.99 26.8 5.64 7.92-0.342 16.5-2.89 27-8.38 9.18-4.78 20.5-14.2 26.4-23.5 6.45-10.2 13.8-27.9 13.8-49.7 0-10.2-1.69-20.1-5.01-29.6-3.24-9.54-7.95-18.4-14.2-26.5-10.2-13.3-22.9-23.5-38-30.7-15-7.22-31.4-10.8-48.9-10.8zm28 58.6c4.5 0 8.08 1.41 10.7 4.25 2.67 2.83 5.35 6.03 6.52 11.7 0.771 3.28 1.62 7.15 1.95 11.3 0.424 5.42-5.1 10.7-10.5 10.7h-28.3c-1.2 0-2.13-0.97-2.06-2.16 0.667-11.3 2.9-20 6.66-26.1 4.17-6.5 9.16-9.74 15-9.74zm-42 55.2v9.53c0 9.33-1.16 16.8-3.49 22.5-2.17 5.5-5.1 9.44-8.77 11.8-3.5 2.34-7.33 3.49-11.5 3.49s-7.74-1.67-10.7-5.01c-3-3.5-4.49-9.01-4.49-16.5 0-8.33 2.16-14.7 6.49-19 4.5-4.5 11.6-7.07 21.2-7.74l9.53-0.642c0.936-0.063 1.73 0.679 1.73 1.62z" style="paint-order:markers stroke fill"/>
+  <path d="m505 296v-0.699a7.87 7.87 0 0 1 7.87-7.87h44.6a8.32 8.32 0 0 1 8.32 8.32v0.193a8.7 8.7 0 0 1-8.7 8.7h-43.4a8.65 8.65 0 0 1-8.65-8.65z" style="paint-order:markers stroke fill"/>
+ </g>
+</svg>

contrib/logo.svg
@@ -1,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!--
+ The aerc logo by Robin Jarry <robin@jarry.cc> distributed under a
+ Creative Commons Attribution 4.0 International License:
+ http://creativecommons.org/licenses/by/4.0/
+-->
+<svg width="256" height="224" version="1.1" xmlns="http://www.w3.org/2000/svg">
+ <g transform="translate(-37.6 -1086)" fill="#808080">
+  <path id="ae" d="m152 1086c-15.7 0-30.5 2.82-44.5 8.47-14 5.56-26.4 13.6-37.1 24.1-10.5 10.4-18.5 22.4-24.2 36.1-5.64 13.7-8.44 28.1-8.44 43.2 0 15.7 2.75 30.4 8.31 44.1 5.56 13.6 13.7 25.6 24.3 36s22.7 18.3 36.4 23.7c13.6 5.48 28.1 8.22 43.5 8.22 12.6 0 25-2.18 37.2-6.49 9.35-3.22 18.2-7.55 26.4-13 4.31-2.84 4.96-8.68 1.71-12.7l-0.504-0.622c-2.66-3.28-7.49-3.8-11-1.48-6.92 4.58-14.3 8.22-22.3 11-10.3 3.65-20.8 5.49-31.5 5.49-13 0-25.3-2.29-36.7-6.86-11.5-4.48-21.5-11-30.3-19.7-9.05-8.8-16-19-20.8-30.5-4.73-11.5-7.1-23.9-7.1-37.1 0-16 3.65-30.9 11-44.6 7.39-13.8 17.6-24.9 30.6-33.4 7.97-5.31 16.6-9.3 26-12 9.38-2.74 19.2-4.13 29.4-4.13 14.6 0 28.1 2.93 40.5 8.74 12.4 5.73 23 14 31.6 24.9 5.31 6.64 9.26 13.9 11.8 21.7 2.66 7.8 3.98 16.1 3.98 25 0 14.7-3.04 29.1-9.86 38.5-6.74 9.22-13.1 15.1-18.1 18.2-5.27 3.32-15.8 6.61-22.8 6.46-7.4-0.155-12.9-1.67-17.3-4.71-4.36-2.96-7.76-7.91-10.3-14.7-2.13-5.97-3.42-13.6-3.86-22.9-0.128-2.71 2.05-4.92 4.77-4.92h26.7c16.4 0 30.6-13.3 28.7-29.6-0.0344-0.306-0.0701-0.612-0.107-0.918-0.745-5.81-1.91-11-3.5-15.7-2.83-9-7-15.8-12.5-20.5-5.5-4.67-12.1-6.98-19.8-6.98-7.17 0-13.3 1.75-18.5 5.25-4.33 2.79-7.85 6.62-10.5 11.5-0.88 1.6-1.99 1.57-2.8-0.068-2.6-5.3-6.15-9.28-10.7-12-5.17-3.17-11.8-4.73-19.8-4.73-5.17 0-10.8 1.15-17 3.49-3.26 1.18-6.25 2.53-8.97 4.05-3.65 2.04-4.82 7.26-2.99 11l0.234 0.484c2.32 4.8 7.48 5.76 12.2 3.39 1.05-0.522 2.12-1 3.22-1.44 4.33-1.83 8.43-2.76 12.3-2.76 6.33 0 11.2 2.16 14.5 6.49 3.33 4.33 5.01 11.9 5.01 22.8v6.12c0 1.33-1.06 2.41-2.38 2.44l-17.1 0.45c-8.33 0.171-15.5 1.99-21.5 5.49-6 3.33-10.6 8.17-13.7 14.5-3.17 6.17-4.73 13.7-4.73 22.5 0 13.7 3.09 23.8 9.26 30.3 6.17 6.5 13.5 9.74 22 9.74 9 0 16.5-2.09 22.5-6.25 5.1-3.44 9.52-8.75 13.3-16 1.34-2.59 3.02-2.53 4.23 0.125 3.22 7.11 7.11 11.5 13.8 16.3 8.08 5.86 18.9 5.99 26.8 5.64 7.92-0.342 16.5-2.89 27-8.38 9.18-4.78 20.5-14.2 26.4-23.5 6.45-10.2 13.8-27.9 13.8-49.7 0-10.2-1.69-20.1-5.01-29.6-3.24-9.54-7.95-18.4-14.2-26.5-10.2-13.3-22.9-23.5-38-30.7-15-7.22-31.4-10.8-48.9-10.8zm28 58.6c4.5 0 8.08 1.41 10.7 4.25 2.67 2.83 5.35 6.03 6.52 11.7 0.771 3.28 1.62 7.15 1.95 11.3 0.424 5.42-5.1 10.7-10.5 10.7h-28.3c-1.2 0-2.13-0.97-2.06-2.16 0.667-11.3 2.9-20 6.66-26.1 4.17-6.5 9.16-9.74 15-9.74zm-42 55.2v9.53c0 9.33-1.16 16.8-3.49 22.5-2.17 5.5-5.1 9.44-8.77 11.8-3.5 2.34-7.33 3.49-11.5 3.49s-7.74-1.67-10.7-5.01c-3-3.5-4.49-9.01-4.49-16.5 0-8.33 2.16-14.7 6.49-19 4.5-4.5 11.6-7.07 21.2-7.74l9.53-0.642c0.936-0.063 1.73 0.679 1.73 1.62z" style="paint-order:markers stroke fill"/>
+  <path id="dash" d="m233 1302v-0.699a7.87 7.87 135 0 1 7.87-7.87h44.6a8.32 8.32 45 0 1 8.32 8.32v0.193a8.7 8.7 135 0 1-8.7 8.7h-43.4a8.65 8.65 45 0 1-8.65-8.65z" style="paint-order:markers stroke fill"/>
+ </g>
+</svg>

commit 97c3eff57743c4dc407b4753a999f9455a5b7110

icon: fix installation path
Currently, the aerc icon is installed to:

  /usr/share/icons/hicolor/scalable/apps/aerc.svg/icon.svg
                                        ^^^^^^^^^

The install_file macro expects an installation directory, not file path.

Rename the source file so that the target install path is correct.

Fixes: 29413dc7833a ("contrib: add application icon and logos")
Reported-by: Matěj Cepl <mcepl@cepl.eu>
Signed-off-by: Robin Jarry <robin@jarry.cc>
Tested-by: Matěj Cepl <mcepl@cepl.eu>
Reviewed-by: Simon Martin <simon@nasilyan.com>

GNUmakefile
@@ -212,7 +212,7 @@ $(foreach s,$(wildcard stylesets/*),\
 $(foreach t,$(wildcard templates/*),\
 	$(eval $(call install_file,$t,644,$(DESTDIR)$(SHAREDIR)/templates)))
 $(eval $(call install_file,contrib/aerc.desktop,644,$(DESTDIR)$(PREFIX)/share/applications))
-$(eval $(call install_file,contrib/icon.svg,644,$(DESTDIR)$(PREFIX)/share/icons/hicolor/scalable/apps/aerc.svg))
+$(eval $(call install_file,contrib/aerc.svg,644,$(DESTDIR)$(PREFIX)/share/icons/hicolor/scalable/apps))
 $(eval $(call install_file,aerc,755,$(DESTDIR)$(BINDIR)))
 $(eval $(call install_file,contrib/carddav-query,755,$(DESTDIR)$(BINDIR)))
 