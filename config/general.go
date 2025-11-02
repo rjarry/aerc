rjarry/aerc

commit 05ec40d81d02475237d71c0c33d5026cf101ba61

config: allow specifying the temporary directory
Add a configuration option to specify a directory where all temporary
files are saved.

Changelog-added: A custom directory can now be specified for temporary
 files.
Signed-off-by: inwit <inwit@sindominio.net>
Acked-by: Robin Jarry <robin@jarry.cc>

config/general.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"errors"
 	"fmt"
+	"io/fs"
 	"os"
 	"path/filepath"
 	"sync/atomic"
@@ -26,6 +28,7 @@ type GeneralConfig struct {
 	DefaultMenuCmd     string       `ini:"default-menu-cmd"`
 	QuakeMode          bool         `ini:"enable-quake-mode" default:"false"`
 	UsePinentry        bool         `ini:"use-terminal-pinentry" default:"false"`
+	TempDir            string       `ini:"temporary-directory" default:"" parse:"ParseTempDir"`
 }
 
 var generalConfig atomic.Pointer[GeneralConfig]
@@ -83,4 +86,12 @@ func (gen *GeneralConfig) ParsePgpProvider(sec *ini.Section, key *ini.Key) (stri
 		return key.String(), nil
 	}
 	return "", fmt.Errorf("must be either auto, gpg or internal")
+}
+
+func (gen *GeneralConfig) ParseTempDir(_ *ini.Section, key *ini.Key) (string, error) {
+	tmpPath := xdg.ExpandHome(key.String())
+	if _, err := os.Stat(tmpPath); errors.Is(err, fs.ErrNotExist) {
+		return "", err
+	}
+	return tmpPath, nil
 }

commands/compose/attach.go
@@ -124,7 +124,7 @@ func (a Attach) openMenu() error {
 		filePickerCmd = strings.ReplaceAll(filePickerCmd, "%s", a.Path)
 	}
 
-	picks, err := os.CreateTemp("", "aerc-filepicker-*")
+	picks, err := os.CreateTemp(config.General().TempDir, "aerc-filepicker-*")
 	if err != nil {
 		return err
 	}

commands/menu.go
@@ -73,7 +73,7 @@ func (m Menu) Execute([]string) error {
 		return m.fallback(title, lines)
 	}
 
-	pick, err := os.CreateTemp("", "aerc-menu-*")
+	pick, err := os.CreateTemp(config.General().TempDir, "aerc-menu-*")
 	if err != nil {
 		return err
 	}

commands/msg/forward.go
@@ -141,7 +141,7 @@ func (f forward) Execute(args []string) error {
 	}
 
 	if f.AttachFull {
-		tmpDir, err := os.MkdirTemp("", "aerc-tmp-attachment")
+		tmpDir, err := os.MkdirTemp(config.General().TempDir, "aerc-tmp-attachment")
 		if err != nil {
 			return err
 		}

commands/msgview/open.go
@@ -10,6 +10,7 @@ import (
 
 	"git.sr.ht/~rjarry/aerc/app"
 	"git.sr.ht/~rjarry/aerc/commands"
+	"git.sr.ht/~rjarry/aerc/config"
 	"git.sr.ht/~rjarry/aerc/lib"
 	"git.sr.ht/~rjarry/aerc/lib/log"
 )
@@ -52,7 +53,7 @@ func (o Open) Execute(args []string) error {
 		}
 		mimeType = part.FullMIMEType()
 
-		tmpDir, err := os.MkdirTemp(os.TempDir(), "aerc-*")
+		tmpDir, err := os.MkdirTemp(config.General().TempDir, "aerc-*")
 		if err != nil {
 			app.PushError(err.Error())
 			return

commands/patch/rebase.go
@@ -67,7 +67,7 @@ func (r Rebase) Execute(args []string) error {
 	}
 
 	rebase := newRebase(commits)
-	f, err := os.CreateTemp("", "aerc-patch-rebase-*")
+	f, err := os.CreateTemp(config.General().TempDir, "aerc-patch-rebase-*")
 	if err != nil {
 		return err
 	}

config/aerc.conf
@@ -82,6 +82,12 @@
 #
 #default-menu-cmd=
 
+# Directory where aerc saves temporary files. The directory must exist and have
+# adequate permissions. If this is empty (which is the default), the platform-
+# specific default directory for temporary files is used.
+#
+#temporary-directory=
+
 [ui]
 #
 # Describes the format for each row in a mailbox view. This is a comma

doc/aerc-config.5.scd
@@ -131,6 +131,11 @@ These options are configured in the *[general]* section of _aerc.conf_.
 	Example:
 		*default-menu-cmd* = _fzf_
 
+*temporary-directory* = _<path>_
+	Directory where aerc saves temporary files. The directory must exist and
+	have adequate permissions. If this is empty (which is the default), the
+	platform-specific default directory for temporary files is used.
+
 # UI OPTIONS
 
 These options are configured in the *[ui]* section of _aerc.conf_.

commit b630cd1d2a952ced64ec47d6bbee6afd80d2d45e

config: fix error when temporary-directory is not set
Fix the following error on startup:

 error: ~/.config/aerc/aerc.conf: [general].temporary-directory:
   stat "": no such file or directory

When the setting is empty, use the platform default temp directory.

Fixes: 05ec40d81d02 ("config: allow specifying the temporary directory")
Reported-by: Jon Fineman <jon@fineman.me>
Reported-by: Julio B <julio.bacel@gmail.com>
Signed-off-by: Robin Jarry <robin@jarry.cc>
Reviewed-by: Tristan Partin <tristan@partin.io>

config/general.go
@@ -89,7 +89,11 @@ func (gen *GeneralConfig) ParsePgpProvider(sec *ini.Section, key *ini.Key) (stri
 }
 
 func (gen *GeneralConfig) ParseTempDir(_ *ini.Section, key *ini.Key) (string, error) {
-	tmpPath := xdg.ExpandHome(key.String())
+	tmp := key.String()
+	if tmp == "" {
+		tmp = os.TempDir()
+	}
+	tmpPath := xdg.ExpandHome(tmp)
 	if _, err := os.Stat(tmpPath); errors.Is(err, fs.ErrNotExist) {
 		return "", err
 	}