rjarry/aerc

commit 8f9cd5c09e3cf8290f2a9939a43453dfb7b4596a

imap: store and retrieve labels
Just as prior to this commit, the GMail extension for labels is used
towards servers where the X-GM-EXT-1 capability is available. For other
servers, this commit adds storing labels using keyword flags, if
PERMANENTFLAGS is either empty or allows `\*`, in accordance with
section 6.3.2 of rfc9051.

No feature validation has been done with either GMail nor Proton, as I
don't have accounts setup for either of those providers.

Labels are not visible by default. Taking inspiration from something
like these lines and adding to the appropriate sections of the
configuration can make them show up in the UI:

column-subject = {{if .Labels}}{{.Labels | join " "}}|{{ end }}{{.Subject}}
header-layout = From|To,Cc|Bcc,Date,Subject,Labels

Existing unit test coverage for imap is sparse. This commit does not
change that fact, as it is hard do see test cases which might be
meaningful to add.

Implements: https://todo.sr.ht/~rjarry/aerc/320
Changelog-added: Add support for Labels when using imap.
Signed-off-by: cos <aerc-mail.org@gunaha.netizen.se>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/imap/flags.go
@@ -84,11 +84,13 @@ func (imapw *IMAPWorker) handleAnsweredMessages(msg *types.AnsweredMessages) {
 	}
 	imapw.handleStoreOps(msg, msg.Uids, item, flags,
 		func(_msg *imap.Message) error {
+			systemFlags, keywordFlags := translateImapFlags(_msg.Flags)
 			imapw.worker.PostMessage(&types.MessageInfo{
 				Message: types.RespondTo(msg),
 				Info: &models.MessageInfo{
-					Flags: translateImapFlags(_msg.Flags),
-					Uid:   models.Uint32ToUid(_msg.Uid),
+					Flags:  systemFlags,
+					Labels: keywordFlags,
+					Uid:    models.Uint32ToUid(_msg.Uid),
 				},
 			}, nil)
 			return nil
@@ -103,11 +105,13 @@ func (imapw *IMAPWorker) handleFlagMessages(msg *types.FlagMessages) {
 	}
 	imapw.handleStoreOps(msg, msg.Uids, item, flags,
 		func(_msg *imap.Message) error {
+			systemFlags, keywordFlags := translateImapFlags(_msg.Flags)
 			imapw.worker.PostMessage(&types.MessageInfo{
 				Message: types.RespondTo(msg),
 				Info: &models.MessageInfo{
-					Flags: translateImapFlags(_msg.Flags),
-					Uid:   models.Uint32ToUid(_msg.Uid),
+					Flags:  systemFlags,
+					Labels: keywordFlags,
+					Uid:    models.Uint32ToUid(_msg.Uid),
 				},
 				ReplaceFlags: true,
 			}, nil)
@@ -198,10 +202,39 @@ func (imapw *IMAPWorker) handleModifyLabels(msg *types.ModifyLabels) {
 				Directories: impactedVFolders,
 			}, nil)
 		default:
-			imapw.worker.PostMessage(&types.Error{
-				Message: types.RespondTo(msg),
-				Error:   fmt.Errorf("operation only supported for GMail and Proton"),
-			}, nil)
+			if len(imapw.selected.PermanentFlags) == 0 || slices.Contains(imapw.selected.PermanentFlags, "\\*") {
+				var item imap.StoreItem
+				if labelOp == imap.AddFlags {
+					item = "+FLAGS"
+				} else {
+					item = "-FLAGS"
+				}
+				labelsAny := []any{}
+				// This utf7 encoding may or may not be the right thing to do?
+				utf7Encoder := utf7.Encoding.NewEncoder()
+				for _, l := range labels {
+					utf7label, _ := utf7Encoder.String(l)
+					labelsAny = append(labelsAny, utf7label)
+				}
+				refresh_cb := func(_msg *imap.Message) error {
+					systemFlags, keywordFlags := translateImapFlags(_msg.Flags)
+					imapw.worker.PostMessage(&types.MessageInfo{
+						Message: types.RespondTo(msg),
+						Info: &models.MessageInfo{
+							Flags:  systemFlags,
+							Labels: keywordFlags,
+							Uid:    models.Uint32ToUid(_msg.Uid),
+						},
+					}, nil)
+					return nil
+				}
+				imapw.handleStoreOps(msg, msg.Uids, item, labelsAny, refresh_cb)
+			} else {
+				imapw.worker.PostMessage(&types.Error{
+					Message: types.RespondTo(msg),
+					Error:   fmt.Errorf("operation not supported by this imap server"),
+				}, nil)
+			}
 			return
 		}
 	}

doc/aerc.1.scd
@@ -527,15 +527,17 @@ message list, the message in the message viewer, etc).
 		_draft_
 			Message is a draft
 
+	(Arbitrary imap keyword flags are managed using `*modify-labels*` / `*:tag*`.)
+
 *:unflag* [*-t*] _<flag>_
 	Operates exactly like *:flag*, defaulting to unsetting (disabling) flags.
 
 *:modify-labels* [_+_|_-_|_!_]_<label>_...++
 *:tag* [_+_|_-_|_!_]_<label>_...
-	Modify message labels (e.g. notmuch tags, GMail or Proton labels).
+	Modify message labels (e.g. imap and notmuch tags, GMail or Proton labels).
 	Labels prefixed with a *+* are added, those prefixed with a *-* are
 	removed and those prefixed with a *!* are toggled (toggling is not
-	supported for GMail nor Proton). As a convenience, labels without either
+	supported for imap, GMail nor Proton). As a convenience, labels without either
 	operand add the specified label.
 
 	Example: add _inbox_ and _unread_ labels, remove _spam_ label.

worker/imap/fetch.go
@@ -3,6 +3,7 @@ package imap
 import (
 	"bufio"
 	"fmt"
+	"slices"
 	"time"
 
 	"github.com/emersion/go-imap"
@@ -95,10 +96,11 @@ func (imapw *IMAPWorker) handleFetchMessageHeaders(
 				return fmt.Errorf("failed to read part header: %w", err)
 			}
 			header := &mail.Header{Header: message.Header{Header: textprotoHeader}}
+			systemFlags, keywordFlags := translateImapFlags(_msg.Flags)
 			info := &models.MessageInfo{
 				BodyStructure: translateBodyStructure(_msg.BodyStructure),
 				Envelope:      translateEnvelope(_msg.Envelope),
-				Flags:         translateImapFlags(_msg.Flags),
+				Flags:         systemFlags,
 				InternalDate:  _msg.InternalDate,
 				RFC822Headers: header,
 				Refs:          parse.MsgIDList(header, "references"),
@@ -108,6 +110,8 @@ func (imapw *IMAPWorker) handleFetchMessageHeaders(
 
 			if imapw.caps.Has("X-GM-EXT-1") {
 				imapw.attachGMLabels(_msg, info)
+			} else if len(imapw.selected.PermanentFlags) == 0 || slices.Contains(imapw.selected.PermanentFlags, "\\*") {
+				info.Labels = keywordFlags
 			}
 
 			if cacheEnabled && !info.Flags.Has(models.SeenFlag) &&
@@ -191,12 +195,15 @@ func (imapw *IMAPWorker) handleFetchMessageBodyPart(
 				},
 			}, nil)
 			// Update flags (to mark message as read)
+			systemFlags, keywordFlags := translateImapFlags(_msg.Flags)
 			info := &models.MessageInfo{
-				Flags: translateImapFlags(_msg.Flags),
+				Flags: systemFlags,
 				Uid:   models.Uint32ToUid(_msg.Uid),
 			}
 			if imapw.caps.Has("X-GM-EXT-1") {
 				imapw.attachGMLabels(_msg, info)
+			} else if len(imapw.selected.PermanentFlags) == 0 || slices.Contains(imapw.selected.PermanentFlags, "\\*") {
+				info.Labels = keywordFlags
 			}
 			imapw.worker.PostMessage(&types.MessageInfo{
 				Message: types.RespondTo(msg),
@@ -237,12 +244,15 @@ func (imapw *IMAPWorker) handleFetchFullMessages(
 				},
 			}, nil)
 			// Update flags (to mark message as read)
+			systemFlags, keywordFlags := translateImapFlags(_msg.Flags)
 			info := &models.MessageInfo{
-				Flags: translateImapFlags(_msg.Flags),
+				Flags: systemFlags,
 				Uid:   models.Uint32ToUid(_msg.Uid),
 			}
 			if imapw.caps.Has("X-GM-EXT-1") {
 				imapw.attachGMLabels(_msg, info)
+			} else if len(imapw.selected.PermanentFlags) == 0 || slices.Contains(imapw.selected.PermanentFlags, "\\*") {
+				info.Labels = keywordFlags
 			}
 			imapw.worker.PostMessage(&types.MessageInfo{
 				Message: types.RespondTo(msg),
@@ -266,13 +276,16 @@ func (imapw *IMAPWorker) handleFetchMessageFlags(msg *types.FetchMessageFlags) {
 	}
 	imapw.handleFetchMessages(msg, msg.Uids, items,
 		func(_msg *imap.Message) error {
+			systemFlags, keywordFlags := translateImapFlags(_msg.Flags)
 			info := &models.MessageInfo{
-				Flags: translateImapFlags(_msg.Flags),
+				Flags: systemFlags,
 				Uid:   models.Uint32ToUid(_msg.Uid),
 			}
 
 			if imapw.caps.Has("X-GM-EXT-1") {
 				imapw.attachGMLabels(_msg, info)
+			} else if len(imapw.selected.PermanentFlags) == 0 || slices.Contains(imapw.selected.PermanentFlags, "\\*") {
+				info.Labels = keywordFlags
 			}
 
 			imapw.worker.PostMessage(&types.MessageInfo{

worker/imap/imap.go
@@ -98,14 +98,17 @@ var flagToImap = map[models.Flags]string{
 	models.DraftFlag:    imap.DraftFlag,
 }
 
-func translateImapFlags(imapFlags []string) models.Flags {
-	var flags models.Flags
+func translateImapFlags(imapFlags []string) (models.Flags, []string) {
+	var systemFlags models.Flags
+	var keywordFlags []string
 	for _, imapFlag := range imapFlags {
 		if flag, ok := imapToFlag[imapFlag]; ok {
-			flags |= flag
+			systemFlags |= flag
+		} else {
+			keywordFlags = append(keywordFlags, imapFlag)
 		}
 	}
-	return flags
+	return systemFlags, keywordFlags
 }
 
 func translateFlags(flags models.Flags) []string {

worker/imap/worker.go
@@ -335,11 +335,13 @@ func (w *IMAPWorker) handleImapUpdate(update client.Update) {
 		if int(msg.SeqNum) > w.seqMap.Size() {
 			w.seqMap.Put(msg.Uid)
 		}
+		systemFlags, keywordFlags := translateImapFlags(msg.Flags)
 		w.worker.PostMessage(&types.MessageInfo{
 			Info: &models.MessageInfo{
 				BodyStructure: translateBodyStructure(msg.BodyStructure),
 				Envelope:      translateEnvelope(msg.Envelope),
-				Flags:         translateImapFlags(msg.Flags),
+				Flags:         systemFlags,
+				Labels:        keywordFlags,
 				InternalDate:  msg.InternalDate,
 				Uid:           models.Uint32ToUid(msg.Uid),
 			},

commit 8d0dbb2e73e696dc73505b7335d61452d10d0599

imap: report label updates in status bar
I noticed that changing labels does not show the "labels updated"
message in the status bar while it should. The problem is that we don't
report a "Done" in handleModifyLabels, and the callback updating the
status bar is never invoked.

This patch fixes this, along with two missing or misplaced return-s in
handleModifyLabels.

Fixes: 8f9cd5c0 ("imap: store and retrieve labels")
Fixes: 96ed6a95 ("imap: add support for :modify-labels / :tag for gmail and proton")
Signed-off-by: Simon Martin <simon@nasilyan.com>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/imap/flags.go
@@ -194,6 +194,7 @@ func (imapw *IMAPWorker) handleModifyLabels(msg *types.ModifyLabels) {
 						Message: types.RespondTo(msg),
 						Error:   err,
 					}, nil)
+					return
 				}
 			}
 			// Refresh the impacted virtual folders;
@@ -234,10 +235,13 @@ func (imapw *IMAPWorker) handleModifyLabels(msg *types.ModifyLabels) {
 					Message: types.RespondTo(msg),
 					Error:   fmt.Errorf("operation not supported by this imap server"),
 				}, nil)
+				return
 			}
-			return
 		}
 	}
+	imapw.worker.PostMessage(&types.Done{
+		Message: types.RespondTo(msg),
+	}, nil)
 }
 
 func (imapw *IMAPWorker) handleStoreOps(