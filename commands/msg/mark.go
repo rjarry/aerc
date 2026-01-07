rjarry/aerc

commit 6baffcdae5da6697e76f9ad79d5b58694b432e74

mark: add filter-based marking
Expand the :mark command with the ability to combine it with filters to
automatically mark all messages matching a filter. The filters can be
combined with -t and -T to narrow down the selected messages:

 :mark Delivered # mark all messages with "Delivered" in the Subject
 :mark -s amazon # mark all messages from "amazon"
 :unmark invoice # unmark all messages with "invoice" in the subject

Changelog-added: Support for marking multiple messages based on
 filters.
Changelog-deprecated: Support for go 1.22.
Signed-off-by: Moritz Poldrack <git@moritz.sh>
Tested-by: inwit <inwit@sindominio.net>
Acked-by: Robin Jarry <robin@jarry.cc>

commands/msg/mark.go
@@ -2,17 +2,26 @@ package msg
 
 import (
 	"fmt"
+	"iter"
+	"slices"
+	"strings"
 
 	"git.sr.ht/~rjarry/aerc/commands"
+	"git.sr.ht/~rjarry/aerc/lib"
+	"git.sr.ht/~rjarry/aerc/lib/log"
 	"git.sr.ht/~rjarry/aerc/models"
+	"git.sr.ht/~rjarry/aerc/worker/types"
 )
 
 type Mark struct {
-	All         bool `opt:"-a" aliases:"mark,unmark" desc:"Mark all messages in current folder."`
-	Toggle      bool `opt:"-t" aliases:"mark,unmark" desc:"Toggle the marked state."`
-	Visual      bool `opt:"-v" aliases:"mark,unmark" desc:"Enter / leave visual mark mode."`
-	VisualClear bool `opt:"-V" aliases:"mark,unmark" desc:"Same as -v but does not clear existing selection."`
-	Thread      bool `opt:"-T" aliases:"mark,unmark" desc:"Mark all messages from the selected thread."`
+	All             bool   `opt:"-a" aliases:"mark,unmark" desc:"Mark all messages in current folder."`
+	Toggle          bool   `opt:"-t" aliases:"mark,unmark" desc:"Toggle the marked state."`
+	Visual          bool   `opt:"-v" aliases:"mark,unmark" desc:"Enter / leave visual mark mode."`
+	VisualClear     bool   `opt:"-V" aliases:"mark,unmark" desc:"Same as -v but does not clear existing selection."`
+	Thread          bool   `opt:"-T" aliases:"mark,unmark" desc:"Mark all messages from the selected thread."`
+	SenderFilter    bool   `opt:"-s" aliases:"mark,unmark" desc:"Mark all messages having the substring in their From: header."`
+	RecipientFilter bool   `opt:"-r" aliases:"mark,unmark" desc:"Mark all messages having the substring in their To: header."`
+	FilterString    string `opt:"..." required:"false" desc:"Mark messages matching this string."`
 }
 
 func init() {
@@ -53,12 +62,34 @@ func (m Mark) Execute(args []string) error {
 	if m.Thread && m.All {
 		return fmt.Errorf("-a and -T are mutually exclusive")
 	}
-
 	if m.Thread && (m.Visual || m.VisualClear) {
 		return fmt.Errorf("-v and -T are mutually exclusive")
 	}
 	if m.Visual && m.All {
 		return fmt.Errorf("-a and -v are mutually exclusive")
+	}
+	if m.SenderFilter && m.RecipientFilter {
+		return fmt.Errorf("-s and -r are mutually exclusive")
+	}
+	if m.Visual && m.FilterString != "" {
+		return fmt.Errorf("visual mode does not support filtering")
+	}
+	if (m.SenderFilter || m.RecipientFilter) && m.FilterString == "" {
+		return fmt.Errorf("-s and -r require a filter string")
+	}
+
+	// if filtering and only a single message is provided, filter all
+	// instead
+	m.All = (m.FilterString != "" && !(m.Thread || m.All)) || m.All
+
+	filter := slices.Values[[]models.UID, models.UID]
+	switch {
+	case m.SenderFilter:
+		filter = senderFilter(store, m.FilterString)
+	case m.RecipientFilter:
+		filter = recipientFilter(store, m.FilterString)
+	case m.FilterString != "":
+		filter = subjectFilter(store, m.FilterString)
 	}
 
 	switch args[0] {
@@ -72,7 +103,7 @@ func (m Mark) Execute(args []string) error {
 		switch {
 		case m.All:
 			uids := store.Uids()
-			for _, uid := range uids {
+			for uid := range filter(uids) {
 				modFunc(uid)
 			}
 			return nil
@@ -85,7 +116,7 @@ func (m Mark) Execute(args []string) error {
 				if err != nil {
 					return err
 				}
-				for _, uid := range threadPtr.Root().Uids() {
+				for uid := range filter(threadPtr.Root().Uids()) {
 					modFunc(uid)
 				}
 			} else {
@@ -102,7 +133,7 @@ func (m Mark) Execute(args []string) error {
 		switch {
 		case m.All && m.Toggle:
 			uids := store.Uids()
-			for _, uid := range uids {
+			for uid := range filter(uids) {
 				marker.ToggleMark(uid)
 			}
 			return nil
@@ -115,7 +146,7 @@ func (m Mark) Execute(args []string) error {
 				if err != nil {
 					return err
 				}
-				for _, uid := range threadPtr.Root().Uids() {
+				for uid := range filter(threadPtr.Root().Uids()) {
 					marker.Unmark(uid)
 				}
 			} else {
@@ -128,4 +159,88 @@ func (m Mark) Execute(args []string) error {
 		return nil
 	}
 	return nil // never reached
+}
+
+func senderFilter(store *lib.MessageStore, senderMatches string) func([]models.UID) iter.Seq[models.UID] {
+	return func(uids []models.UID) iter.Seq[models.UID] {
+		store.FetchHeaders(uids, func(types.WorkerMessage) {})
+
+		store.Lock()
+		defer store.Unlock()
+
+		var filteredUIDs []models.UID
+		for _, uid := range uids {
+			log.Debugf("checking for %s in messageStore", uid)
+			msg := store.Messages[uid]
+			if msg == nil {
+				log.Warnf("message not found in messageStore")
+				continue
+			}
+			log.Debugf("message: %#v", msg)
+			from := msg.Envelope.From
+			for _, sender := range from {
+				if strings.Contains(sender.String(), senderMatches) {
+					filteredUIDs = append(filteredUIDs, uid)
+					break
+				}
+			}
+		}
+
+		return slices.Values(filteredUIDs)
+	}
+}
+
+func recipientFilter(store *lib.MessageStore, recipientMatches string) func([]models.UID) iter.Seq[models.UID] {
+	return func(uids []models.UID) iter.Seq[models.UID] {
+		store.FetchHeaders(uids, func(types.WorkerMessage) {})
+
+		store.Lock()
+		defer store.Unlock()
+
+		var filteredUIDs []models.UID
+		for _, uid := range uids {
+			log.Debugf("checking for %s in messageStore", uid)
+			msg := store.Messages[uid]
+			if msg == nil {
+				log.Warnf("message not found in messageStore")
+				continue
+			}
+			log.Debugf("message: %#v", msg)
+			recipients := msg.Envelope.To
+			for _, recipient := range recipients {
+				if strings.Contains(recipient.String(), recipientMatches) {
+					filteredUIDs = append(filteredUIDs, uid)
+					break
+				}
+			}
+		}
+
+		return slices.Values(filteredUIDs)
+	}
+}
+
+func subjectFilter(store *lib.MessageStore, subjectMatches string) func([]models.UID) iter.Seq[models.UID] {
+	return func(uids []models.UID) iter.Seq[models.UID] {
+		store.FetchHeaders(uids, func(types.WorkerMessage) {})
+
+		store.Lock()
+		defer store.Unlock()
+
+		var filteredUIDs []models.UID
+		for _, uid := range uids {
+			log.Debugf("checking for %s in messageStore", uid)
+			msg := store.Messages[uid]
+			if msg == nil {
+				log.Warnf("message not found in messageStore")
+				continue
+			}
+			log.Debugf("message: %#v", msg)
+			subject := msg.Envelope.Subject
+			if strings.Contains(subject, subjectMatches) {
+				filteredUIDs = append(filteredUIDs, uid)
+			}
+		}
+
+		return slices.Values(filteredUIDs)
+	}
 }

.builds/alpine-edge.yml
@@ -24,6 +24,6 @@ tasks:
   - install: |
       gmake -C aerc install checkinstall
   - ancient-go-version: |
-      curl -fOL https://dl-cdn.alpinelinux.org/alpine/v3.20/community/x86_64/go-1.22.10-r0.apk
-      sudo apk add ./go-1.22.10-r0.apk
+      curl -fOL https://dl-cdn.alpinelinux.org/alpine/v3.21/community/x86_64/go-1.23.9-r0.apk
+      sudo apk add ./go-1.23.9-r0.apk
       gmake -C aerc clean all

doc/aerc.1.scd
@@ -849,9 +849,11 @@ message list, the message in the message viewer, etc).
 
 	*-A*: Same as *-a* but saves all the named parts, not just attachments.
 
-*:mark* [*-atvT*]
+*:mark* [*-atvTsr*] _<filter>_
 	Marks messages. Commands will execute on all marked messages instead of the
-	highlighted one if applicable. The flags below can be combined as needed.
+	highlighted one if applicable. The flags below can be combined as
+	needed. The existence of a filter implies *-a* unless *-T* has been
+	specified.
 
 	*-a*: Apply to all messages in the current folder
 
@@ -863,12 +865,21 @@ message list, the message in the message viewer, etc).
 
 	*-T*: Marks the displayed message thread of the selected message.
 
-*:unmark* [*-at*]
-	Unmarks messages. The flags below can be combined as needed.
+	*-s*: apply the filter to the From: header (does not work with *-v* or *-V*)
+
+	*-r*: apply the filter to the To: header (does not work with *-v* or *-V*)
+
+*:unmark* [*-atsr*] _<filter>_
+	Unmarks messages. The flags below can be combined as needed. The
+	existence of a filter implies *-a*.
 
 	*-a*: Apply to all messages in the current folder
 
 	*-t*: toggle the mark state instead of unmarking a message
+
+	*-s*: apply the filter to the From: header (does not work with *-v* or *-V*)
+
+	*-r*: apply the filter to the To: header (does not work with *-v* or *-V*)
 
 *:remark*
 	Re-select the last set of marked messages. Can be used to chain commands

go.mod
@@ -1,6 +1,6 @@
 module git.sr.ht/~rjarry/aerc
 
-go 1.22.0
+go 1.23
 
 require (
 	git.sr.ht/~rjarry/go-opt/v2 v2.1.0

commit 70bd4678d062ae687e3b7761930225aaf7b750a8

mark: fix panic on incomplete message
At times messages from the message store may be incomplete and not
contain the sender. This is unexpected, but should not crash the
application.

[signal SIGSEGV: segmentation violation code=0x1 addr=0x28 pc=0x55f3cee9b99e]

panic({0x55f3cf118720?, 0x55f3cf72e6f0?})
	runtime/panic.go:783 +0x132
git.sr.ht/~rjarry/aerc/commands/msg.Mark.Execute.senderFilter.func2({0xc0019c4000, 0x801, 0x0?})
	git.sr.ht/~rjarry/aerc/commands/msg/mark.go:180 +0x1fe
git.sr.ht/~rjarry/aerc/commands/msg.Mark.Execute({0x1, 0x0, 0x0, 0x0, 0x0, 0x1, 0x0, {0xc00190eb70, 0x7}}, {0xc001542810, ...})
	git.sr.ht/~rjarry/aerc/commands/msg/mark.go:106 +0x68c
git.sr.ht/~rjarry/aerc/commands.ExecuteCommand({0x55f3cf21c8f0, 0x55f3cf8cd220?}, {0xc00190ea90?, 0x500c000fb2300?})
	git.sr.ht/~rjarry/aerc/commands/commands.go:221 +0x4d3

Fixes: 6baffcda ("mark: add filter-based marking")
Signed-off-by: Moritz Poldrack <git@moritz.sh>
Acked-by: Robin Jarry <robin@jarry.cc>

commands/msg/mark.go
@@ -172,8 +172,8 @@ func senderFilter(store *lib.MessageStore, senderMatches string) func([]models.U
 		for _, uid := range uids {
 			log.Debugf("checking for %s in messageStore", uid)
 			msg := store.Messages[uid]
-			if msg == nil {
-				log.Warnf("message not found in messageStore")
+			if msg == nil || msg.Envelope == nil {
+				log.Warnf("message not found in messageStore or message incomplete")
 				continue
 			}
 			log.Debugf("message: %#v", msg)