rjarry/aerc

commit c0e1bb1e0b0f55ed5375cf47d9f25d8d3f25cae2

colorize: highlight diffstat summary lines
Colorize the diffstat output that appears before patches, showing
per-file change summaries like:

 README.md        |  6 ++++--

The +/- characters use diff_add/diff_del styles. Binary file stats (Bin
X -> Y bytes) colorize the old size as deletion and new size as
addition. Matches git log --stat.

Changelog-added: Diffstat summary lines before patches are now
 highlighted similar to git diff --stat.
Signed-off-by: Fredrik Foss-Indrehus <fredrik@ffoss.net>
Acked-by: Robin Jarry <robin@jarry.cc>

filters/colorize.c
@@ -494,6 +494,63 @@ static void diff_chunk(const char *in)
 	print(RESET);
 }
 
+static void diff_stat(const char *in)
+{
+	const char *pipe = strstr(in, " | ");
+	if (!pipe) {
+		print_notabs(in, BUFSIZ);
+		return;
+	}
+
+	size_t len;
+	size_t prefix_len = (size_t)(pipe - in) + 3;
+	in += print_notabs(in, prefix_len);
+
+	if (startswith(in, "Bin ")) {
+		print_notabs(in, 4);
+		in += 4;
+
+		len = 0;
+		while (isdigit(in[len]))
+			len++;
+		print(seq(&styles.diff_del));
+		in += print_notabs(in, len);
+		print(RESET);
+
+		if (startswith(in, " -> ")) {
+			print_notabs(in, 4);
+			in += 4;
+		}
+
+		len = 0;
+		while (isdigit(in[len]))
+			len++;
+		print(seq(&styles.diff_add));
+		in += print_notabs(in, len);
+		print(RESET);
+
+		print_notabs(in, BUFSIZ);
+		return;
+	}
+
+	len = 0;
+	while (in[len] == ' ' || isdigit(in[len]))
+		len++;
+	in += print_notabs(in, len);
+
+	while (*in == '+' || *in == '-') {
+		char c = *in;
+		print(seq(c == '+' ? &styles.diff_add : &styles.diff_del));
+		len = 0;
+		while (in[len] == c)
+			len++;
+		in += print_notabs(in, len);
+		print(RESET);
+	}
+
+	print_notabs(in, BUFSIZ);
+}
+
 static inline bool isurichar(char c)
 {
 	if (c == '\0')
@@ -633,6 +690,9 @@ static regex_t diff_start_re;
 	" index|(rename|copy) (to|from)|index|---|\\+\\+\\+) "
 static regex_t diff_meta_re;
 
+#define DIFF_STAT_RE "^ .* [|] +(Bin )?[0-9]+"
+static regex_t diff_stat_re;
+
 static void quote(const char *in)
 {
 	regmatch_t groups[8];
@@ -708,6 +768,8 @@ static void colorize_line(const char *in)
 			print_style(in, &styles.diff_add);
 		} else if (startswith(in, "-")) {
 			print_style(in, &styles.diff_del);
+		} else if (!regexec(&diff_stat_re, in, 1, groups, 0)) {
+			diff_stat(in);
 		} else if (!startswith(in, " ") && strcmp(in, "") != 0) {
 			state = BODY;
 			if (startswith(in, ">")) {
@@ -726,9 +788,14 @@ static void colorize_line(const char *in)
 		if (!regexec(&diff_start_re, in, 8, groups, 0)) {
 			state = DIFF;
 			print_style(in, &styles.diff_meta);
+		} else if (!strcmp(in, "---")) {
+			state = DIFF;
+			print_notabs(in, BUFSIZ);
 		} else if (!strcmp(in, "-- ")) {
 			state = SIGNATURE;
 			signature(in);
+		} else if (!regexec(&diff_stat_re, in, 1, groups, 0)) {
+			diff_stat(in);
 		} else {
 			state = BODY;
 			if (startswith(in, ">")) {
@@ -795,6 +862,7 @@ int main(int argc, char **argv)
 	regcomp(&header_re, HEADER_RE, REG_EXTENDED);
 	regcomp(&diff_start_re, DIFF_START_RE, REG_EXTENDED);
 	regcomp(&diff_meta_re, DIFF_META_RE, REG_EXTENDED);
+	regcomp(&diff_stat_re, DIFF_STAT_RE, REG_EXTENDED);
 	regcomp(&url_re, URL_RE, REG_EXTENDED);
 
 	err = parse_args(argc, argv);

filters/vectors/colorize-patch.expected
@@ -7,7 +7,7 @@ According to scdoc(5), numbered lists start with a period.
 [1mFixes:[0m af63bd0188d1 ("doc: homogenize scdoc markup")
 [1mSigned-off-by:[0m Robin Jarry <[4m]8;id=colorize-1;mailto:robin@jarry.cc\robin@jarry.cc]8;;\[0m>
 ---
- doc/aerc-stylesets.7.scd | 18 +++++++++---------
+ doc/aerc-stylesets.7.scd | 18 [38;2;0;255;0m+++++++++[0m[31m---------[0m
  1 file changed, 9 insertions(+), 9 deletions(-)
 
 [1mdiff --git a/doc/aerc-stylesets.7.scd b/doc/aerc-stylesets.7.scd[0m

commit 14a6b1703d6f96712e74bcf2085b404195fcd606

colorize: fix quoted diff meta line rendering
Check for diff meta lines before +/- diff content when rendering quoted
text. The "+++ b/file" line was incorrectly rendered as a diff addition
because startswith("+") matched before the diff_meta_re check.

Previously in a quoted diff:
  > --- a/file.c     rendered with quote style + bold (correct)
  > +++ b/file.c     rendered as diff addition (wrong, green)

Now both render as bold with the quote color, same as unquoted diff
meta lines.

Fixes: c0e1bb1e0b0f ("colorize: highlight diffstat summary lines")
Signed-off-by: Fredrik Foss-Indrehus <fredrik@ffoss.net>
Acked-by: Robin Jarry <robin@jarry.cc>

filters/colorize.c
@@ -779,16 +779,16 @@ static void quote(const char *in)
 
 	print(seq(s));
 	in += print_notabs(in, q);
-	if (startswith(in, "+")) {
+	if (!regexec(&diff_meta_re, in, 8, groups, 0)) {
+		print(BOLD);
+		print_notabs(in, BUFSIZ);
+		print(RESET);
+	} else if (startswith(in, "+")) {
 		print(RESET);
 		diff_line(in, &styles.diff_add);
 	} else if (startswith(in, "-")) {
 		print(RESET);
 		diff_line(in, &styles.diff_del);
-	} else if (!regexec(&diff_meta_re, in, 8, groups, 0)) {
-		print(BOLD);
-		print_notabs(in, BUFSIZ);
-		print(RESET);
 	} else {
 		urls(in, s);
 		print(RESET);

filters/vectors/colorize-quotes.expected
@@ -36,8 +36,8 @@ facilisi et pri http:// or https://!
 [36m>[0m
 [36m> [1mdiff --git a/foo b/foo[0m
 [36m> [1mindex 4b0fe8dded3a..518b67134639 100644[0m
-[36m> [0m[31m--- a/foo[0m
-[36m> [0m[38;2;0;255;0m+++ b/foo[0m
+[36m> [1m--- a/foo[0m
+[36m> [1m+++ b/foo[0m
 [36m> @@ -131,6 +131,83 @@ func pouet() int {[0m
 [36m>       err := doThis()[0m
 [36m>  [0m