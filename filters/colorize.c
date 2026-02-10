841785b8941d272e12433c01add3068de8a5bdc4
Fredrik Foss-Indrehus <fredrik@ffoss.net>
2026-02-10 23:02:33 +0100

    colorize: add '=' to email local-part pattern
    The URL regex was not matching email addresses containing '=' in the
    local part. Only the portion after '=' was being colorized.
    
    Signed-off-by: Fredrik Foss-Indrehus <fredrik@ffoss.net>
    Acked-by: Robin Jarry <robin@jarry.cc>

--- filters/colorize.c
+++ filters/colorize.c
@@ -617,7 +617,7 @@ static inline bool isurichar(char c)
 
 #define URL_RE \
 	"([a-z]{2,8})://" \
-	"|(mailto:)?[[:alnum:]_+.~/-]*[[:alnum:]]@[[:alnum:]][[:alnum:].-]*[[:alnum:]]"
+	"|(mailto:)?[[:alnum:]_+.=~/-]*[[:alnum:]]@[[:alnum:]][[:alnum:].-]*[[:alnum:]]"
 static regex_t url_re;
 
 static void urls(const char *in, struct style *ctx)
