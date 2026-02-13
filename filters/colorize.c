rjarry/aerc

commit c156b255fcdcb4131092c8e5d74e8eb8ec0c6ba9

colorize: accept wildcards in styles
Allow using * and ? to match multiple style objects for colorize, as
stated in the man page and consistent with aerc's main styles.

Fixes: a5c4f5d (colorize: handle spaces in styleset)
Signed-off-by: Jason Cox <me@jasoncarloscox.com>
Acked-by: Robin Jarry <robin@jarry.cc>

filters/colorize.c
@@ -337,7 +337,7 @@ static struct {const char *n; struct style *s;} ini_objects[] = {
 };
 
 /*                         object            attribute           value */
-#define STYLE_LINE_FORMAT "%127[0-9A-Za-z_-].%127[0-9a-zA-Z_-] = %127[#a-zA-Z0-9]s"
+#define STYLE_LINE_FORMAT "%127[0-9A-Za-z_-*?].%127[0-9a-zA-Z_-] = %127[#a-zA-Z0-9]s"
 
 static int parse_styleset(void)
 {

filters/vectors/colorize-quotes.expected
@@ -1,26 +1,26 @@
 Foo Bar, xxxxx:
-[0m> Lorem ipsum dolor sit amet, insolens adolescens ne usu? In pri denique[0m
-[0m> argumentum, te autem decore convenire mea! Duo nisl esse an, aliquid[0m
-[0m> conceptam sea cu. Ignota copiosae gubergren ad est, ut illum doming vocibus[0m
-[0m> sed. Et vis nulla expetendis mediocritatem, errem option gloriatur at nam?[0m
-[0m> Brute vidisse corpora ut his, sonet omnesque adipiscing ea quo, cum ea errem[0m
-[0m> aliquip reformidans?[0m
+[36m> Lorem ipsum dolor sit amet, insolens adolescens ne usu? In pri denique[0m
+[36m> argumentum, te autem decore convenire mea! Duo nisl esse an, aliquid[0m
+[36m> conceptam sea cu. Ignota copiosae gubergren ad est, ut illum doming vocibus[0m
+[36m> sed. Et vis nulla expetendis mediocritatem, errem option gloriatur at nam?[0m
+[36m> Brute vidisse corpora ut his, sonet omnesque adipiscing ea quo, cum ea errem[0m
+[36m> aliquip reformidans?[0m
 
 Magna delicatissimi ei vel? Quem petentium scribentur eum ne? Et inani debet
 cetero mea, sint conceptam efficiendi mel te. Qui ut senserit interesset, per
 nibh petentium at! Sit docendi laboramus ei, animal insolens ad mea.
 
-[0m>> Nostrud alienum nec in, illum errem audiam no per! Saepe alterum vis ea! Ei[0m
-[0m>> quis minim ius, ut eos mandamus salutandi. Lorem facilisis in nam, ridens[0m
-[0m>> principes sadipscing et eum, pri graecis singulis ut. Mea dolor primis[0m
-[0m>> impetus in, his epicurei tacimates id, vis labitur suscipit ad.[0m
-[0m> Erat alienum interpretaris has et, te vim aliquam molestie. Nam vivendum[0m
-[0m> facilisis qualisque at, ex his mucius qualisque! Fabulas lucilius adversarium[0m
-[0m> eu his. Cu soluta inermis accusata usu, his nulla dolore ne, vis id semper[0m
-[0m> detracto sententia <[4m]8;id=colorize-0;https://foobar.com\https://foobar.com]8;;\[0m[0m> && "[4m]8;id=colorize-1;https://foobaz.org/\https://foobaz.org/]8;;\[0m[0m".[0m
-[0m>[0m
-[0m> Error libris deleniti ea mei, vis at elit probo munere, his sint unum[0m
-[0m> albucius ex. [[4m]8;id=colorize-2;https://pouet.com/oksuper\https://pouet.com/oksuper]8;;\[0m[0m]([4m]8;id=colorize-3;https://pouet.com/oksuper\https://pouet.com/oksuper]8;;\[0m[0m).[0m
+[2;36m>> Nostrud alienum nec in, illum errem audiam no per! Saepe alterum vis ea! Ei[0m
+[2;36m>> quis minim ius, ut eos mandamus salutandi. Lorem facilisis in nam, ridens[0m
+[2;36m>> principes sadipscing et eum, pri graecis singulis ut. Mea dolor primis[0m
+[2;36m>> impetus in, his epicurei tacimates id, vis labitur suscipit ad.[0m
+[36m> Erat alienum interpretaris has et, te vim aliquam molestie. Nam vivendum[0m
+[36m> facilisis qualisque at, ex his mucius qualisque! Fabulas lucilius adversarium[0m
+[36m> eu his. Cu soluta inermis accusata usu, his nulla dolore ne, vis id semper[0m
+[36m> detracto sententia <[4m]8;id=colorize-0;https://foobar.com\https://foobar.com]8;;\[0m[36m> && "[4m]8;id=colorize-1;https://foobaz.org/\https://foobaz.org/]8;;\[0m[36m".[0m
+[36m>[0m
+[36m> Error libris deleniti ea mei, vis at elit probo munere, his sint unum[0m
+[36m> albucius ex. [[4m]8;id=colorize-2;https://pouet.com/oksuper\https://pouet.com/oksuper]8;;\[0m[36m]([4m]8;id=colorize-3;https://pouet.com/oksuper\https://pouet.com/oksuper]8;;\[0m[36m).[0m
 
 Graece definiebas scripserit ne est? Nec nonumes explicari contentiones ne,
 vocent iuvaret placerat no vix. Nec et partem salutandi deseruisse, his no
@@ -28,23 +28,23 @@ possim malorum pericula. Te quando reprehendunt nam, at consul sadipscing vel?
 Velit possim aliquando ei per, ne simul quodsi antiopam sea, ullum choro
 facilisi et pri http:// or https://!
 
-[0m> Dico soleat partem ea pro, ad vix impetus splendide. Primis melius principes[0m
-[0m> pri ad, tacimates pertinacia ei pro? Appareat atomorum oportere at nam, eu[0m
-[0m> per quod minim reprimique, ornatus graecis ad vel. Malis vulputate ea qui,[0m
-[0m> eum tacimates recteque et, usu ea dolore vidisse. Brute mediocrem molestiae[0m
-[0m> sed te. No stet prompta pri, rebum populo nominati eos te.[0m
-[0m>[0m
-[0m> [1mdiff --git a/foo b/foo[0m
-[0m> [1mindex 4b0fe8dded3a..518b67134639 100644[0m
-[0m> [0m[31m--- a/foo[0m
-[0m> [0m[38;2;0;255;0m+++ b/foo[0m
-[0m> @@ -131,6 +131,83 @@ func pouet() int {[0m
-[0m>          err := doThis()[0m
-[0m>  [0m
-[0m> [0m[31m-        err2 := doThat()[0m
-[0m> [0m[38;2;0;255;0m+        err2 := notDoThat()[0m
-[0m>  [0m
-[0m>          if err != nil || err2 != nil {[0m
+[36m> Dico soleat partem ea pro, ad vix impetus splendide. Primis melius principes[0m
+[36m> pri ad, tacimates pertinacia ei pro? Appareat atomorum oportere at nam, eu[0m
+[36m> per quod minim reprimique, ornatus graecis ad vel. Malis vulputate ea qui,[0m
+[36m> eum tacimates recteque et, usu ea dolore vidisse. Brute mediocrem molestiae[0m
+[36m> sed te. No stet prompta pri, rebum populo nominati eos te.[0m
+[36m>[0m
+[36m> [1mdiff --git a/foo b/foo[0m
+[36m> [1mindex 4b0fe8dded3a..518b67134639 100644[0m
+[36m> [0m[31m--- a/foo[0m
+[36m> [0m[38;2;0;255;0m+++ b/foo[0m
+[36m> @@ -131,6 +131,83 @@ func pouet() int {[0m
+[36m>          err := doThis()[0m
+[36m>  [0m
+[36m> [0m[31m-        err2 := doThat()[0m
+[36m> [0m[38;2;0;255;0m+        err2 := notDoThat()[0m
+[36m>  [0m
+[36m>          if err != nil || err2 != nil {[0m
 
 Id vix referrentur philosophia, veri labores an nec. Noster denique no duo, sit
 ei diam inermis vocibus! Mutat principes ex pro, at [4m]8;id=colorize-4;mailto://~rjarry/aerc-devel@lists.sr.ht\~rjarry/aerc-devel@lists.sr.ht]8;;\[0m.
@@ -59,7 +59,7 @@ oporteat assueverit disputationi nam ne, nonumes iracundia mea ad! Duo libris
 recusabo id, ceteros salutatus inciderint vim ea. Et graeco reformidans vel? Ei
 has labore quidam [4m]8;id=colorize-8;https://foobaz.com/ooo<uuuu>okf\https://foobaz.com/ooo<uuuu>okf]8;;\[0m?
 
-[0m>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> sympa, non?[0m
+[2;36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> sympa, non?[0m
 
 [2m-- [0m
 [2mBatman[0m

commit b754a89e9e9654ab790bee168db7f93fec07c8be

colorize: fix implementation-defined scanf scanset range
Per https://pubs.opengroup.org/onlinepubs/9799919799/functions/fscanf.html:

  If a '-' is in the scanlist and is not the first character, nor the
  second where the first character is a '^', nor the last character, the
  behavior is implementation-defined.

The scanset [_-*] places a hyphen between '_' and '*', which POSIX
treats as a character range. Since '_' (0x5F) > '*' (0x2A), the range is
implementation-defined; glibc and musl seem to silently accept it, while
macOS libc matches nothing. This breaks parsing of style names
containing '_' or '*', causing silent fallbacks to compiled-in defaults.

Fix by reordering the scanset to [*?_-], placing the hyphen last where
POSIX guarantees it is treated literally.

Fixes: c156b255fcdc ("colorize: accept wildcards in styles")
Signed-off-by: Fredrik Foss-Indrehus <fredrik@ffoss.net>
Acked-by: Robin Jarry <robin@jarry.cc>

filters/colorize.c
@@ -364,7 +364,7 @@ static struct {const char *n; struct style *s;} ini_objects[] = {
 };
 
 /*                         object            attribute           value */
-#define STYLE_LINE_FORMAT "%127[0-9A-Za-z_-*?].%127[0-9a-zA-Z_-] = %127[#a-zA-Z0-9]"
+#define STYLE_LINE_FORMAT "%127[0-9A-Za-z*?_-].%127[0-9a-zA-Z_-] = %127[#a-zA-Z0-9]"
 
 static int parse_styleset(void)
 {