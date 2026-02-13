rjarry/aerc

commit 1e5a3097f89c29e33d540cdc2cc8f6fb3ac75478

colorize: fix tab expansion to respect column position
Tabs move the cursor to the next 8-column boundary, not 8 spaces
unconditionally. The old code replaced all tabs with 8 spaces regardless
of position, breaking alignment when tabs appeared mid-line.

  "x\ty" (x at col 0, tab, y):
    terminal: y appears at column 8
    old filter: "x        y"        y at column 9
    new filter: "x       y"         y at column 8 (7 spaces)

  "abcd\ty" (4 chars, tab, y):
    terminal: y appears at column 8
    old filter: "abcd        y"     y at column 12
    new filter: "abcd    y"         y at column 8 (4 spaces)

Also handle multibyte characters via mbtowc/wcwidth for correct column
width calculation with UTF-8. A 2-column wide character (like CJK or
some emoji) followed by a tab needs 6 spaces to reach column 8, not 7.

Signed-off-by: Fredrik Foss-Indrehus <fredrik@ffoss.net>
Acked-by: Robin Jarry <robin@jarry.cc>

filters/colorize.c
@@ -14,6 +14,8 @@
 #include <string.h>
 #include <wchar.h>
 
+#define TAB_WIDTH 8
+
 static void usage(void)
 {
 	puts("usage: colorize [-h] [-8] [-s FILE] [-f FILE]");
@@ -179,6 +181,7 @@ struct styles {
 
 static FILE *in_file;
 static bool osc8_urls;
+static size_t current_column;
 static const char *styleset;
 static struct styles styles = {
 	.url = { .underline = true, .fg = { .type = PALETTE, .index = 3 } },
@@ -452,20 +455,43 @@ static inline void print(const char *in)
 
 static inline size_t print_notabs(const char *in, size_t max_len)
 {
-	size_t len = 0;
-	while (*in != '\0' && len < max_len) {
-		char c = *in++;
-		if (c == '\t') {
+	size_t consumed = 0;
+	while (*in != '\0' && consumed < max_len) {
+		if (*in == '\t') {
 			/* Tabs are interpreted as cursor movement and are not
 			 * colored like regular characters. Replace them with
-			 * 8 spaces. */
-			fputs("        ", stdout);
-		} else {
-			fputc(c, stdout);
+			 * spaces while still respecting column position. */
+			size_t spaces = TAB_WIDTH -
+				(current_column % TAB_WIDTH);
+			while (spaces--) {
+				fputc(' ', stdout);
+				current_column++;
+			}
+			in++;
+			consumed++;
+			continue;
 		}
-		len++;
+
+		wchar_t wc;
+		int len = mbtowc(&wc, in, MB_CUR_MAX);
+
+		if (len < 1) {
+			/* Invalid sequence, output as-is and reset state */
+			fputc((unsigned char)*in, stdout);
+			current_column++;
+			in++;
+			consumed++;
+			mbtowc(NULL, NULL, 0);
+		} else {
+			fwrite(in, 1, (size_t)len, stdout);
+			int w = wcwidth(wc);
+			if (w > 0)
+				current_column += (size_t)w;
+			in += len;
+			consumed += (size_t)len;
+		}
 	}
-	return len;
+	return consumed;
 }
 
 static void print_osc8(const char *url, size_t len, size_t id, bool email) {
@@ -924,6 +950,7 @@ int main(int argc, char **argv)
 		buf[strcspn(buf, "\r\n")] = '\0';
 		colorize_line(buf);
 		printf("\n");
+		current_column = 0;
 	}
 end:
 	if (in_file && in_file != stdin) {

filters/vectors/colorize-diff-cvs-bsd.expected
@@ -12,13 +12,13 @@ file + graphics/optipng/Makefile
 [1m--- graphics/optipng/Makefile[0m
 [1m+++ graphics/optipng/Makefile[0m
 [2m@@ -1,7 +1,6 @@[0m[0m[0m
- COMMENT =                lossless PNG optimizer
-[31m-DISTNAME =                optipng-0.7.7[0m
-[38;2;0;255;0m+DISTNAME =                optipng-0.7.8[0m
- CATEGORIES =                graphics
-[31m-REVISION =                1[0m
+ COMMENT =              lossless PNG optimizer
+[31m-DISTNAME =             optipng-0.7.7[0m
+[38;2;0;255;0m+DISTNAME =             optipng-0.7.8[0m
+ CATEGORIES =           graphics
+[31m-REVISION =             1[0m
  
- HOMEPAGE =                https://optipng.sourceforge.net/
+ HOMEPAGE =             https://optipng.sourceforge.net/
  
 blob - 34f525b3d1ea7b210ee34d1b94c98bd76c30d01f
 file + graphics/optipng/distinfo

filters/vectors/colorize-diff-cvs-bsd2.expected
@@ -13,61 +13,61 @@ The previous diff had an obvious issue spotted by ajacoutot@, thanks!
 ===================================================================
 RCS file: /home/cvs/ports/lang/python/Makefile.inc,v
 [1mdiff -u -p -r1.159 Makefile.inc[0m
-[1m--- Makefile.inc        26 Sep 2023 12:02:03 -0000        1.159[0m
+[1m--- Makefile.inc        26 Sep 2023 12:02:03 -0000      1.159[0m
 [1m+++ Makefile.inc        13 Nov 2023 16:11:40 -0000[0m
-[2m@@ -130,7 +130,9 @@[0m[0m CONFIGURE_ARGS +=        --with-lto[0m
+[2m@@ -130,7 +130,9 @@[0m[0m CONFIGURE_ARGS +=   --with-lto[0m
  .    if ${MACHINE_ARCH} != "arm" && ${MACHINE_ARCH} != "powerpc"
  # On armv7, clang errors out due to lack of memory.
  # On powerpc, the python binary would crash by "Segmentation fault".
-[31m-CONFIGURE_ARGS +=        --enable-optimizations[0m
+[31m-CONFIGURE_ARGS +=      --enable-optimizations[0m
 [38;2;0;255;0m+# XXX lld from llvm-16 errors out with:[0m
 [38;2;0;255;0m+# ld.lld: error: relocation R_X86_64_PC32 cannot be used against symbol '__profd_isdigit'; recompile with -fPIC[0m
-[38;2;0;255;0m+#CONFIGURE_ARGS +=        --enable-optimizations[0m
+[38;2;0;255;0m+#CONFIGURE_ARGS +=     --enable-optimizations[0m
  .    endif
  .  endif
- TEST_IS_INTERACTIVE =        Yes
+ TEST_IS_INTERACTIVE =  Yes
 Index: 3.10/Makefile
 ===================================================================
 RCS file: /home/cvs/ports/lang/python/3.10/Makefile,v
 [1mdiff -u -p -r1.36 Makefile[0m
-[1m--- 3.10/Makefile        1 Sep 2023 18:48:06 -0000        1.36[0m
-[1m+++ 3.10/Makefile        13 Nov 2023 15:53:49 -0000[0m
+[1m--- 3.10/Makefile       1 Sep 2023 18:48:06 -0000       1.36[0m
+[1m+++ 3.10/Makefile       13 Nov 2023 15:53:49 -0000[0m
 [2m@@ -4,6 +4,7 @@[0m[0m[0m
  # Python itself.
  
- FULL_VERSION =                3.10.13
-[38;2;0;255;0m+REVISION =                0[0m
- SHARED_LIBS =                python3.10 0.0
- VERSION_SPEC =                >=3.10,<3.11
- PORTROACH =                limit:^3\.10
+ FULL_VERSION =         3.10.13
+[38;2;0;255;0m+REVISION =             0[0m
+ SHARED_LIBS =          python3.10 0.0
+ VERSION_SPEC =         >=3.10,<3.11
+ PORTROACH =            limit:^3\.10
 Index: 3.11/Makefile
 ===================================================================
 RCS file: /home/cvs/ports/lang/python/3.11/Makefile,v
 [1mdiff -u -p -r1.12 Makefile[0m
-[1m--- 3.11/Makefile        20 Oct 2023 09:18:48 -0000        1.12[0m
-[1m+++ 3.11/Makefile        13 Nov 2023 15:54:05 -0000[0m
+[1m--- 3.11/Makefile       20 Oct 2023 09:18:48 -0000      1.12[0m
+[1m+++ 3.11/Makefile       13 Nov 2023 15:54:05 -0000[0m
 [2m@@ -4,6 +4,7 @@[0m[0m[0m
  # Python itself.
  
- FULL_VERSION =                3.11.6
-[38;2;0;255;0m+REVISION =                0[0m
- SHARED_LIBS =                python3.11 0.0
- VERSION_SPEC =                >=3.11,<3.12
- PORTROACH =                limit:^3\.11
+ FULL_VERSION =         3.11.6
+[38;2;0;255;0m+REVISION =             0[0m
+ SHARED_LIBS =          python3.11 0.0
+ VERSION_SPEC =         >=3.11,<3.12
+ PORTROACH =            limit:^3\.11
 Index: 3.9/Makefile
 ===================================================================
 RCS file: /home/cvs/ports/lang/python/3.9/Makefile,v
 [1mdiff -u -p -r1.42 Makefile[0m
-[1m--- 3.9/Makefile        1 Sep 2023 18:50:44 -0000        1.42[0m
+[1m--- 3.9/Makefile        1 Sep 2023 18:50:44 -0000       1.42[0m
 [1m+++ 3.9/Makefile        13 Nov 2023 15:53:58 -0000[0m
 [2m@@ -4,6 +4,7 @@[0m[0m[0m
  # Python itself.
  
- FULL_VERSION =                3.9.18
-[38;2;0;255;0m+REVISION =                0[0m
- SHARED_LIBS =                python3.9 0.0
- VERSION_SPEC =                >=3.9,<3.10
- PORTROACH =                limit:^3\.9
+ FULL_VERSION =         3.9.18
+[38;2;0;255;0m+REVISION =             0[0m
+ SHARED_LIBS =          python3.9 0.0
+ VERSION_SPEC =         >=3.9,<3.10
+ PORTROACH =            limit:^3\.9
 
 
 [2m-- [0m

filters/vectors/colorize-quotes.expected
@@ -39,12 +39,12 @@ facilisi et pri http:// or https://!
 [36m> [0m[31m--- a/foo[0m
 [36m> [0m[38;2;0;255;0m+++ b/foo[0m
 [36m> @@ -131,6 +131,83 @@ func pouet() int {[0m
-[36m>          err := doThis()[0m
+[36m>       err := doThis()[0m
 [36m>  [0m
-[36m> [0m[31m-        err2 := doThat()[0m
-[36m> [0m[38;2;0;255;0m+        err2 := notDoThat()[0m
+[36m> [0m[31m-     err2 := doThat()[0m
+[36m> [0m[38;2;0;255;0m+     err2 := notDoThat()[0m
 [36m>  [0m
-[36m>          if err != nil || err2 != nil {[0m
+[36m>       if err != nil || err2 != nil {[0m
 
 Id vix referrentur philosophia, veri labores an nec. Noster denique no duo, sit
 ei diam inermis vocibus! Mutat principes ex pro, at [4m]8;id=colorize-4;mailto:~rjarry/aerc-devel@lists.sr.ht\~rjarry/aerc-devel@lists.sr.ht]8;;\[0m.

commit b4eb511d57d22208d810a7be83147dfed053636a

colorize: cast MB_CUR_MAX to size_t in mbtowc call
MB_CUR_MAX is correctly defined as size_t on Linux but is defined as an
int on macOS. Cast to size_t to silence -Wsign-conversion warning in the
mbtowc call.

Fixes: 1e5a3097f89c ("colorize: fix tab expansion to respect column position")
Signed-off-by: Fredrik Foss-Indrehus <fredrik@ffoss.net>
Acked-by: Robin Jarry <robin@jarry.cc>

filters/colorize.c
@@ -473,7 +473,7 @@ static inline size_t print_notabs(const char *in, size_t max_len)
 		}
 
 		wchar_t wc;
-		int len = mbtowc(&wc, in, MB_CUR_MAX);
+		int len = mbtowc(&wc, in, (size_t)MB_CUR_MAX);
 
 		if (len < 1) {
 			/* Invalid sequence, output as-is and reset state */