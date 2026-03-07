rjarry/aerc

commit cb2ea2be4ad209dcedf2d058eb2367bda9953f7a

viewer: add support for special headers styling
Now the user is able to customize the recipients name, email, subject
and date headers style in the message viewer's header by customizing the
'realname', 'email', 'subject' and 'date' style objects respectively.

Draw individual components of header fields according to the configured
styleset.

Changelog-added: New styleset objects available to customize special
 headers in the message viewer.
Signed-off-by: Sahil Gautam <printfdebugging@gmail.com>
Acked-by: Robin Jarry <robin@jarry.cc>

app/msgviewer.go
@@ -928,6 +928,14 @@ type HeaderView struct {
 	uiConfig   *config.UIConfig
 }
 
+func drawHeaderView(hv *HeaderView, ctx *ui.Context, xpos int, vstyle vaxis.Style, value string, lim int) {
+	if hv.ValueField == nil {
+		ctx.Printf(xpos, 0, vstyle, "%s", value)
+	} else {
+		hv.ValueField.Draw(ctx.Subcontext(xpos, 0, lim, 1))
+	}
+}
+
 func (hv *HeaderView) Draw(ctx *ui.Context) {
 	name := hv.Name
 	xpos := runewidth.StringWidth(name + ":")
@@ -940,8 +948,34 @@ func (hv *HeaderView) Draw(ctx *ui.Context) {
 	vstyle := hv.uiConfig.GetStyle(config.STYLE_DEFAULT)
 	hstyle := hv.uiConfig.GetStyle(config.STYLE_HEADER)
 
-	// TODO: Make this more robust and less dumb
-	if hv.Name == "PGP" {
+	ctx.Fill(0, 0, ctx.Width(), ctx.Height(), ' ', vstyle)
+	ctx.Printf(0, 0, hstyle, "%s:", name)
+
+	switch strings.ToLower(name) {
+	case "from", "to", "cc", "bcc":
+		for pos, char := range value {
+			switch char {
+			case '<':
+				drawHeaderView(hv, ctx, xpos+pos, hv.uiConfig.GetStyle(config.STYLE_DEFAULT), string(char), lim)
+				vstyle = hv.uiConfig.GetStyle(config.STYLE_EMAIL)
+				continue
+			case '>':
+				vstyle = hv.uiConfig.GetStyle(config.STYLE_DEFAULT)
+			case ',':
+				drawHeaderView(hv, ctx, xpos+pos, hv.uiConfig.GetStyle(config.STYLE_DEFAULT), string(char), lim)
+				vstyle = hv.uiConfig.GetStyle(config.STYLE_REALNAME)
+				continue
+			case ' ':
+				vstyle = hv.uiConfig.GetStyle(config.STYLE_REALNAME)
+			}
+			drawHeaderView(hv, ctx, xpos+pos, vstyle, string(char), lim)
+		}
+		return
+	case "date":
+		vstyle = hv.uiConfig.GetStyle(config.STYLE_DATE)
+	case "subject":
+		vstyle = hv.uiConfig.GetStyle(config.STYLE_SUBJECT)
+	case "pgp":
 		vstyle = hv.uiConfig.GetStyle(config.STYLE_SUCCESS)
 	}
 
@@ -952,6 +986,7 @@ func (hv *HeaderView) Draw(ctx *ui.Context) {
 	} else {
 		hv.ValueField.Draw(ctx.Subcontext(xpos, 0, lim, 1))
 	}
+	drawHeaderView(hv, ctx, xpos, vstyle, value, lim)
 }
 
 func (hv *HeaderView) Invalidate() {

config/style.go
@@ -25,6 +25,10 @@ const (
 
 	STYLE_TITLE
 	STYLE_HEADER
+	STYLE_REALNAME
+	STYLE_EMAIL
+	STYLE_SUBJECT
+	STYLE_DATE
 
 	STYLE_STATUSLINE_DEFAULT
 	STYLE_STATUSLINE_ERROR
@@ -75,8 +79,12 @@ var StyleNames = map[string]StyleObject{
 	"warning": STYLE_WARNING,
 	"success": STYLE_SUCCESS,
 
-	"title":  STYLE_TITLE,
-	"header": STYLE_HEADER,
+	"title":    STYLE_TITLE,
+	"header":   STYLE_HEADER,
+	"realname": STYLE_REALNAME,
+	"email":    STYLE_EMAIL,
+	"subject":  STYLE_SUBJECT,
+	"date":     STYLE_DATE,
 
 	"statusline_default": STYLE_STATUSLINE_DEFAULT,
 	"statusline_error":   STYLE_STATUSLINE_ERROR,

doc/aerc-stylesets.7.scd
@@ -87,6 +87,14 @@ styling.
 :  The style object used to style titles in ui elements.
 |  *header*
 :  The style object used to style headers in ui elements.
+|  *realname*
+:  The style object used to style real name(s) in the email fields in the header section of the message viewer.
+|  *email*
+:  The style object used to style email(s) in the email fields in the header section of the message viewer.
+|  *subject*
+:  The style object used to style subject in the header section of the message viewer.
+|  *date*
+:  The style object used to style date in the header section of the message viewer.
 |  *statusline_default*
 :  The default style applied to the statusline.
 |  *statusline_error*

commit 62f87e7cbbc3220bfd5d897b1679213adf32c7eb

viewer: correctly render the recipient names on the header
Take into account the fact that names can have Unicode characters wider
than one cell and that they can be 1-4 bytes wide. So instead of using
the byte position, move `xpos` forward by the cell width of the rendered
character.

Fixes: cb2ea2be ("viewer: add support for special headers styling")
Signed-off-by: Sahil Gautam <printfdebugging@gmail.com>
Tested-by: Inwit <inwit@sindominio.net>
Acked-by: Robin Jarry <robin@jarry.cc>

app/msgviewer.go
@@ -953,22 +953,25 @@ func (hv *HeaderView) Draw(ctx *ui.Context) {
 
 	switch strings.ToLower(name) {
 	case "from", "to", "cc", "bcc":
-		for pos, char := range value {
+		for _, char := range value {
 			switch char {
 			case '<':
-				drawHeaderView(hv, ctx, xpos+pos, hv.uiConfig.GetStyle(config.STYLE_DEFAULT), string(char), lim)
+				drawHeaderView(hv, ctx, xpos, hv.uiConfig.GetStyle(config.STYLE_DEFAULT), string(char), lim)
 				vstyle = hv.uiConfig.GetStyle(config.STYLE_EMAIL)
+				xpos++
 				continue
 			case '>':
 				vstyle = hv.uiConfig.GetStyle(config.STYLE_DEFAULT)
 			case ',':
-				drawHeaderView(hv, ctx, xpos+pos, hv.uiConfig.GetStyle(config.STYLE_DEFAULT), string(char), lim)
+				drawHeaderView(hv, ctx, xpos, hv.uiConfig.GetStyle(config.STYLE_DEFAULT), string(char), lim)
 				vstyle = hv.uiConfig.GetStyle(config.STYLE_REALNAME)
+				xpos++
 				continue
 			case ' ':
 				vstyle = hv.uiConfig.GetStyle(config.STYLE_REALNAME)
 			}
-			drawHeaderView(hv, ctx, xpos+pos, vstyle, string(char), lim)
+			drawHeaderView(hv, ctx, xpos, vstyle, string(char), lim)
+			xpos += runewidth.RuneWidth(char)
 		}
 		return
 	case "date":