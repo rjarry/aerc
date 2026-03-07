rjarry/aerc

commit 8a151bac2323fef56b66e91ae941a6edbdd7d91c

models: add validity identifier to Directory structs
Add a Uid field to Directory and DirectoryInfo that stores a unique
identifier which changes when the directory contents become invalid.
For IMAP, this is the UIDVALIDITY value; other backends leave it empty.

Fetch UIDVALIDITY during directory listing when LIST-STATUS is
available, otherwise fetch it during CheckMail status queries. Include
the validity in Directory messages sent to the UI.

Refactor handleListDirectories to collect all mailboxes and statuses
before sending Directory messages, allowing the validity to be included
from the status response.

Signed-off-by: Robin Jarry <robin@jarry.cc>
Reviewed-by: Simon Martin <simon@nasilyan.com>

worker/imap/list.go
@@ -1,6 +1,7 @@
 package imap
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/emersion/go-imap"
@@ -11,64 +12,103 @@ import (
 )
 
 func (imapw *IMAPWorker) handleListDirectories(msg *types.ListDirectories) error {
-	mailboxes := make(chan *imap.MailboxInfo)
 	imapw.worker.Tracef("Listing mailboxes")
-	done := make(chan any)
 
-	go func() {
-		defer log.PanicHandler()
+	var mailboxes []*imap.MailboxInfo
+	var statuses []*imap.MailboxStatus
+	var err error
 
-		labels := make([]string, 0)
-		provider := imapw.config.provider
-		useLabels := provider == GMail || provider == Proton
+	if imapw.liststatus {
+		items := []imap.StatusItem{imap.StatusUidValidity}
+		mailboxes, statuses, err = imapw.listMailboxesStatus(items)
+	} else {
+		mailboxes, err = imapw.listMailboxes()
+	}
+	if err != nil {
+		return err
+	}
 
-		for mbox := range mailboxes {
-			if !canOpen(mbox) {
-				// no need to pass this to handlers if it can't be opened
-				continue
+	statusMap := make(map[string]*imap.MailboxStatus)
+	for _, status := range statuses {
+		statusMap[status.Name] = status
+	}
+
+	labels := make([]string, 0)
+	provider := imapw.config.provider
+	useLabels := provider == GMail || provider == Proton
+
+	for _, mbox := range mailboxes {
+		if !canOpen(mbox) {
+			continue
+		}
+		dir := &models.Directory{
+			Name: mbox.Name,
+		}
+		if status, ok := statusMap[mbox.Name]; ok && status.UidValidity != 0 {
+			dir.Uid = fmt.Sprintf("%d", status.UidValidity)
+		}
+		switch provider {
+		case GMail:
+			labels = append(labels, mbox.Name)
+		case Proton:
+			if after, ok := strings.CutPrefix(mbox.Name, "Labels/"); ok {
+				labels = append(labels, after)
 			}
-			dir := &models.Directory{
-				Name: mbox.Name,
-			}
-			switch provider {
-			case GMail:
-				labels = append(labels, mbox.Name)
-			case Proton:
-				if after, ok := strings.CutPrefix(mbox.Name, "Labels/"); ok {
-					labels = append(labels, after)
-				}
-			default:
-				// No label support
-			}
-			for _, attr := range mbox.Attributes {
-				attr = strings.TrimPrefix(attr, "\\")
-				attr = strings.ToLower(attr)
-				role, ok := models.Roles[attr]
-				if !ok {
-					continue
-				}
+		}
+		for _, attr := range mbox.Attributes {
+			attr = strings.TrimPrefix(attr, "\\")
+			attr = strings.ToLower(attr)
+			if role, ok := models.Roles[attr]; ok {
 				dir.Role = role
 			}
-			if mbox.Name == "INBOX" {
-				dir.Role = models.InboxRole
-			}
-			imapw.worker.PostMessage(&types.Directory{
-				Message: types.RespondTo(msg),
-				Dir:     dir,
-			}, nil)
 		}
-
-		if useLabels {
-			imapw.worker.Debugf("Available labels: %s", labels)
-			imapw.worker.PostMessage(&types.LabelList{Labels: labels}, nil)
+		if mbox.Name == "INBOX" {
+			dir.Role = models.InboxRole
 		}
+		imapw.worker.PostMessage(&types.Directory{
+			Message: types.RespondTo(msg),
+			Dir:     dir,
+		}, nil)
+	}
 
-		done <- nil
+	if useLabels {
+		imapw.worker.Debugf("Available labels: %s", labels)
+		imapw.worker.PostMessage(&types.LabelList{Labels: labels}, nil)
+	}
+
+	return nil
+}
+
+func (imapw *IMAPWorker) listMailboxes() ([]*imap.MailboxInfo, error) {
+	ch := make(chan *imap.MailboxInfo)
+	done := make(chan []*imap.MailboxInfo)
+	go func() {
+		defer log.PanicHandler()
+		var list []*imap.MailboxInfo
+		for mbox := range ch {
+			list = append(list, mbox)
+		}
+		done <- list
 	}()
+	err := imapw.client.List("", "*", ch)
+	return <-done, err
+}
 
-	err := imapw.client.List("", "*", mailboxes)
-	<-done
-	return err
+func (imapw *IMAPWorker) listMailboxesStatus(
+	items []imap.StatusItem,
+) ([]*imap.MailboxInfo, []*imap.MailboxStatus, error) {
+	ch := make(chan *imap.MailboxInfo)
+	done := make(chan []*imap.MailboxInfo)
+	go func() {
+		defer log.PanicHandler()
+		var list []*imap.MailboxInfo
+		for mbox := range ch {
+			list = append(list, mbox)
+		}
+		done <- list
+	}()
+	statuses, err := imapw.client.liststatus.ListStatus("", "*", items, ch)
+	return <-done, statuses, err
 }
 
 const NonExistentAttr = "\\NonExistent"

models/models.go
@@ -72,6 +72,10 @@ const (
 
 type Directory struct {
 	Name string
+	// Uid is a unique identifier for this directory that changes when
+	// the directory contents become invalid (e.g. IMAP UIDVALIDITY change).
+	// For IMAP: UIDVALIDITY as string, empty for other backends.
+	Uid string
 	// Exists messages in the Directory
 	Exists int
 	// Recent messages in the Directory
@@ -84,6 +88,10 @@ type Directory struct {
 
 type DirectoryInfo struct {
 	Name string
+	// Uid is a unique identifier for this directory that changes when
+	// the directory contents become invalid (e.g. IMAP UIDVALIDITY change).
+	// For IMAP: UIDVALIDITY as string, empty for other backends.
+	Uid string
 	// The total number of messages in this mailbox.
 	Exists int
 	// The number of messages not seen since the last time the mailbox was opened.

worker/imap/checkmail.go
@@ -1,6 +1,8 @@
 package imap
 
 import (
+	"fmt"
+
 	"git.sr.ht/~rjarry/aerc/models"
 	"git.sr.ht/~rjarry/aerc/worker/types"
 	"github.com/emersion/go-imap"
@@ -12,6 +14,7 @@ func (w *IMAPWorker) handleCheckMailMessage(msg *types.CheckMail) error {
 		imap.StatusRecent,
 		imap.StatusUnseen,
 		imap.StatusUidNext,
+		imap.StatusUidValidity,
 	}
 	var (
 		statuses  []*imap.MailboxStatus
@@ -64,13 +67,17 @@ func (w *IMAPWorker) handleCheckMailMessage(msg *types.CheckMail) error {
 			}
 			w.selected = status
 		}
+		info := &models.DirectoryInfo{
+			Name:   status.Name,
+			Exists: int(status.Messages),
+			Recent: int(status.Recent),
+			Unseen: int(status.Unseen),
+		}
+		if status.UidValidity != 0 {
+			info.Uid = fmt.Sprintf("%d", status.UidValidity)
+		}
 		w.worker.PostMessage(&types.DirectoryInfo{
-			Info: &models.DirectoryInfo{
-				Name:   status.Name,
-				Exists: int(status.Messages),
-				Recent: int(status.Recent),
-				Unseen: int(status.Unseen),
-			},
+			Info:    info,
 			Refetch: refetch,
 		}, nil)
 	}

commit 4d5d7db82baba7f42f9af5e14e834cda6617c547

imap: don't use list-status to discover the list of directories
This is a redo of 194365b9, that uses LIST (not LIST-STATUS) to properly
discover the list of mailboxes. The goal was to properly get the role of
each mailbox, and fully support the `restrict-deletes` functionality
(incidentally it also fixes mailbox listing for Strato); it was lost via
8a151bac.

This patch reinstates this and then uses LIST-STATUS (if it's supported)
to get each mailbox's UIDVALIDITY.

Fixes: 8a151bac ("models: add validity identifier to Directory structs")
Reported-by: post <post@sonnenkinder.org>
Signed-off-by: Simon Martin <simon@nasilyan.com>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/imap/list.go
@@ -18,11 +18,13 @@ func (imapw *IMAPWorker) handleListDirectories(msg *types.ListDirectories) error
 	var statuses []*imap.MailboxStatus
 	var err error
 
+	mailboxes, err = imapw.listMailboxes()
+	if err != nil {
+		return err
+	}
 	if imapw.liststatus {
 		items := []imap.StatusItem{imap.StatusUidValidity}
-		mailboxes, statuses, err = imapw.listMailboxesStatus(items)
-	} else {
-		mailboxes, err = imapw.listMailboxes()
+		_, statuses, err = imapw.listMailboxesStatus(items)
 	}
 	if err != nil {
 		return err