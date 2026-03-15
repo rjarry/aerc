rjarry/aerc

commit 018e6f81f4d5c1cf4d26bc6514458e2eea2a0ccb

lib: consolidate SASL authentication code
SASL and OAuth2 authentication logic was duplicated across the IMAP
worker and the SMTP sender code. Both implementations handled XOAUTH2,
OAUTHBEARER, and token caching independently, making maintenance
difficult and bug fixes error-prone.

Move this shared logic into lib/auth where it can be reused by all
backends. The IMAP worker no longer needs its own OAuth structs and
configuration parsing since it can now rely on the common
implementation also used by the SMTP sender.

Signed-off-by: Robin Jarry <robin@jarry.cc>
Reviewed-by: Simon Martin <simon@nasilyan.com>

lib/auth/sasl.go
@@ -1,0 +1,76 @@
+package auth
+
+import (
+	"fmt"
+	"net/url"
+	"slices"
+	"strings"
+
+	"github.com/emersion/go-sasl"
+	"golang.org/x/oauth2"
+)
+
+func ParseScheme(uri *url.URL) (protocol string, mech string, err error) {
+	protocol = ""
+	mech = "plain"
+	if uri.Scheme != "" {
+		parts := strings.Split(uri.Scheme, "+")
+		if len(parts) == 0 {
+			return "", "", fmt.Errorf("Unknown scheme %s", uri.Scheme)
+		}
+		protocol = parts[0]
+		parts = slices.Delete(parts, 0, 1)
+		i := slices.Index(parts, "insecure")
+		if i != -1 {
+			protocol += "+insecure"
+			parts = slices.Delete(parts, i, i+1)
+		}
+		if len(parts) > 0 {
+			mech = strings.Join(parts, "+")
+		}
+	}
+	return protocol, mech, nil
+}
+
+func NewSaslClient(mech string, uri *url.URL, acct string) (sasl.Client, error) {
+	var saslClient sasl.Client
+
+	user := uri.User.Username()
+	password, _ := uri.User.Password()
+
+	switch mech {
+	case "", "none":
+		saslClient = nil
+	case "login":
+		saslClient = sasl.NewLoginClient(user, password)
+	case "plain":
+		saslClient = sasl.NewPlainClient("", user, password)
+	case "oauthbearer", "xoauth2":
+		q := uri.Query()
+		o := oauth2.Config{
+			ClientID:     q.Get("client_id"),
+			ClientSecret: q.Get("client_secret"),
+			Scopes:       strings.Split(q.Get("scope"), " "),
+			Endpoint: oauth2.Endpoint{
+				TokenURL: q.Get("token_endpoint"),
+			},
+		}
+		password, err := GetAccessToken(&o, acct, mech, password)
+		if err != nil {
+			return nil, err
+		}
+		if mech == "xoauth2" {
+			saslClient = NewXoauth2Client(user, password)
+		} else {
+			saslClient = sasl.NewOAuthBearerClient(
+				&sasl.OAuthBearerOptions{
+					Username: uri.User.Username(),
+					Token:    password,
+				},
+			)
+		}
+	default:
+		return nil, fmt.Errorf("Unsupported auth mechanism %q", mech)
+	}
+	return saslClient, nil
+}

lib/send/smtp.go
@@ -7,6 +7,7 @@ import (
 	"net/url"
 	"strings"
 
+	"git.sr.ht/~rjarry/aerc/lib/auth"
 	"github.com/emersion/go-message/mail"
 	"github.com/emersion/go-smtp"
 	"github.com/pkg/errors"
@@ -81,8 +82,9 @@ func (s *smtpSender) Close() error {
 }
 
 func newSmtpSender(
-	protocol string, auth string, uri *url.URL, domain string,
-	from *mail.Address, rcpts []*mail.Address, requestDSN bool,
+	protocol string, mech string, uri *url.URL, domain string,
+	from *mail.Address, rcpts []*mail.Address, account string,
+	requestDSN bool,
 ) (io.WriteCloser, error) {
 	var err error
 	var conn *smtp.Client
@@ -101,7 +103,7 @@ func newSmtpSender(
 		return nil, errors.Wrap(err, "Connection failed")
 	}
 
-	saslclient, err := newSaslClient(auth, uri)
+	saslclient, err := auth.NewSaslClient(mech, uri, account)
 	if err != nil {
 		conn.Close()
 		return nil, err

worker/imap/connect.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 	"time"
 
 	"git.sr.ht/~rjarry/aerc/lib"
+	"git.sr.ht/~rjarry/aerc/lib/auth"
 	"git.sr.ht/~rjarry/aerc/lib/log"
 	"github.com/emersion/go-imap"
 	"github.com/emersion/go-imap/client"
-	"github.com/emersion/go-sasl"
 )
 
 // connect establishes a new tcp connection to the imap server, logs in and
@@ -24,7 +25,17 @@ func (w *IMAPWorker) connect() (*client.Client, error) {
 		c    *client.Client
 	)
 
-	conn, err = newTCPConn(w.config.addr, w.config.connection_timeout)
+	protocol, mech, err := auth.ParseScheme(w.config.url)
+	if err != nil {
+		return nil, err
+	}
+
+	addr := w.config.url.Host
+	if !strings.Contains(addr, ":") {
+		addr += ":" + protocol
+	}
+
+	conn, err = newTCPConn(addr, w.config.connection_timeout)
 	if conn == nil || err != nil {
 		return nil, err
 	}
@@ -44,22 +55,24 @@ func (w *IMAPWorker) connect() (*client.Client, error) {
 		}
 	}
 
-	serverName, _, _ := net.SplitHostPort(w.config.addr)
+	serverName, _, _ := net.SplitHostPort(addr)
 	tlsConfig := &tls.Config{ServerName: serverName}
 
-	switch w.config.scheme {
+	protocol, insecure := strings.CutSuffix(protocol, "+insecure")
+
+	switch protocol {
 	case "imap":
 		c, err = client.New(conn)
 		if err != nil {
 			return nil, err
 		}
-		if !w.config.insecure {
+		if !insecure {
 			if err = c.StartTLS(tlsConfig); err != nil {
 				return nil, err
 			}
 		}
 	case "imaps":
-		if w.config.insecure {
+		if insecure {
 			tlsConfig.InsecureSkipVerify = true
 		}
 		tlsConn := tls.Client(conn, tlsConfig)
@@ -68,36 +81,33 @@ func (w *IMAPWorker) connect() (*client.Client, error) {
 			return nil, err
 		}
 	default:
-		return nil, fmt.Errorf("Unknown IMAP scheme %s", w.config.scheme)
+		return nil, fmt.Errorf("Unknown IMAP scheme %s", protocol)
 	}
 
 	c.ErrorLog = log.ErrorLogger()
 
-	if w.config.user != nil {
-		username := w.config.user.Username()
-
-		// TODO: 2nd parameter false if no password is set. ask for it
-		// if unset.
-		password, _ := w.config.user.Password()
-
-		if w.config.oauthBearer.Enabled {
-			if err := w.config.oauthBearer.Authenticate(
-				username, password, c); err != nil {
-				return nil, err
-			}
-		} else if w.config.xoauth2.Enabled {
-			if err := w.config.xoauth2.Authenticate(
-				username, password, w.config.name, c); err != nil {
-				return nil, err
-			}
-		} else if plain, err := c.SupportAuth("PLAIN"); err != nil {
+	if w.config.url.User != nil && mech == "" {
+		if plain, err := c.SupportAuth("PLAIN"); err != nil {
 			return nil, err
 		} else if plain {
-			auth := sasl.NewPlainClient("", username, password)
-			if err := c.Authenticate(auth); err != nil {
-				return nil, err
-			}
-		} else if err := c.Login(username, password); err != nil {
+			mech = "plain"
+		} else {
+			mech = "login"
+		}
+	}
+
+	saslClient, err := auth.NewSaslClient(mech, w.config.url, w.config.name)
+	if err != nil {
+		return nil, err
+	}
+	if saslClient != nil {
+		if ok, err := c.SupportAuth(strings.ToUpper(mech)); err != nil {
+			return nil, err
+		} else if !ok {
+			return nil, fmt.Errorf("%s auth not supported", mech)
+		}
+		err = c.Authenticate(saslClient)
+		if err != nil {
 			return nil, err
 		}
 	}

lib/send/sasl.go
@@ -1,77 +1,0 @@
-package send
-
-import (
-	"fmt"
-	"net/url"
-
-	"github.com/emersion/go-sasl"
-	"golang.org/x/oauth2"
-
-	"git.sr.ht/~rjarry/aerc/lib"
-)
-
-func newSaslClient(auth string, uri *url.URL) (sasl.Client, error) {
-	var saslClient sasl.Client
-	switch auth {
-	case "":
-		fallthrough
-	case "none":
-		saslClient = nil
-	case "login":
-		password, _ := uri.User.Password()
-		saslClient = sasl.NewLoginClient(uri.User.Username(), password)
-	case "plain":
-		password, _ := uri.User.Password()
-		saslClient = sasl.NewPlainClient("", uri.User.Username(), password)
-	case "oauthbearer":
-		q := uri.Query()
-		oauth2 := &oauth2.Config{}
-		if q.Get("token_endpoint") != "" {
-			oauth2.ClientID = q.Get("client_id")
-			oauth2.ClientSecret = q.Get("client_secret")
-			oauth2.Scopes = []string{q.Get("scope")}
-			oauth2.Endpoint.TokenURL = q.Get("token_endpoint")
-		}
-		password, _ := uri.User.Password()
-		bearer := lib.OAuthBearer{
-			OAuth2:  oauth2,
-			Enabled: true,
-		}
-		if bearer.OAuth2.Endpoint.TokenURL != "" {
-			token, err := bearer.ExchangeRefreshToken(password)
-			if err != nil {
-				return nil, err
-			}
-			password = token.AccessToken
-		}
-		saslClient = sasl.NewOAuthBearerClient(&sasl.OAuthBearerOptions{
-			Username: uri.User.Username(),
-			Token:    password,
-		})
-	case "xoauth2":
-		q := uri.Query()
-		oauth2 := &oauth2.Config{}
-		if q.Get("token_endpoint") != "" {
-			oauth2.ClientID = q.Get("client_id")
-			oauth2.ClientSecret = q.Get("client_secret")
-			oauth2.Scopes = []string{q.Get("scope")}
-			oauth2.Endpoint.TokenURL = q.Get("token_endpoint")
-		}
-		password, _ := uri.User.Password()
-		bearer := lib.Xoauth2{
-			OAuth2:  oauth2,
-			Enabled: true,
-		}
-		if bearer.OAuth2.Endpoint.TokenURL != "" {
-			token, err := bearer.ExchangeRefreshToken(password)
-			if err != nil {
-				return nil, err
-			}
-			password = token.AccessToken
-		}
-		saslClient = lib.NewXoauth2Client(uri.User.Username(), password)
-	default:
-		return nil, fmt.Errorf("Unsupported auth mechanism %s", auth)
-	}
-	return saslClient, nil
-}

commands/compose/send.go
@@ -206,7 +206,7 @@ func sendHelper(composer *app.Composer, header *mail.Header, uri *url.URL, domai
 		}
 		sender, err := send.NewSender(
 			composer.Worker(), uri, domain,
-			from, rcpts,
+			from, rcpts, composer.Account().Name(),
 			folders, requestDSN,
 		)
 		if err != nil {

commands/msg/bounce.go
@@ -159,7 +159,7 @@ func (b Bounce) Execute(args []string) error {
 				msg.Envelope.MessageId, addresses)
 
 			if sender, err = send.NewSender(acct.Worker(), uri,
-				domain, config.From, rcpts, nil, false); err != nil {
+				domain, config.From, rcpts, acct.Name(), nil, false); err != nil {
 				return
 			}
 			defer func() {

lib/auth/token.go
@@ -1,0 +1,64 @@
+package auth
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path"
+
+	"git.sr.ht/~rjarry/aerc/lib/xdg"
+	"golang.org/x/oauth2"
+)
+
+func exchangeRefreshToken(o *oauth2.Config, refreshToken string) (*oauth2.Token, error) {
+	token := new(oauth2.Token)
+	token.RefreshToken = refreshToken
+	token.TokenType = "Bearer"
+	return o.TokenSource(context.TODO(), token).Token()
+}
+
+func tokenCachePath(account, mech string) string {
+	return xdg.CachePath("aerc", account+"-"+mech+".token")
+}
+
+func saveRefreshToken(refreshToken, account, mech string) error {
+	p := tokenCachePath(account, mech)
+	if err := os.MkdirAll(path.Dir(p), 0o700); err != nil {
+		return err
+	}
+	return os.WriteFile(p, []byte(refreshToken), 0o600)
+}
+
+func getRefreshToken(account, mech string) (string, error) {
+	buf, err := os.ReadFile(tokenCachePath(account, mech))
+	if err != nil {
+		return "", err
+	}
+	return string(buf), nil
+}
+
+func GetAccessToken(
+	o *oauth2.Config, account, mech, password string,
+) (string, error) {
+	if o.Endpoint.TokenURL == "" {
+		return password, nil
+	}
+	usedCache := false
+	if r, err := getRefreshToken(account, mech); err == nil && len(r) > 0 {
+		password = string(r)
+		usedCache = true
+	}
+
+	token, err := exchangeRefreshToken(o, password)
+	if err != nil {
+		if usedCache {
+			return "", fmt.Errorf("%w: try deleting %s",
+				err, tokenCachePath(account, mech))
+		}
+		return "", err
+	}
+	if err := saveRefreshToken(token.RefreshToken, account, mech); err != nil {
+		return "", err
+	}
+	return token.AccessToken, nil
+}

lib/auth/xoauth2.go
@@ -1,0 +1,57 @@
+//
+// This code is derived from the go-sasl library.
+//
+// Copyright (c) 2016 emersion
+// Copyright (c) 2022, Oracle and/or its affiliates.
+//
+// SPDX-License-Identifier: MIT
+
+package auth
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/emersion/go-sasl"
+)
+
+// An XOAUTH2 error.
+type Xoauth2Error struct {
+	Status  string `json:"status"`
+	Schemes string `json:"schemes"`
+	Scope   string `json:"scope"`
+}
+
+// Implements error.
+func (err *Xoauth2Error) Error() string {
+	return fmt.Sprintf("XOAUTH2 authentication error (%v)", err.Status)
+}
+
+const XoauthMechanism string = "XOAUTH2"
+
+type xoauth2Client struct {
+	Username string
+	Token    string
+}
+
+func (a *xoauth2Client) Start() (mech string, ir []byte, err error) {
+	mech = XoauthMechanism
+	ir = []byte("user=" + a.Username + "\x01auth=Bearer " + a.Token + "\x01\x01")
+	return
+}
+
+func (a *xoauth2Client) Next(challenge []byte) ([]byte, error) {
+	// Server sent an error response
+	xoauth2Err := &Xoauth2Error{}
+	if err := json.Unmarshal(challenge, xoauth2Err); err != nil {
+		return nil, err
+	} else {
+		return nil, xoauth2Err
+	}
+}
+
+// An implementation of the XOAUTH2 authentication mechanism, as
+// described in https://developers.google.com/gmail/xoauth2_protocol.
+func NewXoauth2Client(username, token string) sasl.Client {
+	return &xoauth2Client{username, token}
+}

commit daea912a83d5fa4ad20b3bdef9c4aaa4ce56a788

auth: fallback to login if plain isn't supported
Do not assume "plain" is the default authentication mechanism. If no
mechanism is explicitly set in the connection URI, return an empty
string. The IMAP worker already falls back to "login" if "plain" is not
supported by the server.

Fix the "login" fallback in the IMAP worker to issue an IMAP LOGIN [1]
command instead of a SASL AUTHENTICATE [2] command. This is necessary
because the server may not advertise LOGIN as a separate AUTH
capability.
If, however, the server explicitly advertises the LOGINDISABLED
capability do not fall back to LOGIN, because [1] states:

  A client implementation MUST NOT send a LOGIN command if the
  LOGINDISABLED capability is advertised.

[1]: https://www.rfc-editor.org/rfc/rfc3501#section-6.2.3
[2]: https://www.rfc-editor.org/rfc/rfc3501#section-6.2.2

Fixes: 018e6f81f4d5 ("lib: consolidate SASL authentication code")
Signed-off-by: Folker Schwesinger <dev@folker-schwesinger.de>
Acked-by: Robin Jarry <robin@jarry.cc>

lib/auth/sasl.go
@@ -12,7 +12,7 @@ import (
 
 func ParseScheme(uri *url.URL) (protocol string, mech string, err error) {
 	protocol = ""
-	mech = "plain"
+	mech = ""
 	if uri.Scheme != "" {
 		parts := strings.Split(uri.Scheme, "+")
 		if len(parts) == 0 {

lib/send/smtp.go
@@ -106,6 +106,14 @@ func newSmtpSender(
 		return nil, errors.Wrap(err, "Connection failed")
 	}
 
+	if mech == "" && uri.User != nil {
+		if conn.SupportsAuth("PLAIN") {
+			mech = "plain"
+		} else {
+			mech = "login"
+		}
+	}
+
 	saslclient, err := auth.NewSaslClient(mech, uri, account)
 	if err != nil {
 		conn.Close()

worker/imap/connect.go
@@ -91,24 +91,41 @@ func (w *IMAPWorker) connect() (*client.Client, error) {
 			return nil, err
 		} else if plain {
 			mech = "plain"
-		} else {
+		} else if nologin, err := c.SupportAuth("LOGINDISABLED"); err != nil {
+			return nil, err
+		} else if !nologin {
 			mech = "login"
+		} else {
+			return nil, fmt.Errorf("No auth method supported")
 		}
 	}
 
-	saslClient, err := auth.NewSaslClient(mech, w.config.url, w.config.name)
-	if err != nil {
-		return nil, err
-	}
-	if saslClient != nil {
-		if ok, err := c.SupportAuth(strings.ToUpper(mech)); err != nil {
-			return nil, err
-		} else if !ok {
-			return nil, fmt.Errorf("%s auth not supported", mech)
+	switch mech {
+	case "login":
+		if w.config.url.User != nil {
+			username := w.config.url.User.Username()
+			password, _ := w.config.url.User.Password()
+
+			if err := c.Login(username, password); err != nil {
+				return nil, err
+			}
 		}
-		err = c.Authenticate(saslClient)
+	default:
+		saslClient, err := auth.NewSaslClient(mech, w.config.url, w.config.name)
 		if err != nil {
 			return nil, err
+		}
+		if saslClient != nil {
+			if ok, err := c.SupportAuth(strings.ToUpper(mech)); err != nil {
+				return nil, err
+			} else if !ok {
+				return nil, fmt.Errorf("%s auth not supported", mech)
+			}
+
+			err = c.Authenticate(saslClient)
+			if err != nil {
+				return nil, err
+			}
 		}
 	}
 