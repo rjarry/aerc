rjarry/aerc

commit e9852a25582396c1ae9dc0d88b7898cf78e19d40

imap: include directory and validity in message UIDs
Change the UID format from a simple numeric string to include the
directory name and UIDVALIDITY value in the format "dir:validity:uid".
This makes UIDs globally unique across the entire account rather than
just within a single mailbox.

The new format allows the UI and offline cache to unambiguously identify
messages even when switching between directories. It also provides
automatic cache invalidation when UIDVALIDITY changes, as the UID
prefix will differ.

Move the UID conversion functions from the models package to IMAPWorker
methods since they now depend on the selected mailbox state.

Signed-off-by: Robin Jarry <robin@jarry.cc>
Reviewed-by: Simon Martin <simon@nasilyan.com>

worker/imap/imap.go
@@ -1,6 +1,8 @@
 package imap
 
 import (
+	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/emersion/go-imap"
@@ -14,10 +16,37 @@ func init() {
 	imap.CharsetReader = charset.Reader
 }
 
-func toSeqSet(uids []models.UID) *imap.SeqSet {
+func (w *IMAPWorker) UidToUint32(uid models.UID) uint32 {
+	_, s, _ := strings.Cut(string(uid), ":")
+	_, s, _ = strings.Cut(s, ":")
+	u, _ := strconv.ParseUint(s, 10, 32)
+	return uint32(u)
+}
+
+func (w *IMAPWorker) Uint32ToUid(u uint32) models.UID {
+	return models.UID(fmt.Sprintf("%s:%d:%d", w.selected.Name, w.selected.UidValidity, u))
+}
+
+func (w *IMAPWorker) UidToUint32List(uids []models.UID) []uint32 {
+	ulist := make([]uint32, 0, len(uids))
+	for _, uid := range uids {
+		ulist = append(ulist, w.UidToUint32(uid))
+	}
+	return ulist
+}
+
+func (w *IMAPWorker) Uint32ToUidList(ulist []uint32) []models.UID {
+	uids := make([]models.UID, 0, len(ulist))
+	for _, u := range ulist {
+		uids = append(uids, w.Uint32ToUid(u))
+	}
+	return uids
+}
+
+func (w *IMAPWorker) UidListToSeqSet(uids []models.UID) *imap.SeqSet {
 	set := new(imap.SeqSet)
 	for _, uid := range uids {
-		set.AddNum(models.UidToUint32(uid))
+		set.AddNum(w.UidToUint32(uid))
 	}
 	return set
 }

models/models.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io"
 	"slices"
-	"strconv"
 	"strings"
 	"time"
 
@@ -105,31 +104,6 @@ func (c *Capabilities) Has(s string) bool {
 }
 
 type UID string
-
-func UidToUint32(uid UID) uint32 {
-	u, _ := strconv.ParseUint(string(uid), 10, 32)
-	return uint32(u)
-}
-
-func Uint32ToUid(u uint32) UID {
-	return UID(fmt.Sprintf("%012d", u))
-}
-
-func UidToUint32List(uids []UID) []uint32 {
-	ulist := make([]uint32, 0, len(uids))
-	for _, uid := range uids {
-		ulist = append(ulist, UidToUint32(uid))
-	}
-	return ulist
-}
-
-func Uint32ToUidList(ulist []uint32) []UID {
-	uids := make([]UID, 0, len(ulist))
-	for _, u := range ulist {
-		uids = append(uids, Uint32ToUid(u))
-	}
-	return uids
-}
 
 // A MessageInfo holds information about the structure of a message
 type MessageInfo struct {

worker/imap/fetch.go
@@ -113,7 +113,7 @@ func (imapw *IMAPWorker) handleFetchMessageHeaders(
 				RFC822Headers: header,
 				Refs:          parse.MsgIDList(header, "references"),
 				Size:          _msg.Size,
-				Uid:           models.Uint32ToUid(_msg.Uid),
+				Uid:           imapw.Uint32ToUid(_msg.Uid),
 			}
 
 			if imapw.caps.Has("X-GM-EXT-1") {
@@ -199,14 +199,14 @@ func (imapw *IMAPWorker) handleFetchMessageBodyPart(
 				Message: types.RespondTo(msg),
 				Part: &models.MessageBodyPart{
 					Reader: part.Body,
-					Uid:    models.Uint32ToUid(_msg.Uid),
+					Uid:    imapw.Uint32ToUid(_msg.Uid),
 				},
 			}, nil)
 			// Update flags (to mark message as read)
 			systemFlags, keywordFlags := translateImapFlags(_msg.Flags)
 			info := &models.MessageInfo{
 				Flags: systemFlags,
-				Uid:   models.Uint32ToUid(_msg.Uid),
+				Uid:   imapw.Uint32ToUid(_msg.Uid),
 			}
 			if imapw.caps.Has("X-GM-EXT-1") {
 				imapw.attachGMLabels(_msg, info)
@@ -248,14 +248,14 @@ func (imapw *IMAPWorker) handleFetchFullMessages(
 				Message: types.RespondTo(msg),
 				Content: &models.FullMessage{
 					Reader: bufio.NewReader(r),
-					Uid:    models.Uint32ToUid(_msg.Uid),
+					Uid:    imapw.Uint32ToUid(_msg.Uid),
 				},
 			}, nil)
 			// Update flags (to mark message as read)
 			systemFlags, keywordFlags := translateImapFlags(_msg.Flags)
 			info := &models.MessageInfo{
 				Flags: systemFlags,
-				Uid:   models.Uint32ToUid(_msg.Uid),
+				Uid:   imapw.Uint32ToUid(_msg.Uid),
 			}
 			if imapw.caps.Has("X-GM-EXT-1") {
 				imapw.attachGMLabels(_msg, info)
@@ -280,7 +280,7 @@ func (imapw *IMAPWorker) handleFetchMessageFlags(msg *types.FetchMessageFlags) e
 			systemFlags, keywordFlags := translateImapFlags(_msg.Flags)
 			info := &models.MessageInfo{
 				Flags: systemFlags,
-				Uid:   models.Uint32ToUid(_msg.Uid),
+				Uid:   imapw.Uint32ToUid(_msg.Uid),
 			}
 
 			if imapw.caps.Has("X-GM-EXT-1") {
@@ -321,14 +321,14 @@ func (imapw *IMAPWorker) handleFetchMessages(
 				if _msg == nil {
 					goto out
 				}
-				delete(missingUids, models.Uint32ToUid(_msg.Uid))
+				delete(missingUids, imapw.Uint32ToUid(_msg.Uid))
 				err := procFunc(_msg)
 				if err != nil {
 					imapw.worker.Errorf("failed to process message <%d>: %v", _msg.Uid, err)
 					imapw.worker.PostMessage(&types.MessageInfo{
 						Message: types.RespondTo(msg),
 						Info: &models.MessageInfo{
-							Uid:   models.Uint32ToUid(_msg.Uid),
+							Uid:   imapw.Uint32ToUid(_msg.Uid),
 							Error: err,
 						},
 					}, nil)
@@ -343,7 +343,7 @@ func (imapw *IMAPWorker) handleFetchMessages(
 		items = append(items, "X-GM-LABELS")
 	}
 
-	set := toSeqSet(uids)
+	set := imapw.UidListToSeqSet(uids)
 	if err := imapw.client.UidFetch(set, items, messages); err != nil {
 		return err
 	}

worker/imap/flags.go
@@ -56,11 +56,11 @@ func (imapw *IMAPWorker) handleDeleteMessages(msg *types.DeleteMessages) error {
 	defer drain.Close()
 
 	// Build provider-dependent EXPUNGE handler.
-	imapw.BuildExpungeHandler(models.UidToUint32List(msg.Uids), true)
+	imapw.BuildExpungeHandler(imapw.UidToUint32List(msg.Uids), true)
 
 	item := imap.FormatFlagsOp(imap.AddFlags, true)
 	flags := []any{imap.DeletedFlag}
-	uids := toSeqSet(msg.Uids)
+	uids := imapw.UidListToSeqSet(msg.Uids)
 	if err := imapw.client.UidStore(uids, item, flags, nil); err != nil {
 		return err
 	}
@@ -84,7 +84,7 @@ func (imapw *IMAPWorker) handleAnsweredMessages(msg *types.AnsweredMessages) err
 				Info: &models.MessageInfo{
 					Flags:  systemFlags,
 					Labels: keywordFlags,
-					Uid:    models.Uint32ToUid(_msg.Uid),
+					Uid:    imapw.Uint32ToUid(_msg.Uid),
 				},
 			}, nil)
 			return nil
@@ -105,7 +105,7 @@ func (imapw *IMAPWorker) handleFlagMessages(msg *types.FlagMessages) error {
 				Info: &models.MessageInfo{
 					Flags:  systemFlags,
 					Labels: keywordFlags,
-					Uid:    models.Uint32ToUid(_msg.Uid),
+					Uid:    imapw.Uint32ToUid(_msg.Uid),
 				},
 				ReplaceFlags: true,
 			}, nil)
@@ -164,7 +164,7 @@ func (imapw *IMAPWorker) handleModifyLabels(msg *types.ModifyLabels) error {
 			// is obtained by moving messages to/from label virtual
 			// folders
 			// (https://proton.me/support/labels-in-bridge#how-to-apply-labels-in-bridge)
-			uids := toSeqSet(msg.Uids)
+			uids := imapw.UidListToSeqSet(msg.Uids)
 			var impactedVFolders []string
 			for _, l := range labels {
 				var destination string
@@ -206,7 +206,7 @@ func (imapw *IMAPWorker) handleModifyLabels(msg *types.ModifyLabels) error {
 						Info: &models.MessageInfo{
 							Flags:  systemFlags,
 							Labels: keywordFlags,
-							Uid:    models.Uint32ToUid(_msg.Uid),
+							Uid:    imapw.Uint32ToUid(_msg.Uid),
 						},
 					}, nil)
 					return nil
@@ -245,7 +245,7 @@ func (imapw *IMAPWorker) handleStoreOps(
 		done <- reterr
 	}()
 
-	set := toSeqSet(uids)
+	set := imapw.UidListToSeqSet(uids)
 	if err := imapw.client.UidStore(set, item, flag, messages); err != nil {
 		return err
 	}

worker/imap/list.go
@@ -108,7 +108,7 @@ func (imapw *IMAPWorker) handleSearchDirectory(msg *types.SearchDirectory) error
 
 	imapw.worker.PostMessage(&types.SearchResults{
 		Message: types.RespondTo(msg),
-		Uids:    models.Uint32ToUidList(uids),
+		Uids:    imapw.Uint32ToUidList(uids),
 	}, nil)
 
 	return nil

worker/imap/movecopy.go
@@ -3,12 +3,11 @@ package imap
 import (
 	"io"
 
-	"git.sr.ht/~rjarry/aerc/models"
 	"git.sr.ht/~rjarry/aerc/worker/types"
 )
 
 func (imapw *IMAPWorker) handleCopyMessages(msg *types.CopyMessages) error {
-	uids := toSeqSet(msg.Uids)
+	uids := imapw.UidListToSeqSet(msg.Uids)
 	if err := imapw.client.UidCopy(uids, msg.Destination); err != nil {
 		return err
 	}
@@ -45,9 +44,9 @@ func (imapw *IMAPWorker) handleMoveMessages(msg *types.MoveMessages) error {
 	defer drain.Close()
 
 	// Build provider-dependent EXPUNGE handler.
-	imapw.BuildExpungeHandler(models.UidToUint32List(msg.Uids), false)
+	imapw.BuildExpungeHandler(imapw.UidToUint32List(msg.Uids), false)
 
-	uids := toSeqSet(msg.Uids)
+	uids := imapw.UidListToSeqSet(msg.Uids)
 	if err := imapw.client.UidMove(uids, msg.Destination); err != nil {
 		return err
 	}

worker/imap/open.go
@@ -5,7 +5,6 @@ import (
 
 	sortthread "github.com/emersion/go-imap-sortthread"
 
-	"git.sr.ht/~rjarry/aerc/models"
 	"git.sr.ht/~rjarry/aerc/worker/types"
 )
 
@@ -76,7 +75,7 @@ func (imapw *IMAPWorker) handleFetchDirectoryContents(
 
 	imapw.worker.PostMessage(&types.DirectoryContents{
 		Message: types.RespondTo(msg),
-		Uids:    models.Uint32ToUidList(uids),
+		Uids:    imapw.Uint32ToUidList(uids),
 	}, nil)
 	return nil
 }
@@ -120,7 +119,7 @@ func (imapw *IMAPWorker) handleDirectoryThreaded(
 	if err != nil {
 		return err
 	}
-	aercThreads, count := convertThreads(threads, nil)
+	aercThreads, count := imapw.convertThreads(threads, nil)
 	sort.Sort(types.ByUID(aercThreads))
 	imapw.worker.Tracef("Found %d threaded messages", count)
 	if msg.Filter == nil {
@@ -128,7 +127,7 @@ func (imapw *IMAPWorker) handleDirectoryThreaded(
 		var uids []uint32
 		for i := len(aercThreads) - 1; i >= 0; i-- {
 			aercThreads[i].Walk(func(t *types.Thread, level int, currentErr error) error { //nolint:errcheck // error indicates skipped threads
-				uids = append(uids, models.UidToUint32(t.Uid))
+				uids = append(uids, imapw.UidToUint32(t.Uid))
 				return nil
 			})
 		}
@@ -144,7 +143,7 @@ func (imapw *IMAPWorker) handleDirectoryThreaded(
 	return nil
 }
 
-func convertThreads(threads []*sortthread.Thread, parent *types.Thread) ([]*types.Thread, int) {
+func (imapw *IMAPWorker) convertThreads(threads []*sortthread.Thread, parent *types.Thread) ([]*types.Thread, int) {
 	if threads == nil {
 		return nil, 0
 	}
@@ -154,11 +153,11 @@ func convertThreads(threads []*sortthread.Thread, parent *types.Thread) ([]*type
 	for i := range threads {
 		t := threads[i]
 		conv[i] = &types.Thread{
-			Uid: models.Uint32ToUid(t.Id),
+			Uid: imapw.Uint32ToUid(t.Id),
 		}
 
 		// Set the first child node
-		children, childCount := convertThreads(t.Children, conv[i])
+		children, childCount := imapw.convertThreads(t.Children, conv[i])
 		if len(children) > 0 {
 			conv[i].FirstChild = children[0]
 		}

worker/imap/worker.go
@@ -306,7 +306,7 @@ func (w *IMAPWorker) handleImapUpdate(update client.Update) {
 				Flags:         systemFlags,
 				Labels:        keywordFlags,
 				InternalDate:  msg.InternalDate,
-				Uid:           models.Uint32ToUid(msg.Uid),
+				Uid:           w.Uint32ToUid(msg.Uid),
 			},
 			Unsolicited: true,
 		}, nil)
@@ -335,7 +335,7 @@ func (w *IMAPWorker) handleImapUpdate(update client.Update) {
 		}
 		if uid != 0 {
 			w.worker.PostMessage(&types.MessagesDeleted{
-				Uids: []models.UID{models.Uint32ToUid(uid)},
+				Uids: []models.UID{w.Uint32ToUid(uid)},
 			}, nil)
 		}
 	}

commit 9b605c08cc2255e4ae0acd6268cfe5643a9bed71

imap: fix message sorting
Re-add leading zeroes to message UIDs to fix sorting for the IMAP
backend.

Fixes: e9852a255823 ("imap: include directory and validity in message UIDs"
Reported-by: Jens Grassel <jan0sch@mailbox.org>
Reported-by: Terrance <srht@terrance.allofti.me>
Signed-off-by: Folker Schwesinger <dev@folker-schwesinger.de>
Tested-by: Jens Grassel <jan0sch@mailbox.org>
Tested-by: Terrance <srht@terrance.allofti.me>

worker/imap/imap.go
@@ -24,7 +24,7 @@ func (w *IMAPWorker) UidToUint32(uid models.UID) uint32 {
 }
 
 func (w *IMAPWorker) Uint32ToUid(u uint32) models.UID {
-	return models.UID(fmt.Sprintf("%s:%d:%d", w.selected.Name, w.selected.UidValidity, u))
+	return models.UID(fmt.Sprintf("%s:%d:%012d", w.selected.Name, w.selected.UidValidity, u))
 }
 
 func (w *IMAPWorker) UidToUint32List(uids []models.UID) []uint32 {