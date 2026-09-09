rjarry/aerc

commit d9ff08b05ffeefc942e2ee4bfaa2811fdbad29dc

search: add option to invert filters/searches
Add a new `-v` option to :filter and :search, inverting the search
expression and excluding messages which match.  When multiple filters
are applied, each one can be inverted or not.

Search criteria are combined in two halves, Match and Exclude, with
each worker translating those as needed:

- IMAP searches use `NOT` expressions for excluded terms.

- JMAP isn't currently implemented and searches including any exclude
  components will return an error.  From reading the search spec, it
  looks like only keywords can be flipped, rather than headers etc.

- Gmail extension searches use a `-` prefix on each excluded term.

- Notmuch searches use a `not` prefix on each excluded term.

- Raw message searches just apply excludes followed by matches.

Signed-off-by: Terrance <git@terrance.allofti.me>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/lib/search.go
@@ -32,8 +32,20 @@ func Search(messages []rfc822.RawMessage, criteria *types.SearchCriteria) ([]mod
 
 // searchMessage executes the search criteria for the given RawMessage,
 // returns true if search succeeded
-func SearchMessage(message rfc822.RawMessage, criteria *types.SearchCriteria,
-	parts MsgParts,
+func SearchMessage(
+	message rfc822.RawMessage, criteria *types.SearchCriteria, parts MsgParts,
+) (bool, error) {
+	if criteria.Exclude != nil {
+		excluded, err := searchMessage(message, criteria.Exclude, parts)
+		if err != nil || excluded {
+			return false, err
+		}
+	}
+	return searchMessage(message, criteria.Match, parts)
+}
+
+func searchMessage(
+	message rfc822.RawMessage, criteria *types.SearchCriteriaPart, parts MsgParts,
 ) (bool, error) {
 	if criteria == nil {
 		return true, nil
@@ -174,6 +186,10 @@ const (
 // Returns a bitmask of the parts of the message required to be loaded for the
 // given criteria
 func GetRequiredParts(criteria *types.SearchCriteria) MsgParts {
+	return getRequiredParts(criteria.Match) | getRequiredParts(criteria.Exclude)
+}
+
+func getRequiredParts(criteria *types.SearchCriteriaPart) MsgParts {
 	required := NONE
 	if criteria == nil {
 		return required

commands/account/search.go
@@ -23,6 +23,7 @@ type SearchFilter struct {
 	Unread       bool                 `opt:"-u" action:"ParseUnread" desc:"Search for unread messages."`
 	Body         bool                 `opt:"-b" desc:"Search in the body of the messages."`
 	All          bool                 `opt:"-a" desc:"Search in the entire text of the messages."`
+	Invert       bool                 `opt:"-v" desc:"Exclude messages which match this search."`
 	UseExtension bool                 `opt:"-e" desc:"Use custom search backend extension."`
 	Headers      textproto.MIMEHeader `opt:"-H" action:"ParseHeader" metavar:"<header>:<value>" desc:"Search for messages with the specified header."`
 	WithFlags    models.Flags         `opt:"-x" action:"ParseFlag" complete:"CompleteFlag" desc:"Search messages with specified flag."`
@@ -165,7 +166,7 @@ func (s SearchFilter) Execute(args []string) error {
 		return errors.New("Cannot perform action. Messages still loading")
 	}
 
-	criteria := types.SearchCriteria{
+	criteriaPart := types.SearchCriteriaPart{
 		WithFlags:    s.WithFlags,
 		WithoutFlags: s.WithoutFlags,
 		From:         s.From,
@@ -177,7 +178,7 @@ func (s SearchFilter) Execute(args []string) error {
 		SearchBody:   s.Body,
 		SearchAll:    s.All,
 		Terms:        []string{s.Terms},
-		UseExtension: s.UseExtension,
+		Invert:       s.Invert,
 	}
 
 	if args[0] == "filter" {
@@ -185,7 +186,7 @@ func (s SearchFilter) Execute(args []string) error {
 			return Clear{}.Execute([]string{"clear"})
 		}
 		acct.SetStatus(state.FilterActivity("Filtering..."), state.Search(""))
-		store.SetFilter(&criteria)
+		store.SetFilter(&criteriaPart)
 		cb := func(msg types.WorkerMessage) {
 			if _, ok := msg.(*types.Done); ok {
 				acct.SetStatus(state.FilterResult(strings.Join(args, " ")))
@@ -201,6 +202,12 @@ func (s SearchFilter) Execute(args []string) error {
 			store.ApplySearch(uids)
 			// TODO: Remove when stores have multiple OnUpdate handlers
 			ui.Invalidate()
+		}
+		criteria := types.SearchCriteria{}
+		if criteriaPart.Invert {
+			criteria.Exclude = &criteriaPart
+		} else {
+			criteria.Match = &criteriaPart
 		}
 		store.Search(&criteria, cb)
 	}

worker/imap/search.go
@@ -11,6 +11,17 @@ import (
 )
 
 func translateSearch(c *types.SearchCriteria) *imap.SearchCriteria {
+	if c == nil {
+		return imap.NewSearchCriteria()
+	}
+	criteria := translateSearchPart(c.Match)
+	if c.Exclude != nil {
+		criteria.Not = append(criteria.Not, translateSearchPart(c.Exclude))
+	}
+	return criteria
+}
+
+func translateSearchPart(c *types.SearchCriteriaPart) *imap.SearchCriteria {
 	criteria := imap.NewSearchCriteria()
 	if c == nil {
 		return criteria

worker/jmap/search.go
@@ -1,6 +1,7 @@
 package jmap
 
 import (
+	"errors"
 	"strings"
 
 	"git.sr.ht/~rjarry/aerc/worker/types"
@@ -11,6 +12,15 @@ import (
 
 func (w *JMAPWorker) translateSearch(
 	mbox jmap.ID, criteria *types.SearchCriteria,
+) (email.Filter, error) {
+	if criteria.Exclude != nil {
+		return nil, errors.New("Exclude filters not supported in JMAP")
+	}
+	return w.translateSearchPart(mbox, criteria.Match), nil
+}
+
+func (w *JMAPWorker) translateSearchPart(
+	mbox jmap.ID, criteria *types.SearchCriteriaPart,
 ) email.Filter {
 	cond := new(email.FilterCondition)
 

worker/notmuch/search.go
@@ -36,11 +36,27 @@ func (q *queryBuilder) or(s string) {
 	q.s += "(" + s + ")"
 }
 
-func translate(crit *types.SearchCriteria) string {
-	if crit == nil {
+func invert(s string, invert bool) string {
+	if len(s) == 0 || !invert {
+		return s
+	}
+	return "not (" + s + ")"
+}
+
+func translate(criteria *types.SearchCriteria) string {
+	if criteria == nil {
 		return ""
 	}
 	var base queryBuilder
+	translatePart(base, criteria.Match)
+	translatePart(base, criteria.Exclude)
+	return base.s
+}
+
+func translatePart(base queryBuilder, crit *types.SearchCriteriaPart) {
+	if crit == nil {
+		return
+	}
 
 	// recipients
 	var from queryBuilder
@@ -48,7 +64,7 @@ func translate(crit *types.SearchCriteria) string {
 		from.or("from:" + opt.QuoteArg(f))
 	}
 	if from.s != "" {
-		base.and(from.s)
+		base.and(invert(from.s, crit.Invert))
 	}
 
 	var to queryBuilder
@@ -56,7 +72,7 @@ func translate(crit *types.SearchCriteria) string {
 		to.or("to:" + opt.QuoteArg(t))
 	}
 	if to.s != "" {
-		base.and(to.s)
+		base.and(invert(to.s, crit.Invert))
 	}
 
 	var cc queryBuilder
@@ -64,42 +80,40 @@ func translate(crit *types.SearchCriteria) string {
 		cc.or("cc:" + opt.QuoteArg(c))
 	}
 	if cc.s != "" {
-		base.and(cc.s)
+		base.and(invert(cc.s, crit.Invert))
 	}
 
 	// flags
 	for f := range flagToTag {
 		if crit.WithFlags.Has(f) {
-			base.and(getParsedFlag(f, false))
+			base.and(invert(getParsedFlag(f, false), crit.Invert))
 		}
 		if crit.WithoutFlags.Has(f) {
-			base.and(getParsedFlag(f, true))
+			base.and(invert(getParsedFlag(f, true), crit.Invert))
 		}
 	}
 
 	// dates
 	switch {
 	case !crit.StartDate.IsZero() && !crit.EndDate.IsZero():
-		base.and(fmt.Sprintf("date:@%d..@%d",
-			crit.StartDate.Unix(), crit.EndDate.Unix()))
+		base.and(invert(fmt.Sprintf("date:@%d..@%d",
+			crit.StartDate.Unix(), crit.EndDate.Unix()), crit.Invert))
 	case !crit.StartDate.IsZero():
-		base.and(fmt.Sprintf("date:@%d..", crit.StartDate.Unix()))
+		base.and(invert(fmt.Sprintf("date:@%d..", crit.StartDate.Unix()), crit.Invert))
 	case !crit.EndDate.IsZero():
-		base.and(fmt.Sprintf("date:..@%d", crit.EndDate.Unix()))
+		base.and(invert(fmt.Sprintf("date:..@%d", crit.EndDate.Unix()), crit.Invert))
 	}
 
 	// other terms
 	if len(crit.Terms) > 0 {
 		if crit.SearchBody {
-			base.and("body:" + opt.QuoteArg(strings.Join(crit.Terms, " ")))
+			base.and(invert("body:"+opt.QuoteArg(strings.Join(crit.Terms, " ")), crit.Invert))
 		} else {
 			for _, term := range crit.Terms {
-				base.and(term)
+				base.and(invert(term, crit.Invert))
 			}
 		}
 	}
-
-	return base.s
 }
 
 func getParsedFlag(flag models.Flags, inverse bool) string {

worker/types/search.go
@@ -9,6 +9,12 @@ import (
 )
 
 type SearchCriteria struct {
+	Match        *SearchCriteriaPart
+	Exclude      *SearchCriteriaPart
+	UseExtension bool
+}
+
+type SearchCriteriaPart struct {
 	WithFlags    models.Flags
 	WithoutFlags models.Flags
 	From         []string
@@ -20,10 +26,18 @@ type SearchCriteria struct {
 	SearchBody   bool
 	SearchAll    bool
 	Terms        []string
-	UseExtension bool
+	Invert       bool
 }
 
 func (c *SearchCriteria) PrepareHeader() {
+	if c == nil {
+		return
+	}
+	c.Match.PrepareHeader()
+	c.Exclude.PrepareHeader()
+}
+
+func (c *SearchCriteriaPart) PrepareHeader() {
 	if c == nil {
 		return
 	}
@@ -41,7 +55,19 @@ func (c *SearchCriteria) PrepareHeader() {
 	}
 }
 
-func (c *SearchCriteria) Combine(other *SearchCriteria) *SearchCriteria {
+func (c *SearchCriteria) Combine(other *SearchCriteriaPart) *SearchCriteria {
+	if c == nil {
+		c = &SearchCriteria{}
+	}
+	if other.Invert {
+		c.Exclude = c.Exclude.Combine(other)
+	} else {
+		c.Match = c.Match.Combine(other)
+	}
+	return c
+}
+
+func (c *SearchCriteriaPart) Combine(other *SearchCriteriaPart) *SearchCriteriaPart {
 	if c == nil {
 		return other
 	}
@@ -65,7 +91,7 @@ func (c *SearchCriteria) Combine(other *SearchCriteria) *SearchCriteria {
 	cc := make([]string, len(c.Cc)+len(other.Cc))
 	copy(cc[:len(c.Cc)], c.Cc)
 	copy(cc[len(c.Cc):], other.Cc)
-	return &SearchCriteria{
+	return &SearchCriteriaPart{
 		WithFlags:    c.WithFlags | other.WithFlags,
 		WithoutFlags: c.WithoutFlags | other.WithoutFlags,
 		From:         from,
@@ -77,5 +103,6 @@ func (c *SearchCriteria) Combine(other *SearchCriteria) *SearchCriteria {
 		SearchBody:   c.SearchBody || other.SearchBody,
 		SearchAll:    c.SearchAll || other.SearchAll,
 		Terms:        append(c.Terms, other.Terms...),
+		Invert:       c.Invert,
 	}
 }

doc/aerc-search.1.scd
@@ -8,8 +8,8 @@ aerc-search - search and filter patterns and options for *aerc*(1)
 
 This syntax is common to all backends.
 
-*:filter* [*-rubae*] [*-x* _<flag>_] [*-X* _<flag>_] [*-H* _<header>:[<value>]_] [*-f* _<from>_] [*-t* _<to>_] [*-c* _<cc>_] [*-d* _<start[..end]>_] [_<terms>_...]++
-*:search* [*-rubae*] [*-x* _<flag>_] [*-X* _<flag>_] [*-H* _<header>:[<value>]_] [*-f* _<from>_] [*-t* _<to>_] [*-c* _<cc>_] [*-d* _<start[..end]>_] [_<terms>_...]
+*:filter* [*-rubave*] [*-x* _<flag>_] [*-X* _<flag>_] [*-H* _<header>:[<value>]_] [*-f* _<from>_] [*-t* _<to>_] [*-c* _<cc>_] [*-d* _<start[..end]>_] [_<terms>_...]++
+*:search* [*-rubave*] [*-x* _<flag>_] [*-X* _<flag>_] [*-H* _<header>:[<value>]_] [*-f* _<from>_] [*-t* _<to>_] [*-c* _<cc>_] [*-d* _<start[..end]>_] [_<terms>_...]
 	Searches the current folder for messages matching the given set of
 	conditions.
 
@@ -48,6 +48,8 @@ This syntax is common to all backends.
 	*-b*: Search in the body of the messages
 
 	*-a*: Search in the entire text of the messages
+
+	*-v*: Exclude messages which match this search
 
 	*-e*: Instruct the backend to use a custom search extension
 		(such as X-GM-EXT-1 if available). Search terms are expected

lib/msgstore.go
@@ -946,8 +946,9 @@ func (store *MessageStore) IsResult(uid models.UID) bool {
 	return slices.Contains(store.results, uid)
 }
 
-func (store *MessageStore) SetFilter(terms *types.SearchCriteria) {
+func (store *MessageStore) SetFilter(terms *types.SearchCriteriaPart) *types.SearchCriteria {
 	store.filter = store.filter.Combine(terms)
+	return store.filter
 }
 
 func (store *MessageStore) ApplyClear() {

commit 73f201257314631723fe0b9e223960abe7a7ba1f

search: fix mbox criteria nil checks
The mbox search helpers SearchMessage and GetRequiredParts receive a nil
SearchCriteria when no filter is active, but don't check for this before
trying to access the inner SearchCriteriaPart fields.

Add further nil checks against the top-level criteria.

Fixes: d9ff08b05ffe ("search: add option to invert filters/searches")
Signed-off-by: Terrance <git@terrance.allofti.me>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/lib/search.go
@@ -35,6 +35,9 @@ func Search(messages []rfc822.RawMessage, criteria *types.SearchCriteria) ([]mod
 func SearchMessage(
 	message rfc822.RawMessage, criteria *types.SearchCriteria, parts MsgParts,
 ) (bool, error) {
+	if criteria == nil {
+		return true, nil
+	}
 	if criteria.Exclude != nil {
 		excluded, err := searchMessage(message, criteria.Exclude, parts)
 		if err != nil || excluded {
@@ -186,6 +189,9 @@ const (
 // Returns a bitmask of the parts of the message required to be loaded for the
 // given criteria
 func GetRequiredParts(criteria *types.SearchCriteria) MsgParts {
+	if criteria == nil {
+		return NONE
+	}
 	return getRequiredParts(criteria.Match) | getRequiredParts(criteria.Exclude)
 }
 