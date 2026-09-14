rjarry/aerc

commit 90c0cd151983a505c3fcadbe5df9710186773b24

maildir: fix error when using archive month
`:archive month` fails when used with maildir protocol if parent dirs
missing:

 ERROR [...] worker.go:629: could not create directory Archive/2026/01: mkdir [...]/Archive/2026/01: no such file or directory
 ERROR [...] account.go:523: unexpected error: mkdir [...]/Archive/2026/01: no such file or directory

This could have been solved in go-maildir directly but the maintainer
doesn't seem sure about it.

Link: https://github.com/emersion/go-maildir/issues/27
Signed-off-by: Francesc Elies <elies@posteo.net>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/maildir/worker.go
@@ -650,6 +650,12 @@ func (w *Worker) threads(ctx context.Context, uids []models.UID,
 }
 
 func (w *Worker) handleCreateDirectory(msg *types.CreateDirectory) error {
+	if err := os.MkdirAll(msg.Directory, 0o700); err != nil && !os.IsExist(err) {
+		w.worker.Errorf("could not create directory %s: %v",
+			msg.Directory, err)
+		return err
+	}
+
 	dir := w.c.Store.Dir(msg.Directory)
 	if err := dir.Init(); err != nil {
 		w.worker.Errorf("could not create directory %s: %v",

commit 55ae32a3f28395a2edb4cc5ce7a77fb2c066e2e0

maildir: fix folders created in the working directory
This change joins the folder name with the maildir root before creating
the parents.

Fixes: 90c0cd151983 ("maildir: fix error when using archive month")
Fixes: https://todo.sr.ht/~rjarry/aerc/354
Link: https://github.com/emersion/go-maildir/issues/27
Changelog-fixed: Folders are no longer created in aerc's current
 working directory when using `copy-to` or `:archive`.
Signed-off-by: Markus Unkel <markus@markusunkel.com>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/maildir/worker.go
@@ -649,13 +649,14 @@ func (w *Worker) threads(ctx context.Context, uids []models.UID,
 }
 
 func (w *Worker) handleCreateDirectory(msg *types.CreateDirectory) error {
-	if err := os.MkdirAll(msg.Directory, 0o700); err != nil && !os.IsExist(err) {
+	dir := w.c.Store.Dir(msg.Directory)
+
+	if err := os.MkdirAll(string(dir), 0o700); err != nil && !os.IsExist(err) {
 		w.worker.Errorf("could not create directory %s: %v",
 			msg.Directory, err)
 		return err
 	}
 
-	dir := w.c.Store.Dir(msg.Directory)
 	if err := dir.Init(); err != nil {
 		w.worker.Errorf("could not create directory %s: %v",
 			msg.Directory, err)