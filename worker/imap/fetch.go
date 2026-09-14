rjarry/aerc

commit 6dc701a2a042fdd778c855eecccca6ab9bddea42

imap: centralize error and response handling in Run loop
Refactor all IMAP message handlers to return errors instead of posting
Done or Error messages directly. The main Run loop now handles all
response posting in a single location, reducing code duplication and
ensuring consistent error handling across all operations.

Introduce two sentinel errors: ErrUnsupported for operations not
implemented by a backend and ErrNoop for operations that completed
without effect and should not trigger a Done message. Use
context.Canceled for cancelled operations.

Move connection timeout management from handleMessage to the
stopIdler/startIdler functions where it is more logically placed. Set
timeout before stopping IDLE when real commands execute, and clear it
when starting IDLE to allow indefinite waiting.

Signed-off-by: Robin Jarry <robin@jarry.cc>
Reviewed-by: Simon Martin <simon@nasilyan.com>

worker/imap/fetch.go
@@ -2,6 +2,7 @@ package imap
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"slices"
 	"time"
@@ -38,12 +39,9 @@ func (imapw *IMAPWorker) attachGMLabels(_msg *imap.Message, info *models.Message
 
 func (imapw *IMAPWorker) handleFetchMessageHeaders(
 	msg *types.FetchMessageHeaders,
-) {
+) error {
 	if msg.Context().Err() != nil {
-		imapw.worker.PostMessage(&types.Cancelled{
-			Message: types.RespondTo(msg),
-		}, nil)
-		return
+		return msg.Context().Err()
 	}
 	toFetch := msg.Uids
 	cacheEnabled := imapw.config.cacheEnabled && imapw.cache != nil
@@ -51,9 +49,7 @@ func (imapw *IMAPWorker) handleFetchMessageHeaders(
 		toFetch = imapw.getCachedHeaders(msg)
 	}
 	if len(toFetch) == 0 {
-		imapw.worker.PostMessage(&types.Done{Message: types.RespondTo(msg)},
-			nil)
-		return
+		return nil
 	}
 	imapw.worker.Tracef("Fetching message headers: %v", toFetch)
 	hdrBodyPart := imap.BodyPartName{
@@ -81,7 +77,7 @@ func (imapw *IMAPWorker) handleFetchMessageHeaders(
 		section.FetchItem(),
 	}
 
-	imapw.handleFetchMessages(msg, toFetch, items,
+	return imapw.handleFetchMessages(msg.Context(), msg, toFetch, items,
 		func(_msg *imap.Message) error {
 			if len(_msg.Body) == 0 {
 				// ignore duplicate messages with only flag updates
@@ -134,7 +130,7 @@ func (imapw *IMAPWorker) handleFetchMessageHeaders(
 
 func (imapw *IMAPWorker) handleFetchMessageBodyPart(
 	msg *types.FetchMessageBodyPart,
-) {
+) error {
 	imapw.worker.Tracef("Fetching message %d part: %v", msg.Uid, msg.Part)
 
 	var partHeaderSection imap.BodySectionName
@@ -163,7 +159,7 @@ func (imapw *IMAPWorker) handleFetchMessageBodyPart(
 		partHeaderSection.FetchItem(),
 		partBodySection.FetchItem(),
 	}
-	imapw.handleFetchMessages(msg, []models.UID{msg.Uid}, items,
+	return imapw.handleFetchMessages(msg.Context(), msg, []models.UID{msg.Uid}, items,
 		func(_msg *imap.Message) error {
 			if len(_msg.Body) == 0 {
 				// ignore duplicate messages with only flag updates
@@ -215,7 +211,7 @@ func (imapw *IMAPWorker) handleFetchMessageBodyPart(
 
 func (imapw *IMAPWorker) handleFetchFullMessages(
 	msg *types.FetchFullMessages,
-) {
+) error {
 	imapw.worker.Tracef("Fetching full messages: %v", msg.Uids)
 	section := &imap.BodySectionName{
 		Peek: true,
@@ -226,7 +222,7 @@ func (imapw *IMAPWorker) handleFetchFullMessages(
 		imap.FetchUid,
 		section.FetchItem(),
 	}
-	imapw.handleFetchMessages(msg, msg.Uids, items,
+	return imapw.handleFetchMessages(msg.Context(), msg, msg.Uids, items,
 		func(_msg *imap.Message) error {
 			if len(_msg.Body) == 0 {
 				// ignore duplicate messages with only flag updates
@@ -262,19 +258,12 @@ func (imapw *IMAPWorker) handleFetchFullMessages(
 		})
 }
 
-func (imapw *IMAPWorker) handleFetchMessageFlags(msg *types.FetchMessageFlags) {
+func (imapw *IMAPWorker) handleFetchMessageFlags(msg *types.FetchMessageFlags) error {
 	items := []imap.FetchItem{
 		imap.FetchFlags,
 		imap.FetchUid,
 	}
-
-	if msg.Context().Err() != nil {
-		imapw.worker.PostMessage(&types.Cancelled{
-			Message: types.RespondTo(msg),
-		}, nil)
-		return
-	}
-	imapw.handleFetchMessages(msg, msg.Uids, items,
+	return imapw.handleFetchMessages(msg.Context(), msg, msg.Uids, items,
 		func(_msg *imap.Message) error {
 			systemFlags, keywordFlags := translateImapFlags(_msg.Flags)
 			info := &models.MessageInfo{
@@ -297,11 +286,11 @@ func (imapw *IMAPWorker) handleFetchMessageFlags(msg *types.FetchMessageFlags) {
 }
 
 func (imapw *IMAPWorker) handleFetchMessages(
-	msg types.WorkerMessage, uids []models.UID, items []imap.FetchItem,
-	procFunc func(*imap.Message) error,
-) {
+	ctx context.Context, msg types.WorkerMessage, uids []models.UID,
+	items []imap.FetchItem, procFunc func(*imap.Message) error,
+) error {
 	messages := make(chan *imap.Message)
-	done := make(chan struct{})
+	done := make(chan error)
 
 	missingUids := make(map[models.UID]bool)
 	for _, uid := range uids {
@@ -311,20 +300,30 @@ func (imapw *IMAPWorker) handleFetchMessages(
 	go func() {
 		defer log.PanicHandler()
 
-		for _msg := range messages {
-			delete(missingUids, models.Uint32ToUid(_msg.Uid))
-			err := procFunc(_msg)
-			if err != nil {
-				imapw.worker.Errorf("failed to process message <%d>: %v", _msg.Uid, err)
-				imapw.worker.PostMessage(&types.MessageInfo{
-					Message: types.RespondTo(msg),
-					Info: &models.MessageInfo{
-						Uid:   models.Uint32ToUid(_msg.Uid),
-						Error: err,
-					},
-				}, nil)
+		for {
+			select {
+			case <-ctx.Done():
+				done <- ctx.Err()
+				goto out
+			case _msg := <-messages:
+				if _msg == nil {
+					goto out
+				}
+				delete(missingUids, models.Uint32ToUid(_msg.Uid))
+				err := procFunc(_msg)
+				if err != nil {
+					imapw.worker.Errorf("failed to process message <%d>: %v", _msg.Uid, err)
+					imapw.worker.PostMessage(&types.MessageInfo{
+						Message: types.RespondTo(msg),
+						Info: &models.MessageInfo{
+							Uid:   models.Uint32ToUid(_msg.Uid),
+							Error: err,
+						},
+					}, nil)
+				}
 			}
 		}
+	out:
 		close(done)
 	}()
 
@@ -334,13 +333,12 @@ func (imapw *IMAPWorker) handleFetchMessages(
 
 	set := toSeqSet(uids)
 	if err := imapw.client.UidFetch(set, items, messages); err != nil {
-		imapw.worker.PostMessage(&types.Error{
-			Message: types.RespondTo(msg),
-			Error:   err,
-		}, nil)
-		return
+		return err
 	}
-	<-done
+	err := <-done
+	if err != nil {
+		return err
+	}
 
 	for uid := range missingUids {
 		imapw.worker.PostMessage(&types.MessageInfo{
@@ -352,6 +350,5 @@ func (imapw *IMAPWorker) handleFetchMessages(
 		}, nil)
 	}
 
-	imapw.worker.PostMessage(
-		&types.Done{Message: types.RespondTo(msg)}, nil)
+	return nil
 }

worker/imap/checkmail.go
@@ -6,7 +6,7 @@ import (
 	"github.com/emersion/go-imap"
 )
 
-func (w *IMAPWorker) handleCheckMailMessage(msg *types.CheckMail) {
+func (w *IMAPWorker) handleCheckMailMessage(msg *types.CheckMail) error {
 	items := []imap.StatusItem{
 		imap.StatusMessages,
 		imap.StatusRecent,
@@ -34,11 +34,7 @@ func (w *IMAPWorker) handleCheckMailMessage(msg *types.CheckMail) {
 			statuses, err = w.client.liststatus.ListStatus("", "*", items, nil)
 		}
 		if err != nil {
-			w.worker.PostMessage(&types.Error{
-				Message: types.RespondTo(msg),
-				Error:   err,
-			}, nil)
-			return
+			return err
 		}
 	default:
 		for _, dir := range msg.Directories {
@@ -49,11 +45,7 @@ func (w *IMAPWorker) handleCheckMailMessage(msg *types.CheckMail) {
 			w.worker.Tracef("Getting status of directory %s", dir)
 			status, err := w.client.Status(dir, items)
 			if err != nil {
-				w.worker.PostMessage(&types.Error{
-					Message: types.RespondTo(msg),
-					Error:   err,
-				}, nil)
-				continue
+				return err
 			}
 			statuses = append(statuses, status)
 		}
@@ -87,7 +79,6 @@ func (w *IMAPWorker) handleCheckMailMessage(msg *types.CheckMail) {
 			Message:     types.RespondTo(msg),
 			Directories: remaining,
 		}, nil)
-		return
 	}
-	w.worker.PostMessage(&types.Done{Message: types.RespondTo(msg)}, nil)
+	return nil
 }

worker/imap/create.go
@@ -4,16 +4,9 @@ import (
 	"git.sr.ht/~rjarry/aerc/worker/types"
 )
 
-func (imapw *IMAPWorker) handleCreateDirectory(msg *types.CreateDirectory) {
-	if err := imapw.client.Create(msg.Directory); err != nil {
-		if msg.Quiet {
-			return
-		}
-		imapw.worker.PostMessage(&types.Error{
-			Message: types.RespondTo(msg),
-			Error:   err,
-		}, nil)
-	} else {
-		imapw.worker.PostMessage(&types.Done{Message: types.RespondTo(msg)}, nil)
+func (imapw *IMAPWorker) handleCreateDirectory(msg *types.CreateDirectory) error {
+	if err := imapw.client.Create(msg.Directory); err != nil && !msg.Quiet {
+		return err
 	}
+	return nil
 }

worker/imap/flags.go
@@ -2,6 +2,7 @@ package imap
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"slices"
 
@@ -50,7 +51,7 @@ func (d *drainCloser) Close() error {
 	return nil
 }
 
-func (imapw *IMAPWorker) handleDeleteMessages(msg *types.DeleteMessages) {
+func (imapw *IMAPWorker) handleDeleteMessages(msg *types.DeleteMessages) error {
 	drain := imapw.drainUpdates()
 	defer drain.Close()
 
@@ -61,29 +62,21 @@ func (imapw *IMAPWorker) handleDeleteMessages(msg *types.DeleteMessages) {
 	flags := []any{imap.DeletedFlag}
 	uids := toSeqSet(msg.Uids)
 	if err := imapw.client.UidStore(uids, item, flags, nil); err != nil {
-		imapw.worker.PostMessage(&types.Error{
-			Message: types.RespondTo(msg),
-			Error:   err,
-		}, nil)
-		return
+		return err
 	}
 	if err := imapw.client.Expunge(nil); err != nil {
-		imapw.worker.PostMessage(&types.Error{
-			Message: types.RespondTo(msg),
-			Error:   err,
-		}, nil)
-	} else {
-		imapw.worker.PostMessage(&types.Done{Message: types.RespondTo(msg)}, nil)
+		return err
 	}
+	return nil
 }
 
-func (imapw *IMAPWorker) handleAnsweredMessages(msg *types.AnsweredMessages) {
+func (imapw *IMAPWorker) handleAnsweredMessages(msg *types.AnsweredMessages) error {
 	item := imap.FormatFlagsOp(imap.AddFlags, false)
 	flags := []any{imap.AnsweredFlag}
 	if !msg.Answered {
 		item = imap.FormatFlagsOp(imap.RemoveFlags, false)
 	}
-	imapw.handleStoreOps(msg, msg.Uids, item, flags,
+	return imapw.handleStoreOps(msg.Uids, item, flags,
 		func(_msg *imap.Message) error {
 			systemFlags, keywordFlags := translateImapFlags(_msg.Flags)
 			imapw.worker.PostMessage(&types.MessageInfo{
@@ -98,13 +91,13 @@ func (imapw *IMAPWorker) handleAnsweredMessages(msg *types.AnsweredMessages) {
 		})
 }
 
-func (imapw *IMAPWorker) handleFlagMessages(msg *types.FlagMessages) {
+func (imapw *IMAPWorker) handleFlagMessages(msg *types.FlagMessages) error {
 	flags := []any{flagToImap[msg.Flags]}
 	item := imap.FormatFlagsOp(imap.AddFlags, false)
 	if !msg.Enable {
 		item = imap.FormatFlagsOp(imap.RemoveFlags, false)
 	}
-	imapw.handleStoreOps(msg, msg.Uids, item, flags,
+	return imapw.handleStoreOps(msg.Uids, item, flags,
 		func(_msg *imap.Message) error {
 			systemFlags, keywordFlags := translateImapFlags(_msg.Flags)
 			imapw.worker.PostMessage(&types.MessageInfo{
@@ -120,27 +113,19 @@ func (imapw *IMAPWorker) handleFlagMessages(msg *types.FlagMessages) {
 		})
 }
 
-func (imapw *IMAPWorker) handleModifyLabels(msg *types.ModifyLabels) {
+func (imapw *IMAPWorker) handleModifyLabels(msg *types.ModifyLabels) error {
 	if len(msg.Toggle) > 0 {
 		// To toggle, we need to query the current message, and it's
 		// not straightforward from here; cowardly bail out.
-		imapw.worker.PostMessage(&types.Error{
-			Message: types.RespondTo(msg),
-			Error:   fmt.Errorf("label toggling not supported"),
-		}, nil)
-		return
+		return errors.New("label toggling not supported")
 	}
 	if imapw.config.provider == Proton {
 		// If any label removal is requested, make sure that we're on that
 		// label's virtual folder (otherwise the removal is silently a no-op)
 		for _, l := range msg.Remove {
 			if imapw.selected.Name != fmt.Sprintf("Labels/%s", l) {
-				imapw.worker.PostMessage(&types.Error{
-					Message: types.RespondTo(msg),
-					Error: fmt.Errorf("Proton labels can only " +
-						"be removed from their virtual folder"),
-				}, nil)
-				return
+				return errors.New("Proton labels can only " +
+					"be removed from their virtual folder")
 			}
 		}
 	}
@@ -173,7 +158,7 @@ func (imapw *IMAPWorker) handleModifyLabels(msg *types.ModifyLabels) {
 				labelsAny = append(labelsAny, utf7label)
 			}
 			nop_cb := func(_ *imap.Message) error { return nil }
-			imapw.handleStoreOps(msg, msg.Uids, item, labelsAny, nop_cb)
+			return imapw.handleStoreOps(msg.Uids, item, labelsAny, nop_cb)
 		case Proton:
 			// Per Proton documentation, adding/removing labels
 			// is obtained by moving messages to/from label virtual
@@ -191,11 +176,7 @@ func (imapw *IMAPWorker) handleModifyLabels(msg *types.ModifyLabels) {
 					destination = "INBOX"
 				}
 				if err := imapw.client.UidMove(uids, destination); err != nil {
-					imapw.worker.PostMessage(&types.Error{
-						Message: types.RespondTo(msg),
-						Error:   err,
-					}, nil)
-					return
+					return err
 				}
 			}
 			// Refresh the impacted virtual folders;
@@ -230,25 +211,19 @@ func (imapw *IMAPWorker) handleModifyLabels(msg *types.ModifyLabels) {
 					}, nil)
 					return nil
 				}
-				imapw.handleStoreOps(msg, msg.Uids, item, labelsAny, refresh_cb)
+				return imapw.handleStoreOps(msg.Uids, item, labelsAny, refresh_cb)
 			} else {
-				imapw.worker.PostMessage(&types.Error{
-					Message: types.RespondTo(msg),
-					Error:   fmt.Errorf("operation not supported by this imap server"),
-				}, nil)
-				return
+				return errors.New("operation not supported by this imap server")
 			}
 		}
 	}
-	imapw.worker.PostMessage(&types.Done{
-		Message: types.RespondTo(msg),
-	}, nil)
+	return nil
 }
 
 func (imapw *IMAPWorker) handleStoreOps(
-	msg types.WorkerMessage, uids []models.UID, item imap.StoreItem, flag any,
+	uids []models.UID, item imap.StoreItem, flag any,
 	procFunc func(*imap.Message) error,
-) {
+) error {
 	messages := make(chan *imap.Message)
 	done := make(chan error)
 
@@ -270,25 +245,15 @@ func (imapw *IMAPWorker) handleStoreOps(
 		done <- reterr
 	}()
 
-	emitErr := func(err error) {
-		imapw.worker.PostMessage(&types.Error{
-			Message: types.RespondTo(msg),
-			Error:   err,
-		}, nil)
-	}
-
 	set := toSeqSet(uids)
 	if err := imapw.client.UidStore(set, item, flag, messages); err != nil {
-		emitErr(err)
-		return
+		return err
 	}
 	if err := <-done; err != nil {
-		emitErr(err)
-		return
+		return err
 	}
 	imapw.worker.PostAction(context.TODO(), &types.CheckMail{
 		Directories: []string{imapw.selected.Name},
 	}, nil)
-	imapw.worker.PostMessage(
-		&types.Done{Message: types.RespondTo(msg)}, nil)
+	return nil
 }

worker/imap/list.go
@@ -10,7 +10,7 @@ import (
 	"git.sr.ht/~rjarry/aerc/worker/types"
 )
 
-func (imapw *IMAPWorker) handleListDirectories(msg *types.ListDirectories) {
+func (imapw *IMAPWorker) handleListDirectories(msg *types.ListDirectories) error {
 	mailboxes := make(chan *imap.MailboxInfo)
 	imapw.worker.Tracef("Listing mailboxes")
 	done := make(chan any)
@@ -67,17 +67,8 @@ func (imapw *IMAPWorker) handleListDirectories(msg *types.ListDirectories) {
 	}()
 
 	err := imapw.client.List("", "*", mailboxes)
-	if err != nil {
-		<-done
-		imapw.worker.PostMessage(&types.Error{
-			Message: types.RespondTo(msg),
-			Error:   err,
-		}, nil)
-		return
-	}
 	<-done
-	imapw.worker.PostMessage(
-		&types.Done{Message: types.RespondTo(msg)}, nil)
+	return err
 }
 
 const NonExistentAttr = "\\NonExistent"
@@ -92,39 +83,27 @@ func canOpen(mbox *imap.MailboxInfo) bool {
 	return true
 }
 
-func (imapw *IMAPWorker) handleSearchDirectory(msg *types.SearchDirectory) {
-	emitError := func(err error) {
-		imapw.worker.PostMessage(&types.Error{
-			Message: types.RespondTo(msg),
-			Error:   err,
-		}, nil)
-	}
-
+func (imapw *IMAPWorker) handleSearchDirectory(msg *types.SearchDirectory) error {
 	imapw.worker.Tracef("Executing search")
 	criteria := translateSearch(msg.Criteria)
 
 	if msg.Context().Err() != nil {
-		imapw.worker.PostMessage(&types.Cancelled{
-			Message: types.RespondTo(msg),
-		}, nil)
-		return
+		return msg.Context().Err()
 	}
 
 	uids, err := imapw.client.UidSearch(criteria)
 	if err != nil {
-		emitError(err)
-		return
+		return err
 	}
 
 	if msg.Context().Err() != nil {
-		imapw.worker.PostMessage(&types.Cancelled{
-			Message: types.RespondTo(msg),
-		}, nil)
-		return
+		return msg.Context().Err()
 	}
 
 	imapw.worker.PostMessage(&types.SearchResults{
 		Message: types.RespondTo(msg),
 		Uids:    models.Uint32ToUidList(uids),
 	}, nil)
+
+	return nil
 }

worker/imap/movecopy.go
@@ -7,21 +7,17 @@ import (
 	"git.sr.ht/~rjarry/aerc/worker/types"
 )
 
-func (imapw *IMAPWorker) handleCopyMessages(msg *types.CopyMessages) {
+func (imapw *IMAPWorker) handleCopyMessages(msg *types.CopyMessages) error {
 	uids := toSeqSet(msg.Uids)
 	if err := imapw.client.UidCopy(uids, msg.Destination); err != nil {
-		imapw.worker.PostMessage(&types.Error{
-			Message: types.RespondTo(msg),
-			Error:   err,
-		}, nil)
-	} else {
-		imapw.worker.PostMessage(&types.MessagesCopied{
-			Message:     types.RespondTo(msg),
-			Destination: msg.Destination,
-			Uids:        msg.Uids,
-		}, nil)
-		imapw.worker.PostMessage(&types.Done{Message: types.RespondTo(msg)}, nil)
+		return err
 	}
+	imapw.worker.PostMessage(&types.MessagesCopied{
+		Message:     types.RespondTo(msg),
+		Destination: msg.Destination,
+		Uids:        msg.Uids,
+	}, nil)
+	return nil
 }
 
 type appendLiteral struct {
@@ -33,22 +29,18 @@ func (m appendLiteral) Len() int {
 	return m.Length
 }
 
-func (imapw *IMAPWorker) handleAppendMessage(msg *types.AppendMessage) {
+func (imapw *IMAPWorker) handleAppendMessage(msg *types.AppendMessage) error {
 	if err := imapw.client.Append(msg.Destination, translateFlags(msg.Flags), msg.Date,
 		&appendLiteral{
 			Reader: msg.Reader,
 			Length: msg.Length,
 		}); err != nil {
-		imapw.worker.PostMessage(&types.Error{
-			Message: types.RespondTo(msg),
-			Error:   err,
-		}, nil)
-	} else {
-		imapw.worker.PostMessage(&types.Done{Message: types.RespondTo(msg)}, nil)
+		return err
 	}
+	return nil
 }
 
-func (imapw *IMAPWorker) handleMoveMessages(msg *types.MoveMessages) {
+func (imapw *IMAPWorker) handleMoveMessages(msg *types.MoveMessages) error {
 	drain := imapw.drainUpdates()
 	defer drain.Close()
 
@@ -57,16 +49,12 @@ func (imapw *IMAPWorker) handleMoveMessages(msg *types.MoveMessages) {
 
 	uids := toSeqSet(msg.Uids)
 	if err := imapw.client.UidMove(uids, msg.Destination); err != nil {
-		imapw.worker.PostMessage(&types.Error{
-			Message: types.RespondTo(msg),
-			Error:   err,
-		}, nil)
-	} else {
-		imapw.worker.PostMessage(&types.MessagesMoved{
-			Message:     types.RespondTo(msg),
-			Destination: msg.Destination,
-			Uids:        msg.Uids,
-		}, nil)
-		imapw.worker.PostMessage(&types.Done{Message: types.RespondTo(msg)}, nil)
+		return err
 	}
+	imapw.worker.PostMessage(&types.MessagesMoved{
+		Message:     types.RespondTo(msg),
+		Destination: msg.Destination,
+		Uids:        msg.Uids,
+	}, nil)
+	return nil
 }

worker/imap/open.go
@@ -9,34 +9,27 @@ import (
 	"git.sr.ht/~rjarry/aerc/worker/types"
 )
 
-func (imapw *IMAPWorker) handleOpenDirectory(msg *types.OpenDirectory) {
+func (imapw *IMAPWorker) handleOpenDirectory(msg *types.OpenDirectory) error {
 	imapw.worker.Debugf("Opening %s", msg.Directory)
 
 	sel, err := imapw.client.Select(msg.Directory, false)
 	if err != nil {
-		imapw.worker.PostMessage(&types.Error{
-			Message: types.RespondTo(msg),
-			Error:   err,
-		}, nil)
-		return
+		return err
 	}
 	select {
 	case <-msg.Context().Done():
-		imapw.worker.PostMessage(&types.Cancelled{Message: types.RespondTo(msg)}, nil)
+		return msg.Context().Err()
 	default:
 		imapw.selected = sel
-		imapw.worker.PostMessage(&types.Done{Message: types.RespondTo(msg)}, nil)
 	}
+	return nil
 }
 
 func (imapw *IMAPWorker) handleFetchDirectoryContents(
 	msg *types.FetchDirectoryContents,
-) {
+) error {
 	if msg.Context().Err() != nil {
-		imapw.worker.PostMessage(&types.Cancelled{
-			Message: types.RespondTo(msg),
-		}, nil)
-		return
+		return msg.Context().Err()
 	}
 	imapw.worker.Tracef("Fetching UID list")
 
@@ -52,11 +45,7 @@ func (imapw *IMAPWorker) handleFetchDirectoryContents(
 	case imapw.caps.Sort && hasSortCriteria:
 		uids, err = imapw.client.sort.UidSort(sortCriteria, searchCriteria)
 		if err != nil {
-			imapw.worker.PostMessage(&types.Error{
-				Message: types.RespondTo(msg),
-				Error:   err,
-			}, nil)
-			return
+			return err
 		}
 		// copy in reverse as msgList displays backwards
 		for i, j := 0, len(uids)-1; i < j; i, j = i+1, j-1 {
@@ -68,18 +57,11 @@ func (imapw *IMAPWorker) handleFetchDirectoryContents(
 		}
 		uids, err = imapw.client.UidSearch(searchCriteria)
 		if err != nil {
-			imapw.worker.PostMessage(&types.Error{
-				Message: types.RespondTo(msg),
-				Error:   err,
-			}, nil)
-			return
+			return err
 		}
 	}
 	if msg.Context().Err() != nil {
-		imapw.worker.PostMessage(&types.Cancelled{
-			Message: types.RespondTo(msg),
-		}, nil)
-		return
+		return msg.Context().Err()
 	}
 	imapw.worker.Tracef("Found %d UIDs", len(uids))
 	if msg.Filter == nil {
@@ -91,7 +73,7 @@ func (imapw *IMAPWorker) handleFetchDirectoryContents(
 		Message: types.RespondTo(msg),
 		Uids:    models.Uint32ToUidList(uids),
 	}, nil)
-	imapw.worker.PostMessage(&types.Done{Message: types.RespondTo(msg)}, nil)
+	return nil
 }
 
 type sortFieldMapT map[types.SortField]sortthread.SortField
@@ -121,12 +103,9 @@ func translateSortCriterions(
 
 func (imapw *IMAPWorker) handleDirectoryThreaded(
 	msg *types.FetchDirectoryThreaded,
-) {
+) error {
 	if msg.Context().Err() != nil {
-		imapw.worker.PostMessage(&types.Cancelled{
-			Message: types.RespondTo(msg),
-		}, nil)
-		return
+		return msg.Context().Err()
 	}
 	imapw.worker.Tracef("Fetching threaded UID list")
 
@@ -134,11 +113,7 @@ func (imapw *IMAPWorker) handleDirectoryThreaded(
 	threads, err := imapw.client.thread.UidThread(imapw.threadAlgorithm,
 		searchCriteria)
 	if err != nil {
-		imapw.worker.PostMessage(&types.Error{
-			Message: types.RespondTo(msg),
-			Error:   err,
-		}, nil)
-		return
+		return err
 	}
 	aercThreads, count := convertThreads(threads, nil)
 	sort.Sort(types.ByUID(aercThreads))
@@ -155,16 +130,13 @@ func (imapw *IMAPWorker) handleDirectoryThreaded(
 		imapw.seqMap.Initialize(uids)
 	}
 	if msg.Context().Err() != nil {
-		imapw.worker.PostMessage(&types.Cancelled{
-			Message: types.RespondTo(msg),
-		}, nil)
-		return
+		return msg.Context().Err()
 	}
 	imapw.worker.PostMessage(&types.DirectoryThreaded{
 		Message: types.RespondTo(msg),
 		Threads: aercThreads,
 	}, nil)
-	imapw.worker.PostMessage(&types.Done{Message: types.RespondTo(msg)}, nil)
+	return nil
 }
 
 func convertThreads(threads []*sortthread.Thread, parent *types.Thread) ([]*types.Thread, int) {

worker/imap/remove.go
@@ -4,16 +4,9 @@ import (
 	"git.sr.ht/~rjarry/aerc/worker/types"
 )
 
-func (imapw *IMAPWorker) handleRemoveDirectory(msg *types.RemoveDirectory) {
-	if err := imapw.client.Delete(msg.Directory); err != nil {
-		if msg.Quiet {
-			return
-		}
-		imapw.worker.PostMessage(&types.Error{
-			Message: types.RespondTo(msg),
-			Error:   err,
-		}, nil)
-	} else {
-		imapw.worker.PostMessage(&types.Done{Message: types.RespondTo(msg)}, nil)
+func (imapw *IMAPWorker) handleRemoveDirectory(msg *types.RemoveDirectory) error {
+	if err := imapw.client.Delete(msg.Directory); err != nil && !msg.Quiet {
+		return err
 	}
+	return nil
 }

commit 0123f979b19db3442c6d3ffd377feec2e240d68a

imap: always post a messageinfo when fetching message headers
aerc ended up in an infinite loop trying to fetch the headers for a
message that somehow confused go-imap.

While I can't reproduce, it looks like 6dc701a2a introduced a regression
in case of error when fetching any message's headers: we used to still
post a MessageInfo for all messages (possibly reporting an error), while
we don't anymore... and will retry right away.

This patch fixes this.

Fixes: 6dc701a2a ("imap: centralize error and response handling in Run loop")
Signed-off-by: Simon Martin <simon@nasilyan.com>
Acked-by: Robin Jarry <robin@jarry.cc>

worker/imap/fetch.go
@@ -368,9 +368,6 @@ func (imapw *IMAPWorker) handleFetchMessages(
 		return err
 	}
 	err := <-done
-	if err != nil {
-		return err
-	}
 
 	for uid := range missingUids {
 		imapw.worker.PostMessage(&types.MessageInfo{
@@ -382,5 +379,5 @@ func (imapw *IMAPWorker) handleFetchMessages(
 		}, nil)
 	}
 
-	return nil
+	return err
 }