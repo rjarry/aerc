rjarry/aerc

commit 39041fd9f179e06fefffd274371df5a57fb4fb79

ircbot: workaround to detect patch applications by simartin
He uses Proton, that strips all but a handful of headers, so his emails
will never have X-Sourcehut-Patchset-Update.

Signed-off-by: Simon Martin <simon@nasilyan.com>

contrib/ircbot/Sourcehut/plugin.py
@@ -99,7 +99,8 @@ class SourcehutServerCallback(httpserver.SupyHTTPServerCallback):
                 return
 
             if hook["event"] == "EMAIL_RECEIVED":
-                if hook["email"]["patchset_update"] == ["APPLIED"]:
+                if (hook["email"]["patchset_update"] == ["APPLIED"] or
+                    appliedByProtonUser(hook["email"])):
                     self.announce_apply(hook["email"])
                 handler.send_response(200)
                 handler.end_headers()
@@ -117,5 +118,14 @@ class SourcehutServerCallback(httpserver.SupyHTTPServerCallback):
     def log_message(self, format, *args):
         pass
 
+    def appliedByProtonUser(email):
+        # Workaround for maintainers using Proton, that strips mail headers
+        root = email["thread"]["root"]
+        if not "canonicalName" in root["sender"]:
+            return False
+        if root["sender"]["canonicalName"] != "~simartin":
+            return False
+        subject = re.sub(r"\s+", " ", root["subject"])
+        return subject.startswith("Applied: [PATCH aerc")
 
 Class = Sourcehut

commit 87876556aab0e14641c6b31562f25e32a2880e90

ircbot: fix detection of patch applications by simartin
The workaround was looking at the root email's subject and sender, while
we need to look at the current email's. This patch fixes this.

Fixes: 39041fd9f ("ircbot: workaround to detect patch applications by simartin")
Signed-off-by: Simon Martin <simon@nasilyan.com>
Acked-by: Robin Jarry <robin@jarry.cc>

contrib/ircbot/Sourcehut/plugin.py
@@ -120,12 +120,13 @@ class SourcehutServerCallback(httpserver.SupyHTTPServerCallback):
 
     def appliedByProtonUser(email):
         # Workaround for maintainers using Proton, that strips mail headers
-        root = email["thread"]["root"]
-        if not "canonicalName" in root["sender"]:
+        if not "sender" in email:
+            return False # Old version of the webhook
+        if not "canonicalName" in email["sender"]:
             return False
-        if root["sender"]["canonicalName"] != "~simartin":
+        if email["sender"]["canonicalName"] != "~simartin":
             return False
-        subject = re.sub(r"\s+", " ", root["subject"])
+        subject = re.sub(r"\s+", " ", email["subject"])
         return subject.startswith("Applied: [PATCH aerc")
 
 Class = Sourcehut

contrib/ircbot/install-webhook.sh
@@ -51,6 +51,11 @@ query {
 				id
 				subject
 				patchset_update: header(want: "X-Sourcehut-Patchset-Update")
+				sender {
+					... on User {
+						canonicalName
+					}
+				}
 				list {
 					name
 					owner {